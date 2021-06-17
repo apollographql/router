397dbbfdf4b2b3f7f817941bc24a2235cff222a1
bryncooke <bryncooke@gmail.com>
2021-06-17 11:48:05 +0200

    Fix ci-build script

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-RUSTFLAGS="-D warnings -D missing_docs -D future_incompatible -D missing_debug_implementations -D nonstandard_style -D future_incompatible -D unreachable_pub -D rust_2018_idioms"
-cargo --version
+export RUSTFLAGS="-D warnings -D missing_docs -D future_incompatible -D missing_debug_implementations -D nonstandard_style -D future_incompatible -D unreachable_pub -D rust_2018_idioms"
 cargo test --all
 cargo fmt -- --check
 cargo clippy --all --no-default-features -- -D warnings
