e409895681df0a63c85e5e4f0bf560e80a5ff81b
Jeremy Lempereur <jeremy.lempereur@iomentum.com>
2021-10-27 20:38:50 +0200

    fix: unfold BytesStream into a graphql::ResponseStream (#261)
    * fix: unfold BytesStream into a graphql::ResponseStream
    
    fixes #259 and #260
    
    When running the router with big queries, the first request performed by the router ended up with an error: unexpected EOF while deserializing.
    This was because the first subgraph response payload took a while to fully arrive, thus being two chunks within the BytesStream, and we were missconceiving one BytesStream chunk with one graphql::Response. Deserialization would fail with an unexpected EOF error.
    
    This commit fixes it by:
    - Unfolding the BytesStream to generate a graphql::ResponseStream
    - Trying to deserialize a graphql::Response when a chunk arrives.
    - While deserialize error is EOF, don't yield. Keep the current buffer, extend it with the next awaited chunk, and try to deserialize again.
    - If the deserialize is Ok() or an other Err(), Yield it, and repeat the process on the next incoming chunk.
    
    This assumes no chunk will contain the end of one response, and the beginning of an other one, in which case we will probably yield a graphql::FetchError::SubrequestMalformedResponse.
    
    A lower priority issue, #262 tracks this.
    
    Co-authored-by: Cecile Tonglet <cecile.tonglet@cecton.com>

--- crates/apollo-router/src/http_subgraph.rs
+++ crates/apollo-router/src/http_subgraph.rs
@@ -1,6 +1,6 @@
 use apollo_router_core::prelude::*;
 use async_trait::async_trait;
-use bytes::Bytes;
+use bytes::BytesMut;
 use derivative::Derivative;
 use futures::prelude::*;
 use std::pin::Pin;
@@ -71,31 +71,94 @@ impl HttpSubgraphFetcher {
     }
 
     fn map_to_graphql(&self, bytes_stream: BytesStream) -> graphql::ResponseStream {
-        // Map the stream of bytes to our response type.
-        let service = self.service.to_owned();
-        let response_stream = bytes_stream
-            // We want to know if this is the primary response or not, so attach the response count
-            .enumerate()
-            .map(move |(index, result)| {
-                let service = service.to_owned();
-                match result {
-                    Ok(bytes) => to_response(service, &bytes, index == 0),
-                    Err(err) => err.to_response(index == 0),
+        // A `BytesStream` chunk doesn't always represent a `graphql::Response`.
+        // We need to accumulate the bytes until we have a deserialization that doesn't end up in EOF, and then yield a `graphql::Response`.
+        // `stream::unfold` allows us to do just that: keep some state around, and yield `Some((new_item, new_state))` until we're done (at which point we'll yield `None`).
+        //
+        // Here's the state we're keeping around:
+        //
+        // `bytes_stream` is the actual response we're trying to deserialize, and we'll repeatedly await.
+        //
+        // `current_payload_bytes` starts empty, and will accumulate until we have a full deserialization.
+        // It will then reset to an empty `BytesMut` and accumulate for the next payload.
+        //
+        // `service_name` is useful so we can correctly track and propagate errors.
+        //
+        // `is_primary` les the graphql::ResponseStream users know whether an error occured during a graphql::Response,
+        // or during a subsequent HTTP patch that appends an update.
+        stream::unfold(
+            (
+                bytes_stream.fuse(),
+                BytesMut::new(),
+                self.service.clone(),
+                true,
+            ),
+            |(mut bytes_stream, mut current_payload_bytes, service_name, is_primary)| async move {
+                while let Some(next_chunk) = bytes_stream.next().await {
+                    match next_chunk {
+                        Ok(bytes) => {
+                            current_payload_bytes.extend(&bytes);
+                            match serde_json::from_slice::<graphql::Response>(
+                                &current_payload_bytes,
+                            ) {
+                                Err(e) if e.is_eof() => {
+                                    // Couldn't parse a full graphql::Response. This means the message is not complete yet.
+                                    continue;
+                                }
+                                Ok(response) => {
+                                    // Yield a graphql::Response
+                                    return Some((
+                                        response,
+                                        (bytes_stream, BytesMut::new(), service_name, false),
+                                    ));
+                                }
+                                Err(error) => {
+                                    // Yield a graphql::Response
+                                    return Some((
+                                        graphql::FetchError::SubrequestMalformedResponse {
+                                            service: service_name.clone(),
+                                            reason: error.to_string(),
+                                        }
+                                        .to_response(is_primary),
+                                        (bytes_stream, BytesMut::new(), service_name, false),
+                                    ));
+                                }
+                            }
+                        }
+                        Err(fetch_error) => {
+                            return Some((
+                                fetch_error.to_response(is_primary),
+                                (bytes_stream, BytesMut::new(), service_name, false),
+                            ));
+                        }
+                    }
                 }
-            });
-        response_stream.boxed()
-    }
-}
 
-fn to_response(service: impl Into<String>, bytes: &Bytes, primary: bool) -> graphql::Response {
-    serde_json::from_slice::<graphql::Response>(bytes)
-        .map_err(
-            move |err| graphql::FetchError::SubrequestMalformedResponse {
-                service: service.into(),
-                reason: err.to_string(),
+                // we're done with the `BytesStream`, yield a last response if any.
+                // since `bytes_stream` is fused, reentering the fold will lead us here,
+                // with an empty `current_payload_bytes` thus effectively yielding None,
+                // and exiting the fold
+                if !current_payload_bytes.is_empty() {
+                    let last_response =
+                        serde_json::from_slice::<graphql::Response>(&current_payload_bytes)
+                            .unwrap_or_else(|error| {
+                                graphql::FetchError::SubrequestMalformedResponse {
+                                    service: service_name.clone(),
+                                    reason: error.to_string(),
+                                }
+                                .to_response(is_primary)
+                            });
+                    Some((
+                        last_response,
+                        (bytes_stream, BytesMut::new(), service_name, false),
+                    ))
+                } else {
+                    None
+                }
             },
         )
-        .unwrap_or_else(|err| err.to_response(primary))
+        .boxed()
+    }
 }
 
 #[async_trait]
