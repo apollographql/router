5e5796598d6dc7bd9326e2c82041b8017bbfeba7
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-11-04 12:44:35 +0100

    Move internal function logic back inside

--- crates/apollo-router-core/src/request.rs
+++ crates/apollo-router-core/src/request.rs
@@ -49,6 +49,129 @@ impl Query {
     /// This will discard unrequested fields and re-order the output to match the order of the
     /// query.
     pub fn format_response(&self, response: &mut Response) -> Result<(), QueryError> {
+        fn apply_selection_set(
+            selection_set: &ast::SelectionSet,
+            input: &mut Object,
+            output: &mut Object,
+            fragments: &HashMap<String, ast::SelectionSet>,
+        ) {
+            for selection in selection_set.selections() {
+                match selection {
+                    // Spec: https://spec.graphql.org/draft/#Field
+                    ast::Selection::Field(field) => {
+                        let name = field
+                            .name()
+                            .expect("the node Name is not optional in the spec; qed")
+                            .text()
+                            .to_string();
+
+                        if let Some(input_value) = input.remove(&name) {
+                            if let Some(selection_set) = field.selection_set() {
+                                match input_value {
+                                    Value::Object(mut input_object) => {
+                                        let mut output_object = Object::default();
+                                        apply_selection_set(
+                                            &selection_set,
+                                            &mut input_object,
+                                            &mut output_object,
+                                            fragments,
+                                        );
+                                        output.insert(name, output_object.into());
+                                    }
+                                    Value::Array(input_array) => {
+                                        let output_array = input_array
+                                            .into_iter()
+                                            .enumerate()
+                                            .map(|(i, mut element)| {
+                                                if let Some(input_object) = element.as_object_mut()
+                                                {
+                                                    let mut output_object = Object::default();
+                                                    apply_selection_set(
+                                                        &selection_set,
+                                                        input_object,
+                                                        &mut output_object,
+                                                        fragments,
+                                                    );
+                                                    output_object.into()
+                                                } else {
+                                                    failfast_debug!(
+                                                        "Array element is not an object: {}[{}]",
+                                                        name,
+                                                        i,
+                                                    );
+                                                    element
+                                                }
+                                            })
+                                            .collect::<Value>();
+                                        output.insert(name.clone(), output_array);
+                                    }
+                                    _ => {
+                                        output.insert(name.clone(), input_value);
+                                        failfast_debug!(
+                                            "Field is not an object nor an array of object: {}",
+                                            name,
+                                        );
+                                    }
+                                }
+                            } else {
+                                output.insert(name, input_value);
+                            }
+                        } else {
+                            failfast_debug!("Missing field: {}", name);
+                        }
+                    }
+                    // Spec: https://spec.graphql.org/draft/#InlineFragment
+                    ast::Selection::InlineFragment(inline_fragment) => {
+                        let selection_set = inline_fragment
+                            .selection_set()
+                            .expect("the node SelectionSet is not optional in the spec; qed");
+
+                        apply_selection_set(&selection_set, input, output, fragments);
+                    }
+                    // Spec: https://spec.graphql.org/draft/#FragmentSpread
+                    ast::Selection::FragmentSpread(fragment_spread) => {
+                        let name = fragment_spread
+                            .fragment_name()
+                            .expect("the node FragmentName is not optional in the spec; qed")
+                            .name()
+                            .unwrap()
+                            .text()
+                            .to_string();
+
+                        if let Some(selection_set) = fragments.get(&name) {
+                            apply_selection_set(selection_set, input, output, fragments);
+                        } else {
+                            failfast_debug!("Missing fragment named: {}", name);
+                        }
+                    }
+                }
+            }
+        }
+
+        fn fragments(document: &ast::Document) -> HashMap<String, ast::SelectionSet> {
+            document
+                .definitions()
+                .filter_map(|definition| match definition {
+                    // Spec: https://spec.graphql.org/draft/#FragmentDefinition
+                    ast::Definition::FragmentDefinition(fragment_definition) => {
+                        let name = fragment_definition
+                            .fragment_name()
+                            .expect("the node FragmentName is not optional in the spec; qed")
+                            .name()
+                            .unwrap()
+                            .text()
+                            .to_string();
+                        let selection_set = fragment_definition
+                            .selection_set()
+                            .expect("the node SelectionSet is not optional in the spec; qed");
+
+                        Some((name, selection_set))
+                    }
+                    _ => None,
+                })
+                .collect()
+        }
+
         let parser = apollo_parser::Parser::new(self.as_str());
         let tree = parser.parse();
 
@@ -57,7 +180,7 @@ impl Query {
         }
 
         let document = tree.document();
-        let fragments = Self::fragments(&document);
+        let fragments = fragments(&document);
 
         for definition in document.definitions() {
             // Spec: https://spec.graphql.org/draft/#sec-Language.Operations
@@ -71,7 +194,7 @@ impl Query {
                     .ok_or(QueryError::InvalidDataTypeInResponse)?;
                 let mut output = Object::default();
 
-                Self::apply_selection_set(&selection_set, data, &mut output, &fragments);
+                apply_selection_set(&selection_set, data, &mut output, &fragments);
 
                 response.data = output.into();
                 return Ok(());
@@ -79,128 +202,6 @@ impl Query {
         }
 
         Err(QueryError::NoSuitableDefinition)
-    }
-
-    fn apply_selection_set(
-        selection_set: &ast::SelectionSet,
-        input: &mut Object,
-        output: &mut Object,
-        fragments: &HashMap<String, ast::SelectionSet>,
-    ) {
-        for selection in selection_set.selections() {
-            match selection {
-                // Spec: https://spec.graphql.org/draft/#Field
-                ast::Selection::Field(field) => {
-                    let name = field
-                        .name()
-                        .expect("the node Name is not optional in the spec; qed")
-                        .text()
-                        .to_string();
-
-                    if let Some(input_value) = input.remove(&name) {
-                        if let Some(selection_set) = field.selection_set() {
-                            match input_value {
-                                Value::Object(mut input_object) => {
-                                    let mut output_object = Object::default();
-                                    Self::apply_selection_set(
-                                        &selection_set,
-                                        &mut input_object,
-                                        &mut output_object,
-                                        fragments,
-                                    );
-                                    output.insert(name, output_object.into());
-                                }
-                                Value::Array(input_array) => {
-                                    let output_array = input_array
-                                        .into_iter()
-                                        .enumerate()
-                                        .map(|(i, mut element)| {
-                                            if let Some(input_object) = element.as_object_mut() {
-                                                let mut output_object = Object::default();
-                                                Self::apply_selection_set(
-                                                    &selection_set,
-                                                    input_object,
-                                                    &mut output_object,
-                                                    fragments,
-                                                );
-                                                output_object.into()
-                                            } else {
-                                                failfast_debug!(
-                                                    "Array element is not an object: {}[{}]",
-                                                    name,
-                                                    i,
-                                                );
-                                                element
-                                            }
-                                        })
-                                        .collect::<Value>();
-                                    output.insert(name.clone(), output_array);
-                                }
-                                _ => {
-                                    output.insert(name.clone(), input_value);
-                                    failfast_debug!(
-                                        "Field is not an object nor an array of object: {}",
-                                        name,
-                                    );
-                                }
-                            }
-                        } else {
-                            output.insert(name, input_value);
-                        }
-                    } else {
-                        failfast_debug!("Missing field: {}", name);
-                    }
-                }
-                // Spec: https://spec.graphql.org/draft/#InlineFragment
-                ast::Selection::InlineFragment(inline_fragment) => {
-                    let selection_set = inline_fragment
-                        .selection_set()
-                        .expect("the node SelectionSet is not optional in the spec; qed");
-
-                    Self::apply_selection_set(&selection_set, input, output, fragments);
-                }
-                // Spec: https://spec.graphql.org/draft/#FragmentSpread
-                ast::Selection::FragmentSpread(fragment_spread) => {
-                    let name = fragment_spread
-                        .fragment_name()
-                        .expect("the node FragmentName is not optional in the spec; qed")
-                        .name()
-                        .unwrap()
-                        .text()
-                        .to_string();
-
-                    if let Some(selection_set) = fragments.get(&name) {
-                        Self::apply_selection_set(selection_set, input, output, fragments);
-                    } else {
-                        failfast_debug!("Missing fragment named: {}", name);
-                    }
-                }
-            }
-        }
-    }
-
-    fn fragments(document: &ast::Document) -> HashMap<String, ast::SelectionSet> {
-        document
-            .definitions()
-            .filter_map(|definition| match definition {
-                // Spec: https://spec.graphql.org/draft/#FragmentDefinition
-                ast::Definition::FragmentDefinition(fragment_definition) => {
-                    let name = fragment_definition
-                        .fragment_name()
-                        .expect("the node FragmentName is not optional in the spec; qed")
-                        .name()
-                        .unwrap()
-                        .text()
-                        .to_string();
-                    let selection_set = fragment_definition
-                        .selection_set()
-                        .expect("the node SelectionSet is not optional in the spec; qed");
-
-                    Some((name, selection_set))
-                }
-                _ => None,
-            })
-            .collect()
     }
 }
 
