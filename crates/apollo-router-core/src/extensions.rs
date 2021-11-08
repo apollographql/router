dd2ef6c8df8783755cd3f14d7381732cfb0885df
Geoffroy Couprie <geoffroy@apollographql.com>
2021-11-08 11:21:35 +0100

    add wasi and linker

--- crates/apollo-router-core/src/extensions.rs
+++ crates/apollo-router-core/src/extensions.rs
@@ -1,11 +1,11 @@
 use std::{collections::HashMap, sync::Arc};
-
-use wasmtime::{Engine, Instance, Module, Store};
-use wasmtime_wasi::sync::WasiCtxBuilder;
+use wasmtime::{Engine, Instance, Linker, Module, Store};
+use wasmtime_wasi::{sync::WasiCtxBuilder, WasiCtx};
 
 #[derive(Clone)]
 pub struct Extensions {
     engine: Engine,
+    linker: Linker<WasiCtx>,
     modules: Arc<HashMap<String, Module>>,
     configuration: Arc<HashMap<String, Configuration>>,
 }
@@ -22,14 +22,19 @@ impl Extensions {
     ) -> anyhow::Result<Self> {
         // FIXME: investigate Engine configuration, especially around TLS
         let engine = Engine::default();
+        let mut linker = Linker::new(&engine);
+        wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
+
         let mut modules = HashMap::new();
 
         for (name, extension) in &configuration {
             tracing::info!("building module {} at path {}", name, extension.path);
             let module = Module::from_file(&engine, extension.path.clone())?;
+            tracing::info!("IMPORTS");
             for import in module.imports() {
                 tracing::info!("import: {:?}", import);
             }
+            tracing::info!("EXPORTS");
             for export in module.exports() {
                 tracing::info!("export: {:?}", export);
             }
@@ -38,13 +43,21 @@ impl Extensions {
 
         Ok(Self {
             engine,
+            linker,
             modules: Arc::new(modules),
             configuration: Arc::new(configuration),
         })
     }
 
     pub fn context(&self) -> ExecutionContext {
-        let store = Store::new(&self.engine, ());
+        let wasi = WasiCtxBuilder::new()
+            .env("RUST_LOG", "info")
+            //.inherit_stdio()
+            //.inherit_args()?
+            //.build()
+            .unwrap()
+            .build();
+        let store = Store::new(&self.engine, wasi);
 
         ExecutionContext {
             extensions: self.clone(),
@@ -66,7 +79,7 @@ impl Extensions {
 
 pub struct ExecutionContext {
     extensions: Extensions,
-    pub store: Store<()>,
+    pub store: Store<WasiCtx>,
     instances: HashMap<String, Instance>,
 }
 
@@ -78,8 +91,17 @@ impl ExecutionContext {
 
         if let Some(name) = self.extensions.find(hook) {
             if let Some(module) = self.extensions.modules.get(&name) {
+                self.extensions
+                    .linker
+                    .module(&mut self.store, "", &module)
+                    .unwrap();
                 //FIXME: error, imports
-                let instance = Instance::new(&mut self.store, &module, &[]).unwrap();
+                //let instance = Instance::new(&mut self.store, &module, &[]).unwrap();
+                let instance = self
+                    .extensions
+                    .linker
+                    .instantiate(&mut self.store, &module)
+                    .unwrap();
                 self.instances.insert(name, instance);
 
                 return Some(instance);
