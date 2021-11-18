222f0682f64ab0abd179cc947c9e5f3a31c343fd
Jeremy Lempereur <jeremy.lempereur@iomentum.com>
2021-11-18 17:36:37 +0100

    set up cargo-deny (#89)
    feat: set up cargo-deny
    
    fixes #37
    
    This commit sets up cargo-deny with the following settings:
    - Security advisories: warn for unmaintained and yanked crates. deny CVEs except `RUSTSEC-2020-0159` (pending https://github.com/chronotope/chrono/issues/499 to close somehow)
    - Licenses: Allow ELv2 compatible licenses: `"Apache-2.0", "Apache-2.0 WITH LLVM-exception", "BSD-2-Clause", "BSD-3-Clause", "CC0-1.0", "LicenseRef-ELv2", "ISC", "MIT",`. `LicenseRef-ELv2` is a placeholder while we're waiting for a SPDX identifier.
    - Sources: Explicitly allow references to `apollographql` and `opentelemetry` github repositories only.
    - Bans: No crates are banned as of yet
    
    This commit also adds `cargo-deny -L error check` on Linux and OSX in CI. Note it doesn't run on windows, possibly because of an environment variable issue. However this is fine given [cargo-deny checks for every targets by default](https://github.com/EmbarkStudios/cargo-deny/blob/d0ea0a2e7d7376ee212faec756cb12ab05f55c4d/examples/08_target_filtering/README.md#description).

--- xtask/src/commands/mod.rs
+++ xtask/src/commands/mod.rs
@@ -1,8 +1,10 @@
+pub(crate) mod check;
 pub(crate) mod dist;
 pub(crate) mod lint;
 pub(crate) mod package;
 pub(crate) mod test;
 
+pub(crate) use check::Check;
 pub(crate) use dist::Dist;
 pub(crate) use lint::Lint;
 pub(crate) use package::Package;
