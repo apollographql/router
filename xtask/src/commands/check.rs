6f1f45c23531d202df600fb8cbf0461eb36f7d5f
o0Ignition0o <jeremy.lempereur@gmail.com>
2021-11-19 13:08:42 +0100

    add licenses.html check to the check-compliance run. Update the dependencies in CI so this check passes as well

--- xtask/src/commands/check.rs
+++ xtask/src/commands/check.rs
@@ -1,6 +1,9 @@
-use anyhow::Result;
+use anyhow::{ensure, Result};
+use std::fs::read_to_string;
 use structopt::StructOpt;
 use xtask::*;
+
+static LICENSES_HTML_PATH: &str = "licenses.html";
 
 #[derive(Debug, StructOpt)]
 pub struct Compliance {}
@@ -9,6 +12,29 @@ impl Compliance {
     pub fn run(&self) -> Result<()> {
         cargo!(["deny", "-L", "error", "check"]);
 
+        eprintln!("checking generated licenses.html file");
+
+        let licenses_html_before = read_to_string(LICENSES_HTML_PATH)?;
+
+        cargo!([
+            "about",
+            "-L",
+            "error",
+            "generate",
+            "--workspace",
+            "-o",
+            "licenses.html",
+            "about.hbs"
+        ]);
+
+        let licences_html_after = read_to_string(LICENSES_HTML_PATH)?;
+
+        ensure!(
+            licenses_html_before == licences_html_after,
+            r#"🚨🚨🚨 licenses.html file is not up to date. 🚨🚨🚨
+please run `cargo about generate --workspace -o licenses.html about.hbs` to generate an up to date licenses list, and check the file in to the repository. 
+        "#
+        );
         Ok(())
     }
 }
