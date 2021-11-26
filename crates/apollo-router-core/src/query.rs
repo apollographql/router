fad02c665312a5620c34972a5fb7116011d9c94d
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-11-26 13:31:41 +0100

    Convert ast::Selection to Selection

--- crates/apollo-router-core/src/query.rs
+++ crates/apollo-router-core/src/query.rs
@@ -161,7 +161,6 @@ impl Query {
     }
 
     fn fragments(document: &ast::Document) -> HashMap<String, Vec<Selection>> {
-        /*
         document
             .definitions()
             .filter_map(|definition| match definition {
@@ -178,13 +177,11 @@ impl Query {
                         .selection_set()
                         .expect("the node SelectionSet is not optional in the spec; qed");
 
-                    Some((name, selection_set))
+                    Some((name, selection_set.selections().map(Into::into).collect()))
                 }
                 _ => None,
             })
             .collect()
-        */
-        todo!()
     }
 }
 
@@ -223,6 +220,59 @@ enum Selection {
     FragmentSpread {
         name: String,
     },
+}
+
+impl From<ast::Selection> for Selection {
+    fn from(selection: ast::Selection) -> Self {
+        match selection {
+            // Spec: https://spec.graphql.org/draft/#Field
+            ast::Selection::Field(field) => {
+                let name = field
+                    .name()
+                    .expect("the node Name is not optional in the spec; qed")
+                    .text()
+                    .to_string();
+                let alias = field.alias().map(|x| x.name().unwrap().text().to_string());
+                let name = alias.unwrap_or(name);
+                let selection_set = field
+                    .selection_set()
+                    .into_iter()
+                    .flat_map(|x| x.selections())
+                    .into_iter()
+                    .map(Into::into)
+                    .collect();
+
+                Self::Field {
+                    name,
+                    selection_set,
+                }
+            }
+            // Spec: https://spec.graphql.org/draft/#InlineFragment
+            ast::Selection::InlineFragment(inline_fragment) => {
+                let selection_set = inline_fragment
+                    .selection_set()
+                    .expect("the node SelectionSet is not optional in the spec; qed")
+                    .selections()
+                    .into_iter()
+                    .map(Into::into)
+                    .collect();
+
+                Self::InlineFragment { selection_set }
+            }
+            // Spec: https://spec.graphql.org/draft/#FragmentSpread
+            ast::Selection::FragmentSpread(fragment_spread) => {
+                let name = fragment_spread
+                    .fragment_name()
+                    .expect("the node FragmentName is not optional in the spec; qed")
+                    .name()
+                    .unwrap()
+                    .text()
+                    .to_string();
+
+                Self::FragmentSpread { name }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
