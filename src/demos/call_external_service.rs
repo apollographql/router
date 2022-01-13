554276277ae6fa001db9dac99fad18c10d71a4f2
Bryn Cooke <bryn@apollographql.com>
2022-01-13 12:17:42 +0100

    Update external service demo

--- src/demos/call_external_service.rs
+++ src/demos/call_external_service.rs
@@ -5,44 +5,27 @@ use tower::{BoxError, ServiceBuilder, ServiceExt};
 
 use crate::{graphql, ApolloRouter, Extension, RouterResponse, SubgraphRequest};
 
-#[derive(Default)]
-struct MyExtension {
-    client: reqwest::Client,
-}
-impl Extension for MyExtension {
-    fn subgraph_service(
-        &mut self,
-        name: &str,
-        service: BoxService<SubgraphRequest, RouterResponse, BoxError>,
-    ) -> BoxService<SubgraphRequest, RouterResponse, BoxError> {
-        if name == "books" {
-            let client = self.client.clone();
-            // Here we provide a completely new implementation for the service
-            // This could use a special client to a bespoke service or something else completely
-            return ServiceBuilder::new()
-                .service_fn(move |req: SubgraphRequest| {
-                    let client = client.clone();
-                    async move {
-                        let response = client.get("http://apollographql.com").send().await;
-                        Ok(RouterResponse {
-                            request: req.request,
-                            response: Response::new(graphql::Response {
-                                body: response.unwrap().text().await.unwrap(),
-                            }),
-                            context: Default::default(),
-                        })
-                    }
-                })
-                .boxed();
-        }
-        service
-    }
-}
-
 #[tokio::test]
 async fn call_external_service() -> Result<(), BoxError> {
+    let client = reqwest::Client::default();
+
     let router = ApolloRouter::builder()
-        .with_extension(MyExtension::default())
+        .with_service(
+            "books",
+            ServiceBuilder::new().service_fn(move |req: SubgraphRequest| {
+                let client = client.clone();
+                async move {
+                    let response = client.get("http://apollographql.com").send().await;
+                    Ok(RouterResponse {
+                        request: req.request,
+                        response: Response::new(graphql::Response {
+                            body: response.unwrap().text().await.unwrap(),
+                        }),
+                        context: Default::default(),
+                    })
+                }
+            }),
+        )
         .build();
 
     let response = router
