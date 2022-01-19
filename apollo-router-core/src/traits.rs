f4b3b1a2bb1daa5908a2e58d0a1a3428ad734b7b
Cecile Tonglet <cecile.tonglet@cecton.com>
2022-01-19 09:29:22 +0100

    Update apollo-router-core/src/traits.rs
    Co-authored-by: Gary Pennington <gary@apollographql.com>

--- apollo-router-core/src/traits.rs
+++ apollo-router-core/src/traits.rs
@@ -70,7 +70,7 @@ where
 
 impl<T: ?Sized> WithCaching for T where T: QueryPlanner + Sized + 'static {}
 
-/// An object that accepts a [`Request`] and allows creating [`PreparedQuery`]'s.
+/// An object that accepts a [`Request`] and allows for creating [`PreparedQuery`]'s.
 ///
 /// The call to the function will either succeeds and return a [`PreparedQuery`] or it will fail
 /// and return a [`Response`] that can be returned immediately to the user. This is because GraphQL
