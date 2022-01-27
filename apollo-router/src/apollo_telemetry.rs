aaf8ba047fa861fb8be7f05d498345094c579371
Gary Pennington <gary@apollographql.com>
2022-01-27 13:53:52 +0000

    Improve performance and scalability of telemetry
    A bag of changes which means that we can scale usage reporting in the
    router much higher.
    
     - Cache normalised keys for the life of an exporter
     - Configure OT parameters to increase exporter batch and queue sizes
     - Cache batch specific DurationHistograms to submit one set of stats
       for each client name, client version, normalized key per batch

--- apollo-router/src/apollo_telemetry.rs
+++ apollo-router/src/apollo_telemetry.rs
@@ -43,6 +43,7 @@ use opentelemetry::{
     Value,
 };
 use std::borrow::Cow;
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::time::Duration;
 use tokio::task::JoinError;
@@ -101,8 +102,12 @@ impl PipelineBuilder {
     pub fn install_batch(mut self) -> Result<sdk::trace::Tracer, ApolloError> {
         let exporter = self.get_exporter()?;
 
-        let mut provider_builder =
-            sdk::trace::TracerProvider::builder().with_batch_exporter(exporter, Tokio);
+        let batch = sdk::trace::BatchSpanProcessor::builder(exporter, Tokio)
+            .with_max_queue_size(131_072)
+            .with_max_export_batch_size(131_072)
+            .build();
+
+        let mut provider_builder = sdk::trace::TracerProvider::builder().with_span_processor(batch);
         if let Some(config) = self.trace_config.take() {
             provider_builder = provider_builder.with_config(config);
         }
@@ -169,12 +174,19 @@ impl PipelineBuilder {
 pub struct Exporter {
     collector: String,
     graph: Option<StudioGraph>,
+    reporter: tokio::sync::OnceCell<Reporter>,
+    normalized_keys: HashMap<(String, String), String>,
 }
 
 impl Exporter {
     /// Create a new apollo telemetry `Exporter`.
     pub fn new(collector: String, graph: Option<StudioGraph>) -> Self {
-        Self { collector, graph }
+        Self {
+            collector,
+            graph,
+            reporter: tokio::sync::OnceCell::new(),
+            normalized_keys: HashMap::new(),
+        }
     }
 }
 
@@ -214,17 +226,22 @@ impl From<&StudioGraph> for ReporterGraph {
 impl SpanExporter for Exporter {
     /// Export spans to apollo telemetry
     async fn export(&mut self, batch: Vec<SpanData>) -> ExportResult {
+        tracing::debug!("batch size: {}", batch.len());
         if self.graph.is_none() {
             // It's an error to try and export statistics without
             // graph details. We enforce that elsewhere in the code
             // and panic here in case a logic bug creeps in elsewhere.
             panic!("cannot export statistics without graph details")
         }
-        let mut reporter = Reporter::try_new(self.collector.clone())
-            .await
-            .map_err::<ApolloError, _>(Into::into)?;
+        // In every batch we'll have a varying number of actual stats reports to submit
+        // Each report is unique by client name, client version and key (derived from op_name)
+        // The dh_map contains a batch specific map, keyed on the unique report triple,
+        // referencing DurationHistogram values.
+        // After processing the batch, we drain the HashMap and send the generated reports
+        // to the Reporter.
+        let mut dh_map = HashMap::new();
         /*
-         * Break down batch and send to spaceport
+         * Process the batch
          */
         for (index, span) in batch
             .into_iter()
@@ -237,48 +254,77 @@ impl SpanExporter for Exporter {
                 .attributes
                 .get(&opentelemetry::Key::from_static_str("query"))
             {
-                // Convert the configuration data into a reportable form
-                let graph: ReporterGraph = self.graph.as_ref().unwrap().into();
-                let mut dh = DurationHistogram::new(None);
-                dh.increment_duration(span.end_time.duration_since(span.start_time).unwrap(), 1);
                 tracing::debug!("query: {}", query);
-
                 let not_found = Value::String(Cow::from("not found"));
-                let stats = ContextualizedStats {
-                    context: Some(StatsContext {
-                        client_name: span
-                            .attributes
-                            .get(&opentelemetry::Key::from_static_str("client_name"))
-                            .unwrap_or(&not_found)
-                            .to_string(),
-                        client_version: span
-                            .attributes
-                            .get(&opentelemetry::Key::from_static_str("client_version"))
-                            .unwrap_or(&not_found)
-                            .to_string(),
-                    }),
-                    query_latency_stats: Some(QueryLatencyStats {
-                        latency_count: dh.buckets,
-                        request_count: 1,
-                        ..Default::default()
-                    }),
-                    ..Default::default()
-                };
+                let client_name = span
+                    .attributes
+                    .get(&opentelemetry::Key::from_static_str("client_name"))
+                    .unwrap_or(&not_found)
+                    .to_string();
+                let client_version = span
+                    .attributes
+                    .get(&opentelemetry::Key::from_static_str("client_version"))
+                    .unwrap_or(&not_found)
+                    .to_string();
                 let operation_name = span
                     .attributes
                     .get(&opentelemetry::Key::from_static_str("operation_name"));
+                let op_name: String = match operation_name {
+                    Some(v) => v.as_str().into_owned(),
+                    None => GRAPHQL_UNKNOWN_OPERATION_NAME.to_string(),
+                };
                 // XXX The normalize function isn't complete yet, but does the
                 // minimum amount of normalization.
-                let key = normalize(operation_name, &query.as_str());
+                // Cache the normalized keys since this is a fairly slow
+                // operation...
+                let key = self
+                    .normalized_keys
+                    .entry((op_name, query.as_str().to_string()))
+                    .or_insert_with(|| normalize(operation_name, &query.as_str()));
 
-                let msg = reporter
-                    .submit_stats(graph, key, stats)
-                    .await
-                    .map_err::<TraceError, _>(|e| e.to_string().into())?
-                    .into_inner()
-                    .message;
-                tracing::trace!("server response: {}", msg);
+                // Retrieve DurationHistogram from our HashMap, or add a new one
+                let dh = dh_map
+                    .entry((client_name, client_version, key.clone()))
+                    .or_insert_with(|| DurationHistogram::new(None));
+                dh.increment_duration(span.end_time.duration_since(span.start_time).unwrap(), 1);
             }
+        }
+
+        // Guarantee that the reporter is initialised
+        self.reporter
+            .get_or_try_init(|| async {
+                Reporter::try_new(self.collector.clone())
+                    .await
+                    .map_err::<ApolloError, _>(Into::into)
+            })
+            .await?;
+        let reporter = self.reporter.get_mut().unwrap();
+        // Convert the configuration data into a reportable form
+        let graph: ReporterGraph = self.graph.as_ref().unwrap().into();
+
+        // Report our consolidated statistics
+        for ((client_name, client_version, key), dh) in dh_map.drain() {
+            tracing::debug!("reporting entries: {}", dh.entries);
+            let stats = ContextualizedStats {
+                context: Some(StatsContext {
+                    client_name,
+                    client_version,
+                }),
+                query_latency_stats: Some(QueryLatencyStats {
+                    latency_count: dh.buckets,
+                    request_count: dh.entries,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            };
+
+            let msg = reporter
+                .submit_stats(graph.clone(), key, stats)
+                .await
+                .map_err::<TraceError, _>(|e| e.to_string().into())?
+                .into_inner()
+                .message;
+            tracing::trace!("server response: {}", msg);
         }
 
         Ok(())
@@ -341,6 +387,7 @@ fn normalize(op: Option<&opentelemetry::Value>, query: &str) -> String {
 
 struct DurationHistogram {
     buckets: Vec<i64>,
+    entries: u64,
 }
 
 static EXPONENT_LOG: OnceCell<f64> = OnceCell::new();
@@ -352,6 +399,7 @@ impl DurationHistogram {
     fn new(init_size: Option<usize>) -> Self {
         Self {
             buckets: vec![0; init_size.unwrap_or(DurationHistogram::DEFAULT_SIZE)],
+            entries: 0,
         }
     }
 
@@ -379,6 +427,7 @@ impl DurationHistogram {
         if bucket > DurationHistogram::MAXIMUM_SIZE {
             panic!("bucket is out of bounds of the bucket array");
         }
+        self.entries += value as u64;
         if bucket >= self.buckets.len() {
             self.buckets.resize(bucket + 1, 0);
         }
