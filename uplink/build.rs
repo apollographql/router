bf1395480b70a8f2d528690b22f39eee2f91c92b
Geoffroy Couprie <geoffroy@apollographql.com>
2022-02-18 14:51:47 +0100

    only check the uplink schema when in debug mode

--- uplink/build.rs
+++ uplink/build.rs
@@ -1,23 +1,40 @@
-use std::{fs::File, io::Write, process::Command};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    process::Command,
+};
 use which::which;
 
 fn main() {
-    //~/.rover/bin/rover graph introspect https://uplink.api.apollographql.com/
-    which("rover")
+    if let Ok("debug") = std::env::var("PROFILE").as_deref() {
+        //~/.rover/bin/rover graph introspect https://uplink.api.apollographql.com/
+        which("rover")
     .map_err(|_| "could not find path to rover executable, see installation instructions at https://github.com/apollographql/rover#installation-methods").unwrap();
 
-    let output = Command::new("rover")
-        .args([
-            "graph",
-            "introspect",
-            "https://uplink.api.apollographql.com/",
-        ])
-        .output()
-        .expect("failed to execute process");
+        let output = Command::new("rover")
+            .args([
+                "graph",
+                "introspect",
+                "https://uplink.api.apollographql.com/",
+            ])
+            .output()
+            .expect("failed to execute process");
 
-    let data = output.stdout;
-    File::create("uplink.graphql")
-        .expect("could not createuplink.graphql file")
-        .write_all(&data)
-        .expect("could not write downloaded uplink schema");
+        let mut buf = Vec::new();
+        let _ = File::open("uplink.graphql")
+            .unwrap()
+            .read_to_end(&mut buf)
+            .unwrap();
+        let data = output.stdout;
+        assert_eq!(
+            std::str::from_utf8(&buf).unwrap(),
+            std::str::from_utf8(&data).unwrap(),
+            "Uplink schema changed"
+        );
+
+        File::create("uplink.graphql")
+            .expect("could not createuplink.graphql file")
+            .write_all(&data)
+            .expect("could not write downloaded uplink schema");
+    }
 }
