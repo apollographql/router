be91450e13bbd0d17c13d0bce6aa8bd2ea5be2a1
Gary Pennington <gary@apollographql.com>
2022-02-18 13:32:14 +0000

    spaceport listener should be more secure by default (#507)
    Configure the listener to listen at 127.0.0.1 rather than 0.0.0.0 by
    default.
    
    fixes: #487

--- apollo-router/src/apollo_telemetry.rs
+++ apollo-router/src/apollo_telemetry.rs
@@ -50,7 +50,7 @@ use tokio::task::JoinError;
 use crate::configuration::{SpaceportConfig, StudioGraph};
 
 pub(crate) const DEFAULT_SERVER_URL: &str = "https://127.0.0.1:50051";
-pub(crate) const DEFAULT_LISTEN: &str = "0.0.0.0:50051";
+pub(crate) const DEFAULT_LISTEN: &str = "127.0.0.1:50051";
 
 /// Pipeline builder
 #[derive(Debug)]
