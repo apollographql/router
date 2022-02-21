60a3e192e60f2f7adb4f7c75a7f6efd4a3a23ae9
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2022-02-21 10:52:58 +0100

    fix(context): change context methods about extensions to avoid deadlock
    resolves #476
    
    Renamed `extensions()` and `extensions_mut()` with a prefix `lock_` to let users know they are locking the resources
    Add common methods to context for extensions to avoid as much as possible to use `lock_extensions()` and `lock_extensions_mut()`

--- apollo-router-core/src/context.rs
+++ apollo-router-core/src/context.rs
@@ -1,7 +1,9 @@
 use crate::prelude::graphql::*;
 use crate::services::http_compat;
 use futures::Future;
-use std::sync::Arc;
+use serde_json_bytes::ByteString;
+use std::hash::Hash;
+use std::{borrow::Borrow, sync::Arc};
 use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 #[derive(Clone, Debug)]
@@ -44,12 +46,50 @@ impl From<Context<http_compat::Request<Request>>> for Context<Arc<http_compat::R
 }
 
 impl<T> Context<T> {
-    pub fn extensions(&self) -> impl Future<Output = RwLockReadGuard<Object>> {
+    /// Take a read lock of extensions
+    /// Be careful when using this method, you could create deadlock if you use `lock_extensions_mut` in the same scope
+    pub fn lock_extensions(&self) -> impl Future<Output = RwLockReadGuard<Object>> {
         self.extensions.read()
     }
 
-    pub fn extensions_mut(&self) -> impl Future<Output = RwLockWriteGuard<Object>> {
+    /// Take a write lock of extensions
+    /// Be careful when using this method, you could create deadlock if you use `lock_extensions` in the same scope
+    pub fn lock_extensions_mut(&self) -> impl Future<Output = RwLockWriteGuard<Object>> {
         self.extensions.write()
+    }
+
+    pub async fn cloned_extensions(&self) -> Object {
+        self.extensions.read().await.clone()
+    }
+
+    pub async fn extensions_len(&self) -> usize {
+        self.extensions.read().await.len()
+    }
+
+    pub async fn is_extensions_empty(&self) -> bool {
+        self.extensions.read().await.is_empty()
+    }
+
+    pub async fn clear(&self) {
+        self.extensions.write().await.clear()
+    }
+
+    /// Inserts a key-value pair into extensions.
+    /// If extensions did not have this key present, None is returned.
+    /// If extensions did have this key present, the value is updated, and the old value is returned.
+    pub async fn insert_extension(&self, k: String, v: Value) -> Option<Value> {
+        self.extensions.write().await.insert(k, v)
+    }
+
+    /// Inserts a key-value pair into extensions.
+    /// If extensions did not have this key present, None is returned.
+    /// If extensions did have this key present, the value is updated, and the old value is returned.
+    pub async fn remove_extension<Q>(&self, k: &Q) -> Option<Value>
+    where
+        ByteString: Borrow<Q>,
+        Q: ?Sized + Ord + Eq + Hash,
+    {
+        self.extensions.write().await.remove(k)
     }
 }
 
