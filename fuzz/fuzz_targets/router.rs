7bc68788ebca2884e996c84e3d33054b9cefd13e
Coenen Benjamin <benjamin.coenen@hotmail.com>
2022-03-02 12:10:11 +0100

    add fuzzy test with apollo-smith to check the router vs gateway (#566)
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

---
+++ fuzz/fuzz_targets/router.rs
@@ -0,0 +1,110 @@
+#![no_main]
+use apollo_router_core::Response;
+use libfuzzer_sys::fuzz_target;
+use log::debug;
+use router_fuzz::generate_valid_operation;
+use serde_json::json;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+const GATEWAY_URL: &str = "http://localhost:4100/graphql";
+const ROUTER_URL: &str = "http://localhost:4000/graphql";
+
+fuzz_target!(|data: &[u8]| {
+    let generated_operation = match generate_valid_operation(data) {
+        Ok(d) => d,
+        Err(_err) => {
+            return;
+        }
+    };
+
+    let http_client = reqwest::blocking::Client::new();
+    let router_response = http_client
+        .post(ROUTER_URL)
+        .json(&json!({ "query": generated_operation }))
+        .send()
+        .unwrap()
+        .json::<Response>();
+    let gateway_response = http_client
+        .post(GATEWAY_URL)
+        .json(&json!({ "query": generated_operation }))
+        .send()
+        .unwrap()
+        .json::<Response>();
+
+    debug!("======= DOCUMENT =======");
+    debug!("{}", generated_operation);
+    debug!("========================");
+    debug!("======= RESPONSE =======");
+    assert_eq!(router_response.is_ok(), gateway_response.is_ok());
+    if router_response.is_ok() != gateway_response.is_ok() {
+        let router_error = if let Err(err) = &router_response {
+            Some(err)
+        } else {
+            None
+        };
+        let gateway_error = if let Err(err) = &gateway_response {
+            Some(err)
+        } else {
+            None
+        };
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open("router_errors.txt")
+            .unwrap();
+
+        let errors = format!(
+            r#"
+
+
+====DOCUMENT===
+{generated_operation}
+
+====GATEWAY====
+{gateway_error:?}
+
+====ROUTER====
+{router_error:?}
+
+
+"#
+        );
+        debug!("{errors}");
+        file.write_all(errors.as_bytes()).unwrap();
+    } else if router_response.is_ok() {
+        let router_response = router_response.unwrap();
+        let gateway_response = gateway_response.unwrap();
+        if router_response != gateway_response {
+            let mut file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .append(true)
+                .open("router_errors.txt")
+                .unwrap();
+
+            let errors = format!(
+                r#"
+
+
+====DOCUMENT===
+{generated_operation}
+
+====GATEWAY====
+{gateway_response:?}
+
+====ROUTER====
+{router_response:?}
+
+
+"#
+            );
+            debug!("{errors}");
+            file.write_all(errors.as_bytes()).unwrap();
+        }
+    }
+    debug!("========================");
+});
