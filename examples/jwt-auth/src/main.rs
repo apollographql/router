d161682cb5f43efee736a3fe276fbc28137e2945
Gary Pennington <gary@apollographql.com>
2022-03-10 13:49:32 +0000

    Merge with main and use exported main fn
    Merged with main and adopted main function from apollo-router.

--- examples/jwt-auth/src/main.rs
+++ examples/jwt-auth/src/main.rs
@@ -1,54 +1,25 @@
-//! Main entry point for CLI command to start server.
+//! curl -v \
+//!     --header 'content-type: application/json' \
+//!     --header 'authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDY4MTM1NzMsImV4cCI6MTY0NjgyMDc3MywibmJmIjoxNjQ2ODEzNTczfQ.vywNkhZ7mX2KU8cu6o1FG4xNYR7YvXyavzzta9g7fQE' \
+//!     --url 'http://127.0.0.1:4000' \
+//!     --data '{"query":"query Query {\n  me {\n    name\n  }\n}"}'
+//! [...]
+//! < HTTP/1.1 403 Forbidden
+//! < content-length: 242
+//! < date: Thu, 10 Mar 2022 13:47:30 GMT
+//! <
+//! * Connection #0 to host 127.0.0.1 left intact
+//! {"errors":[{"message":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDY4MTM1NzMsImV4cCI6MTY0NjgyMDc3MywibmJmIjoxNjQ2ODEzNTczfQ.vywNkhZ7mX2KU8cu6o1FG4xNYR7YvXyavzzta9g7fQE is not authorized: Token has expired","locations":[],"path":null}]}
 
 use anyhow::Result;
-use apollo_router::configuration::Configuration;
-use apollo_router::{
-    set_global_subscriber, ApolloRouterBuilder, ConfigurationKind, RouterSubscriber, SchemaKind,
-    ShutdownKind,
-};
-use apollo_router_core::Schema;
-use tracing_subscriber::EnvFilter;
 
+// adding the module to your main.rs file
+// will automatically register it to the router plugin registry.
+//
+// you can use the plugin by adding it to `config.yml`
 mod jwt;
 
-// curl -v \
-//     --header 'content-type: application/json' \
-//     --url 'http://127.0.0.1:4000' \
-//     --data '{"query":"query { topProducts { reviews { author { name } } name } }"}'
-// [...]
-// {"data":{"topProducts":[{"reviews":[{"author":{"name":"Ada Lovelace"}},{"author":{"name":"Alan Turing"}}],"name":"Table"},{"reviews":[{"author":{"name":"Ada Lovelace"}}],"name":"Couch"},{"reviews":[{"author":{"name":"Alan Turing"}}],"name":"Chair"}]}}
-#[tokio::main]
-async fn main() -> Result<()> {
-    let builder = tracing_subscriber::fmt::fmt()
-        .with_env_filter(EnvFilter::try_new("info").expect("could not parse log"));
-
-    set_global_subscriber(RouterSubscriber::TextSubscriber(builder.finish()))?;
-
-    let schema = SchemaKind::Instance(
-        include_str!("../../supergraph.graphql")
-            .parse::<Schema>()?
-            .boxed(),
-    );
-
-    let configuration = ConfigurationKind::Instance(
-        include_str!("../config.yaml")
-            .parse::<Configuration>()?
-            .boxed(),
-    );
-
-    let server = ApolloRouterBuilder::default()
-        .configuration(configuration)
-        .schema(schema)
-        .shutdown(ShutdownKind::CtrlC)
-        .build();
-
-    let mut server_handle = server.serve();
-    server_handle.with_default_state_receiver().await;
-
-    if let Err(err) = server_handle.await {
-        tracing::error!("{}", err);
-        return Err(err.into());
-    }
-
-    Ok(())
+// `cargo run -- -s ../supergraph.graphql -c ./config.yml`
+fn main() -> Result<()> {
+    apollo_router::main()
 }
