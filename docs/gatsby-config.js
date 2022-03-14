643e27fce8d66899c487edf3d2e45b0110e81936
Jesse Rosenberger <git@jro.cc>
2022-03-14 08:23:01 +0000

    docs: Start to introduce information architecture for Router Preview docs.
    This brings in some docs from some other places and re-jiggers some of the
    existing Managed Federation stuff.
    
    Note that for the moment, it doesn't actually keep the documentation for
    Spaceport public as it needs a bit more massaging to make that crisp.
    (Someone can definitely do that, but it just needs to move from
    `spaceport-configuration-not-currently-linked-in-gatsby-config` into the
    `managed-federation/setup` file's workflow.

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -14,7 +14,21 @@ module.exports = {
         algoliaIndexName: 'router',
         algoliaFilters: ['docset:router'],
         sidebarCategories: {
-          null: ["index", "quickstart", "configuration", "build-from-source", "managed-federation"],
+          null: ["index", "quickstart", "configuration", "build-from-source"],
+          'Managed Federation': [
+            'managed-federation/overview',
+            'managed-federation/setup',
+            '[Studio features](https://www.apollographql.com/docs/studio/federated-graphs/)',
+          ],
+          'Development workflow': [
+            'development-workflow/build-run-queries',
+            'development-workflow/requests',
+            '[Apollo Studio Explorer](https://www.apollographql.com/docs/studio/explorer/)',
+          ],
+          'Third-Party Support': [
+            '[Subgraph-compatible libraries](https://www.apollographql.com/docs/federation/v2/other-servers/)',
+            '[Subgraph specification](https://www.apollographql.com/docs/federation/v2/federation-spec/)',
+          ],
         },
       },
     },
