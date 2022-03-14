apollographql/router

commit 1f8ee7af2df49aebd1b355844a232c45bf077520

Update the documentation around headers. (#609)
* Update the documentation around headers.

* Update the documentation around headers.
Delete subgraphs section

* Expand headers example

* Add yaml anchor example.

* Rename the allow_client_id_from_file plugin

Co-authored-by: bryn <bryn@apollographql.com>

examples/async-allow-client-id/src/allow_client_id_from_file.rs
@@ -151,7 +151,7 @@ impl Plugin for AllowClientIdFromFile {
 // we use using the `Reverse domain name notation`
 register_plugin!(
     "example",
-    "allow-client-id-from-file",
+    "allow_client_id_from_file",
     AllowClientIdFromFile
 );
 

docs/source/configuration.mdx
@@ -77,6 +77,23 @@ Default: `info`
 </tbody>
 </table>
 
+## Generating a configuration schema
+
+The Apollo Router is able to produce a JSON schema for use in your text editor. It significantly eases the burden of making
+sure that your yaml file is correctly formatted, and even gives you content assist.
+
+To generate the schema use:
+```bash
+router --schema > configuration_schema.json
+```
+
+One you have generated the schema configure your text editor. Here are some instructions for well known editors:
+* [Emacs](https://emacs-lsp.github.io/lsp-mode/page/lsp-yaml)
+* [IntelliJ](https://www.jetbrains.com/help/idea/json.html#ws_json_using_schemas)
+* [Sublime](https://github.com/sublimelsp/LSP-yaml)
+* [Vim](https://github.com/Quramy/vison)
+* [VsCode](https://code.visualstudio.com/docs/languages/json#_json-schemas-and-settings)
+
 ## Handling CORS
 
 If you attempt to query the Apollo Router from a browser-based client, your request might be rejected due to CORS policies.
@@ -140,118 +157,37 @@ server:
     # Which response headers should be made available to scripts running in the
     # browser in response to a cross-origin request.
     expose_headers:
-
-#
-# Subgraphs
-#
-# This configuration affects the communication with underlying subgraphs.
-# The `subgraphs` property is a map to individual named subgraphs and should
-# map to the subgraph name that is defined in the Supergraph.
-#
-# On each named subgraph of the subgraphs map, these properties can be set:
-#
-#  - routing_url
-#  - layers
-#
-# These are explained further below.
-#
-# * routing_url *
-#
-# The URL the Router should connect to for this subgraph.
-#
-# By default, routing information is parsed from the supergraph which is
-# typically sufficient for most environments and respects the settings which
-# you've set during composition in Apollo Studio or via Rover.  Provide it here
-# only to *override* the supergraph's subgraph routing URLs.
-#
-#
-# * layers *
-#
-# This is a map of configurable layers which will be invoked.
-#
-# Layers are features (either native or extensions) which can be enabled for
-# each subgraph.  Common layers are the "headers*" layers, and an example can be
-# found for below.  For more information, see later in the documentation.
-#
-
-subgraphs:
-  # Specifies the runtime configuration for talking to the "accounts" subgraph.
-  accounts:
-    # The URL of the "accounts" subgraph's GraphQL endpoint
-    routing_url: http://localhost:4001/graphql
-
-    # Layers to be applied to the "accounts" subgraph.
-    layers:
-      - headers_propagate:
-          matching:
-            regex: ^upstream-header-.*
-      - headers_propagate:
-          named:
-            name: "old-name"
-            rename: "new-name"
-            default_value: "0"
-      - headers_propagate:
-          named:
-            name: "old-name"
-            rename: "new-name"
-            default_value: "0"
-      - headers_remove:
-          name: "x-legacy-account-id"
-      - headers_remove:
-          matching:
-            regex: ^x-deprecated.*
-      - headers_insert:
-          name: "router-subgraph-name"
-          value: "accounts"
-
-  # Specifies the runtime configuration for talking to the "products" subgraph.
-  products:
-    # The URL of the "products" subgraph's GraphQL endpoint
-    routing_url: http://localhost:4003/graphql
-
-    # Layers to be applied to the "products" subgraph.
-    layers:
-      - headers_insert:
-          name: "router-subgraph-name"
-          value: "products"
-      - headers_remove:
-          name: "x-legacy-tracking-id"
-
-
-# graph configuration, see documentation below
-# graph:
-
-# spaceport configuration, see documentation below
-# spaceport:
-
-# OpenTelemetry configuration, see documentation below
-# opentelemetry:
+plugins:
+    # See below for plugins
 ```
-
 ## Passing headers to subgraphs
 
 You can customize which HTTP headers the Apollo Router includes in its requests to your individual subgraphs. You specify these headers on a per-subgraph basis in your [YAML configuration file](#configuration-file).
 
-More specifically, each subgraph's configuration includes a `layers` array that can include header-related actions for the router to perform (in order) before sending a request:
+More specifically, each the headers plugin include can be configured with an array of header-related actions for the router to perform (in order) before sending a request:
 
 ```yaml{4-12}
-products:
+plugins:
   # ...other configuration...
 
-  layers:
-    - headers_propagate:
+  apollo.headers:
+    all:
+    - propagate:
         matching:
           regex: ^upstream-header-.*
-    - headers_remove:
+    - remove:
         name: "x-legacy-account-id"
-    - headers_insert:
-        name: "router-subgraph-name"
-        value: "accounts"
+    subgraphs:
+      products:
+        - insert:
+            name: "router-subgraph-name"
+            value: "products"
+
 ```
 
 The following transformations are specific to customizing headers sent to subgraphs:
 
-### `headers_propagate`
+### `propagate`
 
 Enables you to selectively propagate headers that were included in the client's request to the router.
 
@@ -260,7 +196,7 @@ Enables you to selectively propagate headers that were included in the client's 
 You can specify which headers to propagate based on a matching [regex pattern](https://docs.rs/regex/latest/regex/):
 
 ```yaml
-- headers_propagate:
+- propagate:
     matching:
       regex: .*
 ```
@@ -271,16 +207,13 @@ You can also provide a static string with the `named` option. These `named` conf
 - `rename`: Renames the header's key to the provided value
 
 ```yaml
-- headers_propagate:
-    named:
-      name: "x-user-id"
-      default_value: "abc123"
+- propagate:
+      named: "x-user-id"
+      value: "abc123"
       rename: "account-id"
-    named:
-      name: "Authorization"
 ```
 
-### `headers_remove`
+### `remove`
 
 Enables you to selectively remove headers that were included in the client's request to the router. Similar to [`headers_propagate`](#headers-propagate), this option can match either a static string or a [regular expression](https://docs.rs/regex/latest/regex/).
 
@@ -288,9 +221,9 @@ Enables you to selectively remove headers that were included in the client's req
 
 ```yaml
 # Do not send this subgraph the "Cookie" header.
-- headers_remove:
-    name: "Cookie"
-- headers_remove:
+- remove:
+    named: "Cookie"
+- remove:
     matching:
       # Remove headers that include the legacy 'x-' prefix.
       regex: ^x-.*$
@@ -301,7 +234,7 @@ Enables you to selectively remove headers that were included in the client's req
 Enables you to add custom headers to requests going to a specific subgraph. These headers are always static strings that originate in the router, instead of originating in the client.
 
 ```yaml
-- headers_insert:
+- insert:
     name: "sent-from-our-apollo-router"
     value: "indeed"
 ```
@@ -311,21 +244,43 @@ Enables you to add custom headers to requests going to a specific subgraph. Thes
 Here's an example of customizing different headers for two subgraphs: `accounts` and `products`. These two subgraphs are both sent `router-subgraph-name` headers with either the `accounts` or `products` value, respectively. Any other subgraph receives _no_ headers.
 
 ```yaml
-subgraphs:
-  # These headers apply to the "accounts" subgraph only.
-  # They are not passed to the "products" subgraph (below).
-  accounts:
-    layers:
-      - headers_insert:
-          name: "router-subgraph-name"
-          value: "accounts"
-  products:
-    # Layers to be applied to the "products" subgraph only.
-    # They are not passed to the "accounts" subgraph (above).
-    layers:
-      - headers_insert:
-          name: "router-subgraph-name"
-          value: "products"
+plugins:
+  apollo.headers:
+    all:
+    # All subgraphs will propagate matching headers.
+    - propagate:
+        matching:
+          regex: ^upstream-header-.*
+    # All subgraphs will propagate matching headers.
+    - propagate:
+        named: "some-header"
+        default: "default-value"
+        rename: "destination-header"
+    # All subgraphs will remove the "x-legacy-account-id" header.
+    - remove:
+        named: "x-legacy-account-id"
+    - remove:
+        matching: "x-legacy-account-id"
+    # Subgraph specific configuration
+    subgraphs:
+      products:
+        # Calls to the products subgraph will have the "router-subgraph-name" header.
+        - insert:
+            name: "router-subgraph-name"
+            value: "products"
+```
+
+You can also use Yaml anchors to eliminate repetition in your config:
+```yaml
+plugins:
+  apollo.headers:
+    subgraphs:
+      products:
+        - insert: &custom_header
+            name: "custom-header"
+            value: "something"
+      reviews:
+        - insert: *custom_header
 ```
 
 ## OpenTelemetry tracing

examples/async-allow-client-id/config.yml
@@ -6,6 +6,6 @@ plugins:
   # this plugin takes a path
   # and will read from it everytime a request comes in
   # to check if a client can run a query
-  example.allow-client-id-from-file:
+  example.allow_client_id_from_file:
     header: "x-client-id" # the header to check for
     path: "allowedClientIds.json" # the file that contains the allowed clients list

commit fdfd9795c49d72f7810f1184fbcb70a35c6f1c5c

Fix regression caused by plugin name change
The change in: 1f8ee7af2df49aebd1b355844a232c45bf077520 broke the
test.

This fixes it.

examples/async-allow-client-id/src/allow_client_id_from_file.rs
@@ -176,7 +176,7 @@ mod tests {
     #[tokio::test]
     async fn plugin_registered() {
         apollo_router_core::plugins()
-            .get("example.allow-client-id-from-file")
+            .get("example.allow_client_id_from_file")
             .expect("Plugin not found")
             .create_instance(&json!({"header": "x-client-id","path": "allowedClientIds.json"}))
             .unwrap();