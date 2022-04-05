9bea90bc279846f2c771fdce2ab8450c9db334d2
bryn <bryn@apollographql.com>
2022-04-05 22:16:47 +0200

    Remove redundant propagator

--- apollo-router/src/executable.rs
+++ apollo-router/src/executable.rs
@@ -249,16 +249,6 @@ pub async fn rt_main() -> Result<()> {
         }
     };
 
-    // Create your text map propagator & assign it as the global propagator.
-    //
-    // This is required in order to create the header traceparent used in http_subgraph to
-    // propagate the trace id to the subgraph services.
-    //
-    // /!\ If this is not called, there will be no warning and no header will be sent to the
-    //     subgraphs!
-    let propagator = opentelemetry::sdk::propagation::TraceContextPropagator::new();
-    opentelemetry::global::set_text_map_propagator(propagator);
-
     let server = ApolloRouterBuilder::default()
         .configuration(configuration)
         .schema(schema)
