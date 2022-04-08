7c698b31c25d4664a2522c364066dcf4a9e2993e
Stephen Barlow <stephen@apollographql.com>
2022-04-08 01:13:02 -0700

    Attempt at new Router startup message (#780)
    Co-authored-by: Geoffroy Couprie <geoffroy@apollographql.com>

--- apollo-router/src/executable.rs
+++ apollo-router/src/executable.rs
@@ -172,12 +172,6 @@ pub async fn rt_main() -> Result<()> {
 
     GLOBAL_ENV_FILTER.set(opt.log_level).unwrap();
 
-    tracing::info!(
-        "{}@{}",
-        std::env!("CARGO_PKG_NAME"),
-        std::env!("CARGO_PKG_VERSION")
-    );
-
     let current_directory = std::env::current_dir()?;
 
     let configuration = opt
@@ -200,6 +194,11 @@ pub async fn rt_main() -> Result<()> {
 
     let schema = match (opt.supergraph_path, opt.apollo_key) {
         (Some(supergraph_path), _) => {
+            tracing::info!(
+                "{}@{}",
+                std::env!("CARGO_PKG_NAME"),
+                std::env!("CARGO_PKG_VERSION")
+            );
             let supergraph_path = if supergraph_path.is_relative() {
                 current_directory.join(supergraph_path)
             } else {
@@ -212,6 +211,11 @@ pub async fn rt_main() -> Result<()> {
             }
         }
         (None, Some(apollo_key)) => {
+            tracing::info!(
+                "{}@{}",
+                std::env!("CARGO_PKG_NAME"),
+                std::env!("CARGO_PKG_VERSION")
+            );
             let apollo_graph_ref = opt.apollo_graph_ref.ok_or_else(||anyhow!("cannot fetch the supergraph from Apollo Studio without setting the APOLLO_GRAPH_REF environment variable"))?;
             if opt.apollo_schema_poll_interval < Duration::from_secs(10) {
                 return Err(anyhow!("Apollo poll interval must be at least 10s"));
@@ -225,34 +229,37 @@ pub async fn rt_main() -> Result<()> {
             }
         }
         _ => {
+            let version = std::env!("CARGO_PKG_VERSION");
             return Err(anyhow!(
                 r#"
-    💫 Apollo Router requires a supergraph to be set using '--supergraph':
+===
+APOLLO ROUTER v{version}
 
-        $ ./router --supergraph <file>`
+⚠️  The Apollo Router requires a composed supergraph schema at startup. ⚠️
 
-        Alternatively, to retrieve the supergraph from Apollo Studio, set the APOLLO_KEY
-        and APOLLO_GRAPH_REF environment variables to your graph's settings.
-        
-          $ APOLLO_KEY="..." APOLLO_GRAPH_REF="..." ./router
-          
-        For more on Apollo Studio and Managed Federation, see our documentation:
-        
-          https://www.apollographql.com/docs/router/managed-federation/
+👉 DO ONE:
 
-    🪐 The supergraph can be built or downloaded from the Apollo Registry
-       using the Rover CLI. To find out how, see:
+  * Pass a local schema file with the '--supergraph' option:
 
-        https://www.apollographql.com/docs/rover/supergraphs/.
+      $ ./router --supergraph <file_path>
 
-    🧪 If you're just experimenting, you can download and use an example
-       supergraph with pre-deployed subgraphs:
+  * Fetch a registered schema from Apollo Studio by setting
+    these environment variables:
 
-        $ curl -L https://supergraph.demo.starstuff.dev/ > starstuff.graphql
+      $ APOLLO_KEY="..." APOLLO_GRAPH_REF="..." ./router
 
-       Then run the Apollo Router with that supergraph:
+      For details, see the Apollo docs:
+      https://www.apollographql.com/docs/router/managed-federation/setup
 
-        $ ./router --supergraph starstuff.graphql
+🔬 TESTING THINGS OUT?
+
+  1. Download an example supergraph schema with Apollo-hosted subgraphs:
+
+    $ curl -L https://supergraph.demo.starstuff.dev/ > starstuff.graphql
+
+  2. Run the Apollo Router with the supergraph schema:
+
+    $ ./router --supergraph starstuff.graphql
 
     "#,
             ));
