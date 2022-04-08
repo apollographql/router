30e7edd3858da34074c44f5d480cfb980a937ca2
Geoffroy Couprie <geoffroy@apollographql.com>
2022-04-08 12:26:16 +0200

    remove apollo-parser from precompiled dependencies
    it appears multiple times and that confuses cargo build

--- precompile.sh
+++ precompile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for PACKAGE in opentelemetry router-bridge opentelemetry-otlp opentelemetry-jaeger opentelemetry-prometheus tracing tokio moka mockall tower-http tonic apollo-parser hyper-rustls
+for PACKAGE in router-bridge opentelemetry opentelemetry-otlp opentelemetry-jaeger opentelemetry-prometheus tracing tokio moka mockall tower-http tonic hyper-rustls
 do
   cargo build -p $PACKAGE
 done
