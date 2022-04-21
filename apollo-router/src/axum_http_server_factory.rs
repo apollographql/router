91733b5f37bde3fc257c945710f1ae4743d78cb7
Jeremy Lempereur <jeremy.lempereur@gmail.com>
2022-04-21 10:44:04 +0200

    fix: Correctly flag incoming POST requests.  (#873)
    fixes #865
    
    A regression happened during our switch to Axum which would mark incoming POST requests as GET, thus triggering our forbid_http_mutations layer, and preventing mutations on the router.
    This PR fixes this, and introduces some tests. More integration or axum oriented tests should follow.
    
    When writing a unit test to assert subgraph requests are POST, I noticed the test would never fail. this is because withf only acts as a request router, and panicking (or in our case asserting) within wouldnt cause the router to fail.
    
    This commit rewrites a couple of subgraph tests so they properly notify whether they completed successfully or failed.

--- apollo-router/src/axum_http_server_factory.rs
+++ apollo-router/src/axum_http_server_factory.rs
@@ -99,15 +99,10 @@ impl HttpServerFactory for AxumHttpServerFactory {
                         move |host: Host,
                               service: Extension<BufferedService>,
                               http_request: Request<Body>| {
-                            redirect_or_run_graphql_operation(
-                                host,
-                                service,
-                                http_request,
-                                display_landing_page,
-                            )
+                            handle_get(host, service, http_request, display_landing_page)
                         }
                     })
-                    .post(run_graphql_operation),
+                    .post(handle_post),
                 )
                 .route(
                     "/graphql",
@@ -116,15 +111,10 @@ impl HttpServerFactory for AxumHttpServerFactory {
                         move |host: Host,
                               service: Extension<BufferedService>,
                               http_request: Request<Body>| {
-                            redirect_or_run_graphql_operation(
-                                host,
-                                service,
-                                http_request,
-                                display_landing_page,
-                            )
+                            handle_get(host, service, http_request, display_landing_page)
                         }
                     })
-                    .post(run_graphql_operation),
+                    .post(handle_post),
                 )
                 .route("/.well-known", get(health_check))
                 .route("/apollo", get(health_check))
@@ -407,7 +397,7 @@ async fn custom_plugin_handler(
     Ok::<_, String>(res)
 }
 
-async fn redirect_or_run_graphql_operation(
+async fn handle_get(
     Host(host): Host,
     Extension(service): Extension<BufferedService>,
     http_request: Request<Body>,
@@ -439,20 +429,19 @@ async fn redirect_or_run_graphql_operation(
     (StatusCode::BAD_REQUEST, "Invalid Graphql request").into_response()
 }
 
-async fn run_graphql_operation(
+async fn handle_post(
     Host(host): Host,
     OriginalUri(uri): OriginalUri,
     Json(request): Json<graphql::Request>,
     Extension(service): Extension<BufferedService>,
     header_map: HeaderMap,
 ) -> impl IntoResponse {
-    let mut http_request = Request::builder()
-        .uri(
-            Uri::from_str(&format!("http://{}{}", host, uri))
-                .expect("the URL is already valid because it comes from axum; qed"),
-        )
-        .body(request)
-        .expect("body has already been parsed; qed");
+    let mut http_request = Request::post(
+        Uri::from_str(&format!("http://{}{}", host, uri))
+            .expect("the URL is already valid because it comes from axum; qed"),
+    )
+    .body(request)
+    .expect("body has already been parsed; qed");
     *http_request.headers_mut() = header_map;
 
     run_graphql_request(service, http_request)
