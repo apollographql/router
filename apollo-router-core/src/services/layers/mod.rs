6e8609c83e89e92c688fa581cbdc649d514add6c
o0Ignition0o <jeremy.lempereur@gmail.com>
2022-05-06 16:38:33 +0200

    fix: allow only HTTP Post mutations

--- apollo-router-core/src/services/layers/mod.rs
+++ apollo-router-core/src/services/layers/mod.rs
@@ -1,4 +1,4 @@
 //! Layers that are internal to the execution pipeline.
+pub(crate) mod allow_only_http_post_mutations;
 pub(crate) mod apq;
 pub(crate) mod ensure_query_presence;
-pub(crate) mod forbid_http_get_mutations;
