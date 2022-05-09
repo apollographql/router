a14d990ed698c900ac611d43a75c1ee584594698
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2022-05-09 15:44:07 +0200

    better welcome message
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- apollo-router/src/executable.rs
+++ apollo-router/src/executable.rs
@@ -186,9 +186,9 @@ pub async fn rt_main() -> Result<()> {
         })
         .unwrap_or_else(|| ConfigurationKind::Instance(Configuration::builder().build().boxed()));
 
+    tracing::info!("Apollo Router v{} // (c) ApolloGraph, Inc. // Licensed as ELv2 (https://go.apollo.dev/elv2)", std::env!("CARGO_PKG_VERSION"));
     let schema = match (opt.supergraph_path, opt.apollo_key) {
         (Some(supergraph_path), _) => {
-            tracing::info!("Apollo Router v{} // (c) ApolloGraph, Inc. // Licensed as ELv2 (https://go.apollo.dev/elv2)", std::env!("CARGO_PKG_VERSION"));
             let supergraph_path = if supergraph_path.is_relative() {
                 current_directory.join(supergraph_path)
             } else {
@@ -201,7 +201,6 @@ pub async fn rt_main() -> Result<()> {
             }
         }
         (None, Some(apollo_key)) => {
-            tracing::info!("Apollo Router v{} // (c) ApolloGraph, Inc. // Licensed as ELv2 (https://go.apollo.dev/elv2)", std::env!("CARGO_PKG_VERSION"));
             let apollo_graph_ref = opt.apollo_graph_ref.ok_or_else(||anyhow!("cannot fetch the supergraph from Apollo Studio without setting the APOLLO_GRAPH_REF environment variable"))?;
             if opt.apollo_schema_poll_interval < Duration::from_secs(10) {
                 return Err(anyhow!("Apollo poll interval must be at least 10s"));
@@ -215,10 +214,8 @@ pub async fn rt_main() -> Result<()> {
             }
         }
         _ => {
-            let version = std::env!("CARGO_PKG_VERSION");
             return Err(anyhow!(
                 r#"
-Apollo Router v{version} // (c) ApolloGraph, Inc. // Licensed as ELv2 (https://go.apollo.dev/elv2)"
 
 ⚠️  The Apollo Router requires a composed supergraph schema at startup. ⚠️
 
