7ed84741257e45018bc7be177b0bdcd3c70d7e64
Geoffroy Couprie <geoffroy@apollographql.com>
2022-05-13 11:11:49 +0200

    use the API schema in response formatting tests

--- apollo-router-core/src/spec/query.rs
+++ apollo-router-core/src/spec/query.rs
@@ -847,9 +847,10 @@ mod tests {
         }};
 
         ($schema:expr, $query:expr, $response:expr, $operation:expr, $variables:expr, $expected:expr $(,)?) => {{
-            let schema: Schema = with_supergraph_boilerplate($schema)
-                .parse()
+            let schema = with_supergraph_boilerplate($schema)
+                .parse::<Schema>()
                 .expect("could not parse schema");
+            let api_schema = schema.api_schema();
             let query = Query::parse($query, &schema).expect("could not parse query");
             let mut response = Response::builder().data($response.clone()).build();
 
@@ -857,7 +858,7 @@ mod tests {
                 &mut response,
                 $operation,
                 $variables.as_object().unwrap().clone(),
-                &schema,
+                api_schema,
             );
             assert_eq_and_ordered!(response.data.as_ref().unwrap(), &$expected);
         }};
@@ -869,11 +870,14 @@ mod tests {
             r#"
         schema
             @core(feature: "https://specs.apollo.dev/core/v0.1")
-            @core(feature: "https://specs.apollo.dev/join/v0.1") {
+            @core(feature: "https://specs.apollo.dev/join/v0.1")
+            @core(feature: "https://specs.apollo.dev/inaccessible/v0.1")
+             {
             query: Query
         }
         directive @core(feature: String!) repeatable on SCHEMA
         directive @join__graph(name: String!, url: String!) on ENUM_VALUE
+        directive @inaccessible on OBJECT | FIELD_DEFINITION | INTERFACE | UNION
         enum join__Graph {
             TEST @join__graph(name: "test", url: "http://localhost:4001/graphql")
         }
