eb848c33d49f9b5d6fd694539a5a7a0d4c7d4a24
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2022-06-07 16:43:03 +0200

    fix: Show a helpful error message when a subgraph does not return JSON or bad status code
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- apollo-router/src/services/tower_subgraph_service.rs
+++ apollo-router/src/services/tower_subgraph_service.rs
@@ -4,8 +4,8 @@ use crate::prelude::*;
 use futures::future::BoxFuture;
 use global::get_text_map_propagator;
 use http::{
-    header::{ACCEPT, CONTENT_TYPE},
-    HeaderValue,
+    header::{self, ACCEPT, CONTENT_TYPE},
+    HeaderValue, StatusCode,
 };
 use hyper::client::HttpConnector;
 use hyper_rustls::HttpsConnector;
@@ -66,7 +66,7 @@ impl tower::Service<graphql::SubgraphRequest> for TowerSubgraphService {
             let body = serde_json::to_string(&body).expect("JSON serialization should not fail");
 
             let mut request = http::request::Request::from_parts(parts, body.into());
-            let app_json: HeaderValue = "application/json".parse().unwrap();
+            let app_json: HeaderValue = HeaderValue::from_static("application/json");
             request.headers_mut().insert(CONTENT_TYPE, app_json.clone());
             request.headers_mut().insert(ACCEPT, app_json);
 
@@ -111,6 +111,18 @@ impl tower::Service<graphql::SubgraphRequest> for TowerSubgraphService {
 
             // Keep our parts, we'll need them later
             let (parts, body) = response.into_parts();
+            if let Some(content_type) = parts.headers.get(header::CONTENT_TYPE) {
+                if let Ok(content_type_str) = content_type.to_str() {
+                    // Using .contains because sometimes we could have charset included (example: "application/json; charset=utf-8")
+                    if !content_type_str.contains("application/json") {
+                        return Err(BoxError::from(graphql::FetchError::SubrequestHttpError {
+                            service: service_name.clone(),
+                            reason: format!("subgraph doesn't return JSON content (current content-type: {content_type:?})"),
+                        }));
+                    }
+                }
+            }
+
             let body = hyper::body::to_bytes(body)
                 .instrument(tracing::debug_span!("aggregate_response_data"))
                 .await
@@ -122,6 +134,16 @@ impl tower::Service<graphql::SubgraphRequest> for TowerSubgraphService {
                         reason: err.to_string(),
                     }
                 })?;
+            if parts.status != StatusCode::OK {
+                return Err(BoxError::from(graphql::FetchError::SubrequestHttpError {
+                    service: service_name.clone(),
+                    reason: format!(
+                        "subgraph returns status error '{}': {})",
+                        parts.status,
+                        String::from_utf8_lossy(&body)
+                    ),
+                }));
+            }
 
             let graphql: graphql::Response = tracing::debug_span!("parse_subgraph_response")
                 .in_scope(|| {
@@ -140,5 +162,128 @@ impl tower::Service<graphql::SubgraphRequest> for TowerSubgraphService {
                 context,
             ))
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::convert::Infallible;
+    use std::net::SocketAddr;
+    use std::str::FromStr;
+
+    use axum::Server;
+    use http::header::HOST;
+    use http::Uri;
+    use hyper::service::make_service_fn;
+    use hyper::Body;
+    use tower::{service_fn, ServiceExt};
+
+    use crate::fetch::OperationKind;
+    use crate::{http_compat, Context, Request, Response, SubgraphRequest};
+
+    use super::*;
+
+    // starts a local server emulating a subgraph returning status code 400
+    async fn emulate_subgraph_bad_request(socket_addr: SocketAddr) {
+        async fn handle(_request: http::Request<Body>) -> Result<http::Response<Body>, Infallible> {
+            Ok(http::Response::builder()
+                .header("Content-Type", "application/json")
+                .status(StatusCode::BAD_REQUEST)
+                .body(r#"BAD REQUEST"#.into())
+                .unwrap())
+        }
+
+        let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+        let server = Server::bind(&socket_addr).serve(make_svc);
+        if let Err(e) = server.await {
+            eprintln!("server error: {}", e);
+        }
+    }
+
+    // starts a local server emulating a subgraph returning bad response format
+    async fn emulate_subgraph_bad_response_format(socket_addr: SocketAddr) {
+        async fn handle(_request: http::Request<Body>) -> Result<http::Response<Body>, Infallible> {
+            Ok(http::Response::builder()
+                .header("Content-Type", "text/html")
+                .status(StatusCode::OK)
+                .body(r#"TEST"#.into())
+                .unwrap())
+        }
+
+        let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+        let server = Server::bind(&socket_addr).serve(make_svc);
+        if let Err(e) = server.await {
+            eprintln!("server error: {}", e);
+        }
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_bad_status_code() {
+        let socket_addr = SocketAddr::from_str("127.0.0.1:2626").unwrap();
+        tokio::task::spawn(emulate_subgraph_bad_request(socket_addr));
+        let subgraph_service = TowerSubgraphService::new("test");
+
+        let url = Uri::from_str(&format!("http://{}", socket_addr)).unwrap();
+        let err = subgraph_service
+            .oneshot(SubgraphRequest {
+                originating_request: Arc::new(
+                    http_compat::Request::fake_builder()
+                        .header(HOST, "host")
+                        .header(CONTENT_TYPE, "application/json")
+                        .body(Request::builder().query(Some("query".to_string())).build())
+                        .build()
+                        .expect("expecting valid request"),
+                ),
+                subgraph_request: http_compat::Request::fake_builder()
+                    .header(HOST, "rhost")
+                    .header(CONTENT_TYPE, "application/json")
+                    .uri(url)
+                    .body(Request::builder().query(Some("query".to_string())).build())
+                    .build()
+                    .expect("expecting valid request"),
+                operation_kind: OperationKind::Query,
+                context: Context::new(),
+            })
+            .await
+            .unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            "HTTP fetch failed from 'test': subgraph returns status error '400 Bad Request': BAD REQUEST)"
+        );
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_bad_content_type() {
+        let socket_addr = SocketAddr::from_str("127.0.0.1:2525").unwrap();
+        tokio::task::spawn(emulate_subgraph_bad_response_format(socket_addr));
+        let subgraph_service = TowerSubgraphService::new("test");
+
+        let url = Uri::from_str(&format!("http://{}", socket_addr)).unwrap();
+        let err = subgraph_service
+            .oneshot(SubgraphRequest {
+                originating_request: Arc::new(
+                    http_compat::Request::fake_builder()
+                        .header(HOST, "host")
+                        .header(CONTENT_TYPE, "application/json")
+                        .body(Request::builder().query(Some("query".to_string())).build())
+                        .build()
+                        .expect("expecting valid request"),
+                ),
+                subgraph_request: http_compat::Request::fake_builder()
+                    .header(HOST, "rhost")
+                    .header(CONTENT_TYPE, "application/json")
+                    .uri(url)
+                    .body(Request::builder().query(Some("query".to_string())).build())
+                    .build()
+                    .expect("expecting valid request"),
+                operation_kind: OperationKind::Query,
+                context: Context::new(),
+            })
+            .await
+            .unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            "HTTP fetch failed from 'test': subgraph doesn't return JSON content (current content-type: \"text/html\")"
+        );
     }
 }
