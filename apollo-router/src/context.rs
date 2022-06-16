accb4f92c124850b49eef651a2f928a5ffe83fb6
Simon Sapin <simon@apollographql.com>
2022-06-16 13:59:29 +0200

    Fix some links in doc-comments.
    This fixes some of the warnings emitted by `cargo doc`.
    
    Remaining warnings involve either making some types public,
    or removing mentions of them in public docs and in some cases
    making fields private. See:
    https://github.com/apollographql/router/issues/1150#issuecomment-1157692784

--- apollo-router/src/context.rs
+++ apollo-router/src/context.rs
@@ -13,14 +13,14 @@ use tower::BoxError;
 /// Holds [`Context`] entries.
 pub type Entries = Arc<DashMap<String, Value>>;
 
-/// Context for a [`http_compat::Request`]
+/// Context for a [`crate::http_compat::Request`]
 ///
 /// Context makes use of [`DashMap`] under the hood which tries to handle concurrency
 /// by allowing concurrency across threads without requiring locking. This is great
 /// for usability but could lead to surprises when updates are highly contested.
 ///
 /// Within the router, contention is likely to be highest within plugins which
-/// provide [`SubgraphRequest`] or [`SubgraphResponse`] processing. At such times,
+/// provide [`crate::SubgraphRequest`] or [`crate::SubgraphResponse`] processing. At such times,
 /// plugins should restrict themselves to the [`Context::get`] and [`Context::upsert`]
 /// functions to minimise the possibility of mis-sequenced updates.
 #[derive(Clone, Debug)]
