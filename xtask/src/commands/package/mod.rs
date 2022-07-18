7904f1d414d220374a53738e7645c83467151972
o0Ignition0o <jeremy.lempereur@gmail.com>
2022-07-18 15:53:06 +0200

    Revert "insert the full target triplet in the package name (#1393)"
    This reverts commit b38d59476de1ce966e45358d388a3d4240da0932.

--- xtask/src/commands/package/mod.rs
+++ xtask/src/commands/package/mod.rs
@@ -1,9 +1,7 @@
 #[cfg(target_os = "macos")]
 mod macos;
 
-use std::fmt;
 use std::path::Path;
-use std::str::FromStr;
 
 use anyhow::ensure;
 use anyhow::Context;
@@ -13,16 +11,6 @@ use structopt::StructOpt;
 use xtask::*;
 
 const INCLUDE: &[&str] = &["README.md", "LICENSE", "licenses.html"];
-pub(crate) const TARGET_X86_64_MUSL_LINUX: &str = "x86_64-unknown-linux-musl";
-pub(crate) const TARGET_X86_64_GNU_LINUX: &str = "x86_64-unknown-linux-gnu";
-pub(crate) const TARGET_X86_64_WINDOWS: &str = "x86_64-pc-windows-msvc";
-pub(crate) const TARGET_X86_64_MACOS: &str = "x86_64-apple-darwin";
-pub(crate) const POSSIBLE_TARGETS: [&str; 4] = [
-    TARGET_X86_64_MUSL_LINUX,
-    TARGET_X86_64_GNU_LINUX,
-    TARGET_X86_64_WINDOWS,
-    TARGET_X86_64_MACOS,
-];
 
 #[derive(Debug, StructOpt)]
 pub struct Package {
@@ -33,9 +21,6 @@ pub struct Package {
     #[cfg(target_os = "macos")]
     #[structopt(flatten)]
     macos: macos::PackageMacos,
-
-    #[structopt(long, default_value, possible_values = &POSSIBLE_TARGETS)]
-    target: Target,
 }
 
 impl Package {
@@ -57,8 +42,13 @@ impl Package {
             }
             self.output.to_owned()
         } else if self.output.is_dir() {
-            self.output
-                .join(format!("router-{}-{}.tar.gz", *PKG_VERSION, self.target))
+            self.output.join(format!(
+                "router-{}-{}-{}.tar.gz",
+                *PKG_VERSION,
+                // NOTE: same as xtask
+                std::env::consts::ARCH,
+                std::env::consts::OS,
+            ))
         } else {
             self.output.to_owned()
         };
@@ -90,65 +80,5 @@ impl Package {
         ar.finish().context("could not finish TGZ archive")?;
 
         Ok(())
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub(crate) enum Target {
-    MuslLinux,
-    GnuLinux,
-    Windows,
-    MacOS,
-    Other,
-}
-
-impl Default for Target {
-    fn default() -> Self {
-        if cfg!(target_arch = "x86_64") {
-            if cfg!(target_os = "windows") {
-                Target::Windows
-            } else if cfg!(target_os = "linux") {
-                if cfg!(target_env = "gnu") {
-                    Target::GnuLinux
-                } else if cfg!(target_env = "musl") {
-                    Target::MuslLinux
-                } else {
-                    Target::Other
-                }
-            } else if cfg!(target_os = "macos") {
-                Target::MacOS
-            } else {
-                Target::Other
-            }
-        } else {
-            Target::Other
-        }
-    }
-}
-
-impl FromStr for Target {
-    type Err = anyhow::Error;
-
-    fn from_str(input: &str) -> Result<Self, Self::Err> {
-        match input {
-            TARGET_X86_64_MUSL_LINUX => Ok(Self::MuslLinux),
-            TARGET_X86_64_GNU_LINUX => Ok(Self::GnuLinux),
-            TARGET_X86_64_WINDOWS => Ok(Self::Windows),
-            TARGET_X86_64_MACOS => Ok(Self::MacOS),
-            _ => Ok(Self::Other),
-        }
-    }
-}
-
-impl fmt::Display for Target {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let msg = match &self {
-            Target::MuslLinux => TARGET_X86_64_MUSL_LINUX,
-            Target::GnuLinux => TARGET_X86_64_GNU_LINUX,
-            Target::Windows => TARGET_X86_64_WINDOWS,
-            Target::MacOS => TARGET_X86_64_MACOS,
-            Target::Other => "unknown-target",
-        };
-        write!(f, "{}", msg)
     }
 }
