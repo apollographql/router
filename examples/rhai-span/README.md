ee3c3c7f13bb82500ca3563eb02bb23ca0fc6dca
Gary Pennington <gary@apollographql.com>
2022-08-16 13:29:12 +0100

    remember to check in new example
    or things won't build too well.

---
+++ examples/rhai-span/README.md
@@ -0,0 +1,8 @@
+# Rhai script
+
+Demonstrates extracting body details and header manipulation via Rhai script.
+
+Usage:
+```bash
+cargo run -- -s ../graphql/supergraph.graphql -c ./router.yaml
+```
---
+++ examples/rhai-span/src/main.rs
@@ -0,0 +1,80 @@
+//! % curl -v \
+//!    --request POST \
+//!    --header 'content-type: application/json' \
+//!    --url 'http://127.0.0.1:4000' \
+//!    --data '{"query":"query Me {\n  me {\n    name\n  }\n}"}'
+
+use anyhow::Result;
+
+// `cargo run -- -s ../graphql/supergraph.graphql -c ./router.yaml`
+fn main() -> Result<()> {
+    apollo_router::main()
+}
+
+#[cfg(test)]
+mod tests {
+    use apollo_router::plugin::test;
+    use apollo_router::plugin::Plugin;
+    use apollo_router::plugin::PluginInit;
+    use apollo_router::plugins::rhai::Conf;
+    use apollo_router::plugins::rhai::Rhai;
+    use apollo_router::services::RouterRequest;
+    use apollo_router::services::RouterResponse;
+    use http::StatusCode;
+    use tower::util::ServiceExt;
+
+    #[tokio::test]
+    async fn test_subgraph_processes_operation_name() {
+        // create a mock service we will use to test our plugin
+        let mut mock = test::MockRouterService::new();
+
+        // The expected reply is going to be JSON returned in the RouterResponse { data } section.
+        let expected_mock_response_data = "response created within the mock";
+
+        // Let's set up our mock to make sure it will be called once
+        mock.expect_call()
+            .once()
+            .returning(move |_req: RouterRequest| {
+                Ok(RouterResponse::fake_builder()
+                    .data(expected_mock_response_data)
+                    .build()
+                    .unwrap())
+            });
+
+        // The mock has been set up, we can now build a service from it
+        let mock_service = mock.build();
+
+        let conf: Conf = serde_json::from_value(serde_json::json!({
+            "scripts": "src",
+            "main": "rhai_span.rhai",
+        }))
+        .expect("valid conf supplied");
+
+        // Build an instance of our plugin to use in the test harness
+        let rhai = Rhai::new(PluginInit::new(conf, Default::default()))
+            .await
+            .expect("created plugin");
+
+        let service_stack = rhai.router_service(mock_service.boxed());
+
+        // Let's create a request with our operation name
+        let request_with_appropriate_name = RouterRequest::fake_builder()
+            .operation_name("me".to_string())
+            .build()
+            .unwrap();
+
+        // ...And call our service stack with it
+        let mut service_response = service_stack
+            .oneshot(request_with_appropriate_name)
+            .await
+            .unwrap();
+
+        // Rhai should return a 200...
+        assert_eq!(StatusCode::OK, service_response.response.status());
+
+        // with the expected message
+        let response = service_response.next_response().await.unwrap();
+        assert!(response.errors.is_empty());
+        assert_eq!(expected_mock_response_data, response.data.as_ref().unwrap());
+    }
+}
