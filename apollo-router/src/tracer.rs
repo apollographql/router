c8281ea053f50ba855d58dc064950b1e7ad51b96
Gary Pennington <gary@apollographql.com>
2022-09-02 09:08:37 +0100

    fix the tests so they work in parallel
    By adding a mutex to the tests to serialize them with respect to global
    resources.

--- apollo-router/src/tracer.rs
+++ apollo-router/src/tracer.rs
@@ -45,12 +45,22 @@ impl fmt::Display for TraceId {
 
 #[cfg(test)]
 mod test {
+    use std::sync::Mutex;
+
+    use once_cell::sync::Lazy;
     use opentelemetry::sdk::export::trace::stdout;
     use tracing::span;
     use tracing_subscriber::layer::SubscriberExt;
     use tracing_subscriber::Registry;
 
     use super::TraceId;
+
+    // If we try to run more than one test concurrently which relies on the existence of a pipeline,
+    // then the tests will fail due to manipulation of global state in the opentelemetry crates.
+    // If we set test-threads=1, then this avoids the problem but means all our tests will run slowly.
+    // So: to avoid this problem, we have a mutex lock which just exists to serialize access to the
+    // global resources.
+    static TRACING_LOCK: Lazy<Mutex<()>> = Lazy::new(|| Mutex::new(()));
 
     #[test]
     fn it_returns_invalid_trace_id() {
@@ -59,7 +69,38 @@ mod test {
     }
 
     #[test]
+    fn it_correctly_compares_invalid_and_invalid_trace_id() {
+        let my_id = TraceId::maybe_new();
+        let other_id = TraceId::maybe_new();
+        assert!(my_id.is_none());
+        assert!(other_id.is_none());
+        assert!(other_id == my_id);
+    }
+
+    #[test]
     fn it_returns_valid_trace_id() {
+        let _guard = TRACING_LOCK.lock().unwrap();
+        // Create a new OpenTelemetry pipeline
+        let tracer = stdout::new_pipeline().install_simple();
+        // Create a tracing layer with the configured tracer
+        let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+        // Use the tracing subscriber `Registry`, or any other subscriber
+        // that impls `LookupSpan`
+        let subscriber = Registry::default().with(telemetry);
+        // Trace executed code
+        tracing::subscriber::with_default(subscriber, || {
+            // Spans will be sent to the configured OpenTelemetry exporter
+            let root = span!(tracing::Level::TRACE, "trace test");
+            let _enter = root.enter();
+            assert!(TraceId::maybe_new().is_some());
+        });
+    }
+
+    #[test]
+    fn it_correctly_compares_valid_and_invalid_trace_id() {
+        let _guard = TRACING_LOCK.lock().unwrap();
+        let my_id = TraceId::maybe_new();
+        assert!(my_id.is_none());
         // Create a new OpenTelemetry pipeline
         let tracer = stdout::new_pipeline().install_simple();
         // Create a tracing layer with the configured tracer
@@ -73,7 +114,9 @@ mod test {
             let root = span!(tracing::Level::TRACE, "trace test");
             let _enter = root.enter();
 
-            assert!(TraceId::maybe_new().is_some());
+            let other_id = TraceId::maybe_new();
+            assert!(other_id.is_some());
+            assert!(other_id != my_id);
         });
     }
 }
