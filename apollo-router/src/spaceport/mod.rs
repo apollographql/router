eac5a524a61009de6dfd3b7b402428a63243cd16
Simon Sapin <simon@apollographql.com>
2022-09-12 17:53:44 +0200

    Remove dead code in uplink and spaceport

--- apollo-router/src/spaceport/mod.rs
+++ apollo-router/src/spaceport/mod.rs
@@ -20,7 +20,6 @@ pub(crate) use agent::*;
 pub(crate) use prost::*;
 pub(crate) use report::*;
 use serde::ser::SerializeStruct;
-use sys_info::hostname;
 use tokio::task::JoinError;
 use tonic::codegen::http::uri::InvalidUri;
 use tonic::transport::Channel;
@@ -28,13 +27,6 @@ use tonic::transport::Endpoint;
 use tonic::Request;
 use tonic::Response;
 use tonic::Status;
-
-// FIXME: remove these?
-#[allow(dead_code)]
-fn _unused() {
-    let _ = Report::try_new;
-    let _ = Reporter::try_new_with_static;
-}
 
 /// Reporting Error type
 #[derive(Debug)]
@@ -100,64 +92,6 @@ impl std::fmt::Display for ReporterError {
     }
 }
 
-impl Report {
-    /// Try to create a new Report.
-    ///
-    /// This can fail if the ReportHeader is not created.
-    pub(crate) fn try_new(graph: &str) -> Result<Self, ReporterError> {
-        let header = Some(ReportHeader::try_new(graph)?);
-
-        Ok(Report {
-            header,
-            ..Default::default()
-        })
-    }
-}
-
-#[cfg(target_os = "windows")]
-fn get_uname() -> Result<String, std::io::Error> {
-    // Best we can do on windows right now
-    let sysname = sys_info::os_type().unwrap_or_else(|_| "Windows".to_owned());
-    let nodename = sys_info::hostname().unwrap_or_else(|_| "unknown".to_owned());
-    let release = sys_info::os_release().unwrap_or_else(|_| "unknown".to_owned());
-    let version = "unknown";
-    let machine = "unknown";
-    Ok(format!(
-        "{}, {}, {}, {}, {}",
-        sysname, nodename, release, version, machine
-    ))
-}
-
-#[cfg(not(target_os = "windows"))]
-fn get_uname() -> Result<String, std::io::Error> {
-    let u = uname::uname()?;
-    Ok(format!(
-        "{}, {}, {}, {}, {},",
-        u.sysname, u.nodename, u.release, u.version, u.machine
-    ))
-}
-
-impl ReportHeader {
-    fn try_new(graph: &str) -> Result<Self, ReporterError> {
-        let mut header = ReportHeader {
-            ..Default::default()
-        };
-
-        header.hostname = hostname()?;
-        header.agent_version = format!(
-            "{}@{}",
-            std::env!("CARGO_PKG_NAME"),
-            std::env!("CARGO_PKG_VERSION")
-        );
-        header.runtime_version = "rust".to_string();
-        header.uname = get_uname()?;
-        header.graph_ref = graph.to_string();
-        // TODO: The executable_schema_id field is missing. Fine for
-        // now but will need to be addressed at some point.
-        Ok(header)
-    }
-}
-
 /// The Reporter accepts requests from clients to transfer statistics
 /// and traces to the Apollo spaceport.
 #[derive(Debug)]
@@ -176,17 +110,6 @@ impl Reporter {
         prost::bytes::Bytes: From<T>,
     {
         let ep = Endpoint::from_shared(addr)?;
-        let client = ReporterClient::connect(ep).await?;
-        Ok(Self { client })
-    }
-
-    /// Try to create a new reporter which will communicate with the supplied address.
-    ///
-    /// This can fail if:
-    ///  - the address cannot be parsed
-    ///  - the reporter can't connect to the address
-    pub(crate) async fn try_new_with_static(addr: &'static str) -> Result<Self, ReporterError> {
-        let ep = Endpoint::from_static(addr);
         let client = ReporterClient::connect(ep).await?;
         Ok(Self { client })
     }
