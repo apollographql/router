227e0cb2911b5112dce731a0a4af6fae5d79d0e6
Geoffroy Couprie <geoffroy@apollographql.com>
2022-09-13 12:31:14 +0200

    try to fix

--- apollo-router/src/services/execution_service.rs
+++ apollo-router/src/services/execution_service.rs
@@ -1,11 +1,14 @@
 //! Implements the Execution phase of the request lifecycle.
 
+use std::future::ready;
 use std::sync::Arc;
 use std::task::Poll;
 
 use futures::channel::mpsc::Receiver;
 use futures::channel::mpsc::SendError;
+use futures::channel::mpsc::Sender;
 use futures::future::BoxFuture;
+use futures::stream::once;
 use futures::SinkExt;
 use futures::StreamExt;
 use tower::BoxError;
@@ -68,7 +71,12 @@ where
                 )
                 .await;
 
-            let stream = filter_stream(first, receiver).boxed();
+            let stream = if req.query_plan.root.contains_defer() {
+                println!("contains defer, will filter stream");
+                filter_stream(first, receiver).boxed()
+            } else {
+                once(ready(first)).chain(receiver).boxed()
+            };
 
             Ok(ExecutionResponse::new_from_response(
                 http::Response::new(stream as _),
@@ -85,39 +93,60 @@ fn filter_stream(first: Response, mut stream: Receiver<Response>) -> Receiver<Re
     let (mut sender, receiver) = futures::channel::mpsc::channel(10);
 
     tokio::task::spawn(async move {
-        sender.send(first).await?;
-        while let Some(mut current_response) = stream.next().await {
-            loop {
-                match stream.try_next() {
-                    // no messages available, but the channel is not closed
-                    // this means more deferred responses can come
-                    Err(_) => {
-                        sender.send(current_response).await?;
+        consume_responses(first, &mut stream, &mut sender).await?;
 
-                        break;
-                    }
-
-                    // there might be other deferred responses after this one,
-                    // so we should call `try_next` again
-                    Ok(Some(response)) => {
-                        sender.send(current_response).await?;
-                        current_response = response;
-                    }
-                    // the channel is closed
-                    // there will be no other deferred responses after that,
-                    // so we set `has_next` to `false`
-                    Ok(None) => {
-                        current_response.has_next = Some(false);
-                        sender.send(current_response).await?;
-                        break;
-                    }
-                }
-            }
+        while let Some(current_response) = stream.next().await {
+            consume_responses(current_response, &mut stream, &mut sender).await?;
         }
+        println!("done");
         Ok::<_, SendError>(())
     });
 
     receiver
+}
+
+async fn consume_responses(
+    mut current_response: Response,
+    stream: &mut Receiver<Response>,
+    sender: &mut Sender<Response>,
+) -> Result<(), SendError> {
+    loop {
+        match stream.try_next() {
+            // no messages available, but the channel is not closed
+            // this means more deferred responses can come
+            Err(_) => {
+                println!("[{}]consume", line!());
+                sender.send(current_response).await?;
+
+                break;
+            }
+
+            // there might be other deferred responses after this one,
+            // so we should call `try_next` again
+            Ok(Some(response)) => {
+                println!("[{}]consume", line!());
+
+                sender.send(current_response).await?;
+                current_response = response;
+            }
+            // the channel is closed
+            // there will be no other deferred responses after that,
+            // so we set `has_next` to `false`
+            Ok(None) => {
+                println!("[{}]consume", line!());
+
+                current_response.has_next = Some(false);
+                println!(
+                    "FILTER setting has next to false on  {:?}",
+                    current_response
+                );
+
+                sender.send(current_response).await?;
+                break;
+            }
+        }
+    }
+    Ok::<_, SendError>(())
 }
 
 pub(crate) trait ExecutionServiceFactory:
