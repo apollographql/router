99e4dfc10732953b07dd83c71be73d351c1cf3b3
bryn <bryn@apollographql.com>
2022-10-19 14:41:19 +0100

    Add aggregation for all the metrics types.

--- apollo-router/src/plugins/telemetry/metrics/aggregation.rs
+++ apollo-router/src/plugins/telemetry/metrics/aggregation.rs
@@ -1,6 +1,8 @@
 use itertools::Itertools;
 use opentelemetry::metrics::{
-    Counter, InstrumentProvider, Meter, MeterProvider, SyncCounter, Unit,
+    AsyncCounter, AsyncGauge, AsyncUpDownCounter, Counter, Histogram, InstrumentProvider, Meter,
+    MeterProvider, ObservableCounter, ObservableGauge, ObservableUpDownCounter, SyncCounter,
+    SyncHistogram, SyncUpDownCounter, Unit, UpDownCounter,
 };
 use opentelemetry::{Context, InstrumentationLibrary, KeyValue};
 use std::sync::Arc;
@@ -41,46 +43,178 @@ pub(crate) struct AggregateInstrumentProvider {
     meters: Vec<Meter>,
 }
 
-pub(crate) struct AggregateSyncCounter<T> {
-    counters: Vec<Counter<T>>,
+pub(crate) struct AggregateCounter<T> {
+    delegates: Vec<Counter<T>>,
 }
 
-impl<T: Copy> SyncCounter<T> for AggregateSyncCounter<T> {
+impl<T: Copy> SyncCounter<T> for AggregateCounter<T> {
     fn add(&self, cx: &Context, value: T, attributes: &[KeyValue]) {
-        for counter in &self.counters {
+        for counter in &self.delegates {
             counter.add(cx, value, attributes)
         }
     }
 }
 
-impl InstrumentProvider for AggregateInstrumentProvider {
-    fn f64_counter(
-        &self,
-        name: String,
-        description: Option<String>,
-        unit: Option<Unit>,
-    ) -> opentelemetry::metrics::Result<Counter<f64>> {
-        let counters = self
-            .meters
-            .iter()
-            .map(|p| {
-                let mut b = p.f64_counter(name.clone());
-                if let Some(description) = &description {
-                    b = b.with_description(description);
-                }
-                if let Some(unit) = &unit {
-                    b = b.with_unit(unit.clone());
-                }
-                b.try_init()
-            })
-            .try_collect()?;
-        Ok(Counter::new(Arc::new(AggregateSyncCounter { counters })))
+pub(crate) struct AggregateObservableCounter<T> {
+    delegates: Vec<ObservableCounter<T>>,
+}
+
+impl<T: Copy> AsyncCounter<T> for AggregateObservableCounter<T> {
+    fn observe(&self, cx: &Context, value: T, attributes: &[KeyValue]) {
+        for counter in &self.delegates {
+            counter.observe(cx, value, attributes)
+        }
     }
+}
+
+pub(crate) struct AggregateHistogram<T> {
+    delegates: Vec<Histogram<T>>,
+}
+
+impl<T: Copy> SyncHistogram<T> for AggregateHistogram<T> {
+    fn record(&self, cx: &Context, value: T, attributes: &[KeyValue]) {
+        for histogram in &self.delegates {
+            histogram.record(cx, value, attributes)
+        }
+    }
+}
+
+pub(crate) struct AggregateUpDownCounter<T> {
+    delegates: Vec<UpDownCounter<T>>,
+}
+
+impl<T: Copy> SyncUpDownCounter<T> for AggregateUpDownCounter<T> {
+    fn add(&self, cx: &Context, value: T, attributes: &[KeyValue]) {
+        for counter in &self.delegates {
+            counter.add(cx, value, attributes)
+        }
+    }
+}
+
+pub(crate) struct AggregateObservableUpDownCounter<T> {
+    delegates: Vec<ObservableUpDownCounter<T>>,
+}
+
+impl<T: Copy> AsyncUpDownCounter<T> for AggregateObservableUpDownCounter<T> {
+    fn observe(&self, cx: &Context, value: T, attributes: &[KeyValue]) {
+        for counter in &self.delegates {
+            counter.observe(cx, value, attributes)
+        }
+    }
+}
+
+pub(crate) struct AggregateObservableGauge<T> {
+    delegates: Vec<ObservableGauge<T>>,
+}
+
+impl<T: Copy> AsyncGauge<T> for AggregateObservableGauge<T> {
+    fn observe(&self, cx: &Context, value: T, attributes: &[KeyValue]) {
+        for gauge in &self.delegates {
+            gauge.observe(cx, value, attributes)
+        }
+    }
+}
+
+macro_rules! aggregate_meter_fn {
+    ($name:ident, $ty:ty, $wrapper:ident, $implementation:ident) => {
+        fn $name(
+            &self,
+            name: String,
+            description: Option<String>,
+            unit: Option<Unit>,
+        ) -> opentelemetry::metrics::Result<$wrapper<$ty>> {
+            let delegates = self
+                .meters
+                .iter()
+                .map(|p| {
+                    let mut b = p.$name(name.clone());
+                    if let Some(description) = &description {
+                        b = b.with_description(description);
+                    }
+                    if let Some(unit) = &unit {
+                        b = b.with_unit(unit.clone());
+                    }
+                    b.try_init()
+                })
+                .try_collect()?;
+            Ok($wrapper::new(Arc::new($implementation { delegates })))
+        }
+    };
+}
+
+impl InstrumentProvider for AggregateInstrumentProvider {
+    aggregate_meter_fn!(u64_counter, u64, Counter, AggregateCounter);
+    aggregate_meter_fn!(f64_counter, f64, Counter, AggregateCounter);
+
+    aggregate_meter_fn!(
+        f64_observable_counter,
+        f64,
+        ObservableCounter,
+        AggregateObservableCounter
+    );
+    aggregate_meter_fn!(
+        u64_observable_counter,
+        u64,
+        ObservableCounter,
+        AggregateObservableCounter
+    );
+
+    aggregate_meter_fn!(u64_histogram, u64, Histogram, AggregateHistogram);
+    aggregate_meter_fn!(f64_histogram, f64, Histogram, AggregateHistogram);
+    aggregate_meter_fn!(i64_histogram, i64, Histogram, AggregateHistogram);
+
+    aggregate_meter_fn!(
+        i64_up_down_counter,
+        i64,
+        UpDownCounter,
+        AggregateUpDownCounter
+    );
+    aggregate_meter_fn!(
+        f64_up_down_counter,
+        f64,
+        UpDownCounter,
+        AggregateUpDownCounter
+    );
+
+    aggregate_meter_fn!(
+        i64_observable_up_down_counter,
+        i64,
+        ObservableUpDownCounter,
+        AggregateObservableUpDownCounter
+    );
+    aggregate_meter_fn!(
+        f64_observable_up_down_counter,
+        f64,
+        ObservableUpDownCounter,
+        AggregateObservableUpDownCounter
+    );
+
+    aggregate_meter_fn!(
+        f64_observable_gauge,
+        f64,
+        ObservableGauge,
+        AggregateObservableGauge
+    );
+    aggregate_meter_fn!(
+        i64_observable_gauge,
+        i64,
+        ObservableGauge,
+        AggregateObservableGauge
+    );
+    aggregate_meter_fn!(
+        u64_observable_gauge,
+        u64,
+        ObservableGauge,
+        AggregateObservableGauge
+    );
 
     fn register_callback(
         &self,
         callback: Box<dyn Fn(&Context) + Send + Sync>,
     ) -> opentelemetry::metrics::Result<()> {
+        // The reason that this is OK is that calling observe outside of a callback is a no-op.
+        // So the callback is called, an observable is updated, but only the observable associated with the correct meter will take effect
+
         let callback = Arc::new(callback);
         for meter in &self.meters {
             let callback = callback.clone();
