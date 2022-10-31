157d35f4ad3d8265949016f488861b3afc610f46
Gary Pennington <gary@apollographql.com>
2022-10-31 08:25:42 +0000

    finalise feature support now that dhat 0.3.2 is available
    dhat-rs 0.3.2 works well with the router, so time to look at this
    feature again.
    
    Finalise support for either or both of heap or ad-hoc tracing.
    Cleanup various print statements.

--- apollo-router/src/executable.rs
+++ apollo-router/src/executable.rs
@@ -31,38 +31,43 @@ use crate::router::RouterHttpServer;
 use crate::router::SchemaSource;
 use crate::router::ShutdownSource;
 
+// Note: the dhat-heap and dhat-ad-hoc features should not be both enabled. We name our functions
+// and variables identically to prevent this from happening.
+
 #[cfg(feature = "dhat-heap")]
 #[global_allocator]
 pub(crate) static ALLOC: dhat::Alloc = dhat::Alloc;
 
 #[cfg(feature = "dhat-heap")]
-pub(crate) static mut GLOBAL_PROFILER: OnceCell<dhat::Profiler> = OnceCell::new();
+pub(crate) static mut DHAT_HEAP_PROFILER: OnceCell<dhat::Profiler> = OnceCell::new();
 
 #[cfg(feature = "dhat-ad-hoc")]
-pub(crate) static mut GLOBAL_AD_HOC_PROFILER: OnceCell<dhat::Profiler> = OnceCell::new();
+pub(crate) static mut DHAT_AD_HOC_PROFILER: OnceCell<dhat::Profiler> = OnceCell::new();
 
 pub(crate) static GLOBAL_ENV_FILTER: OnceCell<String> = OnceCell::new();
 pub(crate) const APOLLO_ROUTER_DEV_ENV: &str = "APOLLO_ROUTER_DEV";
 
+// Note: Constructor/Destructor functions may not play nicely with tracing, since they run after
+// main completes, so don't use tracing, use println!() and eprintln!()..
 #[cfg(feature = "dhat-heap")]
 #[crate::_private::ctor::ctor]
-fn create_profiler() {
+fn create_heap_profiler() {
     unsafe {
-        match GLOBAL_PROFILER.set(dhat::Profiler::new_heap()) {
+        match DHAT_HEAP_PROFILER.set(dhat::Profiler::new_heap()) {
             Ok(p) => {
-                println!("profiler installed: {:?}", p);
-                libc::atexit(drop_profiler);
+                println!("heap profiler installed: {:?}", p);
+                libc::atexit(drop_heap_profiler);
             }
-            Err(e) => eprintln!("profiler install failed: {:?}", e),
+            Err(e) => eprintln!("heap profiler install failed: {:?}", e),
         }
     }
 }
 
 #[cfg(feature = "dhat-heap")]
 #[no_mangle]
-extern "C" fn drop_profiler() {
+extern "C" fn drop_heap_profiler() {
     unsafe {
-        if let Some(p) = GLOBAL_PROFILER.take() {
+        if let Some(p) = DHAT_HEAP_PROFILER.take() {
             drop(p);
         }
     }
@@ -70,23 +75,23 @@ extern "C" fn drop_profiler() {
 
 #[cfg(feature = "dhat-ad-hoc")]
 #[crate::_private::ctor::ctor]
-fn create_profiler() {
+fn create_ad_hoc_profiler() {
     unsafe {
-        match GLOBAL_AD_HOC_PROFILER.set(dhat::Profiler::new_ad_hoc()) {
+        match DHAT_AD_HOC_PROFILER.set(dhat::Profiler::new_ad_hoc()) {
             Ok(p) => {
-                println!("profiler installed: {:?}", p);
-                libc::atexit(drop_profiler);
+                println!("ad-hoc profiler installed: {:?}", p);
+                libc::atexit(drop_ad_hoc_profiler);
             }
-            Err(e) => eprintln!("profiler install failed: {:?}", e),
+            Err(e) => eprintln!("ad-hoc profiler install failed: {:?}", e),
         }
     }
 }
 
 #[cfg(feature = "dhat-ad-hoc")]
 #[no_mangle]
-extern "C" fn drop_profiler() {
+extern "C" fn drop_ad_hoc_profiler() {
     unsafe {
-        if let Some(p) = GLOBAL_AD_HOC_PROFILER.take() {
+        if let Some(p) = DHAT_AD_HOC_PROFILER.take() {
             drop(p);
         }
     }
@@ -479,9 +484,9 @@ fn setup_panic_handler(dispatcher: Dispatch) {
         with_default(&dispatcher, || {
             if show_backtraces {
                 let backtrace = backtrace::Backtrace::new();
-                eprintln!("{}\n{:?}", e, backtrace)
+                tracing::error!("{}\n{:?}", e, backtrace)
             } else {
-                eprintln!("{}", e)
+                tracing::error!("{}", e)
             }
             // Once we've panic'ed the behaviour of the router is non-deterministic
             // We've logged out the panic details. Terminate with an error code
