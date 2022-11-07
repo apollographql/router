0e1223695b061456f88b924a2dcbd4dd8a54337a
Simon Sapin <simon@apollographql.com>
2022-11-07 11:16:39 +0100

    Update apollo-parser to 0.3 (#2051)

--- fuzz/src/lib.rs
+++ fuzz/src/lib.rs
@@ -13,7 +13,8 @@ use log::debug;
 pub fn generate_valid_operation(input: &[u8], schema_path: &'static str) -> Result<String> {
     drop(env_logger::try_init());
 
-    let parser = Parser::new(&fs::read_to_string(schema_path).expect("cannot read file"));
+    let contents = fs::read_to_string(schema_path).expect("cannot read file");
+    let parser = Parser::new(&contents);
 
     let tree = parser.parse();
     if tree.errors().len() > 0 {
