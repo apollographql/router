apollographql/router

commit 655689a182736148a5be66fc9cb3f8cd6c72c9ea

Fix naming inconsistency of telemetry.metrics.common.attributes.router (#2116)
### Fix naming inconsistency of
telemetry.metrics.common.attributes.router ([Issue
#2076](https://github.com/apollographql/router/issues/2076))

Mirroring the rest of the config `router` should be `supergraph`

```yaml
telemetry:
  metrics:
    common:
      attributes:
        router: # old
```
becomes
```yaml
telemetry:
  metrics:
    common:
      attributes:
        supergraph: # new
```

### Configuration upgrades ([Issue
#2123](https://github.com/apollographql/router/issues/2123))

Occasionally we will make changes to the Router yaml configuration
format.
When starting the Router if the configuration can be upgraded it will do
so automatically and display a warning:

```
2022-11-22T14:01:46.884897Z  WARN router configuration contains deprecated options: 

  1. telemetry.tracing.trace_config.attributes.router has been renamed to 'supergraph' for consistency

These will become errors in the future. Run `router config upgrade <path_to_router.yaml>` to see a suggested upgraded configuration.
```

Note: If a configuration has errors after upgrading then the
configuration will not be upgraded automatically.

From the CLI users can run:
* `router config upgrade <path_to_router.yaml>` to output configuration
that has been upgraded to match the latest config format.
* `router config upgrade --diff <path_to_router.yaml>` to output a diff
e.g.
```
 telemetry:
   apollo:
     client_name_header: apollographql-client-name
   metrics:
     common:
       attributes:
-        router:
+        supergraph:
           request:
             header:
             - named: "1" # foo
```

There are situations where comments and whitespace are not preserved.
This may be improved in future.

By [@bryncooke](https://github.com/bryncooke) in
https://github.com/apollographql/router/pull/2116


### CLI structure changes ([Issue

apollo-router/src/configuration/upgrade.rs
@@ -1,0 +1,325 @@
+use std::fmt::Write as _;
+
+use itertools::Itertools;
+use proteus::Parser;
+use proteus::TransformBuilder;
+use rust_embed::RustEmbed;
+use serde::Deserialize;
+use serde_json::Value;
+
+use crate::error::ConfigurationError;
+
+#[derive(RustEmbed)]
+#[folder = "src/configuration/migrations"]
+struct Asset;
+
+#[derive(Deserialize, buildstructor::Builder)]
+struct Migration {
+    description: String,
+    actions: Vec<Action>,
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum Action {
+    Delete { path: String },
+    Copy { from: String, to: String },
+    Move { from: String, to: String },
+}
+
+const REMOVAL_VALUE: &str = "__PLEASE_DELETE_ME";
+const REMOVAL_EXPRESSION: &str = r#"const("__PLEASE_DELETE_ME")"#;
+
+pub(crate) fn upgrade_configuration(
+    config: &serde_json::Value,
+    log_warnings: bool,
+) -> Result<serde_json::Value, super::ConfigurationError> {
+    // Transformers are loaded from a file and applied in order
+    let migrations: Vec<Migration> = Asset::iter()
+        .sorted()
+        .filter(|filename| filename.ends_with(".yaml"))
+        .map(|filename| Asset::get(&filename).expect("migration must exist").data)
+        .map(|data| serde_yaml::from_slice(&data).expect("migration must be valid"))
+        .collect();
+
+    let mut config = config.clone();
+
+    let mut effective_migrations = Vec::new();
+    for migration in &migrations {
+        let new_config = apply_migration(&config, migration)?;
+
+        // If the config has been modified by the migration then let the user know
+        if new_config != config {
+            effective_migrations.push(migration);
+        }
+
+        // Get ready for the next migration
+        config = new_config;
+    }
+    if !effective_migrations.is_empty() && log_warnings {
+        tracing::warn!("router configuration contains deprecated options: \n\n{}\n\nThese will become errors in the future. Run `router config upgrade <path_to_router.yaml>` to see a suggested upgraded configuration.", effective_migrations.iter().enumerate().map(|(idx, m)|format!("  {}. {}", idx + 1, m.description)).join("\n\n"));
+    }
+    Ok(config)
+}
+
+fn apply_migration(config: &Value, migration: &Migration) -> Result<Value, ConfigurationError> {
+    let mut transformer_builder = TransformBuilder::default();
+    //We always copy the entire doc to the destination first
+    transformer_builder =
+        transformer_builder.add_action(Parser::parse("", "").expect("migration must be valid"));
+    for action in &migration.actions {
+        match action {
+            Action::Delete { path } => {
+                // Deleting isn't actually supported by protus so we add a magic value to delete later
+                transformer_builder = transformer_builder.add_action(
+                    Parser::parse(REMOVAL_EXPRESSION, path).expect("migration must be valid"),
+                );
+            }
+            Action::Copy { from, to } => {
+                transformer_builder = transformer_builder
+                    .add_action(Parser::parse(from, to).expect("migration must be valid"));
+            }
+            Action::Move { from, to } => {
+                transformer_builder = transformer_builder
+                    .add_action(Parser::parse(from, to).expect("migration must be valid"));
+                // Deleting isn't actually supported by protus so we add a magic value to delete later
+                transformer_builder = transformer_builder.add_action(
+                    Parser::parse(REMOVAL_EXPRESSION, from).expect("migration must be valid"),
+                );
+            }
+        }
+    }
+    let transformer = transformer_builder
+        .build()
+        .expect("transformer for migration must be valid");
+    let mut new_config =
+        transformer
+            .apply(config)
+            .map_err(|e| ConfigurationError::MigrationFailure {
+                error: e.to_string(),
+            })?;
+
+    // Now we need to clean up elements that should be deleted.
+    cleanup(&mut new_config);
+
+    Ok(new_config)
+}
+
+pub(crate) fn generate_upgrade(config: &str, diff: bool) -> Result<String, ConfigurationError> {
+    let parsed_config =
+        serde_yaml::from_str(config).map_err(|e| ConfigurationError::MigrationFailure {
+            error: e.to_string(),
+        })?;
+    let upgraded_config = upgrade_configuration(&parsed_config, true).map_err(|e| {
+        ConfigurationError::MigrationFailure {
+            error: e.to_string(),
+        }
+    })?;
+    let upgraded_config = serde_yaml::to_string(&upgraded_config).map_err(|e| {
+        ConfigurationError::MigrationFailure {
+            error: e.to_string(),
+        }
+    })?;
+    generate_upgrade_output(config, &upgraded_config, diff)
+}
+
+pub(crate) fn generate_upgrade_output(
+    config: &str,
+    upgraded_config: &str,
+    diff: bool,
+) -> Result<String, ConfigurationError> {
+    // serde doesn't deal with whitespace and comments, these are lost in the upgrade process, so instead we try and preserve this in the diff.
+    // It's not ideal, and ideally the upgrade process should work on a DOM that is not serde, but for now we just make a best effort to preserve comments and whitespace.
+    // There absolutely are issues where comments will get stripped, but the output should be `correct`.
+    let mut output = String::new();
+
+    let diff_result = diff::lines(config, upgraded_config);
+
+    for diff_line in diff_result {
+        match diff_line {
+            diff::Result::Left(l) => {
+                let trimmed = l.trim();
+                if !trimmed.starts_with('#') && !trimmed.is_empty() {
+                    if diff {
+                        writeln!(output, "-{}", l).expect("write will never fail");
+                    }
+                } else if diff {
+                    writeln!(output, " {}", l).expect("write will never fail");
+                } else {
+                    writeln!(output, "{}", l).expect("write will never fail");
+                }
+            }
+            diff::Result::Both(l, _) => {
+                if diff {
+                    writeln!(output, " {}", l).expect("write will never fail");
+                } else {
+                    writeln!(output, "{}", l).expect("write will never fail");
+                }
+            }
+            diff::Result::Right(r) => {
+                let trimmed = r.trim();
+                if trimmed != "---" && !trimmed.is_empty() {
+                    if diff {
+                        writeln!(output, "+{}", r).expect("write will never fail");
+                    } else {
+                        writeln!(output, "{}", r).expect("write will never fail");
+                    }
+                }
+            }
+        }
+    }
+    Ok(output)
+}
+
+fn cleanup(value: &mut Value) {
+    match value {
+        Value::Null => {}
+        Value::Bool(_) => {}
+        Value::Number(_) => {}
+        Value::String(_) => {}
+        Value::Array(a) => {
+            a.retain(|v| &Value::String(REMOVAL_VALUE.to_string()) != v);
+            for value in a {
+                cleanup(value);
+            }
+        }
+        Value::Object(o) => {
+            o.retain(|_, v| &Value::String(REMOVAL_VALUE.to_string()) != v);
+            for value in o.values_mut() {
+                cleanup(value);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use serde_json::json;
+    use serde_json::Value;
+
+    use crate::configuration::upgrade::apply_migration;
+    use crate::configuration::upgrade::generate_upgrade_output;
+    use crate::configuration::upgrade::Action;
+    use crate::configuration::upgrade::Migration;
+
+    fn source_doc() -> Value {
+        json!( {
+          "obj": {
+                "field1": 1,
+                "field2": 2
+            },
+          "arr": [
+                "v1",
+                "v2"
+            ]
+        })
+    }
+
+    #[test]
+    fn delete_field() {
+        insta::assert_json_snapshot!(apply_migration(
+            &source_doc(),
+            &Migration::builder()
+                .action(Action::Delete {
+                    path: "obj.field1".to_string()
+                })
+                .description("delete field1")
+                .build(),
+        )
+        .expect("expected successful migration"));
+    }
+
+    #[test]
+    fn delete_array_element() {
+        insta::assert_json_snapshot!(apply_migration(
+            &source_doc(),
+            &Migration::builder()
+                .action(Action::Delete {
+                    path: "arr[0]".to_string()
+                })
+                .description("delete arr[0]")
+                .build(),
+        )
+        .expect("expected successful migration"));
+    }
+
+    #[test]
+    fn move_field() {
+        insta::assert_json_snapshot!(apply_migration(
+            &source_doc(),
+            &Migration::builder()
+                .action(Action::Move {
+                    from: "obj.field1".to_string(),
+                    to: "new.obj.field1".to_string()
+                })
+                .description("move field1")
+                .build(),
+        )
+        .expect("expected successful migration"));
+    }
+
+    #[test]
+    fn move_array_element() {
+        insta::assert_json_snapshot!(apply_migration(
+            &source_doc(),
+            &Migration::builder()
+                .action(Action::Move {
+                    from: "arr[0]".to_string(),
+                    to: "new.arr[0]".to_string()
+                })
+                .description("move arr[0]")
+                .build(),
+        )
+        .expect("expected successful migration"));
+    }
+
+    #[test]
+    fn copy_field() {
+        insta::assert_json_snapshot!(apply_migration(
+            &source_doc(),
+            &Migration::builder()
+                .action(Action::Copy {
+                    from: "obj.field1".to_string(),
+                    to: "new.obj.field1".to_string()
+                })
+                .description("copy field1")
+                .build(),
+        )
+        .expect("expected successful migration"));
+    }
+
+    #[test]
+    fn copy_array_element() {
+        insta::assert_json_snapshot!(apply_migration(
+            &source_doc(),
+            &Migration::builder()
+                .action(Action::Copy {
+                    from: "arr[0]".to_string(),
+                    to: "new.arr[0]".to_string()
+                })
+                .description("copy arr[0]")
+                .build(),
+        )
+        .expect("expected successful migration"));
+    }
+
+    #[test]
+    fn diff_upgrade_output() {
+        insta::assert_snapshot!(generate_upgrade_output(
+            "changed: bar\nstable: 1.0\ndeleted: gone",
+            "changed: bif\nstable: 1.0\nadded: new",
+            true
+        )
+        .expect("expected successful migration"));
+    }
+
+    #[test]
+    fn upgrade_output() {
+        insta::assert_snapshot!(generate_upgrade_output(
+            "changed: bar\nstable: 1.0\ndeleted: gone",
+            "changed: bif\nstable: 1.0\nadded: new",
+            false
+        )
+        .expect("expected successful migration"));
+    }
+}

apollo-router/Cargo.toml
@@ -70,6 +70,7 @@ derive_more = { version = "0.99.17", default-features = false, features = [
     "display",
 ] }
 dhat = { version = "0.3.2", optional = true }
+diff = "0.1.13"
 directories = "4.0.1"
 displaydoc = "0.2"
 flate2 = "1.0.24"
@@ -84,6 +85,7 @@ hyper = { version = "0.14.23", features = ["server", "client"] }
 hyper-rustls = { version = "0.23.1", features = ["http1", "http2"] }
 indexmap = { version = "1.9.2", features = ["serde-1"] }
 itertools = "0.10.5"
+jsonpath_lib = "0.3.0"
 jsonschema = { version = "0.16.1", default-features = false }
 lazy_static = "1.4.0"
 libc = "0.2.137"
@@ -140,6 +142,7 @@ pin-project-lite = "0.2.9"
 prometheus = "0.13"
 prost = "0.9.0"
 prost-types = "0.9.0"
+proteus = "0.5.0"
 rand = "0.8.5"
 rhai = { version = "1.11.0", features = ["sync", "serde", "internals"] }
 redis = { version = "0.21.6", optional = true, features = ["cluster", "tokio-comp"] }
@@ -151,6 +154,7 @@ reqwest = { version = "0.11.13", default-features = false, features = [
     "stream",
 ] }
 router-bridge = "0.1.11"
+rust-embed="6.4.2"
 schemars = { version = "0.8.11", features = ["url"] }
 shellexpand = "2.1.2"
 sha2 = "0.10.6"
@@ -193,7 +197,6 @@ tracing-core = "=0.1.26"
 tracing-futures = { version = "0.2.5", features = ["futures-03"] }
 tracing-opentelemetry = "0.17.4"
 tracing-subscriber = { version = "0.3.11", features = ["env-filter", "json"] }
-
 url = { version = "2.3.1", features = ["serde"] }
 urlencoding = "2.1.2"
 uuid = { version = "1.2.2", features = ["serde", "v4"] }
@@ -209,7 +212,6 @@ uname = "0.1.1"
 [dev-dependencies]
 insta = { version = "1.21.1", features = ["json", "redactions"] }
 introspector-gadget = "0.1.0"
-jsonpath_lib = "0.3.0"
 maplit = "1.0.2"
 memchr = { version = "2.5.0", default-features = false }
 mockall = "0.11.3"

apollo-router/src/configuration/expansion.rs
@@ -84,10 +84,10 @@ impl Expansion {
 
 pub(crate) fn expand_env_variables(
     configuration: &serde_json::Value,
-    expansion: Expansion,
+    expansion: &Expansion,
 ) -> Result<serde_json::Value, ConfigurationError> {
     let mut configuration = configuration.clone();
-    visit(&mut configuration, &expansion)?;
+    visit(&mut configuration, expansion)?;
     Ok(configuration)
 }
 

apollo-router/src/configuration/migrations/0001-telemetry_router_to_supergraph.yaml
@@ -1,0 +1,6 @@
+description: telemetry.tracing.trace_config.attributes.router has been renamed to 'supergraph' for consistency
+actions:
+  - type: move
+    from: telemetry.metrics.common.attributes.router
+    to: telemetry.metrics.common.attributes.supergraph
+

apollo-router/src/configuration/mod.rs
@@ -5,6 +5,7 @@ mod expansion;
 mod schema;
 #[cfg(test)]
 mod tests;
+mod upgrade;
 mod yaml;
 
 use std::fmt;
@@ -20,6 +21,7 @@ use displaydoc::Display;
 use expansion::*;
 use itertools::Itertools;
 pub(crate) use schema::generate_config_schema;
+pub(crate) use schema::generate_upgrade;
 use schemars::gen::SchemaGenerator;
 use schemars::schema::ObjectValidation;
 use schemars::schema::Schema;
@@ -32,6 +34,7 @@ use serde_json::Map;
 use serde_json::Value;
 use thiserror::Error;
 
+use crate::configuration::schema::Mode;
 use crate::executable::APOLLO_ROUTER_DEV_ENV;
 use crate::plugin::plugins;
 
@@ -60,6 +63,9 @@ pub enum ConfigurationError {
 
     /// APOLLO_ROUTER_CONFIG_SUPPORTED_MODES must be of the format env,file,... Possible modes are 'env' and 'file'.
     InvalidExpansionModeConfig,
+
+    /// could not migrate configuration: {error}.
+    MigrationFailure { error: String },
 }
 
 /// The configuration for the router.
@@ -353,7 +359,7 @@ impl FromStr for Configuration {
     type Err = ConfigurationError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        schema::validate_yaml_configuration(s, Expansion::default()?)?.validate()
+        schema::validate_yaml_configuration(s, Expansion::default()?, Mode::Upgrade)?.validate()
     }
 }
 

apollo-router/src/configuration/schema.rs
@@ -18,6 +18,8 @@ use super::yaml;
 use super::Configuration;
 use super::ConfigurationError;
 use super::APOLLO_PLUGIN_PREFIX;
+pub(crate) use crate::configuration::upgrade::generate_upgrade;
+pub(crate) use crate::configuration::upgrade::upgrade_configuration;
 
 /// Generate a JSON schema for the configuration.
 pub(crate) fn generate_config_schema() -> RootSchema {
@@ -37,6 +39,15 @@ pub(crate) fn generate_config_schema() -> RootSchema {
     schema
 }
 
+#[derive(Eq, PartialEq)]
+pub(crate) enum Mode {
+    Upgrade,
+
+    // This is used only in testing to ensure that we don't allow old config in our tests.
+    #[cfg(test)]
+    NoUpgrade,
+}
+
 /// Validate config yaml against the generated json schema.
 /// This is a tricky problem, and the solution here is by no means complete.
 /// In the case that validation cannot be performed then it will let serde validate as normal. The
@@ -54,6 +65,7 @@ pub(crate) fn generate_config_schema() -> RootSchema {
 pub(crate) fn validate_yaml_configuration(
     raw_yaml: &str,
     expansion: Expansion,
+    migration: Mode,
 ) -> Result<Configuration, ConfigurationError> {
     let defaulted_yaml = if raw_yaml.trim().is_empty() {
         "plugins:".to_string()
@@ -61,14 +73,12 @@ pub(crate) fn validate_yaml_configuration(
         raw_yaml.to_string()
     };
 
-    let yaml = &serde_yaml::from_str(&defaulted_yaml).map_err(|e| {
+    let mut yaml = serde_yaml::from_str(&defaulted_yaml).map_err(|e| {
         ConfigurationError::InvalidConfiguration {
             message: "failed to parse yaml",
             error: e.to_string(),
         }
     })?;
-
-    let expanded_yaml = expand_env_variables(yaml, expansion)?;
     let schema = serde_json::to_value(generate_config_schema()).map_err(|e| {
         ConfigurationError::InvalidConfiguration {
             message: "failed to parse schema",
@@ -82,6 +92,18 @@ pub(crate) fn validate_yaml_configuration(
             message: "failed to compile schema",
             error: e.to_string(),
         })?;
+
+    if migration == Mode::Upgrade {
+        let upgraded = upgrade_configuration(&yaml, true)?;
+        let expanded_yaml = expand_env_variables(&upgraded, &expansion)?;
+        if schema.validate(&expanded_yaml).is_ok() {
+            yaml = upgraded;
+        } else {
+            tracing::warn!("configuration could not be upgraded automatically as it had errors")
+        }
+    }
+    let expanded_yaml = expand_env_variables(&yaml, &expansion)?;
+
     if let Err(errors) = schema.validate(&expanded_yaml) {
         // Validation failed, translate the errors into something nice for the user
         // We have to reparse the yaml to get the line number information for each error.

apollo-router/src/configuration/snapshots/apollo_router__configuration__tests__upgrade_old_configuration@telemetry_router_to_supergraph.router.yaml.snap
@@ -1,0 +1,14 @@
+---
+source: apollo-router/src/configuration/tests.rs
+expression: new_config
+---
+---
+telemetry:
+  metrics:
+    common:
+      attributes:
+        supergraph:
+          request:
+            header:
+              - named: fd
+

commit 2603ba2a24c231a797cfe7b7d441849f7488628c

Fixes #2123 Issue was introduced with #2116 but no release had this in.
Move operations would insert data in the config due to the delete magic value always getting added. Now we check before adding such values.

We may need to move to fluvio-jolt longer term.

apollo-router/src/configuration/upgrade.rs
@@ -70,22 +70,37 @@ fn apply_migration(config: &Value, migration: &Migration) -> Result<Value, Confi
     for action in &migration.actions {
         match action {
             Action::Delete { path } => {
-                // Deleting isn't actually supported by protus so we add a magic value to delete later
-                transformer_builder = transformer_builder.add_action(
-                    Parser::parse(REMOVAL_EXPRESSION, path).expect("migration must be valid"),
-                );
+                if !jsonpath_lib::select(config, path)
+                    .unwrap_or_default()
+                    .is_empty()
+                {
+                    // Deleting isn't actually supported by protus so we add a magic value to delete later
+                    transformer_builder = transformer_builder.add_action(
+                        Parser::parse(REMOVAL_EXPRESSION, path).expect("migration must be valid"),
+                    );
+                }
             }
             Action::Copy { from, to } => {
-                transformer_builder = transformer_builder
-                    .add_action(Parser::parse(from, to).expect("migration must be valid"));
+                if !jsonpath_lib::select(config, from)
+                    .unwrap_or_default()
+                    .is_empty()
+                {
+                    transformer_builder = transformer_builder
+                        .add_action(Parser::parse(from, to).expect("migration must be valid"));
+                }
             }
             Action::Move { from, to } => {
-                transformer_builder = transformer_builder
-                    .add_action(Parser::parse(from, to).expect("migration must be valid"));
-                // Deleting isn't actually supported by protus so we add a magic value to delete later
-                transformer_builder = transformer_builder.add_action(
-                    Parser::parse(REMOVAL_EXPRESSION, from).expect("migration must be valid"),
-                );
+                if !jsonpath_lib::select(config, from)
+                    .unwrap_or_default()
+                    .is_empty()
+                {
+                    transformer_builder = transformer_builder
+                        .add_action(Parser::parse(from, to).expect("migration must be valid"));
+                    // Deleting isn't actually supported by protus so we add a magic value to delete later
+                    transformer_builder = transformer_builder.add_action(
+                        Parser::parse(REMOVAL_EXPRESSION, from).expect("migration must be valid"),
+                    );
+                }
             }
         }
     }
@@ -247,6 +262,21 @@ mod test {
     fn move_field() {
         insta::assert_json_snapshot!(apply_migration(
             &source_doc(),
+            &Migration::builder()
+                .action(Action::Move {
+                    from: "obj.field1".to_string(),
+                    to: "new.obj.field1".to_string()
+                })
+                .description("move field1")
+                .build(),
+        )
+        .expect("expected successful migration"));
+    }
+
+    #[test]
+    fn move_non_existent_field() {
+        insta::assert_json_snapshot!(apply_migration(
+            &json!({}),
             &Migration::builder()
                 .action(Action::Move {
                     from: "obj.field1".to_string(),