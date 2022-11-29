36080fffcea4939417ed71c4e9b7cecaae834430
Geoffroy Couprie <geoffroy@apollographql.com>
2022-11-29 11:56:51 +0100

    fixes

--- apollo-router/src/services/layers/apq.rs
+++ apollo-router/src/services/layers/apq.rs
@@ -38,12 +38,6 @@ pub(crate) struct APQLayer {
 }
 
 impl APQLayer {
-    pub(crate) async fn new() -> Self {
-        Self {
-            cache: DeduplicatingCache::new().await,
-        }
-    }
-
     pub(crate) fn with_cache(cache: DeduplicatingCache<String, String>) -> Self {
         Self { cache }
     }
