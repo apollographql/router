apollographql/router

commit de723f6d12b031a579d039008f2ec78fcc127a3d

Traces pre aggregated (#2277)
This new flag on the protobuf means that we no longer have to keep track
if we are sending traces or stats. Instead we are asserting that stats
are comprehensive and traces are ancillary.

From the router side we no longer skip metrics increment if a trace is
being sampled.

Co-authored-by: bryn <bryn@apollographql.com>

apollo-router/src/plugins/telemetry/apollo.rs
@@ -72,7 +72,8 @@ pub(crate) struct Config {
     /// Enable field level instrumentation for subgraphs via ftv1. ftv1 tracing can cause performance issues as it is transmitted in band with subgraph responses.
     /// 0.0 will result in no field level instrumentation. 1.0 will result in always instrumentation.
     /// Value MUST be less than global sampling rate
-    pub(crate) field_level_instrumentation_sampler: Option<SamplerOption>,
+    #[serde(default = "default_field_level_instrumentation_sampler")]
+    pub(crate) field_level_instrumentation_sampler: SamplerOption,
 
     /// To configure which request header names and values are included in trace data that's sent to Apollo Studio.
     #[serde(default)]
@@ -86,7 +87,13 @@ pub(crate) struct Config {
     #[schemars(skip)]
     pub(crate) schema_id: String,
 
-    pub(crate) batch_processor: Option<BatchProcessorConfig>,
+    /// Configuration for batch processing.
+    #[serde(default)]
+    pub(crate) batch_processor: BatchProcessorConfig,
+}
+
+fn default_field_level_instrumentation_sampler() -> SamplerOption {
+    SamplerOption::TraceIdRatioBased(0.01)
 }
 
 #[cfg(test)]
@@ -145,11 +152,10 @@ impl Default for Config {
             client_version_header: client_version_header_default(),
             schema_id: "<no_schema_id>".to_string(),
             buffer_size: default_buffer_size(),
-            field_level_instrumentation_sampler: Some(SamplerOption::TraceIdRatioBased(0.01)),
+            field_level_instrumentation_sampler: default_field_level_instrumentation_sampler(),
             send_headers: ForwardHeaders::None,
             send_variable_values: ForwardValues::None,
-
-            batch_processor: Some(BatchProcessorConfig::default()),
+            batch_processor: BatchProcessorConfig::default(),
         }
     }
 }
@@ -218,6 +224,7 @@ impl Report {
             header: Some(header),
             end_time: Some(SystemTime::now().into()),
             operation_count: self.operation_count,
+            traces_pre_aggregated: true,
             ..Default::default()
         };
 

apollo-router/src/plugins/telemetry/apollo_exporter.rs
@@ -30,15 +30,8 @@ use url::Url;
 
 use super::apollo::Report;
 use super::apollo::SingleReport;
+use crate::plugins::telemetry::tracing::BatchProcessorConfig;
 
-const DEFAULT_QUEUE_SIZE: usize = 65_536;
-// Do not set to 5 secs because it's also the default value for the BatchSpanProcesser of tracing.
-// It's less error prone to set a different value to let us compute traces and metrics
-pub(crate) const EXPORTER_TIMEOUT_DURATION: Duration = Duration::from_secs(6);
-// Set to 90 seconds to err on the side of caution. This + our backoffs and attemps
-// should be more than enough to make sure the reports are sent,
-// while preventing potential hangs.
-pub(crate) const HTTP_CLIENT_TIMEOUT_DURATION: Duration = Duration::from_secs(90);
 const BACKOFF_INCREMENT: Duration = Duration::from_millis(50);
 
 #[derive(thiserror::Error, Debug)]
@@ -92,6 +85,7 @@ impl Default for Sender {
 /// Sending periodically (in the case of metrics).
 #[derive(Clone)]
 pub(crate) struct ApolloExporter {
+    batch_config: BatchProcessorConfig,
     endpoint: Url,
     apollo_key: String,
     header: proto::reports::ReportHeader,
@@ -102,6 +96,7 @@ pub(crate) struct ApolloExporter {
 impl ApolloExporter {
     pub(crate) fn new(
         endpoint: &Url,
+        batch_config: &BatchProcessorConfig,
         apollo_key: &str,
         apollo_graph_ref: &str,
         schema_id: &str,
@@ -121,12 +116,12 @@ impl ApolloExporter {
         };
 
         tracing::debug!("creating apollo exporter {}", endpoint);
-
         Ok(ApolloExporter {
             endpoint: endpoint.clone(),
+            batch_config: batch_config.clone(),
             apollo_key: apollo_key.to_string(),
             client: reqwest::Client::builder()
-                .timeout(HTTP_CLIENT_TIMEOUT_DURATION)
+                .timeout(batch_config.max_export_timeout)
                 .build()
                 .map_err(BoxError::from)?,
             header,
@@ -135,10 +130,9 @@ impl ApolloExporter {
     }
 
     pub(crate) fn start(self) -> Sender {
-        let (tx, mut rx) = mpsc::channel::<SingleReport>(DEFAULT_QUEUE_SIZE);
-        // This is the task that actually sends metrics
+        let (tx, mut rx) = mpsc::channel::<SingleReport>(self.batch_config.max_queue_size);
         tokio::spawn(async move {
-            let timeout = tokio::time::interval(EXPORTER_TIMEOUT_DURATION);
+            let timeout = tokio::time::interval(self.batch_config.scheduled_delay);
             let mut report = Report::default();
 
             tokio::pin!(timeout);

apollo-router/tests/snapshots/apollo_reports__client_name.snap
@@ -589,5 +589,5 @@ traces_per_query:
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
 operation_count: 1
-traces_pre_aggregated: false
+traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__client_version.snap
@@ -589,5 +589,5 @@ traces_per_query:
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
 operation_count: 1
-traces_pre_aggregated: false
+traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__condition_else.snap
@@ -595,5 +595,5 @@ traces_per_query:
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
 operation_count: 1
-traces_pre_aggregated: false
+traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__condition_if.snap
@@ -608,5 +608,5 @@ traces_per_query:
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
 operation_count: 1
-traces_pre_aggregated: false
+traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__non_defer.snap
@@ -586,5 +586,5 @@ traces_per_query:
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
 operation_count: 1
-traces_pre_aggregated: false
+traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__send_header.snap
@@ -592,5 +592,5 @@ traces_per_query:
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
 operation_count: 1
-traces_pre_aggregated: false
+traces_pre_aggregated: true
 

commit 5645b760a84a3ee366b4e08c5e2ced05f02fe72c

As we are now using metrics for all requests we should not set operation count (#2286)
This change has not been released but the issue was introduced in #2277
Fixes #2267
Metrics will always have the definitive operation count.

Co-authored-by: bryn <bryn@apollographql.com>
Co-authored-by: Coenen Benjamin <benjamin.coenen@hotmail.com>

apollo-router/src/plugins/telemetry/apollo.rs
@@ -226,7 +226,7 @@ impl AddAssign<SingleReport> for Report {
 
 impl AddAssign<TracesReport> for Report {
     fn add_assign(&mut self, report: TracesReport) {
-        self.operation_count += report.traces.len() as u64;
+        // Note that operation count is dealt with in metrics so we don't increment this.
         for (operation_signature, trace) in report.traces {
             self.traces_per_query
                 .entry(operation_signature)

apollo-router/src/plugins/telemetry/apollo_exporter.rs
@@ -163,7 +163,8 @@ impl ApolloExporter {
     }
 
     pub(crate) async fn submit_report(&self, report: Report) -> Result<(), ApolloExportError> {
-        if report.operation_count == 0 {
+        // We may be sending traces but with no operation count
+        if report.operation_count == 0 && report.traces_per_query.is_empty() {
             return Ok(());
         }
         tracing::debug!("submitting report: {:?}", report);

apollo-router/tests/snapshots/apollo_reports__client_name.snap
@@ -588,6 +588,6 @@ traces_per_query:
     referenced_fields_by_type: {}
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
-operation_count: 1
+operation_count: 0
 traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__client_version.snap
@@ -588,6 +588,6 @@ traces_per_query:
     referenced_fields_by_type: {}
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
-operation_count: 1
+operation_count: 0
 traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__condition_else.snap
@@ -594,6 +594,6 @@ traces_per_query:
     referenced_fields_by_type: {}
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
-operation_count: 1
+operation_count: 0
 traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__condition_if.snap
@@ -607,6 +607,6 @@ traces_per_query:
     referenced_fields_by_type: {}
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
-operation_count: 1
+operation_count: 0
 traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__non_defer.snap
@@ -585,6 +585,6 @@ traces_per_query:
     referenced_fields_by_type: {}
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
-operation_count: 1
+operation_count: 0
 traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__send_header.snap
@@ -591,6 +591,6 @@ traces_per_query:
     referenced_fields_by_type: {}
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
-operation_count: 1
+operation_count: 0
 traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__send_variable_value.snap
@@ -587,6 +587,6 @@ traces_per_query:
     referenced_fields_by_type: {}
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
-operation_count: 1
+operation_count: 0
 traces_pre_aggregated: true
 

apollo-router/tests/snapshots/apollo_reports__trace_id.snap
@@ -585,6 +585,6 @@ traces_per_query:
     referenced_fields_by_type: {}
     internal_traces_contributing_to_stats: []
 end_time: "[end_time]"
-operation_count: 1
+operation_count: 0
 traces_pre_aggregated: true
 