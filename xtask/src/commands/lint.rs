77c596a083856184bd04f3b199dc6b6dc66ae3a6
Geoffroy Couprie <geoffroy@apollographql.com>
2022-12-19 12:28:04 +0100

    the rustfmt options we use are stable now

--- xtask/src/commands/lint.rs
+++ xtask/src/commands/lint.rs
@@ -9,8 +9,6 @@ use xtask::*;
 #[derive(Debug, StructOpt)]
 pub struct Lint {}
 
-const RUSTFMT_TOOLCHAIN: &str = "nightly-2022-06-26";
-
 const RUSTFMT_CONFIG: &[&str] = &["imports_granularity=Item", "group_imports=StdExternalCrate"];
 
 impl Lint {
@@ -35,30 +33,9 @@ impl Lint {
     }
 
     fn run_common(&self, fmt: impl FnOnce() -> Result<()>) -> Result<()> {
-        Self::install_rustfmt()?;
         fmt()?;
-        cargo!(["clippy", "--all", "--all-targets", "--", "-D", "warnings"]);
+        cargo!(["clippy", "--all", "--all-targets", "--", "-D", "warnings",]);
         cargo!(["doc", "--all", "--no-deps"], env = { "RUSTDOCFLAGS" => "-Dwarnings" });
-        Ok(())
-    }
-
-    fn install_rustfmt() -> Result<()> {
-        let nightly = RUSTFMT_TOOLCHAIN;
-        if !output("rustup", &["toolchain", "list"])?
-            .lines()
-            .any(|line| line.starts_with(nightly))
-        {
-            let args = ["toolchain", "install", nightly, "--profile", "minimal"];
-            run("rustup", &args)?
-        }
-        let args = ["component", "list", "--installed", "--toolchain", nightly];
-        if !output("rustup", &args)?
-            .lines()
-            .any(|line| line.starts_with("rustfmt"))
-        {
-            let args = ["component", "add", "rustfmt", "--toolchain", nightly];
-            run("rustup", &args)?
-        }
         Ok(())
     }
 
@@ -69,32 +46,15 @@ impl Lint {
     }
 
     fn fmt_command() -> Result<Command> {
-        let mut command = Command::new(which::which("rustup")?);
+        let mut command = Command::new(which::which("cargo")?);
         command.current_dir(&*PKG_PROJECT_ROOT).args([
-            "run",
-            RUSTFMT_TOOLCHAIN,
-            "cargo",
             "fmt",
             "--all",
             "--",
             "--config",
             &RUSTFMT_CONFIG.join(","),
+            "-v",
         ]);
         Ok(command)
     }
-}
-
-fn run(program: &str, args: &[&str]) -> Result<()> {
-    let status = Command::new(which::which(program)?).args(args).status()?;
-    ensure!(status.success(), "{} failed", program);
-    Ok(())
-}
-
-fn output(program: &str, args: &[&str]) -> Result<String> {
-    let output = Command::new(which::which(program)?)
-        .args(args)
-        .stderr(Stdio::piped())
-        .output()?;
-    ensure!(output.status.success(), "{} failed", program);
-    Ok(String::from_utf8(output.stdout)?)
 }
