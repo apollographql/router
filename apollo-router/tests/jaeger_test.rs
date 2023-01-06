872cf111198b1353d4738e73c28d57864e6ba4d0
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2023-01-06 15:41:13 +0100

    delete useless dbg and sleep
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- apollo-router/tests/jaeger_test.rs
+++ apollo-router/tests/jaeger_test.rs
@@ -53,10 +53,8 @@ async fn test_jaeger_tracing_and_metrics() -> Result<(), BoxError> {
         router.touch_config()?;
         tokio::time::sleep(Duration::from_millis(100)).await;
     }
-    tokio::time::sleep(Duration::from_millis(1000)).await;
 
     let metrics = router.get_metrics().await.unwrap();
-    dbg!(&metrics);
     assert!(metrics.contains(r#"apollo_router_cache_hit_count{kind="query planner",service_name="apollo-router",storage="memory"} 9"#));
     assert!(metrics.contains(r#"apollo_router_cache_miss_count{kind="query planner",service_name="apollo-router",storage="memory"} 1"#));
     assert!(metrics.contains("apollo_router_cache_hit_time"));
