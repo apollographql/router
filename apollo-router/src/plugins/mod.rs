65325c7860e3c9d997b01e07adb2eb1ae1fbf0a1
Bryn Cooke <BrynCooke@gmail.com>
2023-01-10 13:44:01 +0000

    Update apollo-router/src/plugins/mod.rs
    Co-authored-by: Geoffroy Couprie <geoffroy@apollographql.com>

--- apollo-router/src/plugins/mod.rs
+++ apollo-router/src/plugins/mod.rs
@@ -4,14 +4,7 @@
 
 macro_rules! schemar_fn {
     ($name:ident, $ty:ty, $description:expr) => {
-        // The macro will expand into the contents of this block.
-        fn $name(gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
-            let schema = <$ty>::json_schema(gen);
-            let mut schema = schema.into_object();
-            let mut metadata = schemars::schema::Metadata::default();
-            metadata.description = Some($description.to_string());
-            schema.metadata = Some(Box::new(metadata));
-            schemars::schema::Schema::Object(schema)
+      schemar_fn!($name, $ty, None, $description)
         }
     };
 
