ce68b014c47b9156d650401c353cbae08bbadb3c
Simon Sapin <simon@apollographql.com>
2023-01-12 13:45:49 +0100

    Implement Schema::parse_with_hir

--- apollo-router/tests/compiler_integration.rs
+++ apollo-router/tests/compiler_integration.rs
@@ -2,7 +2,6 @@
 /// `fuzz_schema_parsing` is expected to be used with a wide variety of inputs,
 /// such as fuzzing or a corpus production schemas.
 #[test]
-#[should_panic] // parse_with_hir is not yet implemented
 fn compare_schema_parsing() {
     let (with_ast, with_hir) = apollo_router::_private::compare_schema_parsing(include_str!(
         "../src/query_planner/testdata/schema.graphql"
