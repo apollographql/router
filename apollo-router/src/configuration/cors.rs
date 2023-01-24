84f38fa2b75940451d26b4ec0deaaeb00d660d4e
Omar Abu Samra <114017357+osamra-rbi@users.noreply.github.com>
2023-01-24 03:53:36 -0500

    feat: add optional Access-Control-Max-Age header to CORS plugin (#2331)
    Hi there
    
    As discussed in this
    [issue](https://github.com/apollographql/router/issues/2212), the CORS
    plugin in Apollo Router currently lacks setting the
    Access-Control-Max-Age header. This PR adds a new option called max_age
    that is used like so:
    ```
    cors:
      max_age: 86400
    ```
    
    This does not solve the Cache-Control header mentioned in issue as it's
    a bit separate from CORS.
    
    New test(s) and documentation changes haven't been done yet as I wanted
    to confirm this is fine before proceeding further. If so, happy to do
    them. Thanks!
    
    (FYI I don't have the ability to assign a reviewer or anything, if we're
    able to do that)
    
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
    Co-authored-by: Simon Sapin <simon@apollographql.com>
    Co-authored-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- apollo-router/src/configuration/cors.rs
+++ apollo-router/src/configuration/cors.rs
@@ -2,6 +2,7 @@
 // This entire file is license key functionality
 
 use std::str::FromStr;
+use std::time::Duration;
 
 use http::request::Parts;
 use http::HeaderValue;
@@ -59,6 +60,11 @@ pub(crate) struct Cors {
     /// Allowed request methods. Defaults to GET, POST, OPTIONS.
     #[serde(default = "default_cors_methods")]
     pub(crate) methods: Vec<String>,
+
+    /// The `Access-Control-Max-Age` header value in time units
+    #[serde(deserialize_with = "humantime_serde::deserialize", default)]
+    #[schemars(with = "String", default)]
+    pub(crate) max_age: Option<Duration>,
 }
 
 impl Default for Cors {
@@ -71,6 +77,7 @@ impl Default for Cors {
             allow_headers: Default::default(),
             expose_headers: Default::default(),
             match_origins: Default::default(),
+            max_age: Default::default(),
         }
     }
 }
@@ -96,10 +103,12 @@ impl Cors {
         origins: Option<Vec<String>>,
         match_origins: Option<Vec<String>>,
         methods: Option<Vec<String>>,
+        max_age: Option<Duration>,
     ) -> Self {
         Self {
             expose_headers,
             match_origins,
+            max_age,
             origins: origins.unwrap_or_else(default_origins),
             methods: methods.unwrap_or_else(default_cors_methods),
             allow_any_origin: allow_any_origin.unwrap_or_default(),
@@ -148,6 +157,11 @@ impl Cors {
                         .ok()
                 },
             )));
+        let cors = if let Some(max_age) = self.max_age {
+            cors.max_age(max_age)
+        } else {
+            cors
+        };
 
         if self.allow_any_origin {
             Ok(cors.allow_origin(cors::Any))
