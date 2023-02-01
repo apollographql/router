012ffdda2922978e267af58ac07589bd357d8697
Geoffroy Couprie <geoffroy@apollographql.com>
2023-02-01 10:41:52 +0100

    try to make snapshots more stable

--- apollo-router/tests/redis_test.rs
+++ apollo-router/tests/redis_test.rs
@@ -1,10 +1,13 @@
 #[cfg(all(target_os = "linux", target_arch = "x86_64", test))]
 mod test {
     use apollo_router::graphql;
+    use apollo_router::services::execution::QueryPlan;
     use apollo_router::services::supergraph;
     use http::Method;
     use redis::AsyncCommands;
     use redis::Client;
+    use serde::Deserialize;
+    use serde::Serialize;
     use serde_json::json;
     use tower::ServiceExt;
 
@@ -51,8 +54,14 @@ mod test {
           .get("plan\x005abb5fecf7df056396fb90fdf38d430b8c1fec55ec132fde878161608af18b76\x00{ topProducts { name name2:name } }\x00-")
           .await
           .unwrap();
-        let query_plan: serde_json::Value = serde_json::from_str(&s).unwrap();
-        insta::assert_json_snapshot!(query_plan);
+        let query_plan: QueryPlannerContent = serde_json::from_str(&s).unwrap();
+        insta::assert_json_snapshot!(serde_json::to_value(query_plan).unwrap());
+    }
+
+    #[derive(Deserialize, Serialize)]
+
+    struct QueryPlannerContent {
+        plan: QueryPlan,
     }
 
     #[tokio::test(flavor = "multi_thread")]
