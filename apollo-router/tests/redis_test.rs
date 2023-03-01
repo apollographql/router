672552e534445bc25c8c09c53d00aa89e306522f
Geoffroy Couprie <apollo@geoffroycouprie.com>
2023-03-01 10:58:04 +0100

    test again with fred

--- apollo-router/tests/redis_test.rs
+++ apollo-router/tests/redis_test.rs
@@ -3,6 +3,8 @@ mod test {
     use apollo_router::services::execution::QueryPlan;
     use apollo_router::services::router;
     use apollo_router::services::supergraph;
+    use fred::prelude::ClientLike;
+    use fred::prelude::KeysInterface;
     use futures::StreamExt;
     use http::Method;
     use redis::AsyncCommands;
@@ -15,16 +17,36 @@ mod test {
 
     #[tokio::test(flavor = "multi_thread")]
     async fn query_planner() -> Result<(), BoxError> {
-        let client = Client::open("redis://127.0.0.1:6379").expect("opening ClusterClient");
-        let mut connection = client
-            .get_async_connection()
-            .await
-            .expect("got redis connection");
+        let client = fred::prelude::RedisClient::new(
+            fred::types::RedisConfig::from_url("redis://redis:6379").unwrap(),
+            None,
+            None,
+        );
+        // spawn tasks that listen for connection close or reconnect events
+        let mut error_rx = client.on_error();
+        let mut reconnect_rx = client.on_reconnect();
+        tokio::spawn(async move {
+            while let Ok(error) = error_rx.recv().await {
+                println!("Client disconnected with error: {:?}", error);
+            }
+        });
+        tokio::spawn(async move {
+            while reconnect_rx.recv().await.is_ok() {
+                println!("Redis client reconnected.");
+            }
+        });
 
-        connection
-        .del::<&'static str, ()>("plan\x005abb5fecf7df056396fb90fdf38d430b8c1fec55ec132fde878161608af18b76\x00{ topProducts { name name2:name } }\x00-")
-          .await
-          .unwrap();
+        println!("qp redis wait for connect");
+        tokio::time::timeout(
+            std::time::Duration::from_secs(10),
+            client.wait_for_connect(),
+        )
+        .await
+        .unwrap()
+        .expect("opening redis client");
+
+        client
+        .del::<(), &'static str>("plan\x005abb5fecf7df056396fb90fdf38d430b8c1fec55ec132fde878161608af18b76\x00{ topProducts { name name2:name } }\x00-").await.unwrap();
 
         let supergraph = apollo_router::TestHarness::builder()
             .with_subgraph_network_requests()
