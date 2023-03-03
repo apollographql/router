904c2b42b45c456a7efd8dfbbdb1a4869710ea73
bryn <bryn@apollographql.com>
2023-03-03 17:10:04 +0000

    Revert changes to lint except to not run clippy on deps

--- xtask/src/commands/lint.rs
+++ xtask/src/commands/lint.rs
@@ -10,10 +10,6 @@ pub struct Lint {
     /// apply formatting fixes
     #[structopt(long)]
     fmt: bool,
-
-    /// skip xtask subproject
-    #[structopt(long)]
-    skip_xtask: bool,
 }
 
 const RUSTFMT_CONFIG: &[&str] = &["imports_granularity=Item", "group_imports=StdExternalCrate"];
@@ -21,25 +17,27 @@ const RUSTFMT_CONFIG: &[&str] = &["imports_granularity=Item", "group_imports=Std
 impl Lint {
     pub fn run(&self) -> Result<()> {
         if self.fmt {
-            for mut command in self.fmt_commands()? {
-                let status = command.status()?;
-                ensure!(status.success(), "cargo fmt failed");
-            }
+            let [mut command_1, mut command_2] = Self::fmt_commands()?;
+            let status = command_1.status()?;
+            ensure!(status.success(), "cargo fmt failed");
+            let status = command_2.status()?;
+            ensure!(status.success(), "cargo fmt failed");
             Ok(())
         } else {
-            self.run_common(|| self.check_fmt())
+            self.run_common(Self::check_fmt)
         }
     }
 
     pub fn run_local(&self) -> Result<()> {
         self.run_common(|| {
-            if self.check_fmt().is_err() {
+            if Self::check_fmt().is_err() {
                 // cargo fmt check failed, this means there is some formatting to do
                 // given this task is running locally, let's do it and let our user know
-                for mut command in self.fmt_commands()? {
-                    let status = command.status()?;
-                    ensure!(status.success(), "cargo fmt failed");
-                }
+                let [mut command_1, mut command_2] = Self::fmt_commands()?;
+                let status = command_1.status()?;
+                ensure!(status.success(), "cargo fmt failed");
+                let status = command_2.status()?;
+                ensure!(status.success(), "cargo fmt failed");
                 eprintln!(
                     "🧹 cargo fmt job is complete 🧹\n\
                     Commit the changes and you should be good to go!"
@@ -55,36 +53,32 @@ impl Lint {
             "clippy",
             "--all",
             "--all-targets",
+            "--no-deps",
             "--",
             "-D",
-            "warnings",
-            "--no-deps"
+            "warnings"
         ]);
         cargo!(["doc", "--all", "--no-deps"], env = { "RUSTDOCFLAGS" => "-Dwarnings" });
         Ok(())
     }
 
-    fn check_fmt(&self) -> Result<()> {
-        for mut command in self.fmt_commands()? {
-            let status = command.arg("--check").status()?;
-            ensure!(status.success(), "cargo fmt failed");
-        }
+    fn check_fmt() -> Result<()> {
+        let [mut command_1, mut command_2] = Self::fmt_commands()?;
+        let status = command_1.arg("--check").status()?;
+        ensure!(status.success(), "cargo fmt check failed");
+        let status = command_2.arg("--check").status()?;
+        ensure!(status.success(), "cargo fmt check failed");
         Ok(())
     }
 
-    fn fmt_commands(&self) -> Result<Vec<Command>> {
-        let mut commands = Vec::new();
+    fn fmt_commands() -> Result<[Command; 2]> {
         let cargo = which::which("cargo")?;
         let args = ["fmt", "--all", "--", "--config", &RUSTFMT_CONFIG.join(",")];
+        let mut command_1 = Command::new(&cargo);
+        let mut command_2 = Command::new(&cargo);
         let root = &*PKG_PROJECT_ROOT;
-        let mut command = Command::new(&cargo);
-        command.args(args).current_dir(root);
-        commands.push(command);
-        if !self.skip_xtask {
-            let mut command = Command::new(&cargo);
-            command.args(args).current_dir(root.join("xtask"));
-            commands.push(command);
-        }
-        Ok(commands)
+        command_1.args(args).current_dir(root);
+        command_2.args(args).current_dir(root.join("xtask"));
+        Ok([command_1, command_2])
     }
 }
