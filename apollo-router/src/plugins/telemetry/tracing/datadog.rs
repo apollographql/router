3ada328dd0ed4c6ef63271e5a86b10d5067b4e4b
Samuel Vazquez <samvazquez@expediagroup.com>
2023-03-15 16:32:03 -0700

    feat: add configurable custom mapping to dd exporter

--- apollo-router/src/plugins/telemetry/tracing/datadog.rs
+++ apollo-router/src/plugins/telemetry/tracing/datadog.rs
@@ -1,4 +1,9 @@
 //! Configuration for datadog tracing.
+
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+use opentelemetry::Key;
+use opentelemetry::Value;
 use opentelemetry::sdk::trace::BatchSpanProcessor;
 use opentelemetry::sdk::trace::Builder;
 use schemars::JsonSchema;
@@ -15,6 +20,18 @@ use crate::plugins::telemetry::tracing::BatchProcessorConfig;
 use crate::plugins::telemetry::tracing::SpanProcessorExt;
 use crate::plugins::telemetry::tracing::TracingConfigurator;
 
+lazy_static! {
+    static ref SPAN_RESOURCE_NAME_ATTRIBUTE_MAPPING: HashMap<&'static str, &'static str> = {
+        let mut map = HashMap::new();
+        map.insert("request", "http.route");
+        map.insert("supergraph", "graphql.operation.name");
+        map.insert("query_planning", "graphql.operation.name");
+        map.insert("subgraph", "apollo.subgraph.name");
+        map.insert("subgraph_request", "graphql.operation.name");
+        map
+    };
+}
+
 #[derive(Debug, Clone, Deserialize, Serialize, JsonSchema)]
 #[serde(deny_unknown_fields)]
 pub(crate) struct Config {
@@ -26,6 +43,14 @@ pub(crate) struct Config {
     /// batch processor configuration
     #[serde(default)]
     pub(crate) batch_processor: BatchProcessorConfig,
+
+    /// Enable datadog span mapping for span name and resource name.
+    #[serde(default = "default_enable_span_mapping")]
+    pub(crate) enable_span_mapping: bool,
+}
+
+fn default_enable_span_mapping() -> bool {
+    false
 }
 
 impl TracingConfigurator for Config {
@@ -35,9 +60,27 @@ impl TracingConfigurator for Config {
             AgentEndpoint::Default(_) => None,
             AgentEndpoint::Url(s) => Some(s),
         };
+        let enable_span_mapping = match &self.enable_span_mapping {
+            true => Some(true),
+            false => None,
+        };
         let exporter = opentelemetry_datadog::new_pipeline()
             .with(&url, |b, e| {
-                b.with_agent_endpoint(e.to_string().trim_end_matches('/'))
+                builder.with_agent_endpoint(e.to_string().trim_end_matches('/'))
+            })
+            .with(&enable_span_mapping, |builder, _e| {
+                builder
+                    .with_name_mapping(|span, _model_config| span.name.as_ref())
+                    .with_resource_mapping(|span, _model_config|
+                        SPAN_RESOURCE_NAME_ATTRIBUTE_MAPPING
+                            .get(span.name.as_ref())
+                            .and_then(|key| span.attributes.get(&Key::from_static_str(key)))
+                            .and_then(|value| match value {
+                                Value::String(value) => Some(value.as_str()),
+                                _ => None,
+                            })
+                            .unwrap_or(span.name.as_ref())
+                    )
             })
             .with_service_name(trace_config.service_name.clone())
             .with_trace_config(trace_config.into())
