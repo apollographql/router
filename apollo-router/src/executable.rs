af82df49982770c87708197f0e3f1ae05f7e6864
deweyjose <richard.e.jose@gmail.com>
2023-03-19 21:04:54 -0400

    Update executable.rs

--- apollo-router/src/executable.rs
+++ apollo-router/src/executable.rs
@@ -30,6 +30,7 @@ use crate::router::RouterHttpServer;
 use crate::router::SchemaSource;
 use crate::router::ShutdownSource;
 use crate::EntitlementSource;
+use once_cell::sync::OnceCell;
 
 // Note: the dhat-heap and dhat-ad-hoc features should not be both enabled. We name our functions
 // and variables identically to prevent this from happening.
