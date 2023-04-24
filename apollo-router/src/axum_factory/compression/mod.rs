3cd2e3f4c44bacb3af79321b49e6bc8c0776e7ac
Geoffroy Couprie <apollo@geoffroycouprie.com>
2023-04-24 16:41:29 +0200

    vendor encoders from async-compression
    This integrates the encoders from
    https://github.com/Nemo157/async-compression so we can use them other a
    stream that flushes data regularly

---
+++ apollo-router/src/axum_factory/compression/mod.rs
@@ -0,0 +1,7 @@
+// All code from this module is extracted from https://github.com/Nemo157/async-compression and is under MIT or Apache-2 licence
+// it will be removed when we find a long lasting solution to https://github.com/Nemo157/async-compression/issues/154
+//#[macro_use]
+//mod macros;
+pub(crate) mod codec;
+pub(crate) mod unshared;
+pub(crate) mod util;
