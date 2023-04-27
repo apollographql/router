b19acabce709b284dea7e773b2f3d03ce73c0569
Simon Sapin <simon@apollographql.com>
2023-04-27 14:24:51 +0200

    Mark operation limits as a preview feature

--- apollo-router/src/uplink/entitlement.rs
+++ apollo-router/src/uplink/entitlement.rs
@@ -156,19 +156,19 @@ impl EntitlementReport {
             // Per-operation limits are restricted but parser limits like `parser_max_recursion`
             // where the Router only configures apollo-rs are not.
             ConfigurationRestriction::builder()
-                .path("$.supergraph.limits.max_depth")
+                .path("$.supergraph.preview_limits.max_depth")
                 .name("Operation depth limiting")
                 .build(),
             ConfigurationRestriction::builder()
-                .path("$.supergraph.limits.max_height")
+                .path("$.supergraph.preview_limits.max_height")
                 .name("Operation height limiting")
                 .build(),
             ConfigurationRestriction::builder()
-                .path("$.supergraph.limits.max_root_fields")
+                .path("$.supergraph.preview_limits.max_root_fields")
                 .name("Operation root fields limiting")
                 .build(),
             ConfigurationRestriction::builder()
-                .path("$.supergraph.limits.max_aliases")
+                .path("$.supergraph.preview_limits.max_aliases")
                 .name("Operation aliases limiting")
                 .build(),
         ]
