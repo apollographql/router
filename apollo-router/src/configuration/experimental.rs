a07b6e378b099f2a0734fba58b0b23bd507daf47
Simon Sapin <simon@apollographql.com>
2023-04-27 16:05:31 +0200

    Share some more code for printing experimental and preview feature usage

--- apollo-router/src/configuration/experimental.rs
+++ apollo-router/src/configuration/experimental.rs
@@ -42,42 +42,56 @@ impl Discussed {
     }
 
     pub(crate) fn log_experimental_used(&self, conf: &Value) {
-        let used_experimental_conf = get_configurations(conf, "experimental");
-
-        let needed_discussions: Vec<String> = used_experimental_conf
-            .into_iter()
-            .filter_map(|used_exp_conf| {
-                self.experimental
-                    .get(&used_exp_conf)
-                    .map(|discussion_link| format!("\t- {used_exp_conf}: {discussion_link}"))
-            })
-            .collect();
-        if !needed_discussions.is_empty() {
-            tracing::info!(
-                r#"You're using some "experimental" features (configuration prefixed by "experimental_" or contained within an "experimental" section). We may make breaking changes in future releases.
-To help us design the stable version we need your feedback. Here is a list of links where you can give your opinion:
-
-{}
-"#,
-                needed_discussions.join("\n")
-            );
-        }
+        self.log_used(
+            conf,
+            "experimental",
+            &self.experimental,
+            "We may make breaking changes in future releases. \
+            To help us design the stable version we need your feedback.",
+        )
     }
 
     pub(crate) fn log_preview_used(&self, conf: &Value) {
-        let used_preview_conf = get_configurations(conf, "preview");
+        self.log_used(
+            conf,
+            "preview",
+            &self.preview,
+            "These features are not officially supported with any SLA \
+            and may still contain bugs or undergo iteration. \
+            You're encouraged to try preview features in test environments \
+            to familiarize yourself with upcoming functionality \
+            before it reaches general availability.",
+        )
+    }
 
-        let needed_discussions: Vec<String> = used_preview_conf
+    fn log_used(
+        &self,
+        conf: &Value,
+        prefix: &str,
+        urls: &HashMap<String, String>,
+        stage_description: &str,
+    ) {
+        let used = get_configurations(conf, prefix);
+        let needed_discussions = used
             .into_iter()
-            .filter_map(|used_conf| {
-                self.experimental
-                    .get(&used_conf)
-                    .map(|discussion_link| format!("\t- {used_conf}: {discussion_link}"))
+            .filter_map(|config_key| {
+                urls.get(&config_key)
+                    .map(|discussion_link| format!("\t- {config_key}: {discussion_link}"))
             })
-            .collect();
+            .collect::<Vec<String>>()
+            .join("\n");
+
         if !needed_discussions.is_empty() {
             tracing::info!(
-                r#"You're using some "preview" features of the Apollo Router (those which have their configuration prefixed by "preview").  These features are not officially supported with any SLA and may still contain bugs or undergo iteration. You're encouraged to try preview features in test environments to familiarize yourself with upcoming functionality before it reaches general availability. For more information about launch stages, please see the documentation here: https://www.apollographql.com/docs/resources/product-launch-stages/"#,
+                "You're using some \"{prefix}\" features of the Apollo Router \
+                (those which have their configuration prefixed by \"{prefix}_\"). \
+                {stage_description} \
+                Here is a list of links where you can give your opinion:
+
+                {needed_discussions}
+
+                For more information about launch stages, please see the documentation here: \
+                https://www.apollographql.com/docs/resources/product-launch-stages/",
             );
         }
     }
@@ -92,7 +106,7 @@ fn get_configurations(conf: &Value, prefix: &str) -> Vec<String> {
 fn visit_configurations(conf: &Value, prefix: &str, fields: &mut Vec<String>) {
     if let Value::Object(object) = conf {
         object.iter().for_each(|(field_name, val)| {
-            if field_name.starts_with(&format!("{}_", prefix)) {
+            if field_name.starts_with(prefix) {
                 fields.push(field_name.clone());
             }
             visit_configurations(val, prefix, fields);
