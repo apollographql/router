3ee3355893dc6844a9baa417196cf9fbf3611429
Geoffroy Couprie <apollo@geoffroycouprie.com>
2023-06-13 12:04:18 +0200

    fix mutex usage

--- apollo-router/src/query_planner/bridge_query_planner.rs
+++ apollo-router/src/query_planner/bridge_query_planner.rs
@@ -267,15 +267,15 @@ impl Service<QueryPlannerRequest> for BridgeQueryPlanner {
         let fut = async move {
             let start = Instant::now();
 
-            let compiler = compiler.lock().await;
-            let file_id = compiler
+            let compiler_guard = compiler.lock().await;
+            let file_id = compiler_guard
                 .db
                 .source_file(QUERY_EXECUTABLE.into())
                 .ok_or(QueryPlannerError::SpecError(SpecError::ParsingError(
                     "missing input file for query".to_string(),
                 )))?;
-            let filtered_query = compiler.db.source_code(file_id);
-            drop(compiler)
+            let filtered_query = compiler_guard.db.source_code(file_id);
+            drop(compiler_guard);
 
             let res = this
                 .get(
