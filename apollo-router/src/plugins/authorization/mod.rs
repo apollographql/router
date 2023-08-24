907ca0e5e0d4768f5f890cbcd7da9698f462ea77
Geoffroy Couprie <apollo@geoffroycouprie.com>
2023-08-24 12:23:28 +0200

    lint

--- apollo-router/src/plugins/authorization/mod.rs
+++ apollo-router/src/plugins/authorization/mod.rs
@@ -141,10 +141,8 @@ impl AuthorizationPlugin {
         // if this fails, the query is invalid and will fail at the query planning phase.
         // We do not return validation errors here for now because that would imply a huge
         // refactoring of telemetry and tests
-        if traverse::document(&mut visitor, file_id).is_ok() {
-            if !visitor.found {
-                context.insert(AUTHENTICATED_KEY, true).unwrap();
-            }
+        if traverse::document(&mut visitor, file_id).is_ok() && !visitor.found {
+            context.insert(AUTHENTICATED_KEY, true).unwrap();
         }
 
         let mut visitor = ScopeExtractionVisitor::new(&compiler, file_id);
