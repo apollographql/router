826b840e0d0e5dd742e20a8f3af5c1a192f3d06b
Coenen Benjamin <benjamin.coenen@hotmail.com>
2023-09-12 15:24:21 +0200

    test(subscription): add integration test for subscription (#3752)
    Add a regression test that ensures an unnamed subscription makes it
    through the request pipeline.
    
    Fixes #3740
    
    ---------
    
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- apollo-router/tests/subscription_load_test.rs
+++ apollo-router/tests/subscription_load_test.rs
@@ -10,8 +10,28 @@ use crate::common::Telemetry;
 mod common;
 
 const SUBSCRIPTION_CONFIG: &str = include_str!("fixtures/subscription.router.yaml");
-const SUB_QUERY: &str = r#"subscription {  userWasCreated {    name reviews { body } }}"#;
+const SUB_QUERY: &str =
+    r#"subscription {  userWasCreated(intervalMs: 5, nbEvents: 10) {    name reviews { body } }}"#;
 const UNFEDERATED_SUB_QUERY: &str = r#"subscription {  userWasCreated { name username }}"#;
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_subscription() -> Result<(), BoxError> {
+    let mut router = create_router(SUBSCRIPTION_CONFIG).await?;
+    router.start().await;
+    router.assert_started().await;
+
+    let (_, response) = router.run_subscription(SUB_QUERY).await;
+    assert!(response.status().is_success());
+
+    let mut stream = response.bytes_stream();
+    while let Some(chunk) = stream.next().await {
+        let chunk = chunk.unwrap();
+        assert!(chunk.starts_with(b"\r\n--graphql\r\ncontent-type: application/json\r\n\r\n"));
+        assert!(chunk.ends_with(b"\r\n--graphql--\r\n"));
+    }
+
+    Ok(())
+}
 
 #[ignore]
 #[tokio::test(flavor = "multi_thread")]
