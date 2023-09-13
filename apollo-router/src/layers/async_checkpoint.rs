65aba7f44337752db934acf20a5d2afb66df4231
Gary Pennington <gary@apollographql.com>
2023-09-13 15:46:34 +0100

    Introduce OneShotAsyncCheckpoint
    The existing AsynCheckpoint requires `Clone` and thus introduces a need
    for Service Buffering that reduces the performance and resiliance of the
    router.
    
    This new service set of Services, Layers and utility functions removes
    the requirement for `Clone` and thus the requirement for service
    buffering.
    
    Existing uses of AsyncCheckpoint within the router are replaced with
    OneShotAsyncCheckpoint along with the requirement to `buffer()` such
    services.

--- apollo-router/src/layers/async_checkpoint.rs
+++ apollo-router/src/layers/async_checkpoint.rs
@@ -67,6 +67,82 @@ where
     }
 }
 
+/// [`Service`] for OneShot (single use) Asynchronous Checkpoints. See [`ServiceBuilderExt::oneshot_checkpoint_async()`](crate::layers::ServiceBuilderExt::oneshot_checkpoint_async()).
+#[allow(clippy::type_complexity)]
+pub struct OneShotAsyncCheckpointService<S, Fut, Request>
+where
+    Request: Send + 'static,
+    S: Service<Request, Error = BoxError> + Send + 'static,
+    <S as Service<Request>>::Response: Send + 'static,
+    <S as Service<Request>>::Future: Send + 'static,
+    Fut: Future<Output = Result<ControlFlow<<S as Service<Request>>::Response, Request>, BoxError>>,
+{
+    inner: Option<S>,
+    checkpoint_fn: Arc<Pin<Box<dyn Fn(Request) -> Fut + Send + Sync + 'static>>>,
+}
+
+impl<S, Fut, Request> OneShotAsyncCheckpointService<S, Fut, Request>
+where
+    Request: Send + 'static,
+    S: Service<Request, Error = BoxError> + Send + 'static,
+    <S as Service<Request>>::Response: Send + 'static,
+    <S as Service<Request>>::Future: Send + 'static,
+    Fut: Future<Output = Result<ControlFlow<<S as Service<Request>>::Response, Request>, BoxError>>,
+{
+    /// Create an `OneShotAsyncCheckpointLayer` from a function that takes a Service Request and returns a `ControlFlow`
+    pub fn new<F>(checkpoint_fn: F, service: S) -> Self
+    where
+        F: Fn(Request) -> Fut + Send + Sync + 'static,
+    {
+        Self {
+            checkpoint_fn: Arc::new(Box::pin(checkpoint_fn)),
+            inner: Some(service),
+        }
+    }
+}
+
+impl<S, Fut, Request> Service<Request> for OneShotAsyncCheckpointService<S, Fut, Request>
+where
+    Request: Send + 'static,
+    S: Service<Request, Error = BoxError> + Send + 'static,
+    <S as Service<Request>>::Response: Send + 'static,
+    <S as Service<Request>>::Future: Send + 'static,
+    Fut: Future<Output = Result<ControlFlow<<S as Service<Request>>::Response, Request>, BoxError>>
+        + Send
+        + 'static,
+{
+    type Response = <S as Service<Request>>::Response;
+
+    type Error = BoxError;
+
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.inner
+            .as_mut()
+            .expect("One shot must only be called once")
+            .poll_ready(cx)
+    }
+
+    fn call(&mut self, req: Request) -> Self::Future {
+        let checkpoint_fn = Arc::clone(&self.checkpoint_fn);
+        let inner = self
+            .inner
+            .take()
+            .expect("One shot must only be called once");
+        Box::pin(async move {
+            match (checkpoint_fn)(req).await {
+                Ok(ControlFlow::Break(response)) => Ok(response),
+                Ok(ControlFlow::Continue(request)) => inner.oneshot(request).await,
+                Err(error) => Err(error),
+            }
+        })
+    }
+}
+
 /// [`Service`] for Asynchronous Checkpoints. See [`ServiceBuilderExt::checkpoint_async()`](crate::layers::ServiceBuilderExt::checkpoint_async()).
 #[allow(clippy::type_complexity)]
 pub struct AsyncCheckpointService<S, Fut, Request>
@@ -134,6 +210,52 @@ where
                 Err(error) => Err(error),
             }
         })
+    }
+}
+
+/// [`Layer`] for OneShot (single use) Asynchronous Checkpoints. See [`ServiceBuilderExt::oneshot_checkpoint_async()`](crate::layers::ServiceBuilderExt::oneshot_checkpoint_async()).
+#[allow(clippy::type_complexity)]
+pub struct OneShotAsyncCheckpointLayer<S, Fut, Request>
+where
+    S: Service<Request, Error = BoxError> + Send + 'static,
+    Fut: Future<Output = Result<ControlFlow<<S as Service<Request>>::Response, Request>, BoxError>>,
+{
+    checkpoint_fn: Arc<Pin<Box<dyn Fn(Request) -> Fut + Send + Sync + 'static>>>,
+    phantom: PhantomData<S>, // XXX: The compiler can't detect that S is used in the Future...
+}
+
+impl<S, Fut, Request> OneShotAsyncCheckpointLayer<S, Fut, Request>
+where
+    S: Service<Request, Error = BoxError> + Send + 'static,
+    Fut: Future<Output = Result<ControlFlow<<S as Service<Request>>::Response, Request>, BoxError>>,
+{
+    /// Create an `OneShotAsyncCheckpointLayer` from a function that takes a Service Request and returns a `ControlFlow`
+    pub fn new<F>(checkpoint_fn: F) -> Self
+    where
+        F: Fn(Request) -> Fut + Send + Sync + 'static,
+    {
+        Self {
+            checkpoint_fn: Arc::new(Box::pin(checkpoint_fn)),
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<S, Fut, Request> Layer<S> for OneShotAsyncCheckpointLayer<S, Fut, Request>
+where
+    S: Service<Request, Error = BoxError> + Send + 'static,
+    <S as Service<Request>>::Future: Send,
+    Request: Send + 'static,
+    <S as Service<Request>>::Response: Send + 'static,
+    Fut: Future<Output = Result<ControlFlow<<S as Service<Request>>::Response, Request>, BoxError>>,
+{
+    type Service = OneShotAsyncCheckpointService<S, Fut, Request>;
+
+    fn layer(&self, service: S) -> Self::Service {
+        OneShotAsyncCheckpointService {
+            checkpoint_fn: Arc::clone(&self.checkpoint_fn),
+            inner: Some(service),
+        }
     }
 }
 
@@ -274,6 +396,128 @@ mod async_checkpoint_tests {
                 move |_req| async move { Err(BoxError::from(expected_error)) },
             )
             .layer(router_service);
+
+        let request = ExecutionRequest::fake_builder().build();
+
+        let actual_error = service_stack
+            .oneshot(request)
+            .await
+            .map(|_| unreachable!())
+            .unwrap_err()
+            .to_string();
+
+        assert_eq!(actual_error, expected_error)
+    }
+
+    #[tokio::test]
+    async fn test_service_builder_oneshot() {
+        let expected_label = "from_mock_service";
+
+        let mut execution_service = MockExecutionService::new();
+        execution_service
+            .expect_call()
+            .times(1)
+            .returning(move |req: ExecutionRequest| {
+                Ok(ExecutionResponse::fake_builder()
+                    .label(expected_label.to_string())
+                    .context(req.context)
+                    .build()
+                    .unwrap())
+            });
+
+        let service_stack = ServiceBuilder::new()
+            .oneshot_checkpoint_async(|req: ExecutionRequest| async {
+                Ok(ControlFlow::Continue(req))
+            })
+            .service(execution_service);
+
+        let request = ExecutionRequest::fake_builder().build();
+
+        let actual_label = service_stack
+            .oneshot(request)
+            .await
+            .unwrap()
+            .next_response()
+            .await
+            .unwrap()
+            .label
+            .unwrap();
+
+        assert_eq!(actual_label, expected_label)
+    }
+
+    #[tokio::test]
+    async fn test_continue_oneshot() {
+        let expected_label = "from_mock_service";
+        let mut router_service = MockExecutionService::new();
+        router_service
+            .expect_call()
+            .times(1)
+            .returning(move |_req| {
+                Ok(ExecutionResponse::fake_builder()
+                    .label(expected_label.to_string())
+                    .build()
+                    .unwrap())
+            });
+
+        let service_stack =
+            OneShotAsyncCheckpointLayer::new(|req| async { Ok(ControlFlow::Continue(req)) })
+                .layer(router_service);
+
+        let request = ExecutionRequest::fake_builder().build();
+
+        let actual_label = service_stack
+            .oneshot(request)
+            .await
+            .unwrap()
+            .next_response()
+            .await
+            .unwrap()
+            .label
+            .unwrap();
+
+        assert_eq!(actual_label, expected_label)
+    }
+
+    #[tokio::test]
+    async fn test_return_oneshot() {
+        let expected_label = "returned_before_mock_service";
+        let router_service = MockExecutionService::new();
+
+        let service_stack = OneShotAsyncCheckpointLayer::new(|_req| async {
+            Ok(ControlFlow::Break(
+                ExecutionResponse::fake_builder()
+                    .label("returned_before_mock_service".to_string())
+                    .build()
+                    .unwrap(),
+            ))
+        })
+        .layer(router_service);
+
+        let request = ExecutionRequest::fake_builder().build();
+
+        let actual_label = service_stack
+            .oneshot(request)
+            .await
+            .unwrap()
+            .next_response()
+            .await
+            .unwrap()
+            .label
+            .unwrap();
+
+        assert_eq!(actual_label, expected_label)
+    }
+
+    #[tokio::test]
+    async fn test_error_oneshot() {
+        let expected_error = "checkpoint_error";
+        let router_service = MockExecutionService::new();
+
+        let service_stack = OneShotAsyncCheckpointLayer::new(move |_req| async move {
+            Err(BoxError::from(expected_error))
+        })
+        .layer(router_service);
 
         let request = ExecutionRequest::fake_builder().build();
 
