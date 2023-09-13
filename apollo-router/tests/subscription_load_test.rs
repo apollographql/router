e496f747ca9ffbbb7e6e82814c86db63ef897db6
Coenen Benjamin <benjamin.coenen@hotmail.com>
2023-09-13 11:28:23 +0200

    do not run test_subscription if graph ref and apollo key is not set for tests (#3813)
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- apollo-router/tests/subscription_load_test.rs
+++ apollo-router/tests/subscription_load_test.rs
@@ -16,18 +16,20 @@ const UNFEDERATED_SUB_QUERY: &str = r#"subscription {  userWasCreated { name use
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_subscription() -> Result<(), BoxError> {
-    let mut router = create_router(SUBSCRIPTION_CONFIG).await?;
-    router.start().await;
-    router.assert_started().await;
+    if std::env::var("TEST_APOLLO_KEY").is_ok() && std::env::var("TEST_APOLLO_GRAPH_REF").is_ok() {
+        let mut router = create_router(SUBSCRIPTION_CONFIG).await?;
+        router.start().await;
+        router.assert_started().await;
 
-    let (_, response) = router.run_subscription(SUB_QUERY).await;
-    assert!(response.status().is_success());
+        let (_, response) = router.run_subscription(SUB_QUERY).await;
+        assert!(response.status().is_success());
 
-    let mut stream = response.bytes_stream();
-    while let Some(chunk) = stream.next().await {
-        let chunk = chunk.unwrap();
-        assert!(chunk.starts_with(b"\r\n--graphql\r\ncontent-type: application/json\r\n\r\n"));
-        assert!(chunk.ends_with(b"\r\n--graphql--\r\n"));
+        let mut stream = response.bytes_stream();
+        while let Some(chunk) = stream.next().await {
+            let chunk = chunk.unwrap();
+            assert!(chunk.starts_with(b"\r\n--graphql\r\ncontent-type: application/json\r\n\r\n"));
+            assert!(chunk.ends_with(b"\r\n--graphql--\r\n"));
+        }
     }
 
     Ok(())
