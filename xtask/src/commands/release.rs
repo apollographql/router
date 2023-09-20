8476354ecc3724657ef3102c3fb01e19fb826264
bryn <bryn@apollographql.com>
2023-09-20 12:55:59 +0100

    Improve error messages

--- xtask/src/commands/release.rs
+++ xtask/src/commands/release.rs
@@ -435,7 +435,13 @@ impl PreVerify {
         // Get the git tag name as a string
         let tags_output = std::process::Command::new("git")
             .args(["describe", "--tags", "--exact-match"])
-            .output()?
+            .output()
+            .map_err(|e| {
+                anyhow!(
+                    "failed to execute 'git describe --tags --exact-match': {}",
+                    e
+                )
+            })?
             .stdout;
         let tags_raw = String::from_utf8_lossy(&tags_output);
         let tags_list = tags_raw
@@ -445,11 +451,13 @@ impl PreVerify {
 
         // If the tags contains the version, then we're good
         if tags_list.is_empty() {
-            return Err(anyhow!("current git tree is not tagged"));
+            return Err(anyhow!(
+                "release cannot be performed because current git tree is not tagged"
+            ));
         }
         if !tags_list.contains(&version.as_str()) {
             return Err(anyhow!(
-                "the list of tags {{{}}} does not contain the version {}",
+                "the git tree tags {{{}}} does not contain the version {} from the Cargo.toml",
                 tags_list.join(", "),
                 version
             ));
