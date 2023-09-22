70cb943ed83555638e6cced2cd378e6f54299b2d
David Glasser <glasser@davidglasser.net>
2023-09-22 08:17:55 -0700

    persisted queries: improve Uplink failover and error handling (#3863)
    If an Uplink request fails, the `stream_from_uplink` function
    automatically tries the next configured Uplink endpoint. However, in the
    case of the persisted queries feature, the Uplink response contains an
    URL located in the same cloud provider as the Uplink endpoint (which is
    used to fetch the full PQ list chunk). Before this PR, the PQ code
    fetched this second URL "outside" of `stream_from_uplink`, so an error
    downloading this file would *not* result in re-trying the Uplink request
    itself on the next endpoint.
    
    This PR factors out the body of `stream_from_uplink` into
    `stream_from_uplink_transforming_new_response`, which takes an async
    function that is called on any `UplinkResponse::New` to transform the
    response. (`stream_from_uplink` passes the identity function here, so
    its API is not affected.) If this function returns `Err`, the uplink
    code moves on to the next endpoint URL, just like if the initial Uplink
    fetch had failed. The PQ layer now fetches the PQ manifest chunk bodies
    inside this async function instead of by mapping the uplink stream.
    
    This means that if our GCP Uplink server is up but the GCS servers that
    serve manifests from GCP are down, we will fail over to the AWS Uplink
    server instead of just failing to fetch PQs.
    
    Additionally, the response from Uplink can specify multiple valid URLs
    for each chunk. (Uplink does not currently do this, but the protocol
    allows for it.) Before this PR, Router would only look at the first URL
    listed; after this PR, it tries each URL in order until finding one that
    does not fail.
    
    Additionally, before this PR, any errors fetching PQs from Uplink or GCS
    after a successful startup were ignored. (They would be sent on the
    `ready_sender` channel whose receiver is dropped, which could lead to a
    `debug`-level logged message about how the channel wasn't open, but they
    were not *directly* logged.) After this PR, we don't try to send these
    errors on the channel after the channel was used once; instead, we log
    them at the `error` level (similarly to how schema and license uplink
    errors work). Note that this only occurs if Router fails to fetch PQs
    from all configured Uplink endpoints; a failure to fetch from a single
    endpoint is still only logged at the `debug` level.
    
    Co-authored-by: Jeremy Lempereur <jeremy.lempereur@iomentum.com>
    
    ---------
    
    Co-authored-by: o0Ignition0o <jeremy.lempereur@gmail.com>
    Co-authored-by: Jeremy Lempereur <jeremy.lempereur@iomentum.com>

---
+++ .changesets/fix_persisted_queries_failover_and_error_handling.md
@@ -0,0 +1,5 @@
+### Improve multi-cloud failover and error handling for Persisted Queries
+
+Improves the resilience of the Persisted Queries feature to Uplink outages, and makes errors fetching Persisted Query Manifests from Uplink more visible.
+
+By [@glasser](https://github.com/glasser) in https://github.com/apollographql/router/pull/3863
--- apollo-router/src/test_harness/mocks/persisted_queries.rs
+++ apollo-router/src/test_harness/mocks/persisted_queries.rs
@@ -32,12 +32,20 @@ pub async fn mock_pq_uplink_with_delay(
     manifest: &HashMap<String, String>,
     delay: Duration,
 ) -> (UplinkMockGuard, UplinkConfig) {
-    do_mock_pq_uplink(manifest, Some(delay)).await
+    let (guard, url) = mock_pq_uplink_one_endpoint(manifest, Some(delay)).await;
+    (
+        guard,
+        UplinkConfig::for_tests(Endpoints::fallback(vec![url])),
+    )
 }
 
 /// Mocks an uplink server with a persisted query list containing operations passed to this function.
 pub async fn mock_pq_uplink(manifest: &HashMap<String, String>) -> (UplinkMockGuard, UplinkConfig) {
-    do_mock_pq_uplink(manifest, None).await
+    let (guard, url) = mock_pq_uplink_one_endpoint(manifest, None).await;
+    (
+        guard,
+        UplinkConfig::for_tests(Endpoints::fallback(vec![url])),
+    )
 }
 
 /// Guards for the uplink and GCS mock servers, dropping these structs shuts down the server.
@@ -52,10 +60,12 @@ struct Operation {
     body: String,
 }
 
-async fn do_mock_pq_uplink(
+/// Mocks an uplink server; returns a single Url rather than a full UplinkConfig, so you
+/// can combine it with another one to test failover.
+pub async fn mock_pq_uplink_one_endpoint(
     manifest: &HashMap<String, String>,
     delay: Option<Duration>,
-) -> (UplinkMockGuard, UplinkConfig) {
+) -> (UplinkMockGuard, Url) {
     let operations: Vec<Operation> = manifest
         // clone the manifest so the caller can still make assertions about it
         .clone()
@@ -80,7 +90,7 @@ async fn do_mock_pq_uplink(
 
     let mock_uplink_server = MockServer::start().await;
 
-    let mut gcs_response = ResponseTemplate::new(200).set_body_json(json!({
+    let mut uplink_response = ResponseTemplate::new(200).set_body_json(json!({
           "data": {
             "persistedQueries": {
               "__typename": "PersistedQueriesResult",
@@ -90,7 +100,8 @@ async fn do_mock_pq_uplink(
                 {
                   "id": "graph-id/889406a1-b4f8-44df-a499-6c1e3c1bea09/ec8ae3ae3eb00c738031dbe81603489b5d24fbf58f15bdeec1587282ee4e6eea",
                   "urls": [
-                    mock_gcs_server_uri
+                    "https://a.broken.gcs.url.that.will.get.fetched.and.skipped.unknown/",
+                    mock_gcs_server_uri,
                   ]
                 }
               ]
@@ -99,11 +110,11 @@ async fn do_mock_pq_uplink(
         }));
 
     if let Some(delay) = delay {
-        gcs_response = gcs_response.set_delay(delay);
+        uplink_response = uplink_response.set_delay(delay);
     }
 
     Mock::given(method("POST"))
-        .respond_with(gcs_response)
+        .respond_with(uplink_response)
         .mount(&mock_uplink_server)
         .await;
 
@@ -113,6 +124,37 @@ async fn do_mock_pq_uplink(
             _uplink_mock_guard: mock_uplink_server,
             _gcs_mock_guard: mock_gcs_server,
         },
-        UplinkConfig::for_tests(Endpoints::fallback(vec![url])),
+        url,
     )
+}
+
+/// Mocks an uplink server which returns bad GCS URLs.
+pub async fn mock_pq_uplink_bad_gcs() -> (MockServer, Url) {
+    let mock_uplink_server = MockServer::start().await;
+
+    let  uplink_response = ResponseTemplate::new(200).set_body_json(json!({
+          "data": {
+            "persistedQueries": {
+              "__typename": "PersistedQueriesResult",
+              "id": "889406d7-b4f8-44df-a499-6c1e3c1bea09:1",
+              "minDelaySeconds": 60,
+              "chunks": [
+                {
+                  "id": "graph-id/889406a1-b4f8-44df-a499-6c1e3c1bea09/ec8ae3ae3eb00c738031dbe81603489b5d24fbf58f15bdeec1587282ee4e6eea",
+                  "urls": [
+                    "https://definitely.not.gcs.unknown"
+                  ]
+                }
+              ]
+            }
+          }
+        }));
+
+    Mock::given(method("POST"))
+        .respond_with(uplink_response)
+        .mount(&mock_uplink_server)
+        .await;
+
+    let url = mock_uplink_server.uri().parse().unwrap();
+    (mock_uplink_server, url)
 }
