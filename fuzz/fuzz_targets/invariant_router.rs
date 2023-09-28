bd3cc02e668bcdd41c102c55be8717ab6222ba2f
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2023-09-28 16:11:35 +0200

    add invariant fuzz target for the router
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- fuzz/fuzz_targets/invariant_router.rs
+++ fuzz/fuzz_targets/invariant_router.rs
@@ -1,20 +1,17 @@
-//! Fuzz target to generate random invalid query and detect if the gateway and the router are both throwing errors
+//! Fuzz target to generate random invalid body and query to the router and check it doesn't panic
 #![no_main]
 
 use std::char::REPLACEMENT_CHARACTER;
-use std::ffi::OsString;
+use std::process::Command;
+use std::process::Stdio;
 use std::sync::atomic::AtomicBool;
 use std::time::Duration;
 
-use apollo_router::Configuration;
-use apollo_router::ConfigurationSource;
-use apollo_router::Executable;
-use apollo_router::Opt;
-use apollo_router::SchemaSource;
-use clap::Parser;
 use libfuzzer_sys::fuzz_target;
 use serde_json::json;
 
+const ROUTER_CMD: &str = "/home/bnj/rust/router/target/debug/router";
+const ROUTER_CONFIG_PATH: &str = "./examples/graphql/local.graphql";
 const ROUTER_URL: &str = "http://localhost:4000";
 static ROUTER_INIT: AtomicBool = AtomicBool::new(false);
 
@@ -26,31 +23,40 @@ fuzz_target!(|data: &[u8]| {
     if !ROUTER_INIT.swap(true, std::sync::atomic::Ordering::Relaxed) {
         log::info!("first {:?}", std::env::args_os());
 
-        std::thread::spawn(|| {
-            let mut builder = tokio::runtime::Builder::new_multi_thread();
-            builder.enable_all();
-            let runtime = builder.build()?;
-            let schema_sdl = include_str!("../../examples/graphql/local.graphql").to_string();
-            runtime.block_on(async move {
-                let configuration = Configuration::default();
-                let cli_args = Opt::parse_from(Vec::<OsString>::new());
-                log::info!("coucou");
-                Executable::builder()
-                    .config(ConfigurationSource::Static(Box::new(configuration)))
-                    .schema(SchemaSource::Static { schema_sdl })
-                    .cli_args(cli_args)
-                    .start()
-                    .await
-            })
-        });
-        std::thread::sleep(Duration::from_secs(1));
+        let mut cmd = Command::new(ROUTER_CMD)
+            .arg("--supergraph")
+            .arg(ROUTER_CONFIG_PATH)
+            .arg("--hot-reload")
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .expect("cannot launch the router");
+
+        std::thread::sleep(Duration::from_secs(3));
+        if let Ok(exit_status) = cmd.try_wait() {
+            match exit_status {
+                Some(exit_status) => panic!("the router exited with exit code : {}", exit_status),
+                None => panic!("the router can't start"),
+            }
+        }
     }
 
+    let query = data.to_vec();
+    let http_client = reqwest::blocking::Client::new();
+    let router_response = http_client.post(ROUTER_URL).body(query.clone()).send();
+    if let Err(err) = router_response {
+        eprintln!("bad body: {query:?}");
+        panic!("{}", err);
+    }
     let query = String::from_utf8_lossy(data).replace(REPLACEMENT_CHARACTER, "");
     let http_client = reqwest::blocking::Client::new();
-    let _router_response = http_client
+    let router_response = http_client
         .post(ROUTER_URL)
-        .json(&json!({ "query": query }))
-        .send()
-        .unwrap();
+        .json(&json!({"query": query}))
+        .send();
+
+    if let Err(err) = router_response {
+        eprintln!("bad query: {query:?}");
+        panic!("{}", err);
+    }
 });
