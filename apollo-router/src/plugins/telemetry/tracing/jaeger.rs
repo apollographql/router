ec6d2dd603ca7a662d00f03529cfed2e0b2112e8
Bryn Cooke <BrynCooke@gmail.com>
2023-10-03 17:32:51 +0100

    Add `enabled` field for telemetry exporters (#3952)
    Adds an enabled field to all the telemetry endpoint. 
    We can now move away from use of Optional in the telemetry config structures.
    
    Part of #3226
    
    <!-- start metadata -->
    ---
    
    **Checklist**
    
    Complete the checklist (and note appropriate exceptions) before the PR
    is marked ready-for-review.
    
    - [ ] Changes are compatible[^1]
    - [ ] Documentation[^2] completed
    - [ ] Performance impact assessed and acceptable
    - Tests added and passing[^3]
        - [ ] Unit Tests
        - [ ] Integration Tests
        - [ ] Manual Tests
    
    **Exceptions**
    
    *Note any exceptions here*
    
    **Notes**
    
    [^1]: It may be appropriate to bring upcoming changes to the attention
    of other (impacted) groups. Please endeavour to do this before seeking
    PR approval. The mechanism for doing this will vary considerably, so use
    your judgement as to how and when to do this.
    [^2]: Configuration is an important part of many changes. Where
    applicable please try to document configuration examples.
    [^3]: Tick whichever testing boxes are applicable. If you are adding
    Manual Tests, please document the manual testing (extensively) in the
    Exceptions.
    
    ---------
    
    Co-authored-by: bryn <bryn@apollographql.com>

--- apollo-router/src/plugins/telemetry/tracing/jaeger.rs
+++ apollo-router/src/plugins/telemetry/tracing/jaeger.rs
@@ -19,13 +19,17 @@ use crate::plugins::telemetry::tracing::SpanProcessorExt;
 use crate::plugins::telemetry::tracing::TracingConfigurator;
 
 lazy_static! {
-    static ref DEFAULT_ENDPOINT: Uri = Uri::from_static("http://localhost:14268/api/traces");
+    static ref DEFAULT_ENDPOINT: Uri = Uri::from_static("http://127.0.0.1:14268/api/traces");
 }
 #[derive(Debug, Clone, Deserialize, JsonSchema)]
 #[serde(deny_unknown_fields, untagged)]
 pub(crate) enum Config {
     Agent {
+        /// Enable Jaeger
+        enabled: bool,
+
         /// Agent configuration
+        #[serde(default)]
         agent: AgentConfig,
 
         /// Batch processor configuration
@@ -33,7 +37,11 @@ pub(crate) enum Config {
         batch_processor: BatchProcessorConfig,
     },
     Collector {
+        /// Enable Jaeger
+        enabled: bool,
+
         /// Collector configuration
+        #[serde(default)]
         collector: CollectorConfig,
 
         /// Batch processor configuration
@@ -42,15 +50,15 @@ pub(crate) enum Config {
     },
 }
 
-#[derive(Debug, Clone, Deserialize, JsonSchema)]
-#[serde(deny_unknown_fields)]
+#[derive(Debug, Clone, Deserialize, JsonSchema, Default)]
+#[serde(deny_unknown_fields, default)]
 pub(crate) struct AgentConfig {
     /// The endpoint to send to
     endpoint: SocketEndpoint,
 }
 
-#[derive(Debug, Clone, Deserialize, JsonSchema)]
-#[serde(deny_unknown_fields)]
+#[derive(Debug, Clone, Deserialize, JsonSchema, Default)]
+#[serde(deny_unknown_fields, default)]
 pub(crate) struct CollectorConfig {
     /// The endpoint to send reports to
     endpoint: UriEndpoint,
@@ -64,10 +72,11 @@ impl TracingConfigurator for Config {
     fn apply(&self, builder: Builder, trace_config: &Trace) -> Result<Builder, BoxError> {
         match &self {
             Config::Agent {
+                enabled,
                 agent,
                 batch_processor,
-            } => {
-                tracing::info!("Configuring Jaeger tracing: {}", batch_processor);
+            } if *enabled => {
+                tracing::info!("Configuring Jaeger tracing: {} (agent)", batch_processor);
                 let exporter = opentelemetry_jaeger::new_agent_pipeline()
                     .with_trace_config(trace_config.into())
                     .with_service_name(trace_config.service_name.clone())
@@ -81,10 +90,15 @@ impl TracingConfigurator for Config {
                 ))
             }
             Config::Collector {
+                enabled,
                 collector,
                 batch_processor,
-            } => {
-                tracing::info!("Configuring Jaeger tracing: {}", batch_processor);
+            } if *enabled => {
+                tracing::info!(
+                    "Configuring Jaeger tracing: {} (collector)",
+                    batch_processor
+                );
+
                 let exporter = opentelemetry_jaeger::new_collector_pipeline()
                     .with_trace_config(trace_config.into())
                     .with_service_name(trace_config.service_name.clone())
@@ -107,6 +121,7 @@ impl TracingConfigurator for Config {
                         .build(),
                 ))
             }
+            _ => Ok(builder),
         }
     }
 }
