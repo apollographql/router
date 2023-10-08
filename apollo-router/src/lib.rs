d2febf995b9c58111ed4d4db2490ad8047806a84
Stephen Spalding <sspalding@netflix.com>
2023-10-07 23:31:14 -0700

    expose for external subscription impl

--- apollo-router/src/lib.rs
+++ apollo-router/src/lib.rs
@@ -83,7 +83,7 @@ pub use crate::configuration::ListenAddr;
 pub use crate::context::Context;
 pub use crate::executable::main;
 pub use crate::executable::Executable;
-pub use crate::notification::Notify;
+pub use crate::notification::{HandleStream, Notify, NotifyError};
 pub use crate::router::ApolloRouterError;
 pub use crate::router::ConfigurationSource;
 pub use crate::router::LicenseSource;
