83b6c2c9c892085d3922766faf4fde769cece230
Geoffroy Couprie <apollo@geoffroycouprie.com>
2023-10-09 15:23:12 +0200

    Fix requires selection in arrays (#3975)
    Fix  #3972
    
    When a field has a `@requires` annotation that selects an array, and
    some fields are missing in that array or some of the elements are null,
    the router would short circuit the selection and remove the entire
    array. This relaxes the condition to allow nulls in the selected array
    
    <!-- start metadata -->
    ---
    
    **Checklist**
    
    Complete the checklist (and note appropriate exceptions) before the PR
    is marked ready-for-review.
    
    - [ ] Changes are compatible[^1]
    - [ ] Documentation[^2] completed
    - [ ] Performance impact assessed and acceptable
    - Tests added and passing[^3]
        - [ ] Unit Tests
        - [ ] Integration Tests
        - [ ] Manual Tests
    
    **Exceptions**
    
    *Note any exceptions here*
    
    **Notes**
    
    [^1]: It may be appropriate to bring upcoming changes to the attention
    of other (impacted) groups. Please endeavour to do this before seeking
    PR approval. The mechanism for doing this will vary considerably, so use
    your judgement as to how and when to do this.
    [^2]: Configuration is an important part of many changes. Where
    applicable please try to document configuration examples.
    [^3]: Tick whichever testing boxes are applicable. If you are adding
    Manual Tests, please document the manual testing (extensively) in the
    Exceptions.

--- apollo-router/src/query_planner/tests.rs
+++ apollo-router/src/query_planner/tests.rs
@@ -7,6 +7,7 @@ use futures::StreamExt;
 use http::Method;
 use router_bridge::planner::UsageReporting;
 use serde_json_bytes::json;
+use tower::ServiceExt;
 
 use super::DeferredNode;
 use super::Depends;
@@ -23,12 +24,15 @@ use crate::query_planner;
 use crate::query_planner::fetch::FetchNode;
 use crate::request;
 use crate::services::subgraph_service::MakeSubgraphService;
+use crate::services::supergraph;
 use crate::services::SubgraphResponse;
 use crate::services::SubgraphServiceFactory;
 use crate::spec::Query;
 use crate::spec::Schema;
 use crate::Configuration;
 use crate::Context;
+use crate::MockedSubgraphs;
+use crate::TestHarness;
 
 macro_rules! test_query_plan {
     () => {
@@ -643,4 +647,341 @@ async fn dependent_mutations() {
             None,
         )
         .await;
+}
+
+#[tokio::test]
+async fn alias_renaming() {
+    let schema = r#"schema
+      @link(url: "https://specs.apollo.dev/link/v1.0")
+      @link(url: "https://specs.apollo.dev/join/v0.3", for: EXECUTION)
+    {
+      query: Query
+    }
+    
+    directive @join__enumValue(graph: join__Graph!) repeatable on ENUM_VALUE
+    
+    directive @join__field(graph: join__Graph, requires: join__FieldSet, provides: join__FieldSet, type: String, external: Boolean, override: String, usedOverridden: Boolean) repeatable on FIELD_DEFINITION | INPUT_FIELD_DEFINITION
+    
+    directive @join__graph(name: String!, url: String!) on ENUM_VALUE
+    
+    directive @join__implements(graph: join__Graph!, interface: String!) repeatable on OBJECT | INTERFACE
+    
+    directive @join__type(graph: join__Graph!, key: join__FieldSet, extension: Boolean! = false, resolvable: Boolean! = true, isInterfaceObject: Boolean! = false) repeatable on OBJECT | INTERFACE | UNION | ENUM | INPUT_OBJECT | SCALAR
+    
+    directive @join__unionMember(graph: join__Graph!, member: String!) repeatable on UNION
+    
+    directive @link(url: String, as: String, for: link__Purpose, import: [link__Import]) repeatable on SCHEMA
+    
+    interface I
+      @join__type(graph: S1)
+      @join__type(graph: S2)
+    {
+      id: String!
+    }
+    
+    scalar join__FieldSet
+    
+    enum join__Graph {
+      S1 @join__graph(name: "S1", url: "http://localhost/s1")
+      S2 @join__graph(name: "S2", url: "http://localhost/s2")
+    }
+    
+    scalar link__Import
+    
+    enum link__Purpose {
+      """
+      `SECURITY` features provide metadata necessary to securely resolve fields.
+      """
+      SECURITY
+    
+      """
+      `EXECUTION` features provide metadata necessary for operation execution.
+      """
+      EXECUTION
+    }
+    
+    type Query
+      @join__type(graph: S1)
+      @join__type(graph: S2)
+    {
+      testQuery(id: String!): I @join__field(graph: S1)
+    }
+    
+    type T1 implements I
+      @join__implements(graph: S1, interface: "I")
+      @join__implements(graph: S2, interface: "I")
+      @join__type(graph: S1, key: "id", resolvable: false)
+      @join__type(graph: S2, key: "id")
+    {
+      id: String!
+      foo: Test @join__field(graph: S2)
+    }
+    
+    type T2 implements I
+      @join__implements(graph: S1, interface: "I")
+      @join__implements(graph: S2, interface: "I")
+      @join__type(graph: S1, key: "id", resolvable: false)
+      @join__type(graph: S2, key: "id")
+    {
+      id: String!
+      bar: Test @join__field(graph: S2)
+    }
+    
+    type Test
+      @join__type(graph: S2)
+    {
+      field: String!
+    }"#;
+
+    let query = "query test($tId: String!) {
+            testQuery(id: $tId) {
+            ... on T1 {
+            foo {
+                field
+            }
+            }
+            ... on T2 {
+            foo: bar {
+                field
+            }
+            }
+        }
+        }";
+
+    let subgraphs = MockedSubgraphs([
+        ("S1", MockSubgraph::builder().with_json(
+            serde_json::json!{{"query":
+            "query test__S1__0($tId:String!){testQuery(id:$tId){__typename ...on T1{__typename id}...on T2{__typename id}}}",
+            "operationName": "test__S1__0", "variables":{"tId":"1"}}},
+            serde_json::json!{{"data": {
+                "testQuery": {
+                    "__typename": "T1",
+                    "id": "T1",
+                }
+            } }}
+        ).with_json(
+            serde_json::json!{{"query":
+            "query test__S1__0($tId:String!){testQuery(id:$tId){__typename ...on T1{__typename id}...on T2{__typename id}}}",
+            "operationName": "test__S1__0", "variables":{"tId":"2"}}},
+            serde_json::json!{{"data": {
+                "testQuery": {
+                    "__typename": "T2",
+                    "id": "T2",
+                }
+            } }}
+        ).build()),
+        ("S2", MockSubgraph::builder().with_json(
+            serde_json::json!{{"query":
+            "query test__S2__1($representations:[_Any!]!){_entities(representations:$representations){...on T1{foo{field}}...on T2{foo__alias_0:bar{field}}}}",
+            "operationName": "test__S2__1", "variables":{"representations":[{
+                "__typename": "T1",
+                "id": "T1",
+            }]}}},
+            serde_json::json!{{"data": {
+                "_entities": [{
+                    "foo": {
+                        "field": "aaa"
+                    }
+                }]
+            } }}
+        ).with_json(
+            serde_json::json!{{"query":
+            "query test__S2__1($representations:[_Any!]!){_entities(representations:$representations){...on T1{foo{field}}...on T2{foo__alias_0:bar{field}}}}",
+            "operationName": "test__S2__1", "variables":{"representations":[{
+                "__typename": "T2",
+                "id": "T2",
+            }]}}},
+            serde_json::json!{{"data": {
+                "_entities": [{
+                    "foo__alias_0": {
+                        "field": "bbb"
+                    }
+                }]
+            } }}
+        ).build()),
+        ].into_iter().collect());
+
+    let service = TestHarness::builder()
+        .configuration_json(serde_json::json!({"include_subgraph_errors": { "all": true } }))
+        .unwrap()
+        .schema(schema)
+        .extra_plugin(subgraphs)
+        .build_supergraph()
+        .await
+        .unwrap();
+
+    let request = supergraph::Request::fake_builder()
+        .context(Context::new())
+        .query(query)
+        .variables(
+            serde_json_bytes::json! {{ "tId": "1"}}
+                .as_object()
+                .unwrap()
+                .clone(),
+        )
+        .build()
+        .unwrap();
+
+    let mut stream = service.clone().oneshot(request).await.unwrap();
+    let response = stream.next_response().await.unwrap();
+    insta::assert_json_snapshot!(serde_json::to_value(&response).unwrap());
+
+    let request = supergraph::Request::fake_builder()
+        .context(Context::new())
+        .query(query)
+        .variables(
+            serde_json_bytes::json! {{ "tId": "2"}}
+                .as_object()
+                .unwrap()
+                .clone(),
+        )
+        .build()
+        .unwrap();
+
+    let mut stream = service.clone().oneshot(request).await.unwrap();
+    let response = stream.next_response().await.unwrap();
+    insta::assert_json_snapshot!(serde_json::to_value(&response).unwrap());
+}
+
+#[tokio::test]
+async fn missing_fields_in_requires() {
+    let schema = r#"schema
+    @link(url: "https://specs.apollo.dev/link/v1.0")
+    @link(url: "https://specs.apollo.dev/join/v0.3", for: EXECUTION)
+  {
+    query: Query
+  }
+  
+  directive @join__enumValue(graph: join__Graph!) repeatable on ENUM_VALUE
+  
+  directive @join__field(graph: join__Graph, requires: join__FieldSet, provides: join__FieldSet, type: String, external: Boolean, override: String, usedOverridden: Boolean) repeatable on FIELD_DEFINITION | INPUT_FIELD_DEFINITION
+  
+  directive @join__graph(name: String!, url: String!) on ENUM_VALUE
+  
+  directive @join__implements(graph: join__Graph!, interface: String!) repeatable on OBJECT | INTERFACE
+  
+  directive @join__type(graph: join__Graph!, key: join__FieldSet, extension: Boolean! = false, resolvable: Boolean! = true, isInterfaceObject: Boolean! = false) repeatable on OBJECT | INTERFACE | UNION | ENUM | INPUT_OBJECT | SCALAR
+  
+  directive @join__unionMember(graph: join__Graph!, member: String!) repeatable on UNION
+  
+  directive @link(url: String, as: String, for: link__Purpose, import: [link__Import]) repeatable on SCHEMA
+  
+  type Details
+    @join__type(graph: SUB1)
+    @join__type(graph: SUB2)
+  {
+    enabled: Boolean
+  }
+  
+  scalar join__FieldSet
+  
+  enum join__Graph {
+    SUB1 @join__graph(name: "sub1", url: "http://localhost:4002/test")
+    SUB2 @join__graph(name: "sub2", url: "http://localhost:4002/test2")
+  }
+  
+  scalar link__Import
+  
+  enum link__Purpose {
+    """
+    `SECURITY` features provide metadata necessary to securely resolve fields.
+    """
+    SECURITY
+  
+    """
+    `EXECUTION` features provide metadata necessary for operation execution.
+    """
+    EXECUTION
+  }
+  
+  type Query
+    @join__type(graph: SUB1)
+    @join__type(graph: SUB2)
+  {
+    stuff: Stuff @join__field(graph: SUB1)
+  }
+  
+  type Stuff
+    @join__type(graph: SUB1, key: "id")
+    @join__type(graph: SUB2, key: "id", extension: true)
+  {
+    id: ID
+    details: [Details] @join__field(graph: SUB1) @join__field(graph: SUB2, external: true)
+    aDetailsIsEnabled: Boolean @join__field(graph: SUB2, requires: "details { enabled }")
+  }"#;
+
+    let query = "query {
+        stuff {
+          id
+          aDetailsIsEnabled
+        }
+      }";
+
+    let subgraphs = MockedSubgraphs([
+        ("sub1", MockSubgraph::builder().with_json(
+            serde_json::json!{{"query": "{stuff{__typename id details{enabled}}}",}},
+            serde_json::json!{{"data": {
+                "stuff": {
+                  "__typename": "Stuff",
+                  "id": "1",
+                  "details": [{
+                    "enabled": true
+                  },
+                  null,
+                  {
+                    "enabled": false
+                  }]
+                }
+            } }}
+        ).build()),
+        ("sub2", MockSubgraph::builder().with_json(
+            serde_json::json!{{
+                "query": "query($representations:[_Any!]!){_entities(representations:$representations){...on Stuff{aDetailsIsEnabled}}}",
+                "variables":{"representations": [
+                    {
+                        "__typename": "Stuff",
+                        "id": "1",
+                        "details": [
+                            {
+                                "enabled": true
+                            },
+                            null,
+                            {
+                                "enabled": false
+                            }
+                        ]
+                    }
+                ]}}},
+            serde_json::json!{{"data": {
+                "_entities": [{
+                    "aDetailsIsEnabled": true
+                }]
+            } }}
+        ).build()),
+        ].into_iter().collect());
+
+    let service = TestHarness::builder()
+        .configuration_json(serde_json::json!({"include_subgraph_errors": { "all": true } }))
+        .unwrap()
+        .schema(schema)
+        .extra_plugin(subgraphs)
+        .build_supergraph()
+        .await
+        .unwrap();
+
+    let request = supergraph::Request::fake_builder()
+        .context(Context::new())
+        .query(query)
+        .variables(
+            serde_json_bytes::json! {{ "tId": "1"}}
+                .as_object()
+                .unwrap()
+                .clone(),
+        )
+        .build()
+        .unwrap();
+
+    let mut stream = service.clone().oneshot(request).await.unwrap();
+    let response = stream.next_response().await.unwrap();
+    insta::assert_json_snapshot!(serde_json::to_value(&response).unwrap());
 }
