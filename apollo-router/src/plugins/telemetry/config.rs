f7caed84f1fef6e86f5cb7a5c05528b4fe3e694c
bryn <bryn@apollographql.com>
2023-11-07 09:20:27 +0000

    Remove AttributeValue::U128. This was probably added for datadog, but datadog IDs are not just a straight conversion to 128 bit unsigned. Also Otel doesn't support u128.
    As the datadog logic has been fixed and tested this can be removed.

--- apollo-router/src/plugins/telemetry/config.rs
+++ apollo-router/src/plugins/telemetry/config.rs
@@ -451,8 +451,6 @@ pub(crate) enum AttributeValue {
     Bool(bool),
     /// i64 values
     I64(i64),
-    /// u128 values
-    U128(u128),
     /// f64 values
     F64(f64),
     /// String values
@@ -472,7 +470,6 @@ impl std::fmt::Display for AttributeValue {
         match self {
             AttributeValue::Bool(val) => write!(f, "{val}"),
             AttributeValue::I64(val) => write!(f, "{val}"),
-            AttributeValue::U128(val) => write!(f, "{val}"),
             AttributeValue::F64(val) => write!(f, "{val}"),
             AttributeValue::String(val) => write!(f, "{val}"),
             AttributeValue::Array(val) => write!(f, "{val}"),
@@ -489,9 +486,6 @@ impl TryFrom<serde_json::Value> for AttributeValue {
             serde_json::Value::Number(v) if v.is_i64() => {
                 Ok(AttributeValue::I64(v.as_i64().expect("i64 checked")))
             }
-            serde_json::Value::Number(v) if v.is_u64() => Ok(AttributeValue::U128(
-                v.as_u64().expect("u64 checked") as u128,
-            )),
             serde_json::Value::Number(v) if v.is_f64() => {
                 Ok(AttributeValue::F64(v.as_f64().expect("f64 checked")))
             }
@@ -536,7 +530,6 @@ impl From<AttributeValue> for opentelemetry::Value {
         match value {
             AttributeValue::Bool(v) => Value::Bool(v),
             AttributeValue::I64(v) => Value::I64(v),
-            AttributeValue::U128(v) => Value::I64(v as i64),
             AttributeValue::F64(v) => Value::F64(v),
             AttributeValue::String(v) => Value::String(v.into()),
             AttributeValue::Array(v) => Value::Array(v.into()),
