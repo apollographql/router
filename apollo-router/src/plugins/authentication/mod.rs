ac8075aa38d32cd226321f7ec08571f2d42b1673
Lucas Leadbetter <5595530+lleadbet@users.noreply.github.com>
2023-11-15 14:16:09 -0500

    updating per pr feedback

--- apollo-router/src/plugins/authentication/mod.rs
+++ apollo-router/src/plugins/authentication/mod.rs
@@ -90,7 +90,7 @@ enum AuthenticationError<'a> {
 }
 
 const DEFAULT_AUTHENTICATION_NETWORK_TIMEOUT: Duration = Duration::from_secs(15);
-const DEFAULT_AUTHENTICATION_DOWNLOAD_INTERVAL: u32 = 60;
+const DEFAULT_AUTHENTICATION_DOWNLOAD_INTERVAL: Duration = Duration::from_secs(60);
 
 static CLIENT: Lazy<Result<Client, BoxError>> = Lazy::new(|| Ok(Client::new()));
 
@@ -128,9 +128,10 @@ struct JWTConf {
 struct JwksConf {
     /// Retrieve the JWK Set
     url: String,
-    /// Polling interval in seconds for each JWKS endpoint; defaults to 60
-    #[serde(default="default_poll_interval")]
-    poll_interval: u32,
+    /// Polling interval for each JWKS endpoint in human-readable format; defaults to 60s
+    #[serde(deserialize_with = "humantime_serde::deserialize", default = "default_poll_interval")]
+    #[schemars(with = "String", default = "default_poll_interval")]
+    poll_interval: Duration,
     /// Expected issuer for tokens verified by that JWKS
     issuer: Option<String>,
     /// List of accepted algorithms. Possible values are `HS256`, `HS384`, `HS512`, `ES256`, `ES384`, `RS256`, `RS384`, `RS512`, `PS256`, `PS384`, `PS512`, `EdDSA`
@@ -166,7 +167,7 @@ fn default_header_value_prefix() -> String {
     "Bearer".to_string()
 }
 
-fn default_poll_interval() -> u32 {
+fn default_poll_interval() -> Duration {
     DEFAULT_AUTHENTICATION_DOWNLOAD_INTERVAL
 }
 
@@ -388,7 +389,7 @@ impl Plugin for AuthenticationPlugin {
                         .algorithms
                         .as_ref()
                         .map(|algs| algs.iter().cloned().collect()),
-                    poll_interval: Duration::from_secs(jwks_conf.poll_interval.into()),
+                    poll_interval: jwks_conf.poll_interval,
                 });
             }
 
