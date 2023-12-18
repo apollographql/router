647a02d394b7898c30301c84fc08861fcd0457ff
Geoffroy Couprie <apollo@geoffroycouprie.com>
2023-12-18 14:31:41 +0100

    use the TYPENAME constant

--- apollo-router/src/query_planner/selection.rs
+++ apollo-router/src/query_planner/selection.rs
@@ -7,6 +7,7 @@ use crate::json_ext::Object;
 use crate::json_ext::Value;
 use crate::json_ext::ValueExt;
 use crate::spec::Schema;
+use crate::spec::TYPENAME;
 
 /// A selection that is part of a fetch.
 /// Selections are used to propagate data to subgraph fetches.
@@ -60,7 +61,7 @@ pub(crate) fn execute_selection_set<'a>(
     };
 
     current_type = content
-        .get("__typename")
+        .get(TYPENAME)
         .and_then(|v| v.as_str())
         .or(current_type);
 
@@ -87,7 +88,7 @@ pub(crate) fn execute_selection_set<'a>(
 
                 match content.get_key_value(selection_name) {
                     None => {
-                        if name == "__typename" {
+                        if name == TYPENAME {
                             // if the __typename field was missing but we can infer it, fill it
                             if let Some(ty) = current_type {
                                 output.insert(
