apollographql/router

commit b58a67c92d65b00c6c11c246e220dce9e2755de1

Do not record a trace if telemetry is not configured (#2999)
This introduces a `SamplingFilter` that wraps `OpenTelemetryLayer`. The layer has an overhead on every request, because it records data for each span, even if no exporters are set up. The filter handles sampling ahead of the layer, only sending a trace to the layer when it is actually needed, ie when it is sampled, and an exporter was configured.
This also reduces the overhead of sampling, by managing it outside of the OpenTelemetryLayer.

It is configured through a sampling ratio stored in an atomic u64, that is modifed when the telemetry configuration is activated

apollo-router/src/plugins/telemetry/reload.rs
@@ -1,21 +1,33 @@
+use std::sync::atomic::AtomicU64;
+use std::sync::atomic::Ordering;
+
 use anyhow::anyhow;
 use anyhow::Result;
 use once_cell::sync::OnceCell;
 use opentelemetry::metrics::noop::NoopMeterProvider;
 use opentelemetry::sdk::trace::Tracer;
+use opentelemetry::trace::TraceContextExt;
 use opentelemetry::trace::TracerProvider;
+use rand::thread_rng;
+use rand::Rng;
 use tower::BoxError;
+use tracing_core::Subscriber;
 use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::filter::Filtered;
 use tracing_subscriber::fmt::FormatFields;
+use tracing_subscriber::layer::Filter;
 use tracing_subscriber::layer::Layer;
 use tracing_subscriber::layer::Layered;
 use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::reload::Handle;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::Registry;
 
+use super::config::SamplerOption;
 use super::metrics::span_metrics_exporter::SpanMetricsLayer;
+use crate::axum_factory::utils::REQUEST_SPAN_NAME;
 use crate::plugins::telemetry::formatters::filter_metric_events;
 use crate::plugins::telemetry::formatters::text::TextFormatter;
 use crate::plugins::telemetry::formatters::FilteringFormatter;
@@ -25,8 +37,14 @@ use crate::plugins::telemetry::tracing::reload::ReloadTracer;
 
 pub(crate) type LayeredRegistry = Layered<SpanMetricsLayer, Registry>;
 
-type LayeredTracer =
-    Layered<OpenTelemetryLayer<LayeredRegistry, ReloadTracer<Tracer>>, LayeredRegistry>;
+pub(super) type LayeredTracer = Layered<
+    Filtered<
+        OpenTelemetryLayer<LayeredRegistry, ReloadTracer<Tracer>>,
+        SamplingFilter,
+        LayeredRegistry,
+    >,
+    LayeredRegistry,
+>;
 
 // These handles allow hot tracing of layers. They have complex type definitions because tracing has
 // generic types in the layer definition.
@@ -52,11 +70,15 @@ static FMT_LAYER_HANDLE: OnceCell<
     Handle<Box<dyn Layer<LayeredTracer> + Send + Sync>, LayeredTracer>,
 > = OnceCell::new();
 
+pub(super) static SPAN_SAMPLING_RATE: AtomicU64 = AtomicU64::new(0);
+
 pub(crate) fn init_telemetry(log_level: &str) -> Result<()> {
     let hot_tracer = ReloadTracer::new(
         opentelemetry::sdk::trace::TracerProvider::default().versioned_tracer("noop", None, None),
     );
-    let opentelemetry_layer = tracing_opentelemetry::layer().with_tracer(hot_tracer.clone());
+    let opentelemetry_layer = tracing_opentelemetry::layer()
+        .with_tracer(hot_tracer.clone())
+        .with_filter(SamplingFilter::new());
 
     // We choose json or plain based on tty
     let fmt = if atty::is(atty::Stream::Stdout) {
@@ -130,20 +152,110 @@ pub(super) fn reload_metrics(layer: MetricsLayer) {
     }
 }
 
-#[allow(clippy::type_complexity)]
-pub(super) fn reload_fmt(
-    layer: Box<
-        dyn Layer<
-                Layered<OpenTelemetryLayer<LayeredRegistry, ReloadTracer<Tracer>>, LayeredRegistry>,
-            > + Send
-            + Sync,
-    >,
-) {
+pub(super) fn reload_fmt(layer: Box<dyn Layer<LayeredTracer> + Send + Sync>) {
     if let Some(handle) = FMT_LAYER_HANDLE.get() {
         handle.reload(layer).expect("fmt layer reload must succeed");
     }
 }
 
+pub(crate) struct SamplingFilter {}
+
+#[allow(dead_code)]
+impl SamplingFilter {
+    pub(crate) fn new() -> Self {
+        Self {}
+    }
+
+    pub(super) fn configure(sampler: &SamplerOption) {
+        let ratio = match sampler {
+            SamplerOption::TraceIdRatioBased(ratio) => {
+                // can't use std::cmp::min because f64 is not Ord
+                if *ratio > 1.0 {
+                    1.0
+                } else {
+                    *ratio
+                }
+            }
+            SamplerOption::Always(s) => match s {
+                super::config::Sampler::AlwaysOn => 1f64,
+                super::config::Sampler::AlwaysOff => 0f64,
+            },
+        };
+
+        SPAN_SAMPLING_RATE.store(f64::to_bits(ratio), Ordering::Relaxed);
+    }
+
+    fn sample(&self) -> bool {
+        let s: f64 = thread_rng().gen_range(0.0..=1.0);
+        s <= f64::from_bits(SPAN_SAMPLING_RATE.load(Ordering::Relaxed))
+    }
+}
+
+impl<S> Filter<S> for SamplingFilter
+where
+    S: Subscriber + for<'span> LookupSpan<'span>,
+{
+    fn enabled(
+        &self,
+        meta: &tracing::Metadata<'_>,
+        cx: &tracing_subscriber::layer::Context<'_, S>,
+    ) -> bool {
+        // we ignore events
+        if !meta.is_span() {
+            return false;
+        }
+
+        // if there's an exsting otel context set by the client request, and it is sampled,
+        // then that trace is sampled
+        let current_otel_context = opentelemetry_api::Context::current();
+        if current_otel_context.span().span_context().is_sampled() {
+            return true;
+        }
+
+        let current_span = cx.current_span();
+        if let Some(spanref) = current_span
+            // the current span, which is the parent of the span that might get enabled here,
+            // exists, but it might have been enabled by another layer like metrics
+            .id()
+            .and_then(|id| cx.span(id))
+        {
+            // if this extension is set, that means the parent span was accepted, and so the
+            // entire trace is accepted
+            let extensions = spanref.extensions();
+            return extensions.get::<SampledSpan>().is_some();
+        }
+
+        // we only make the sampling decision on the root span. If we reach here for any other span,
+        // it means that the parent span was not enabled, so we should not enable this span either
+        if meta.name() != REQUEST_SPAN_NAME {
+            return false;
+        }
+
+        // - there's no parent span (it's the root), so we make the sampling decision
+        self.sample()
+    }
+
+    fn on_new_span(
+        &self,
+        _attrs: &tracing_core::span::Attributes<'_>,
+        id: &tracing_core::span::Id,
+        ctx: tracing_subscriber::layer::Context<'_, S>,
+    ) {
+        let span = ctx.span(id).expect("Span not found, this is a bug");
+        let mut extensions = span.extensions_mut();
+        if extensions.get_mut::<SampledSpan>().is_none() {
+            extensions.insert(SampledSpan);
+        }
+    }
+
+    fn on_close(&self, id: tracing_core::span::Id, ctx: tracing_subscriber::layer::Context<'_, S>) {
+        let span = ctx.span(&id).expect("Span not found, this is a bug");
+        let mut extensions = span.extensions_mut();
+        extensions.remove::<SampledSpan>();
+    }
+}
+
+struct SampledSpan;
 /// prevents span fields from being formatted to a string when writing logs
 pub(crate) struct NullFieldFormatter;
 

apollo-router/Cargo.toml
@@ -134,6 +134,7 @@ once_cell = "1.18.0"
 # https://github.com/apollographql/router/pull/1509.  A comment which exists
 # there (and on `tracing` packages below) should be updated should this change.
 opentelemetry = { version = "0.19.0", features = ["rt-tokio", "metrics"] }
+opentelemetry_api = "0.19.0"
 opentelemetry-datadog = { version = "0.7.0", features = ["reqwest-client"] }
 opentelemetry-http = "0.8.0"
 opentelemetry-jaeger = { version = "0.18.0", features = [

apollo-router/src/plugins/telemetry/mod.rs
@@ -48,7 +48,6 @@ use tokio::runtime::Handle;
 use tower::BoxError;
 use tower::ServiceBuilder;
 use tower::ServiceExt;
-use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_opentelemetry::OpenTelemetrySpanExt;
 use tracing_subscriber::fmt::format::JsonFields;
 use tracing_subscriber::Layer;
@@ -60,17 +59,19 @@ use self::apollo::SingleReport;
 use self::apollo_exporter::proto;
 use self::apollo_exporter::Sender;
 use self::config::Conf;
+use self::config::Sampler;
+use self::config::SamplerOption;
 use self::formatters::text::TextFormatter;
 use self::metrics::apollo::studio::SingleTypeStat;
 use self::metrics::AttributesForwardConf;
 use self::metrics::MetricsAttributesConf;
 use self::reload::reload_fmt;
 use self::reload::reload_metrics;
-use self::reload::LayeredRegistry;
+use self::reload::LayeredTracer;
 use self::reload::NullFieldFormatter;
+use self::reload::SamplingFilter;
 use self::reload::OPENTELEMETRY_TRACER_HANDLE;
 use self::tracing::apollo_telemetry::APOLLO_PRIVATE_DURATION_NS;
-use self::tracing::reload::ReloadTracer;
 use crate::axum_factory::utils::REQUEST_SPAN_NAME;
 use crate::context::OPERATION_NAME;
 use crate::layers::ServiceBuilderExt;
@@ -104,6 +105,7 @@ use crate::plugins::telemetry::utils::TracingUtils;
 use crate::query_planner::OperationKind;
 use crate::register_plugin;
 use crate::router_factory::Endpoint;
+use crate::services::apollo_key;
 use crate::services::execution;
 use crate::services::router;
 use crate::services::subgraph;
@@ -156,6 +158,7 @@ pub(crate) struct Telemetry {
     custom_endpoints: MultiMap<ListenAddr, Endpoint>,
     apollo_metrics_sender: apollo_exporter::Sender,
     field_level_instrumentation_ratio: f64,
+    sampling_filter_ratio: SamplerOption,
 
     tracer_provider: Option<opentelemetry::sdk::trace::TracerProvider>,
     meter_provider: AggregateMeterProvider,
@@ -241,14 +244,16 @@ impl Plugin for Telemetry {
             config.calculate_field_level_instrumentation_ratio()?;
         let mut metrics_builder = Self::create_metrics_builder(&config)?;
         let meter_provider = metrics_builder.meter_provider();
+        let (sampling_filter_ratio, tracer_provider) = Self::create_tracer_provider(&config)?;
         Ok(Telemetry {
             custom_endpoints: metrics_builder.custom_endpoints(),
             metrics_exporters: metrics_builder.exporters(),
             metrics: BasicMetrics::new(&meter_provider),
             apollo_metrics_sender: metrics_builder.apollo_metrics_provider(),
             field_level_instrumentation_ratio,
-            tracer_provider: Some(Self::create_tracer_provider(&config)?),
+            tracer_provider: Some(tracer_provider),
             meter_provider,
+            sampling_filter_ratio,
             config: Arc::new(config),
         })
     }
@@ -541,6 +546,8 @@ impl Telemetry {
         // Only apply things if we were executing in the context of a vanilla the Apollo executable.
         // Users that are rolling their own routers will need to set up telemetry themselves.
         if let Some(hot_tracer) = OPENTELEMETRY_TRACER_HANDLE.get() {
+            SamplingFilter::configure(&self.sampling_filter_ratio);
+
             // The reason that this has to happen here is that we are interacting with global state.
             // If we do this logic during plugin init then if a subsequent plugin fails to init then we
             // will already have set the new tracer provider and we will be in an inconsistent state.
@@ -612,20 +619,41 @@ impl Telemetry {
 
     fn create_tracer_provider(
         config: &config::Conf,
-    ) -> Result<opentelemetry::sdk::trace::TracerProvider, BoxError> {
+    ) -> Result<(SamplerOption, opentelemetry::sdk::trace::TracerProvider), BoxError> {
         let tracing_config = config.tracing.clone().unwrap_or_default();
-        let trace_config = &tracing_config.trace_config.unwrap_or_default();
-        let mut builder =
-            opentelemetry::sdk::trace::TracerProvider::builder().with_config(trace_config.into());
+        let mut trace_config = tracing_config.trace_config.unwrap_or_default();
+        let mut sampler = trace_config.sampler;
+        // set it to AlwaysOn: it is now done in the SamplingFilter, so whatever is sent to an exporter
+        // should be accepted
+        trace_config.sampler = SamplerOption::Always(Sampler::AlwaysOn);
 
-        builder = setup_tracing(builder, &tracing_config.jaeger, trace_config)?;
-        builder = setup_tracing(builder, &tracing_config.zipkin, trace_config)?;
-        builder = setup_tracing(builder, &tracing_config.datadog, trace_config)?;
-        builder = setup_tracing(builder, &tracing_config.otlp, trace_config)?;
-        builder = setup_tracing(builder, &config.apollo, trace_config)?;
+        // if APOLLO_KEY was set, the Studio exporter must be active
+        let apollo_config = if config.apollo.is_none() && apollo_key().is_some() {
+            Some(Default::default())
+        } else {
+            config.apollo.clone()
+        };
+
+        let mut builder = opentelemetry::sdk::trace::TracerProvider::builder()
+            .with_config((&trace_config).into());
+
+        builder = setup_tracing(builder, &tracing_config.jaeger, &trace_config)?;
+        builder = setup_tracing(builder, &tracing_config.zipkin, &trace_config)?;
+        builder = setup_tracing(builder, &tracing_config.datadog, &trace_config)?;
+        builder = setup_tracing(builder, &tracing_config.otlp, &trace_config)?;
+        builder = setup_tracing(builder, &apollo_config, &trace_config)?;
+
+        if tracing_config.jaeger.is_none()
+            && tracing_config.zipkin.is_none()
+            && tracing_config.datadog.is_none()
+            && tracing_config.otlp.is_none()
+            && apollo_config.is_none()
+        {
+            sampler = SamplerOption::Always(Sampler::AlwaysOff);
+        }
 
         let tracer_provider = builder.build();
-        Ok(tracer_provider)
+        Ok((sampler, tracer_provider))
     }
 
     fn create_metrics_builder(config: &config::Conf) -> Result<MetricsBuilder, BoxError> {
@@ -664,21 +692,7 @@ impl Telemetry {
         Ok(builder)
     }
 
-    #[allow(clippy::type_complexity)]
-    fn create_fmt_layer(
-        config: &config::Conf,
-    ) -> Box<
-        dyn Layer<
-                ::tracing_subscriber::layer::Layered<
-                    OpenTelemetryLayer<
-                        LayeredRegistry,
-                        ReloadTracer<::opentelemetry::sdk::trace::Tracer>,
-                    >,
-                    LayeredRegistry,
-                >,
-            > + Send
-            + Sync,
-    > {
+    fn create_fmt_layer(config: &config::Conf) -> Box<dyn Layer<LayeredTracer> + Send + Sync> {
         let logging = &config.logging;
         let fmt = match logging.format {
             config::LoggingFormat::Pretty => tracing_subscriber::fmt::layer()

apollo-router/src/router_factory.rs
@@ -573,9 +573,11 @@ pub(crate) async fn create_plugins(
 
 fn inject_schema_id(schema: &Schema, configuration: &mut Value) {
     if configuration.get("apollo").is_none() {
+        /*FIXME: do we really need to set a default configuration for telemetry.apollo ?
         if let Some(telemetry) = configuration.as_object_mut() {
             telemetry.insert("apollo".to_string(), Value::Object(Default::default()));
-        }
+        }*/
+        return;
     }
     if let (Some(schema_id), Some(apollo)) = (
         &schema.api_schema().schema_id,
@@ -730,7 +732,7 @@ mod test {
     fn test_inject_schema_id() {
         let schema = include_str!("testdata/starstuff@current.graphql");
         let schema = Schema::parse_test(schema, &Default::default()).unwrap();
-        let mut config = json!({});
+        let mut config = json!({ "apollo": {} });
         inject_schema_id(&schema, &mut config);
         let config =
             serde_json::from_value::<crate::plugins::telemetry::config::Conf>(config).unwrap();

apollo-router/tests/fixtures/jaeger-0.5-sample.router.yaml
@@ -1,0 +1,29 @@
+telemetry:
+  tracing:
+    experimental_response_trace_id:
+      enabled: true
+      header_name: apollo-custom-trace-id
+    propagation:
+      jaeger: true
+    trace_config:
+      service_name: router
+      sampler: 0.5
+    jaeger:
+      batch_processor:
+        scheduled_delay: 100ms
+      agent:
+        endpoint: default
+  experimental_logging:
+    when_header:
+      - name: apollo-router-log-request
+        value: test
+        headers: true # default: false
+        body: true # default: false
+      # log request for all requests coming from Iphones
+      - name: custom-header
+        match: ^foo.*
+        headers: true
+override_subgraph_url:
+  products: http://localhost:4005
+include_subgraph_errors:
+  all: true

apollo-router/tests/jaeger_test.rs
@@ -123,6 +123,34 @@ async fn test_local_root_no_sample() -> Result<(), BoxError> {
 }
 
 #[tokio::test(flavor = "multi_thread")]
+async fn test_local_root_50_percent_sample() -> Result<(), BoxError> {
+    let mut router = IntegrationTest::builder()
+        .telemetry(Telemetry::Jaeger)
+        .config(include_str!("fixtures/jaeger-0.5-sample.router.yaml"))
+        .build()
+        .await;
+
+    router.start().await;
+    router.assert_started().await;
+    let query = json!({"query":"query ExampleQuery {topProducts{name}}\n","variables":{}, "operationName": "ExampleQuery"});
+
+    for _ in 0..100 {
+        let (id, result) = router.execute_untraced_query(&query).await;
+
+        if result.headers().get("apollo-custom-trace-id").is_some()
+            && validate_trace(id, &query, Some("ExampleQuery"), &["router", "products"])
+                .await
+                .is_ok()
+        {
+            router.graceful_shutdown().await;
+
+            return Ok(());
+        }
+    }
+    panic!("tried 100 requests with telemetry sampled at 50%, no traces were found")
+}
+
+#[tokio::test(flavor = "multi_thread")]
 #[ignore]
 async fn test_no_telemetry() -> Result<(), BoxError> {
     // This test is currently skipped because it will only pass once we default the sampler to always off if there are no exporters.

commit 4c18f9ab03c08ea6659c983cec9e5ceb486cfcb8

send log events with traces (#4486)
Fix #4321 
Fix #3872 (the regression appears in 1.30, when #2999 was merged)

related: #2402

This fixes a regression introduced in #2999, where events were not sent
anymore with traces

apollo-router/src/plugins/telemetry/reload.rs
@@ -175,9 +175,9 @@ where
         meta: &tracing::Metadata<'_>,
         cx: &tracing_subscriber::layer::Context<'_, S>,
     ) -> bool {
-        // we ignore events
+        // we ignore metric events
         if !meta.is_span() {
-            return false;
+            return meta.fields().iter().any(|f| f.name() == "message");
         }
 
         // if there's an exsting otel context set by the client request, and it is sampled,