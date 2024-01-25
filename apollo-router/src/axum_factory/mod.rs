875a0530cc08541df4e37e4707a33f7ef51be537
Bryn Cooke <BrynCooke@gmail.com>
2024-01-25 11:26:57 +0000

    Add endpoint callback (#4547)
    Although part of the public API, this is not intended for use by end
    users, and may change at any time.
    
    <!-- start metadata -->
    ---
    
    **Checklist**
    
    Complete the checklist (and note appropriate exceptions) before the PR
    is marked ready-for-review.
    
    - [ ] Changes are compatible[^1]
    - [ ] Documentation[^2] completed
    - [ ] Performance impact assessed and acceptable
    - Tests added and passing[^3]
        - [ ] Unit Tests
        - [ ] Integration Tests
        - [ ] Manual Tests
    
    **Exceptions**
    
    *Note any exceptions here*
    
    **Notes**
    
    [^1]: It may be appropriate to bring upcoming changes to the attention
    of other (impacted) groups. Please endeavour to do this before seeking
    PR approval. The mechanism for doing this will vary considerably, so use
    your judgement as to how and when to do this.
    [^2]: Configuration is an important part of many changes. Where
    applicable please try to document configuration examples.
    [^3]: Tick whichever testing boxes are applicable. If you are adding
    Manual Tests, please document the manual testing (extensively) in the
    Exceptions.
    
    ---------
    
    Co-authored-by: bryn <bryn@apollographql.com>

--- apollo-router/src/axum_factory/mod.rs
+++ apollo-router/src/axum_factory/mod.rs
@@ -7,5 +7,6 @@ pub(crate) mod tests;
 pub(crate) mod utils;
 
 pub(crate) use axum_http_server_factory::span_mode;
+pub use axum_http_server_factory::unsupported_set_axum_router_callback;
 pub(crate) use axum_http_server_factory::AxumHttpServerFactory;
 pub(crate) use listeners::ListenAddrAndRouter;
