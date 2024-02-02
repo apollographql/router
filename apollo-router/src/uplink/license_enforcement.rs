6ff0959e29bceb9e87d8b89755555e396fd05fe8
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2024-02-02 14:19:07 +0200

    mark this feature as restricted

--- apollo-router/src/uplink/license_enforcement.rs
+++ apollo-router/src/uplink/license_enforcement.rs
@@ -357,6 +357,10 @@ impl LicenseEnforcementReport {
                 .path("$.telemetry..instruments")
                 .name("Advanced telemetry")
                 .build(),
+            ConfigurationRestriction::builder()
+                .path("$.experimental_file_uploads")
+                .name("File uploads plugin")
+                .build(),
         ]
     }
 
