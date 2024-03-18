a1ef57555a12342aa6d89089a427da51ba51d2d3
Taylor Ninesling <taylor.ninesling@apollographql.com>
2024-03-18 16:50:32 -0500

    Created scaffolding for new demand control plugin with a simple config. The max cost setting is planned to come from the limits block of the configuration, so it is not included in this change. The plugin is currently not registered so it does not appear in the config schema during development

---
+++ apollo-router/src/plugins/demand_control/mod.rs
@@ -0,0 +1,62 @@
+//! Demand control plugin.
+use schemars::JsonSchema;
+use serde::Deserialize;
+use tower::BoxError;
+use tower::ServiceBuilder;
+use tower::ServiceExt;
+
+use crate::plugin::Plugin;
+use crate::plugin::PluginInit;
+use crate::services::execution::BoxService;
+
+/// Algorithm for calculating the cost of an incoming query.
+#[derive(Clone, Debug, Default, Deserialize, JsonSchema)]
+#[serde(deny_unknown_fields, rename_all = "lowercase")]
+pub(crate) enum CostCalculationAlgorithm {
+    /// A simple, statically-defined cost mapping for operations and types.
+    ///
+    /// Operation costs:
+    /// - Mutation: 10
+    /// - Query: 0
+    /// - Subscription 0
+    ///
+    /// Type costs:
+    /// - Object: 1
+    /// - Interface: 1
+    /// - Union: 1
+    /// - Scalar: 0
+    /// - Enum: 0
+    #[default]
+    Default,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, JsonSchema)]
+#[serde(default, deny_unknown_fields)]
+pub(crate) struct DemandControlConfig {
+    enabled: bool,
+    algorithm: CostCalculationAlgorithm,
+}
+
+#[derive(Clone, Debug)]
+pub(crate) struct DemandControl {
+    config: DemandControlConfig,
+}
+
+#[async_trait::async_trait]
+impl Plugin for DemandControl {
+    type Config = DemandControlConfig;
+
+    async fn new(init: PluginInit<Self::Config>) -> Result<Self, BoxError> {
+        Ok(DemandControl {
+            config: init.config,
+        })
+    }
+
+    fn execution_service(&self, service: BoxService) -> BoxService {
+        if !self.config.enabled {
+            service
+        } else {
+            ServiceBuilder::new().service(service).boxed()
+        }
+    }
+}
