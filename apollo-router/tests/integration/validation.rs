ad7a95b2ee5351804c15d70335f13b3c093d4fe2
Renée Kooi <renee.kooi@apollographql.com>
2024-04-12 14:31:34 +0200

    Add a test with some validation errors

--- apollo-router/tests/integration/validation.rs
+++ apollo-router/tests/integration/validation.rs
@@ -89,3 +89,82 @@ async fn test_syntax_error() {
     }
     "###);
 }
+
+#[tokio::test]
+async fn test_validation_error() {
+    let request = serde_json::json!({"query": "{...a} fragment unused on Query { me { id } } fragment a on Query{me {id} topProducts(first: 5.5) {id}}"});
+    let request = apollo_router::services::router::Request::fake_builder()
+        .body(request.to_string())
+        .method(hyper::Method::POST)
+        .header("content-type", "application/json")
+        .build()
+        .unwrap();
+    let response = apollo_router::TestHarness::builder()
+        .schema(include_str!("../fixtures/supergraph.graphql"))
+        .build_router()
+        .await
+        .unwrap()
+        .oneshot(request)
+        .await
+        .unwrap()
+        .next_response()
+        .await
+        .unwrap()
+        .unwrap();
+
+    let v: serde_json::Value = serde_json::from_slice(&response).unwrap();
+    insta::assert_json_snapshot!(v, @r###"
+    {
+      "errors": [
+        {
+          "message": "Fragment \"unused\" is never used.",
+          "locations": [
+            {
+              "line": 1,
+              "column": 8
+            }
+          ],
+          "extensions": {
+            "code": "GRAPHQL_VALIDATION_FAILED"
+          }
+        },
+        {
+          "message": "Field \"topProducts\" of type \"Product\" must have a selection of subfields. Did you mean \"topProducts { ... }\"?",
+          "locations": [
+            {
+              "line": 1,
+              "column": 75
+            }
+          ],
+          "extensions": {
+            "code": "GRAPHQL_VALIDATION_FAILED"
+          }
+        },
+        {
+          "message": "Int cannot represent value: 5.5",
+          "locations": [
+            {
+              "line": 1,
+              "column": 94
+            }
+          ],
+          "extensions": {
+            "code": "GRAPHQL_VALIDATION_FAILED"
+          }
+        },
+        {
+          "message": "Cannot query field \"id\" on type \"Product\".",
+          "locations": [
+            {
+              "line": 1,
+              "column": 100
+            }
+          ],
+          "extensions": {
+            "code": "GRAPHQL_VALIDATION_FAILED"
+          }
+        }
+      ]
+    }
+    "###);
+}
