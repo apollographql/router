24024dbabaed2ec72667b4e15cca39c089e3c504
Geoffroy Couprie <apollo@geoffroycouprie.com>
2024-05-03 13:40:41 +0200

    Revert coprocessor changes (#5068)

--- apollo-router/src/query_planner/tests.rs
+++ apollo-router/src/query_planner/tests.rs
@@ -1812,7 +1812,7 @@ fn broken_plan_does_not_panic() {
     let subgraph_schema = apollo_compiler::Schema::parse_and_validate(subgraph_schema, "").unwrap();
     let mut subgraph_schemas = HashMap::new();
     subgraph_schemas.insert("X".to_owned(), Arc::new(subgraph_schema));
-    let result = plan.root.hash_subqueries(&subgraph_schemas, "");
+    let result = plan.root.hash_subqueries(&subgraph_schemas);
     assert_eq!(
         result.unwrap_err().to_string(),
         r#"[1:3] Cannot query field "invalid" on type "Query"."#
