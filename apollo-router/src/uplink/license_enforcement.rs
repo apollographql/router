dbeef455321d1e1c96b2494db684ad1966fb08a1
bryn <bryn@apollographql.com>
2024-05-20 08:24:17 +0100

    License enforcement

--- apollo-router/src/uplink/license_enforcement.rs
+++ apollo-router/src/uplink/license_enforcement.rs
@@ -379,6 +379,10 @@ impl LicenseEnforcementReport {
                 .name("Advanced telemetry")
                 .build(),
             ConfigurationRestriction::builder()
+                .path("$.telemetry..graphql")
+                .name("Advanced telemetry")
+                .build(),
+            ConfigurationRestriction::builder()
                 .path("$.preview_file_uploads")
                 .name("File uploads plugin")
                 .build(),
