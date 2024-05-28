b8326b4ef5f43af040d9b20aa9c54daa6a379790
bryn <bryn@apollographql.com>
2024-05-28 11:48:39 +0100

    Add support for operation name on graphql metrics. This is not enabled by default as cardinality could be high. but it does allow graphql operation to be used in selector and therefore conditions.

--- apollo-router/src/plugins/telemetry/config_new/graphql/attributes.rs
+++ apollo-router/src/plugins/telemetry/config_new/graphql/attributes.rs
@@ -9,6 +9,7 @@ use crate::plugins::telemetry::config_new::graphql::selectors::FieldType;
 use crate::plugins::telemetry::config_new::graphql::selectors::GraphQLSelector;
 use crate::plugins::telemetry::config_new::graphql::selectors::ListLength;
 use crate::plugins::telemetry::config_new::graphql::selectors::TypeName;
+use crate::plugins::telemetry::config_new::selectors::OperationName;
 use crate::plugins::telemetry::config_new::DefaultAttributeRequirementLevel;
 use crate::plugins::telemetry::config_new::DefaultForLevel;
 use crate::plugins::telemetry::config_new::Selector;
@@ -29,6 +30,8 @@ pub(crate) struct GraphQLAttributes {
     /// If the field is a list, the length of the list
     #[serde(rename = "graphql.list.length")]
     pub(crate) list_length: Option<bool>,
+    #[serde(rename = "graphql.operation.name")]
+    pub(crate) operation_name: Option<bool>,
     /// The GraphQL type name
     #[serde(rename = "graphql.type.name")]
     pub(crate) type_name: Option<bool>,
@@ -105,17 +108,29 @@ impl Selectors for GraphQLAttributes {
                 attrs.push(KeyValue::new("graphql.list.length", length));
             }
         }
+        if let Some(true) = self.operation_name {
+            if let Some(length) = (GraphQLSelector::OperationName {
+                operation_name: OperationName::String,
+                default: None,
+            })
+            .on_response_field(typed_value, ctx)
+            {
+                attrs.push(KeyValue::new("graphql.operation.name", length));
+            }
+        }
         attrs
     }
 }
 
 #[cfg(test)]
 mod test {
+    use crate::context::OPERATION_NAME;
     use crate::plugins::demand_control::cost_calculator::schema_aware_response::TypedValue;
     use crate::plugins::telemetry::config_new::test::field;
     use crate::plugins::telemetry::config_new::test::ty;
     use crate::plugins::telemetry::config_new::DefaultForLevel;
     use crate::plugins::telemetry::config_new::Selectors;
+    use crate::Context;
 
     #[test]
     fn test_default_for_level() {
@@ -128,6 +143,7 @@ mod test {
         assert_eq!(attributes.field_type, Some(true));
         assert_eq!(attributes.type_name, Some(true));
         assert_eq!(attributes.list_length, None);
+        assert_eq!(attributes.operation_name, None);
     }
 
     #[test]
@@ -136,18 +152,22 @@ mod test {
             field_name: Some(true),
             field_type: Some(true),
             list_length: Some(true),
+            operation_name: Some(true),
             type_name: Some(true),
         };
         let typed_value = TypedValue::Bool(ty(), field(), &true);
-        let ctx = Default::default();
+        let ctx = Context::default();
+        let _ = ctx.insert(OPERATION_NAME, "operation_name".to_string());
         let result = attributes.on_response_field(&typed_value, &ctx);
-        assert_eq!(result.len(), 3);
+        assert_eq!(result.len(), 4);
         assert_eq!(result[0].key.as_str(), "graphql.field.name");
         assert_eq!(result[0].value.as_str(), "field_name");
         assert_eq!(result[1].key.as_str(), "graphql.field.type");
         assert_eq!(result[1].value.as_str(), "field_type");
         assert_eq!(result[2].key.as_str(), "graphql.type.name");
         assert_eq!(result[2].value.as_str(), "type_name");
+        assert_eq!(result[3].key.as_str(), "graphql.operation.name");
+        assert_eq!(result[3].value.as_str(), "operation_name");
     }
 
     #[test]
@@ -156,6 +176,7 @@ mod test {
             field_name: Some(true),
             field_type: Some(true),
             list_length: Some(true),
+            operation_name: Some(true),
             type_name: Some(true),
         };
         let typed_value = TypedValue::List(
@@ -167,9 +188,10 @@ mod test {
                 TypedValue::Bool(ty(), field(), &true),
             ],
         );
-        let ctx = Default::default();
+        let ctx = Context::default();
+        let _ = ctx.insert(OPERATION_NAME, "operation_name".to_string());
         let result = attributes.on_response_field(&typed_value, &ctx);
-        assert_eq!(result.len(), 4);
+        assert_eq!(result.len(), 5);
         assert_eq!(result[0].key.as_str(), "graphql.field.name");
         assert_eq!(result[0].value.as_str(), "field_name");
         assert_eq!(result[1].key.as_str(), "graphql.field.type");
@@ -178,5 +200,7 @@ mod test {
         assert_eq!(result[2].value.as_str(), "type_name");
         assert_eq!(result[3].key.as_str(), "graphql.list.length");
         assert_eq!(result[3].value.as_str(), "3");
+        assert_eq!(result[4].key.as_str(), "graphql.operation.name");
+        assert_eq!(result[4].value.as_str(), "operation_name");
     }
 }
