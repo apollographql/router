9dbc5928bde9134191849d8bec4d9151538de2b0
Duckki Oe <duckki.oe@apollographql.com>
2024-05-29 13:03:43 -0700

    updated tests
    - Some have been fixed. Updated their annotations.
    - The rest are failing with different issues.

--- apollo-federation/tests/query_plan/build_query_plan_tests/named_fragments_preservation.rs
+++ apollo-federation/tests/query_plan/build_query_plan_tests/named_fragments_preservation.rs
@@ -1,8 +1,8 @@
 use apollo_federation::query_plan::query_planner::QueryPlannerConfig;
 
 #[test]
-#[should_panic(expected = "not yet implemented")]
-// TODO: investigate this failure
+#[should_panic(expected = "snapshot assertion")]
+// TODO: investigate this failure - missing `__typename` under `a`
 fn it_works_with_nested_fragments_1() {
     let planner = planner!(
         Subgraph1: r#"
@@ -127,8 +127,6 @@ fn it_works_with_nested_fragments_1() {
 }
 
 #[test]
-#[should_panic(expected = "not yet implemented")]
-// TODO: investigate this failure
 fn it_avoid_fragments_usable_only_once() {
     let planner = planner!(
             Subgraph1: r#"
@@ -397,8 +395,6 @@ mod respects_query_planner_option_reuse_query_fragments {
 }
 
 #[test]
-#[should_panic(expected = "not yet implemented")]
-// TODO: investigate this failure
 fn it_works_with_nested_fragments_when_only_the_nested_fragment_gets_preserved() {
     let planner = planner!(
         Subgraph1: r#"
@@ -466,10 +462,8 @@ fn it_works_with_nested_fragments_when_only_the_nested_fragment_gets_preserved()
 }
 
 #[test]
-#[should_panic(
-    expected = "Error: variable `$if` of type `Boolean` cannot be used for argument `if` of type `Boolean!`"
-)]
-// TODO: investigate this failure
+#[should_panic(expected = r#"called `Result::unwrap()` on an `Err` value: "#)]
+// TODO: investigate this failure (Error: variable `$if` of type `Boolean` cannot be used for argument `if` of type `Boolean!`)
 fn it_preserves_directives_when_fragment_not_used() {
     // (because used only once)
     let planner = planner!(
@@ -654,8 +648,8 @@ fn it_does_not_try_to_apply_fragments_that_are_not_valid_for_the_subgaph() {
 }
 
 #[test]
-#[should_panic(expected = "not yet implemented")]
-// TODO: investigate this failure
+#[should_panic(expected = "snapshot assertion")]
+// TODO: investigate this failure - snapshot mismatch (complicated)
 fn it_handles_fragment_rebasing_in_a_subgraph_where_some_subtyping_relation_differs() {
     // This test is designed such that type `Outer` implements the interface `I` in `Subgraph1`
     // but not in `Subgraph2`, yet `I` exists in `Subgraph2` (but only `Inner` implements it
@@ -1002,8 +996,8 @@ fn it_handles_fragment_rebasing_in_a_subgraph_where_some_subtyping_relation_diff
 }
 
 #[test]
-#[should_panic(expected = "not yet implemented")]
-// TODO: investigate this failure
+#[should_panic(expected = "called `Result::unwrap()` on an `Err` value")]
+// TODO: investigate this failure (Object type "Outer" has no field "v")
 fn it_handles_fragment_rebasing_in_a_subgraph_where_some_union_membership_relation_differs() {
     // This test is similar to the subtyping case (it tests the same problems), but test the case
     // of unions instead of interfaces.
