79da7ae7500e8f1daffa33aa33a39846530191da
Geoffroy Couprie <apollo@geoffroycouprie.com>
2024-05-31 15:55:50 +0200

    implement the rust subgraph model

--- fuzz/subgraph/src/model.rs
+++ fuzz/subgraph/src/model.rs
@@ -1,6 +1,7 @@
+use std::sync::Once;
+
 use async_graphql::Context;
 use async_graphql::Object;
-
 
 /*
   type Query {
@@ -13,15 +14,8 @@ pub struct Query;
 
 #[Object]
 impl Query {
-    async fn me(&self, _ctx: &Context<'_>) -> User {
-        /*let random = &mut StdRng::seed_from_u64(3974).sample_iter(&Alphanumeric);
-        let mut result = Vec::with_capacity(limit);
-        for count in 0..limit {
-            let data = random.take(size.unwrap_or(1)).map(char::from).collect();
-            result.push(product_for_upc(count.to_string(), data))
-        }
-        result*/
-        todo!()
+    async fn me(&self, _ctx: &Context<'_>) -> Option<User> {
+        users().get(0).cloned()
     }
 
     async fn topProducts(
@@ -29,32 +23,25 @@ impl Query {
         _ctx: &Context<'_>,
         #[graphql(desc = "number of products to return")] first: Option<usize>,
     ) -> Vec<Product> {
-        /*let random = &mut StdRng::seed_from_u64(3974).sample_iter(&Alphanumeric);
-        let mut result = Vec::with_capacity(limit);
-        for count in 0..limit {
-            let data = random.take(size.unwrap_or(1)).map(char::from).collect();
-            result.push(product_for_upc(count.to_string(), data))
-        }
-        result*/
-        todo!()
+        let first = first.unwrap_or(4);
+        let limit = std::cmp::min(first, 2);
+        let p = products();
+        (&p[..limit]).to_owned()
     }
 
     #[graphql(entity)]
-    async fn find_user_by_id(&self, id: String) -> User {
-        //product_for_upc(upc, "1".to_string())
-        todo!()
+    async fn find_user_by_id(&self, id: String) -> Option<User> {
+        users().iter().find(|u| u.id.as_str() == &id).cloned()
     }
 
     #[graphql(entity)]
-    async fn find_product_by_upc(&self, upc: String) -> Product {
-        //product_for_upc(upc, "1".to_string())
-        todo!()
+    async fn find_product_by_upc(&self, upc: String) -> Option<Product> {
+        products().iter().find(|p| p.upc.as_str() == &upc).cloned()
     }
 
     #[graphql(entity)]
-    async fn find_review_by_id(&self, id: String) -> Review {
-        //product_for_upc(upc, "1".to_string())
-        todo!()
+    async fn find_review_by_id(&self, id: String) -> Option<&Review> {
+        reviews().iter().find(|r: &&Review| r.id.as_str() == &id)
     }
 }
 
@@ -69,19 +56,34 @@ pub struct Mutation;
 
 #[Object]
 impl Mutation {
-    async fn topProducts(
+    async fn createProduct(
         &self,
         _ctx: &Context<'_>,
-        #[graphql(desc = "number of products to return")] first: Option<usize>,
-    ) -> Vec<Product> {
-        /*let random = &mut StdRng::seed_from_u64(3974).sample_iter(&Alphanumeric);
-        let mut result = Vec::with_capacity(limit);
-        for count in 0..limit {
-            let data = random.take(size.unwrap_or(1)).map(char::from).collect();
-            result.push(product_for_upc(count.to_string(), data))
+        upc: String,
+        name: Option<String>,
+    ) -> Product {
+        Product {
+            upc,
+            name,
+            price: 0,
+            weight: 0,
+            inStock: true,
         }
-        result*/
-        todo!()
+    }
+
+    async fn createReview(
+        &self,
+        _ctx: &Context<'_>,
+        upc: String,
+        id: String,
+        body: Option<String>,
+    ) -> Review {
+        Review {
+            id,
+            productUpc: upc,
+            body,
+            authorId: "0".to_string(),
+        }
     }
 }
 
@@ -98,7 +100,7 @@ pub struct User {
     id: String,
     name: String,
     username: String,
-    reviews: Vec<Review>,
+    //reviews: Vec<Review>,
 }
 
 #[Object]
@@ -115,14 +117,33 @@ impl User {
         &self.username
     }
 
-    async fn reviews(&self) -> &[Review] {
-        /*Review {
-            body: "1".to_string(),
-            product: Product {
-                upc: "00000000-0000-0000-0000-000000000000".to_string(),
-            },
-        }*/
-        todo!()
+    async fn reviews(&self) -> Vec<&Review> {
+        reviews()
+            .iter()
+            .filter(|r: &&Review| r.authorId.as_str() == &self.id)
+            .collect()
+    }
+}
+
+fn users() -> &'static [User] {
+    static mut USERS: Vec<User> = vec![];
+    static INIT: Once = Once::new();
+    unsafe {
+        INIT.call_once(|| {
+            USERS = vec![
+                User {
+                    id: "1".to_string(),
+                    name: "Ada Lovelace".to_string(),
+                    username: "@ada".to_string(),
+                },
+                User {
+                    id: "2".to_string(),
+                    name: "Alan Turing".to_string(),
+                    username: "@complete".to_string(),
+                },
+            ];
+        });
+        &USERS
     }
 }
 
@@ -142,21 +163,12 @@ impl User {
 #[derive(Clone)]
 pub struct Product {
     upc: String,
-    name: String,
+    name: Option<String>,
     price: u32,
     weight: u32,
     inStock: bool,
-    shippingEstimate: u32,
-    reviews: Vec<Review>,
-}
-
-fn product_for_upc(upc: String, data: String) -> Product {
-    /*Product {
-        upc: "00000000-0000-0000-0000-000000000000".to_string(),
-        name: upc,
-        price: data,
-    }*/
-    todo!()
+    //shippingEstimate: u32,
+    //reviews: Vec<Review>,
 }
 
 #[Object]
@@ -165,8 +177,8 @@ impl Product {
         &self.upc
     }
 
-    async fn name(&self) -> &String {
-        &self.name
+    async fn name(&self) -> Option<&String> {
+        self.name.as_ref()
     }
 
     async fn price(&self) -> &u32 {
@@ -181,18 +193,70 @@ impl Product {
         self.inStock
     }
 
-    async fn shippingEstimate(&self) -> &u32 {
-        &self.shippingEstimate
+    async fn shippingEstimate(&self) -> u32 {
+        // free for expensive items
+        if self.price > 1000 {
+            0
+        } else {
+            // estimate is based on weight
+            self.weight / 2
+        }
     }
 
-    async fn reviews(&self) -> &[Review] {
-        /*Review {
-            body: "1".to_string(),
-            product: Product {
-                upc: "00000000-0000-0000-0000-000000000000".to_string(),
-            },
-        }*/
-        todo!()
+    async fn reviews(&self) -> Vec<Review> {
+        reviews()
+            .iter()
+            .filter(|r: &&Review| r.productUpc.as_str() == &self.upc)
+            .cloned()
+            .collect()
+    }
+
+    async fn reviewsForAuthor(&self, authorID: String) -> Vec<Review> {
+        reviews()
+            .iter()
+            .filter(|r: &&Review| r.productUpc.as_str() == &self.upc && &r.authorId == &authorID)
+            .cloned()
+            .collect()
+    }
+}
+
+fn products() -> &'static [Product] {
+    static mut PRODUCTS: Vec<Product> = vec![];
+    static INIT: Once = Once::new();
+    unsafe {
+        INIT.call_once(|| {
+            PRODUCTS = vec![
+                Product {
+                    upc: "1".to_string(),
+                    name: Some("Table".to_string()),
+                    price: 899,
+                    weight: 100,
+                    inStock: true,
+                },
+                Product {
+                    upc: "2".to_string(),
+                    name: Some("Couch".to_string()),
+                    price: 1299,
+                    weight: 1000,
+                    inStock: false,
+                },
+                Product {
+                    upc: "3".to_string(),
+                    name: Some("Chair".to_string()),
+                    price: 54,
+                    weight: 50,
+                    inStock: true,
+                },
+                Product {
+                    upc: "4".to_string(),
+                    name: Some("Bed".to_string()),
+                    price: 1000,
+                    weight: 1200,
+                    inStock: false,
+                },
+            ];
+        });
+        &PRODUCTS
     }
 }
 
@@ -208,9 +272,9 @@ impl Product {
 #[derive(Clone)]
 struct Review {
     id: String,
-    body: String,
-    author: User,
-    product: Product,
+    body: Option<String>,
+    authorId: String,
+    productUpc: String,
 }
 
 #[Object]
@@ -219,15 +283,55 @@ impl Review {
         &self.id
     }
 
-    async fn body(&self) -> &String {
-        &self.body
+    async fn body(&self) -> Option<&String> {
+        self.body.as_ref()
     }
 
-    async fn author(&self) -> &User {
-        &self.author
+    async fn author(&self) -> Option<&User> {
+        users()
+            .iter()
+            .find(|r: &&User| r.id.as_str() == &self.authorId)
     }
 
-    async fn product(&self) -> &Product {
-        &self.product
+    async fn product(&self) -> Option<&Product> {
+        products()
+            .iter()
+            .find(|p| p.upc.as_str() == &self.productUpc)
+    }
+}
+
+fn reviews() -> &'static [Review] {
+    static mut REVIEWS: Vec<Review> = vec![];
+    static INIT: Once = Once::new();
+    unsafe {
+        INIT.call_once(|| {
+            REVIEWS = vec![
+                Review {
+                    id: "1".to_string(),
+                    authorId: "1".to_string(),
+                    productUpc: "1".to_string(),
+                    body: Some("Love it!".to_string()),
+                },
+                Review {
+                    id: "2".to_string(),
+                    authorId: "1".to_string(),
+                    productUpc: "2".to_string(),
+                    body: Some("Too expensive.".to_string()),
+                },
+                Review {
+                    id: "3".to_string(),
+                    authorId: "2".to_string(),
+                    productUpc: "3".to_string(),
+                    body: Some("Could be better.".to_string()),
+                },
+                Review {
+                    id: "4".to_string(),
+                    authorId: "2".to_string(),
+                    productUpc: "1".to_string(),
+                    body: Some("Prefer something else.".to_string()),
+                },
+            ];
+        });
+        &REVIEWS
     }
 }
