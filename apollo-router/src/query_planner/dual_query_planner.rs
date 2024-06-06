0d2c0c768628e7fa21f1e10c5341172b3c729f49
Simon Sapin <simon@apollographql.com>
2024-06-06 17:41:19 +0200

    Ignore schema_aware_hash and subgraph operation whitespace

--- apollo-router/src/query_planner/dual_query_planner.rs
+++ apollo-router/src/query_planner/dual_query_planner.rs
@@ -1,5 +1,6 @@
 //! Running two query planner implementations and comparing their results
 
+use std::borrow::Borrow;
 use std::sync::Arc;
 use std::sync::OnceLock;
 
@@ -9,10 +10,17 @@ use apollo_compiler::ExecutableDocument;
 use apollo_compiler::NodeStr;
 use apollo_federation::query_plan::query_planner::QueryPlanner;
 
+use super::fetch::FetchNode;
+use super::fetch::SubgraphOperation;
+use super::subscription::SubscriptionNode;
+use super::FlattenNode;
 use crate::error::format_bridge_errors;
 use crate::executable::USING_CATCH_UNWIND;
 use crate::query_planner::convert::convert_root_query_plan_node;
 use crate::query_planner::render_diff;
+use crate::query_planner::DeferredNode;
+use crate::query_planner::PlanNode;
+use crate::query_planner::Primary;
 use crate::query_planner::QueryPlanResult;
 
 /// Jobs are dropped if this many are already queued
@@ -52,10 +60,11 @@ impl BothModeComparisonJob {
         // rather than consume too much resources.
         //
         // Either way we move on and let this thread continue proceed with the query plan from JS.
-        let _ = queue().try_send(self).is_err();
+        let _ = dbg!(queue().try_send(self).is_err());
     }
 
     fn execute(self) {
+        dbg!();
         // TODO: once the Rust query planner does not use `todo!()` anymore,
         // remove `USING_CATCH_UNWIND` and this use of `catch_unwind`.
         let rust_result = std::panic::catch_unwind(|| {
@@ -111,9 +120,9 @@ impl BothModeComparisonJob {
             }
 
             (Ok(js_plan), Ok(rust_plan)) => {
-                let js_root_node = js_plan.query_plan.node.as_deref();
+                let js_root_node = &js_plan.query_plan.node;
                 let rust_root_node = convert_root_query_plan_node(rust_plan);
-                is_matched = js_root_node == rust_root_node.as_ref();
+                is_matched = opt_plan_node_matches(js_root_node, &rust_root_node);
                 if is_matched {
                     tracing::debug!("JS and Rust query plans match{operation_desc}! 🎉");
                 } else {
@@ -139,4 +148,153 @@ impl BothModeComparisonJob {
             "generation.rust_error" = rust_result.is_err()
         );
     }
+}
+
+// Specific comparison functions
+
+fn fetch_node_matches(this: &FetchNode, other: &FetchNode) -> bool {
+    let FetchNode {
+        service_name,
+        requires,
+        variable_usages,
+        operation,
+        operation_name,
+        operation_kind,
+        id,
+        input_rewrites,
+        output_rewrites,
+        context_rewrites,
+        schema_aware_hash: _, // ignored
+        authorization,
+    } = this;
+    *service_name == other.service_name
+        && *requires == other.requires
+        && *variable_usages == other.variable_usages
+        && *operation_name == other.operation_name
+        && *operation_kind == other.operation_kind
+        && *id == other.id
+        && *input_rewrites == other.input_rewrites
+        && *output_rewrites == other.output_rewrites
+        && *context_rewrites == other.context_rewrites
+        && *authorization == other.authorization
+        && operation_matches(operation, &other.operation)
+}
+
+fn subscription_primary_matches(this: &SubscriptionNode, other: &SubscriptionNode) -> bool {
+    let SubscriptionNode {
+        service_name,
+        variable_usages,
+        operation,
+        operation_name,
+        operation_kind,
+        input_rewrites,
+        output_rewrites,
+    } = this;
+    *service_name == other.service_name
+        && *variable_usages == other.variable_usages
+        && *operation_name == other.operation_name
+        && *operation_kind == other.operation_kind
+        && *input_rewrites == other.input_rewrites
+        && *output_rewrites == other.output_rewrites
+        && operation_matches(operation, &other.operation)
+}
+
+fn operation_matches(this: &SubgraphOperation, other: &SubgraphOperation) -> bool {
+    operation_without_whitespace(this) == operation_without_whitespace(other)
+}
+
+fn operation_without_whitespace(op: &SubgraphOperation) -> String {
+    op.as_serialized().replace([' ', '\n'], "")
+}
+
+// The rest is calling the comparison functions above instead of `PartialEq`,
+// but otherwise behave just like `PartialEq`:
+
+fn opt_plan_node_matches(
+    this: &Option<impl Borrow<PlanNode>>,
+    other: &Option<impl Borrow<PlanNode>>,
+) -> bool {
+    match (this, other) {
+        (None, None) => true,
+        (None, Some(_)) | (Some(_), None) => false,
+        (Some(this), Some(other)) => plan_node_matches(this.borrow(), other.borrow()),
+    }
+}
+
+fn vec_matches<T>(this: &Vec<T>, other: &Vec<T>, item_matches: impl Fn(&T, &T) -> bool) -> bool {
+    this.len() == other.len()
+        && std::iter::zip(this, other).all(|(this, other)| item_matches(this, other))
+}
+
+fn plan_node_matches(this: &PlanNode, other: &PlanNode) -> bool {
+    match (this, other) {
+        (PlanNode::Sequence { nodes: this }, PlanNode::Sequence { nodes: other })
+        | (PlanNode::Parallel { nodes: this }, PlanNode::Parallel { nodes: other }) => {
+            vec_matches(this, other, plan_node_matches)
+        }
+        (PlanNode::Fetch(this), PlanNode::Fetch(other)) => fetch_node_matches(this, other),
+        (PlanNode::Flatten(this), PlanNode::Flatten(other)) => flatten_node_matches(this, other),
+        (
+            PlanNode::Defer { primary, deferred },
+            PlanNode::Defer {
+                primary: other_primary,
+                deferred: other_deferred,
+            },
+        ) => {
+            defer_primary_node_matches(primary, other_primary)
+                && vec_matches(deferred, other_deferred, deferred_node_matches)
+        }
+        (
+            PlanNode::Subscription { primary, rest },
+            PlanNode::Subscription {
+                primary: other_primary,
+                rest: other_rest,
+            },
+        ) => {
+            subscription_primary_matches(primary, other_primary)
+                && opt_plan_node_matches(rest, other_rest)
+        }
+        (
+            PlanNode::Condition {
+                condition,
+                if_clause,
+                else_clause,
+            },
+            PlanNode::Condition {
+                condition: other_condition,
+                if_clause: other_if_clause,
+                else_clause: other_else_clause,
+            },
+        ) => {
+            condition == other_condition
+                && opt_plan_node_matches(if_clause, other_if_clause)
+                && opt_plan_node_matches(else_clause, other_else_clause)
+        }
+        _ => false,
+    }
+}
+
+fn defer_primary_node_matches(this: &Primary, other: &Primary) -> bool {
+    let Primary { subselection, node } = this;
+    *subselection == other.subselection && opt_plan_node_matches(node, &other.node)
+}
+
+fn deferred_node_matches(this: &DeferredNode, other: &DeferredNode) -> bool {
+    let DeferredNode {
+        depends,
+        label,
+        query_path,
+        subselection,
+        node,
+    } = this;
+    *depends == other.depends
+        && *label == other.label
+        && *query_path == other.query_path
+        && *subselection == other.subselection
+        && opt_plan_node_matches(node, &other.node)
+}
+
+fn flatten_node_matches(this: &FlattenNode, other: &FlattenNode) -> bool {
+    let FlattenNode { path, node } = this;
+    *path == other.path && plan_node_matches(node, &other.node)
 }
