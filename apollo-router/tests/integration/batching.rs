89e744de2ae5081fbc0e7c1b0c225ae85cf455af
Gary Pennington <gary@apollographql.com>
2024-07-15 09:14:03 +0100

    rhai update to 1.19.0

--- apollo-router/tests/integration/batching.rs
+++ apollo-router/tests/integration/batching.rs
@@ -377,12 +377,12 @@ async fn it_handles_cancelled_by_rhai() -> Result<(), BoxError> {
         entryA:
           index: 0
     - errors:
-        - message: "rhai execution error: 'Runtime error: cancelled expected failure (line 5, position 13)\nin closure call'"
+        - message: "rhai execution error: 'Runtime error: cancelled expected failure (line 5, position 13)'"
     - data:
         entryA:
           index: 1
     - errors:
-        - message: "rhai execution error: 'Runtime error: cancelled expected failure (line 5, position 13)\nin closure call'"
+        - message: "rhai execution error: 'Runtime error: cancelled expected failure (line 5, position 13)'"
     "###);
     }
 
@@ -471,7 +471,7 @@ async fn it_handles_single_request_cancelled_by_rhai() -> Result<(), BoxError> {
         entryA:
           index: 1
     - errors:
-        - message: "rhai execution error: 'Runtime error: cancelled expected failure (line 5, position 13)\nin closure call'"
+        - message: "rhai execution error: 'Runtime error: cancelled expected failure (line 5, position 13)'"
     "###);
     }
 
