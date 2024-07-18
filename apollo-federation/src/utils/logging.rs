b27b841ff3674f196d8b31ceb26ed7aff08b138e
TylerBloom <tylerbloom2222@gmail.com>
2024-07-18 12:07:14 -0400

    Updated test supergraph. Removed doc test from snapshot! doc comment.

--- apollo-federation/src/utils/logging.rs
+++ apollo-federation/src/utils/logging.rs
@@ -7,7 +7,7 @@
 /// for the value you'd like to take a snapshot of. This will tag the snapshot type with the type
 /// name of the value, create data that is JSON string using serde_json, and add the message
 /// literal that you pass in. EX:
-/// ```
+/// ```no_test
 /// snapshot!(dependency_graph, "updated dependency graph");
 /// // Generates:
 /// // trace!(snapshot = "FetchDependencyGraph", data = "{ .. }", "updated dependency graph");
@@ -15,7 +15,7 @@
 /// If you do not want to serialize the data, you can pass the name tag for the snapshot and data
 /// in directly. Note that the data needs to implement the tracing crate's `Value` trait. Ideally,
 /// this is a string representation of the data you're snapshotting. EX:
-/// ```
+/// ```no_test
 /// snapshot!("FetchDependencyGraph", dependency_graph.to_string(), "updated dependency graph");
 /// // Generates:
 /// // trace!(snapshot = "FetchDependencyGraph", data = dependency_graph.to_string(), "updated dependency graph");
