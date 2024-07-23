32106c52d6921e37114215fa827376753f754763
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2024-07-23 10:22:57 +0200

    add more tests
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- apollo-router/src/plugins/cache/invalidation_endpoint.rs
+++ apollo-router/src/plugins/cache/invalidation_endpoint.rs
@@ -213,7 +213,263 @@ mod tests {
     use crate::Notify;
 
     #[tokio::test]
+    async fn test_invalidation_service_bad_shared_key() {
+        #[allow(clippy::type_complexity)]
+        let mut notify: Notify<
+            InvalidationTopic,
+            (
+                Vec<InvalidationRequest>,
+                InvalidationOrigin,
+                Sender<Result<u64, InvalidationError>>,
+            ),
+        > = Notify::new(None, None, None);
+        let (handle, _b) = notify
+            .create_or_subscribe(InvalidationTopic, false)
+            .await
+            .unwrap();
+        let invalidation = Invalidation {
+            enabled: true,
+            handle,
+        };
+        let config = Arc::new(SubgraphConfiguration {
+            all: Subgraph {
+                ttl: None,
+                enabled: true,
+                private_id: None,
+                invalidation: Some(SubgraphInvalidationConfig {
+                    enabled: true,
+                    shared_key: String::from("test"),
+                }),
+            },
+            subgraphs: HashMap::new(),
+        });
+        let service = InvalidationService::new(config, invalidation);
+        let req = router::Request::fake_builder()
+            .method(http::Method::POST)
+            .header(AUTHORIZATION, "testttt")
+            .body(
+                serde_json::to_vec(&[
+                    InvalidationRequest::Subgraph {
+                        subgraph: String::from("test"),
+                    },
+                    InvalidationRequest::Type {
+                        subgraph: String::from("test"),
+                        r#type: String::from("Test"),
+                    },
+                ])
+                .unwrap(),
+            )
+            .build()
+            .unwrap();
+        let res = service.oneshot(req).await.unwrap();
+        assert_eq!(res.response.status(), StatusCode::UNAUTHORIZED);
+    }
+
+    #[tokio::test]
+    async fn test_invalidation_service_good_sub_shared_key() {
+        #[allow(clippy::type_complexity)]
+        let mut notify: Notify<
+            InvalidationTopic,
+            (
+                Vec<InvalidationRequest>,
+                InvalidationOrigin,
+                Sender<Result<u64, InvalidationError>>,
+            ),
+        > = Notify::new(None, None, None);
+        let (handle, _b) = notify
+            .create_or_subscribe(InvalidationTopic, false)
+            .await
+            .unwrap();
+        let h = handle.clone();
+
+        tokio::task::spawn(async move {
+            let mut handle = h.into_stream();
+            let mut called = false;
+            while let Some((requests, origin, response_tx)) = handle.next().await {
+                called = true;
+                if requests
+                    != [
+                        InvalidationRequest::Subgraph {
+                            subgraph: String::from("test"),
+                        },
+                        InvalidationRequest::Type {
+                            subgraph: String::from("test"),
+                            r#type: String::from("Test"),
+                        },
+                    ]
+                {
+                    response_tx
+                        .send(Err(InvalidationError::Custom(format!(
+                            "it's not the right invalidation requests : {requests:?}"
+                        ))))
+                        .unwrap();
+                    return;
+                }
+                if origin != InvalidationOrigin::Endpoint {
+                    response_tx
+                        .send(Err(InvalidationError::Custom(format!(
+                            "it's not the right invalidation origin : {origin:?}"
+                        ))))
+                        .unwrap();
+                    return;
+                }
+                response_tx.send(Ok(0)).unwrap();
+            }
+            assert!(called);
+        });
+
+        let invalidation = Invalidation {
+            enabled: true,
+            handle: handle.clone(),
+        };
+        let config = Arc::new(SubgraphConfiguration {
+            all: Subgraph {
+                ttl: None,
+                enabled: true,
+                private_id: None,
+                invalidation: Some(SubgraphInvalidationConfig {
+                    enabled: true,
+                    shared_key: String::from("test"),
+                }),
+            },
+            subgraphs: [(
+                String::from("test"),
+                Subgraph {
+                    ttl: None,
+                    enabled: true,
+                    private_id: None,
+                    invalidation: Some(SubgraphInvalidationConfig {
+                        enabled: true,
+                        shared_key: String::from("test_test"),
+                    }),
+                },
+            )]
+            .into_iter()
+            .collect(),
+        });
+        let service = InvalidationService::new(config, invalidation);
+        let req = router::Request::fake_builder()
+            .method(http::Method::POST)
+            .header(AUTHORIZATION, "test_test")
+            .body(
+                serde_json::to_vec(&[
+                    InvalidationRequest::Subgraph {
+                        subgraph: String::from("test"),
+                    },
+                    InvalidationRequest::Type {
+                        subgraph: String::from("test"),
+                        r#type: String::from("Test"),
+                    },
+                ])
+                .unwrap(),
+            )
+            .build()
+            .unwrap();
+        let res = service.oneshot(req).await.unwrap();
+        assert_eq!(res.response.status(), StatusCode::ACCEPTED);
+        let h = handle.clone();
+
+        tokio::task::spawn(async move {
+            let mut handle = h.into_stream();
+            let mut called = false;
+            while let Some((requests, origin, response_tx)) = handle.next().await {
+                called = true;
+                if requests
+                    != [
+                        InvalidationRequest::Subgraph {
+                            subgraph: String::from("test"),
+                        },
+                        InvalidationRequest::Type {
+                            subgraph: String::from("test"),
+                            r#type: String::from("Test"),
+                        },
+                    ]
+                {
+                    response_tx
+                        .send(Err(InvalidationError::Custom(format!(
+                            "it's not the right invalidation requests : {requests:?}"
+                        ))))
+                        .unwrap();
+                    return;
+                }
+                if origin != InvalidationOrigin::Endpoint {
+                    response_tx
+                        .send(Err(InvalidationError::Custom(format!(
+                            "it's not the right invalidation origin : {origin:?}"
+                        ))))
+                        .unwrap();
+                    return;
+                }
+                response_tx.send(Ok(0)).unwrap();
+            }
+            assert!(called);
+        });
+    }
+
+    #[tokio::test]
+    async fn test_invalidation_service_bad_shared_key_subgraph() {
+        #[allow(clippy::type_complexity)]
+        let mut notify: Notify<
+            InvalidationTopic,
+            (
+                Vec<InvalidationRequest>,
+                InvalidationOrigin,
+                Sender<Result<u64, InvalidationError>>,
+            ),
+        > = Notify::new(None, None, None);
+        let (handle, _b) = notify
+            .create_or_subscribe(InvalidationTopic, false)
+            .await
+            .unwrap();
+        let invalidation = Invalidation {
+            enabled: true,
+            handle,
+        };
+        let config = Arc::new(SubgraphConfiguration {
+            all: Subgraph {
+                ttl: None,
+                enabled: true,
+                private_id: None,
+                invalidation: Some(SubgraphInvalidationConfig {
+                    enabled: true,
+                    shared_key: String::from("test"),
+                }),
+            },
+            subgraphs: [(
+                String::from("test"),
+                Subgraph {
+                    ttl: None,
+                    enabled: true,
+                    private_id: None,
+                    invalidation: Some(SubgraphInvalidationConfig {
+                        enabled: true,
+                        shared_key: String::from("test_test"),
+                    }),
+                },
+            )]
+            .into_iter()
+            .collect(),
+        });
+        // Trying to invalidation with shared_key on subgraph test for a subgraph foo
+        let service = InvalidationService::new(config, invalidation);
+        let req = router::Request::fake_builder()
+            .method(http::Method::POST)
+            .header(AUTHORIZATION, "test_test")
+            .body(
+                serde_json::to_vec(&[InvalidationRequest::Subgraph {
+                    subgraph: String::from("foo"),
+                }])
+                .unwrap(),
+            )
+            .build()
+            .unwrap();
+        let res = service.oneshot(req).await.unwrap();
+        assert_eq!(res.response.status(), StatusCode::UNAUTHORIZED);
+    }
+
+    #[tokio::test]
     async fn test_invalidation_service() {
+        #[allow(clippy::type_complexity)]
         let mut notify: Notify<
             InvalidationTopic,
             (
@@ -308,6 +564,6 @@ mod tests {
             )
             .unwrap(),
             serde_json::json!({"count": 2})
-        )
+        );
     }
 }
