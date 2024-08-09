9584796baec11122828b48a4d50c227e0f5e01cf
Duckki Oe <duckki.oe@apollographql.com>
2024-08-09 12:57:01 -0700

    added a test reproducing the panic

--- apollo-federation/tests/query_plan/build_query_plan_tests.rs
+++ apollo-federation/tests/query_plan/build_query_plan_tests.rs
@@ -720,3 +720,98 @@ fn defer_gets_stripped_out() {
     );
     assert_eq!(plan_one, plan_two)
 }
+
+#[test]
+fn test_merging_fetches_do_not_create_cycle_in_fetch_dependency_graph() {
+    // This is a test for ROUTER-546 (the second part).
+    let planner = planner!(
+        S: r#"
+          type Query {
+            start: T!
+          }
+
+          type T @key(fields: "id") {
+            id: String!
+          }
+          "#,
+        A: r#"
+          type T @key(fields: "id") {
+            id: String! @shareable
+            u: U! @shareable
+          }
+
+          type U @key(fields: "id") {
+            id: ID!
+            a: String! @shareable
+            b: String @shareable
+          }
+          "#,
+        B: r#"
+          type T @key(fields: "id") {
+            id: String! @external
+            u: U! @shareable
+          }
+
+          type U @key(fields: "id") {
+            id: ID!
+            a: String! @shareable
+            # Note: b is not here.
+          }
+
+          # This definition is necessary.
+          extend type W @key(fields: "id") {
+            id: ID @external
+          }
+          "#,
+        C: r#"
+          extend type U @key(fields: "id") {
+            id: ID! @external
+            a: String! @external
+            b: String @external
+            w: W @requires(fields: "a b")
+          }
+
+          type W @key(fields: "id") {
+            id: ID
+            y: Y
+            w1: Int
+            w2: Int
+            w3: Int
+            w4: Int
+            w5: Int
+          }
+
+          type Y {
+            y1: Int
+            y2: Int
+            y3: Int
+          }
+          "#,
+    );
+    assert_plan!(
+        &planner,
+        r#"
+          {
+            start {
+              u {
+                w {
+                  id
+                  w1
+                  w2
+                  w3
+                  w4
+                  w5
+                  y {
+                    y1
+                    y2
+                    y3
+                  }
+                }
+              }
+            }
+          }
+        "#,
+        @r###"
+        "###
+    );
+}
