cbe91c65d8815f48128251dab1075e7d124bc494
Dylan Anthony <dylan@apollographql.com>
2024-08-19 16:01:53 -0600

    prep release: v2.0.0-alpha.3

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ BINARY_DOWNLOAD_PREFIX="https://github.com/apollographql/router/releases/downloa
 
 # Router version defined in apollo-router's Cargo.toml
 # Note: Change this line manually during the release steps.
-PACKAGE_VERSION="v2.0.0-alpha.2"
+PACKAGE_VERSION="v2.0.0-alpha.3"
 
 download_binary() {
     downloader --check
