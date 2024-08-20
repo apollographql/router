1c7b97b4cedc820e7b7b9a6ae75b8552af897c7d
Lenny Burdette <lenny@apollographql.com>
2024-08-19 21:53:09 -0400

    squash lint

--- apollo-federation/src/sources/connect/validation/extended_type.rs
+++ apollo-federation/src/sources/connect/validation/extended_type.rs
@@ -166,19 +166,17 @@ fn validate_field(
     };
 
     // direct recursion isn't allowed, like a connector on User.friends: [User]
-    if matches!(category, ObjectCategory::Other) {
-        if &object.name == field.ty.inner_named_type() {
-            errors.push(Message {
+    if matches!(category, ObjectCategory::Other) && &object.name == field.ty.inner_named_type() {
+        errors.push(Message {
                     code: Code::CircularReference,
                     message: format!(
                         "Direct circular reference detected in `{}.{}: {}`. For more information, see https://go.apollo.dev/connectors/limitations#circular-references",
                         object.name,
                         field.name,
-                        field.ty.to_string()
+                        field.ty
                     ),
                     locations: field.line_column_range(source_map).into_iter().collect(),
                 });
-        }
     }
 
     errors.extend(validate_selection(field, connect_directive, object, schema));
