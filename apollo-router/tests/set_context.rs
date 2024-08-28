a09401cf85edc2fb3907adcc72fb089ce0895eb7
o0Ignition0o <jeremy.lempereur@gmail.com>
2024-08-28 15:30:47 +0200

    forgot to remove tcf in the context tests

--- apollo-router/tests/set_context.rs
+++ apollo-router/tests/set_context.rs
@@ -35,7 +35,6 @@ macro_rules! snap
 async fn run_single_request(query: &str, mocks: &[(&'static str, &'static str)]) -> Response {
     let harness = setup_from_mocks(
         json! {{
-            "experimental_type_conditioned_fetching": true,
             // will make debugging easier
             "plugins": {
                 "experimental.expose_query_plan": true
