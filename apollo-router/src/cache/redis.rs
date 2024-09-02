f509257572aa7734d4a98c0e778dc4543f338f79
Geoffroy Couprie <apollo@geoffroycouprie.com>
2024-09-02 18:36:05 +0200

    test redis connection pooling

--- apollo-router/src/cache/redis.rs
+++ apollo-router/src/cache/redis.rs
@@ -12,6 +12,7 @@ use fred::prelude::KeysInterface;
 use fred::prelude::RedisClient;
 use fred::prelude::RedisError;
 use fred::prelude::RedisErrorKind;
+use fred::prelude::RedisPool;
 use fred::types::ClusterRouting;
 use fred::types::Expiration;
 use fred::types::FromRedis;
@@ -52,7 +53,7 @@ where
 
 #[derive(Clone)]
 pub(crate) struct RedisCacheStorage {
-    inner: Arc<RedisClient>,
+    inner: Arc<RedisPool>,
     namespace: Option<Arc<String>>,
     pub(crate) ttl: Option<Duration>,
     is_cluster: bool,
@@ -168,7 +169,7 @@ impl RedisCacheStorage {
             });
         }
 
-        let client = RedisClient::new(
+        let client = RedisPool::new(
             client_config,
             Some(PerformanceConfig {
                 default_command_timeout: config.timeout.unwrap_or(Duration::from_millis(500)),
@@ -176,10 +177,12 @@ impl RedisCacheStorage {
             }),
             None,
             Some(ReconnectPolicy::new_exponential(0, 1, 2000, 5)),
-        );
+            3,
+        )
+        .unwrap();
         let _handle = client.connect();
 
-        // spawn tasks that listen for connection close or reconnect events
+        /*// spawn tasks that listen for connection close or reconnect events
         let mut error_rx = client.error_rx();
         let mut reconnect_rx = client.reconnect_rx();
 
@@ -192,7 +195,7 @@ impl RedisCacheStorage {
             while reconnect_rx.recv().await.is_ok() {
                 tracing::info!("Redis client reconnected.");
             }
-        });
+        });*/
 
         // a TLS connection to a TCP Redis could hang, so we add a timeout
         tokio::time::timeout(Duration::from_secs(5), client.wait_for_connect())
@@ -218,7 +221,7 @@ impl RedisCacheStorage {
             ..Default::default()
         };
 
-        let client = RedisClient::new(
+        let client = RedisPool::new(
             client_config,
             Some(PerformanceConfig {
                 default_command_timeout: Duration::from_millis(2),
@@ -226,10 +229,12 @@ impl RedisCacheStorage {
             }),
             None,
             Some(ReconnectPolicy::new_exponential(0, 1, 2000, 5)),
-        );
+            1,
+        )
+        .unwrap();
         let _handle = client.connect();
 
-        // spawn tasks that listen for connection close or reconnect events
+        /*// spawn tasks that listen for connection close or reconnect events
         let mut error_rx = client.error_rx();
         let mut reconnect_rx = client.reconnect_rx();
 
@@ -242,7 +247,7 @@ impl RedisCacheStorage {
             while reconnect_rx.recv().await.is_ok() {
                 tracing::info!("Redis client reconnected.");
             }
-        });
+        });*/
 
         // a TLS connection to a TCP Redis could hang, so we add a timeout
         tokio::time::timeout(Duration::from_secs(5), client.wait_for_connect())
@@ -370,7 +375,7 @@ impl RedisCacheStorage {
         key: RedisKey<K>,
     ) -> Option<RedisValue<V>> {
         if self.reset_ttl && self.ttl.is_some() {
-            let pipeline: fred::clients::Pipeline<RedisClient> = self.inner.pipeline();
+            let pipeline: fred::clients::Pipeline<RedisClient> = self.inner.next().pipeline();
             let key = self.make_key(key);
             let res = pipeline
                 .get::<fred::types::RedisValue, _>(&key)
@@ -541,7 +546,7 @@ impl RedisCacheStorage {
             None => self.inner.mset(data.to_owned()).await,
             Some(ttl) => {
                 let expiration = Some(Expiration::EX(ttl.as_secs() as i64));
-                let pipeline = self.inner.pipeline();
+                let pipeline = self.inner.next().pipeline();
 
                 for (key, value) in data {
                     let _ = pipeline
@@ -593,9 +598,9 @@ impl RedisCacheStorage {
         count: Option<u32>,
     ) -> Pin<Box<dyn Stream<Item = Result<ScanResult, RedisError>> + Send>> {
         if self.is_cluster {
-            Box::pin(self.inner.scan_cluster(pattern, count, None))
+            Box::pin(self.inner.next().scan_cluster(pattern, count, None))
         } else {
-            Box::pin(self.inner.scan(pattern, count, None))
+            Box::pin(self.inner.next().scan(pattern, count, None))
         }
     }
 }
