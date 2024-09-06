cac75c7602400efaaf7c162b1b287ab2ae712fb7
Lenny Burdette <lenny@apollographql.com>
2024-09-06 11:49:26 -0400

    validate uses of `*`

--- apollo-federation/src/sources/connect/validation/mod.rs
+++ apollo-federation/src/sources/connect/validation/mod.rs
@@ -457,6 +457,8 @@ pub enum Code {
     UnresolvedField,
     /// A field resolved by a connector has arguments defined
     FieldWithArguments,
+    /// Invalid star selection
+    InvalidStarSelection,
 }
 
 impl Code {
