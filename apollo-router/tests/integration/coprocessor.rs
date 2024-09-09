779ea024e2799440da676d146c9a3127cd595ee4
Taylor Ninesling <taylor.ninesling@apollographql.com>
2024-09-09 17:15:35 -0500

    Ensure coprocessors have access to demand control context

--- apollo-router/tests/integration/coprocessor.rs
+++ apollo-router/tests/integration/coprocessor.rs
@@ -82,3 +82,77 @@ async fn test_coprocessor_limit_payload() -> Result<(), BoxError> {
     router.graceful_shutdown().await;
     Ok(())
 }
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_coprocessor_demand_control_access() -> Result<(), BoxError> {
+    if !graph_os_enabled() {
+        return Ok(());
+    }
+
+    let mock_server = wiremock::MockServer::start().await;
+    let coprocessor_address = mock_server.uri();
+
+    // Assert the execution request stage has access to the estimated cost
+    Mock::given(method("POST"))
+        .and(path("/"))
+        .and(body_partial_json(json!({
+        "stage": "ExecutionRequest",
+        "context": {
+            "entries": {
+                "cost.estimated": 10.0,
+                "cost.result": "COST_OK",
+                "cost.strategy": "static_estimated"
+            }}})))
+        .respond_with(ResponseTemplate::new(200).set_body_json(json!({
+                        "version":1,
+                        "stage":"ExecutionRequest",
+                        "control":"continue",
+        })))
+        .expect(1)
+        .mount(&mock_server)
+        .await;
+
+    // Assert the supergraph response stage also includes the actual cost
+    Mock::given(method("POST"))
+        .and(path("/"))
+        .and(body_partial_json(json!({
+            "stage": "SupergraphResponse",
+            "context": {"entries": {
+            "cost.actual": 3.0,
+            "cost.estimated": 10.0,
+            "cost.result": "COST_OK",
+            "cost.strategy": "static_estimated"
+        }}})))
+        .respond_with(ResponseTemplate::new(200).set_body_json(json!({
+                        "version":1,
+                        "stage":"SupergraphResponse",
+                        "control":"continue",
+        })))
+        .expect(1)
+        .mount(&mock_server)
+        .await;
+
+    let mut router = IntegrationTest::builder()
+        .config(
+            include_str!("fixtures/coprocessor_demand_control.router.yaml")
+                .replace("<replace>", &coprocessor_address),
+        )
+        .build()
+        .await;
+
+    router.start().await;
+    router.assert_started().await;
+
+    let (_trace_id, response) = router.execute_default_query().await;
+    println!("Received requests:");
+    let received_requests = mock_server.received_requests().await.unwrap();
+    for req in received_requests {
+        let body: serde_json::Value = req.body_json().unwrap();
+        println!("{}", body)
+    }
+    assert_eq!(response.status(), 200);
+
+    router.graceful_shutdown().await;
+
+    Ok(())
+}
