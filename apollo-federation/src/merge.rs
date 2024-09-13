5b1d139c1470a4f92704fcd4097ad9362df9e3c3
Lenny Burdette <lenny@apollographql.com>
2024-09-13 11:24:51 -0400

    fix: merge interface field arguments

--- apollo-federation/src/merge.rs
+++ apollo-federation/src/merge.rs
@@ -510,6 +510,36 @@ impl Merger {
                     }
                 };
 
+                for arg in field.arguments.iter() {
+                    let arguments_to_merge = &mut supergraph_field.make_mut().arguments;
+                    let argument_to_merge = arguments_to_merge
+                        .iter_mut()
+                        .find_map(|a| (a.name == arg.name).then(|| a.make_mut()));
+
+                    if let Some(argument) = argument_to_merge {
+                        self.add_inaccessible(
+                            directive_names,
+                            &mut argument.directives,
+                            &arg.directives,
+                        );
+                    } else {
+                        let mut argument = InputValueDefinition {
+                            name: arg.name.clone(),
+                            description: arg.description.clone(),
+                            directives: Default::default(),
+                            ty: arg.ty.clone(),
+                            default_value: arg.default_value.clone(),
+                        };
+
+                        self.add_inaccessible(
+                            directive_names,
+                            &mut argument.directives,
+                            &arg.directives,
+                        );
+                        arguments_to_merge.push(argument.into());
+                    };
+                }
+
                 self.merge_descriptions(
                     &mut supergraph_field.make_mut().description,
                     &field.description,
