897e12aaf544002e48d4e132fb872618b9a06579
Jesse Rosenberger <git@jro.cc>
2024-09-26 17:55:52 +0300

    prep release: v1.52.2-rc.0

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ BINARY_DOWNLOAD_PREFIX="https://github.com/apollographql/router/releases/downloa
 
 # Router version defined in apollo-router's Cargo.toml
 # Note: Change this line manually during the release steps.
-PACKAGE_VERSION="v1.52.1"
+PACKAGE_VERSION="v1.52.2-rc.0"
 
 download_binary() {
     downloader --check
