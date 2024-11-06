a470dd63b6565e65951111eed3f12db5cf50c3c9
Renée Kooi <renee.kooi@apollographql.com>
2024-11-06 12:10:33 +0100

    Do not report usage to Studio when variable values are invalid

--- apollo-router/src/services/supergraph/service.rs
+++ apollo-router/src/services/supergraph/service.rs
@@ -74,6 +74,7 @@ use crate::services::SupergraphRequest;
 use crate::services::SupergraphResponse;
 use crate::spec::operation_limits::OperationLimits;
 use crate::spec::Schema;
+use crate::spec::GRAPHQL_VALIDATION_FAILURE_ERROR_KEY;
 use crate::Configuration;
 use crate::Context;
 use crate::Notify;
@@ -169,7 +170,7 @@ async fn service_call(
 ) -> Result<SupergraphResponse, BoxError> {
     let context = req.context;
     let body = req.supergraph_request.body();
-    let variables = body.variables.clone();
+    let variables = &body.variables;
 
     let QueryPlannerResponse {
         content,
@@ -234,7 +235,7 @@ async fn service_call(
                 let _ = lock.insert::<OperationLimits<u32>>(query_metrics);
             });
 
-            let is_deferred = plan.is_deferred(&variables);
+            let is_deferred = plan.is_deferred(variables);
             let is_subscription = plan.is_subscription();
 
             if let Some(batching) = context
@@ -266,7 +267,7 @@ async fn service_call(
                     .extensions()
                     .with_lock(|lock| lock.get::<BatchQuery>().cloned());
                 if let Some(batch_query) = batch_query_opt {
-                    let query_hashes = plan.query_hashes(batching, &variables)?;
+                    let query_hashes = plan.query_hashes(batching, variables)?;
                     batch_query
                         .set_query_hashes(query_hashes)
                         .await
@@ -304,6 +305,15 @@ async fn service_call(
                 *response.response.status_mut() = StatusCode::NOT_ACCEPTABLE;
                 Ok(response)
             } else if let Some(err) = plan.query.validate_variables(body, &schema).err() {
+                // Replace the existing usage report so we do not report invalid variable values to
+                // Studio.
+                context.extensions().with_lock(|mut lock| {
+                    lock.insert(Arc::new(UsageReporting {
+                        stats_report_key: GRAPHQL_VALIDATION_FAILURE_ERROR_KEY.to_string(),
+                        referenced_fields_by_type: Default::default(),
+                    }))
+                });
+
                 let mut res = SupergraphResponse::new_from_graphql_response(err, context);
                 *res.response.status_mut() = StatusCode::BAD_REQUEST;
                 Ok(res)
