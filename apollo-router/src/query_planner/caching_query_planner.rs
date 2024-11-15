d6135f69b55f61e67f56f1838820f770113ffceb
Geoffroy Couprie <apollo@geoffroycouprie.com>
2024-11-15 11:02:44 +0100

    Fix introspection query deduplication (#6257)
    Introspection queries are not supposed to be cached, but their result still needs to be sent to deduplicated queries. This also fixes a case where errors returned by `poll_ready` were not sent to deduplicated queries

--- apollo-router/src/query_planner/caching_query_planner.rs
+++ apollo-router/src/query_planner/caching_query_planner.rs
@@ -518,7 +518,19 @@ where
             // of restarting the query planner until another timeout
             tokio::task::spawn(
                 async move {
-                    let res = self.delegate.ready().await?.call(request).await;
+                    let service = match self.delegate.ready().await {
+                        Ok(service) => service,
+                        Err(error) => {
+                            let e = Arc::new(error);
+                            let err = e.clone();
+                            tokio::spawn(async move {
+                                entry.insert(Err(err)).await;
+                            });
+                            return Err(CacheResolverError::RetrievalError(e));
+                        }
+                    };
+
+                    let res = service.call(request).await;
 
                     match res {
                         Ok(QueryPlannerResponse { content, errors }) => {
@@ -535,6 +547,10 @@ where
                                 if can_cache {
                                     tokio::spawn(async move {
                                         entry.insert(Ok(content)).await;
+                                    });
+                                } else {
+                                    tokio::spawn(async move {
+                                        entry.send(Ok(content)).await;
                                     });
                                 }
                             }
