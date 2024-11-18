a8220c6cfb909e67becf0379b62f4b69a983aa83
Ben Newman <ben@apollographql.com>
2024-11-18 17:15:27 -0500

    Implement `ApplyToInternal::compute_output_shape` trait method.

--- apollo-federation/src/sources/connect/json_selection/apply_to.rs
+++ apollo-federation/src/sources/connect/json_selection/apply_to.rs
@@ -7,6 +7,8 @@ use apollo_compiler::collections::IndexSet;
 use serde_json_bytes::json;
 use serde_json_bytes::Map as JSONMap;
 use serde_json_bytes::Value as JSON;
+use shape::Shape;
+use shape::ShapeCase;
 
 use super::helpers::json_type_name;
 use super::immutable::InputPath;
@@ -69,6 +71,44 @@ impl JSONSelection {
 
         (value, errors.into_iter().collect())
     }
+
+    pub fn static_shape(&self) -> Shape {
+        self.compute_output_shape(
+            // If we don't know anything about the shape of the input data, we
+            // can represent the data symbolically using the $root variable
+            // shape. Subproperties needed from this shape will show up as
+            // subpaths like $root.books.4.isbn in the output shape.
+            //
+            // While we do not currently have a $root variable available as a
+            // KnownVariable during apply_to_path execution, we might consider
+            // adding it, since it would align with the way we process other
+            // variable shapes. For now, $root exists only as a shape name that
+            // we are inventing right here.
+            Shape::name("$root"),
+            // If we wanted to specify anything about the shape of the $root
+            // variable, we could define a shape for "$root" in this map.
+            &IndexMap::default(),
+        )
+    }
+
+    pub fn compute_output_shape(
+        &self,
+        input_shape: Shape,
+        named_var_shapes: &IndexMap<&str, Shape>,
+    ) -> Shape {
+        match self {
+            Self::Named(selection) => selection.compute_output_shape(
+                input_shape.clone(),
+                input_shape.clone(),
+                named_var_shapes,
+            ),
+            Self::Path(path_selection) => path_selection.compute_output_shape(
+                input_shape.clone(),
+                input_shape.clone(),
+                named_var_shapes,
+            ),
+        }
+    }
 }
 
 pub(super) trait ApplyToInternal {
@@ -104,6 +144,25 @@ pub(super) trait ApplyToInternal {
 
         (Some(JSON::Array(output)), errors)
     }
+
+    /// Computes the static output shape produced by a JSONSelection, by
+    /// traversing the selection AST, recursively calling `compute_output_shape`
+    /// on the current data/variable shapes at each level.
+    fn compute_output_shape(
+        &self,
+        // Shape of the `@` variable, which typically changes with each
+        // recursive call to compute_output_shape.
+        input_shape: Shape,
+        // Shape of the `$` variable, which is bound to the closest enclosing
+        // subselection object, or the root data object if there is no enclosing
+        // subselection.
+        dollar_shape: Shape,
+        // Shapes of other named variables, with the variable name `String`
+        // including the initial `$` character. This map typically does not
+        // change during the compute_output_shape recursion, and so can be
+        // passed down by immutable reference.
+        named_var_shapes: &IndexMap<&str, Shape>,
+    ) -> Shape;
 }
 
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
@@ -214,6 +273,22 @@ impl ApplyToInternal for JSONSelection {
             Self::Path(path_selection) => path_selection.apply_to_path(data, vars, input_path),
         }
     }
+
+    fn compute_output_shape(
+        &self,
+        input_shape: Shape,
+        dollar_shape: Shape,
+        named_var_shapes: &IndexMap<&str, Shape>,
+    ) -> Shape {
+        match self {
+            Self::Named(selection) => {
+                selection.compute_output_shape(input_shape, dollar_shape, named_var_shapes)
+            }
+            Self::Path(path_selection) => {
+                path_selection.compute_output_shape(input_shape, dollar_shape, named_var_shapes)
+            }
+        }
+    }
 }
 
 impl ApplyToInternal for NamedSelection {
@@ -317,6 +392,52 @@ impl ApplyToInternal for NamedSelection {
 
         (Some(JSON::Object(output)), errors)
     }
+
+    fn compute_output_shape(
+        &self,
+        input_shape: Shape,
+        dollar_shape: Shape,
+        named_var_shapes: &IndexMap<&str, Shape>,
+    ) -> Shape {
+        let mut output = Shape::empty_map();
+
+        match self {
+            Self::Field(alias_opt, key, selection) => {
+                let output_key = alias_opt
+                    .as_ref()
+                    .map_or(key.as_str(), |alias| alias.name());
+                let field_shape = dollar_shape.field(key.as_str());
+                output.insert(
+                    output_key.to_string(),
+                    if let Some(selection) = selection {
+                        selection.compute_output_shape(field_shape, dollar_shape, named_var_shapes)
+                    } else {
+                        field_shape
+                    },
+                );
+            }
+            Self::Path(alias_opt, path_selection) => {
+                let path_shape = path_selection.compute_output_shape(
+                    input_shape,
+                    dollar_shape,
+                    named_var_shapes,
+                );
+                if let Some(alias) = alias_opt {
+                    output.insert(alias.name().to_string(), path_shape);
+                } else {
+                    return path_shape;
+                }
+            }
+            Self::Group(alias, sub_selection) => {
+                output.insert(
+                    alias.name().to_string(),
+                    sub_selection.compute_output_shape(input_shape, dollar_shape, named_var_shapes),
+                );
+            }
+        };
+
+        ShapeCase::Object(output, Shape::none()).simplify()
+    }
 }
 
 impl ApplyToInternal for PathSelection {
@@ -341,6 +462,31 @@ impl ApplyToInternal for PathSelection {
             // guarantee its existence at compile time, somehow.
             // (PathList::Key(_, _), None) => todo!(),
             _ => self.path.apply_to_path(data, vars, input_path),
+        }
+    }
+
+    fn compute_output_shape(
+        &self,
+        input_shape: Shape,
+        dollar_shape: Shape,
+        named_var_shapes: &IndexMap<&str, Shape>,
+    ) -> Shape {
+        match self.path.as_ref() {
+            PathList::Key(_, _) => {
+                // If this is a KeyPath, we need to evaluate the path starting
+                // from the current $ shape, so we pass dollar_shape as the data
+                // *and* dollar_shape to self.path.compute_output_shape.
+                self.path.compute_output_shape(
+                    dollar_shape.clone(),
+                    dollar_shape.clone(),
+                    named_var_shapes,
+                )
+            }
+            // If this is not a KeyPath, keep evaluating against input_shape.
+            // This logic parallels PathSelection::apply_to_path (above).
+            _ => self
+                .path
+                .compute_output_shape(input_shape, dollar_shape, named_var_shapes),
         }
     }
 }
@@ -451,6 +597,138 @@ impl ApplyToInternal for WithRange<PathList> {
             }
         }
     }
+
+    fn compute_output_shape(
+        &self,
+        input_shape: Shape,
+        dollar_shape: Shape,
+        named_var_shapes: &IndexMap<&str, Shape>,
+    ) -> Shape {
+        match self.as_ref() {
+            PathList::Var(ranged_var_name, tail) => {
+                let var_name = ranged_var_name.as_ref();
+                let var_shape = if var_name == &KnownVariable::AtSign {
+                    input_shape
+                } else if var_name == &KnownVariable::Dollar {
+                    dollar_shape.clone()
+                } else if let Some(shape) = named_var_shapes.get(var_name.as_str()) {
+                    shape.clone()
+                } else {
+                    Shape::name(var_name.as_str())
+                };
+                tail.compute_output_shape(var_shape, dollar_shape, named_var_shapes)
+            }
+
+            PathList::Key(key, tail) => {
+                // If this is the first key in the path,
+                // PathSelection::compute_output_shape will have set our
+                // input_shape equal to its dollar_shape, thereby ensuring that
+                // some.nested.path is equivalent to $.some.nested.path.
+                if input_shape.is_none() {
+                    // Following WithRange<PathList>::apply_to_path, we do not
+                    // want to call tail.compute_output_shape recursively with
+                    // an input data shape corresponding to missing data, though
+                    // it might do the right thing.
+                    return input_shape;
+                }
+
+                if let ShapeCase::Array(prefix, rest) = input_shape.case() {
+                    // Map tail.compute_output_shape over the prefix and rest
+                    // elements of the array shape, so we don't have to map
+                    // array shapes for the other PathList variants.
+                    let mapped_prefix = prefix
+                        .iter()
+                        .map(|shape| {
+                            if shape.is_none() {
+                                shape.clone()
+                            } else {
+                                tail.compute_output_shape(
+                                    shape.field_shape(key.as_str()),
+                                    dollar_shape.clone(),
+                                    named_var_shapes,
+                                )
+                            }
+                        })
+                        .collect();
+
+                    let mapped_rest = if rest.is_none() {
+                        rest.clone()
+                    } else {
+                        tail.compute_output_shape(
+                            rest.field_shape(key.as_str()),
+                            dollar_shape.clone(),
+                            named_var_shapes,
+                        )
+                    };
+
+                    Shape::array(mapped_prefix, mapped_rest)
+                } else {
+                    tail.compute_output_shape(
+                        input_shape.field_shape(key.as_str()),
+                        dollar_shape.clone(),
+                        named_var_shapes,
+                    )
+                }
+            }
+
+            PathList::Expr(expr_seq, tail) => {
+                let mut arg_shapes = Vec::with_capacity(expr_seq.len());
+
+                for expr in expr_seq.as_ref() {
+                    let expr_shape = expr.compute_output_shape(
+                        input_shape.clone(),
+                        dollar_shape.clone(),
+                        named_var_shapes,
+                    );
+
+                    arg_shapes.push(tail.compute_output_shape(
+                        expr_shape,
+                        dollar_shape.clone(),
+                        named_var_shapes,
+                    ));
+                }
+
+                Shape::one(&arg_shapes)
+            }
+
+            PathList::Method(method_name, method_args, tail) => {
+                if input_shape.is_none() {
+                    // Following WithRange<PathList>::apply_to_path, we do not
+                    // want to apply methods to missing input data.
+                    return input_shape;
+                }
+
+                if let Some(method) = ArrowMethod::lookup(method_name) {
+                    let method_result_shape = method.shape(
+                        method_name,
+                        method_args.as_ref(),
+                        input_shape,
+                        dollar_shape.clone(),
+                        named_var_shapes,
+                    );
+
+                    if method_result_shape.is_none() {
+                        method_result_shape.clone()
+                    } else {
+                        tail.compute_output_shape(
+                            method_result_shape,
+                            dollar_shape.clone(),
+                            named_var_shapes,
+                        )
+                    }
+                } else {
+                    let message = format!("Method ->{} not found", method_name.as_str());
+                    Shape::error_with_range(message.as_str(), method_name.range())
+                }
+            }
+
+            PathList::Selection(selection) => {
+                selection.compute_output_shape(input_shape, dollar_shape, named_var_shapes)
+            }
+
+            PathList::Empty => input_shape,
+        }
+    }
 }
 
 impl ApplyToInternal for WithRange<LitExpr> {
@@ -488,6 +766,63 @@ impl ApplyToInternal for WithRange<LitExpr> {
                 (Some(JSON::Array(output)), errors)
             }
             LitExpr::Path(path) => path.apply_to_path(data, vars, input_path),
+        }
+    }
+
+    fn compute_output_shape(
+        &self,
+        input_shape: Shape,
+        dollar_shape: Shape,
+        named_var_shapes: &IndexMap<&str, Shape>,
+    ) -> Shape {
+        match self.as_ref() {
+            LitExpr::Null => ShapeCase::Null.simplify(),
+            LitExpr::Bool(value) => ShapeCase::Boolean(Some(*value)).simplify(),
+            LitExpr::String(value) => ShapeCase::String(Some(value.clone())).simplify(),
+
+            LitExpr::Number(value) => {
+                if let Some(n) = value.as_i64() {
+                    ShapeCase::Int(Some(n))
+                } else if value.is_f64() {
+                    ShapeCase::Float
+                } else {
+                    ShapeCase::error("Number neither Int nor Float")
+                }
+            }
+            .simplify(),
+
+            LitExpr::Object(map) => {
+                let mut fields = indexmap::IndexMap::new();
+                for (key, value) in map {
+                    fields.insert(
+                        key.as_string(),
+                        value.compute_output_shape(
+                            input_shape.clone(),
+                            dollar_shape.clone(),
+                            named_var_shapes,
+                        ),
+                    );
+                }
+                ShapeCase::Object(fields, Shape::none())
+            }
+            .simplify(),
+
+            LitExpr::Array(vec) => {
+                let mut shapes = Vec::with_capacity(vec.len());
+                for value in vec {
+                    shapes.push(value.compute_output_shape(
+                        input_shape.clone(),
+                        dollar_shape.clone(),
+                        named_var_shapes,
+                    ));
+                }
+                ShapeCase::Array(shapes, Shape::none())
+            }
+            .simplify(),
+
+            LitExpr::Path(path) => {
+                path.compute_output_shape(input_shape, dollar_shape, named_var_shapes)
+            }
         }
     }
 }
@@ -529,6 +864,61 @@ impl ApplyToInternal for SubSelection {
         }
 
         (Some(JSON::Object(output)), errors)
+    }
+
+    fn compute_output_shape(
+        &self,
+        input_shape: Shape,
+        _previous_dollar_shape: Shape,
+        named_var_shapes: &IndexMap<&str, Shape>,
+    ) -> Shape {
+        // The SubSelection rebinds the $ variable to the selected input object,
+        // so we can ignore _previous_dollar_shape.
+        let dollar_shape = input_shape.clone();
+
+        // Just as SubSelection::apply_to_path calls apply_to_array when data is
+        // an array, so compute_output_shape recursively computes the output
+        // shapes of each array element shape.
+        if let ShapeCase::Array(prefix, tail) = input_shape.case() {
+            let new_prefix = prefix
+                .iter()
+                .map(|shape| {
+                    self.compute_output_shape(shape.clone(), shape.clone(), named_var_shapes)
+                })
+                .collect();
+
+            let new_tail = self.compute_output_shape(tail.clone(), tail.clone(), named_var_shapes);
+
+            return ShapeCase::Array(new_prefix, new_tail).simplify();
+        }
+
+        // Build up the merged object shape using Shape::all to merge the
+        // individual named_selection object shapes.
+        let mut all_shape = Shape::empty_object();
+
+        for named_selection in self.selections.iter() {
+            // Simplifying as we go with Shape::all keeps all_shape relatively
+            // small in the common case when all named_selection items return an
+            // object shape, since those object shapes can all be merged
+            // together into one object.
+            all_shape = Shape::all(&[
+                all_shape,
+                named_selection.compute_output_shape(
+                    input_shape.clone(),
+                    dollar_shape.clone(),
+                    named_var_shapes,
+                ),
+            ]);
+
+            // If any named_selection item returns null instead of an object,
+            // that nullifies the whole object and allows shape computation to
+            // bail out early.
+            if all_shape.is_null() {
+                break;
+            }
+        }
+
+        all_shape
     }
 }
 
@@ -2051,6 +2441,14 @@ mod tests {
         assert_eq!(
             selection!("$.another.'pesky string literal!'.\"identifier\"").apply_to(&data),
             (Some(json!(123)), vec![],),
+        );
+    }
+
+    #[test]
+    fn test_compute_output_shape() {
+        assert_eq!(
+            selection!("id name").static_shape().pretty_print(),
+            "{ id: $root.id, name: $root.name }",
         );
     }
 }
--- apollo-federation/src/sources/connect/json_selection/methods.rs
+++ apollo-federation/src/sources/connect/json_selection/methods.rs
@@ -1,4 +1,6 @@
+use apollo_compiler::collections::IndexMap;
 use serde_json_bytes::Value as JSON;
+use shape::Shape;
 
 use super::immutable::InputPath;
 use super::location::WithRange;
@@ -53,7 +55,7 @@ pub(super) enum ArrowMethod {
 
 #[macro_export]
 macro_rules! impl_arrow_method {
-    ($struct_name:ident, $impl_fn_name:ident) => {
+    ($struct_name:ident, $impl_fn_name:ident, $shape_fn_name:ident) => {
         #[derive(Debug, Clone, Copy, PartialEq, Eq)]
         pub(super) struct $struct_name;
         impl $crate::sources::connect::json_selection::methods::ArrowMethodImpl for $struct_name {
@@ -67,6 +69,24 @@ macro_rules! impl_arrow_method {
                 tail: &WithRange<PathList>,
             ) -> (Option<JSON>, Vec<ApplyToError>) {
                 $impl_fn_name(method_name, method_args, data, vars, input_path, tail)
+            }
+
+            fn shape(
+                &self,
+                method_name: &WithRange<String>,
+                method_args: Option<&MethodArgs>,
+                input_shape: Shape,
+                dollar_shape: Shape,
+                named_var_shapes: &IndexMap<&str, Shape>,
+            ) -> Shape {
+                // TODO
+                $shape_fn_name(
+                    method_name,
+                    method_args,
+                    input_shape,
+                    dollar_shape,
+                    named_var_shapes,
+                )
             }
         }
     };
@@ -82,6 +102,29 @@ pub(super) trait ArrowMethodImpl {
         input_path: &InputPath<JSON>,
         tail: &WithRange<PathList>,
     ) -> (Option<JSON>, Vec<ApplyToError>);
+
+    fn shape(
+        &self,
+        // Shape processing errors for methods can benefit from knowing the name
+        // of the method and its source range. Note that ArrowMethodImpl::shape
+        // is invoked for every invocation of a method, with appropriately
+        // different source ranges.
+        method_name: &WithRange<String>,
+        // Most methods implementing ArrowMethodImpl::shape will need to know
+        // the shapes of their arguments, which can be computed from MethodArgs
+        // using the compute_output_shape method.
+        method_args: Option<&MethodArgs>,
+        // The input_shape is the shape of the @ variable, or the value from the
+        // left hand side of the -> token.
+        input_shape: Shape,
+        // The dollar_shape is the shape of the $ variable, or the input object
+        // associated with the closest enclosing subselection.
+        dollar_shape: Shape,
+        // Other variable shapes may also be provided here, though in general
+        // variables and their subproperties can be represented abstractly using
+        // $var.nested.property ShapeCase::Name shapes.
+        named_var_shapes: &IndexMap<&str, Shape>,
+    ) -> Shape;
 }
 
 // This Deref implementation allows us to call .apply(...) directly on the
