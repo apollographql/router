50f77c1a3c1dde694c7f6bb802f942d56c6f9f96
Lenny Burdette <lenny@apollographql.com>
2024-11-18 13:02:00 -0500

    dry merge tests with a macro

--- apollo-federation/src/merge/tests.rs
+++ apollo-federation/src/merge/tests.rs
@@ -6,54 +6,33 @@ use crate::schema::ValidFederationSchema;
 use crate::ValidFederationSubgraph;
 use crate::ValidFederationSubgraphs;
 
+macro_rules! subgraphs {
+    ($($name:expr => $file:expr),* $(,)?) => {{
+        let mut subgraphs = ValidFederationSubgraphs::new();
+
+        $(
+            subgraphs.add(ValidFederationSubgraph {
+                name: $name.to_string(),
+                url: "".to_string(),
+                schema: ValidFederationSchema::new(
+                    Schema::parse_and_validate(include_str!($file), $file).unwrap(),
+                )
+                .unwrap(),
+            }).unwrap();
+        )*
+
+        subgraphs
+    }};
+}
+
 #[test]
 fn test_steel_thread() {
-    let one_sdl = include_str!("../sources/connect/expand/merge/connector_Query_users_0.graphql");
-    let two_sdl = include_str!("../sources/connect/expand/merge/connector_Query_user_0.graphql");
-    let three_sdl = include_str!("../sources/connect/expand/merge/connector_User_d_1.graphql");
-    let graphql_sdl = include_str!("../sources/connect/expand/merge/graphql.graphql");
-
-    let mut subgraphs = ValidFederationSubgraphs::new();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "connector_Query_users_0".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(one_sdl, "./connector_Query_users_0.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "connector_Query_user_0".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(two_sdl, "./connector_Query_user_0.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "connector_User_d_1".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(three_sdl, "./connector_User_d_1.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "graphql".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(graphql_sdl, "./graphql.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
+    let subgraphs = subgraphs! {
+      "connector_Query_users_0" => "../sources/connect/expand/merge/connector_Query_users_0.graphql",
+      "connector_Query_user_0" => "../sources/connect/expand/merge/connector_Query_user_0.graphql",
+      "connector_User_d_1" => "../sources/connect/expand/merge/connector_User_d_1.graphql",
+      "graphql" => "../sources/connect/expand/merge/graphql.graphql",
+    };
 
     let result = merge_federation_subgraphs(subgraphs).unwrap();
 
@@ -66,30 +45,10 @@ fn test_steel_thread() {
 
 #[test]
 fn test_basic() {
-    let one_sdl = include_str!("../sources/connect/expand/merge/basic_1.graphql");
-    let two_sdl = include_str!("../sources/connect/expand/merge/basic_2.graphql");
-
-    let mut subgraphs = ValidFederationSubgraphs::new();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "basic_1".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(one_sdl, "./basic_1.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "basic_2".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(two_sdl, "./basic_2.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
+    let subgraphs = subgraphs! {
+      "basic_1" => "../sources/connect/expand/merge/basic_1.graphql",
+      "basic_2" => "../sources/connect/expand/merge/basic_2.graphql",
+    };
 
     let result = merge_federation_subgraphs(subgraphs).unwrap();
 
@@ -102,30 +61,10 @@ fn test_basic() {
 
 #[test]
 fn test_inaccessible() {
-    let one_sdl = include_str!("../sources/connect/expand/merge/inaccessible.graphql");
-    let two_sdl = include_str!("../sources/connect/expand/merge/inaccessible_2.graphql");
-
-    let mut subgraphs = ValidFederationSubgraphs::new();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "inaccessible".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(one_sdl, "./inaccessible.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "inaccessible_2".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(two_sdl, "./inaccessible_2.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
+    let subgraphs = subgraphs! {
+      "inaccessible" => "../sources/connect/expand/merge/inaccessible.graphql",
+      "inaccessible_2" => "../sources/connect/expand/merge/inaccessible_2.graphql",
+    };
 
     let result = merge_federation_subgraphs(subgraphs).unwrap();
 
@@ -138,41 +77,11 @@ fn test_inaccessible() {
 
 #[test]
 fn test_interface_object() {
-    let one_sdl = include_str!("../sources/connect/expand/merge/interface_object_1.graphql");
-    let two_sdl = include_str!("../sources/connect/expand/merge/interface_object_2.graphql");
-    let three_sdl = include_str!("../sources/connect/expand/merge/interface_object_3.graphql");
-
-    let mut subgraphs = ValidFederationSubgraphs::new();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "interface_object_1".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(one_sdl, "./interface_object_1.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "interface_object_2".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(two_sdl, "./interface_object_2.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
-    subgraphs
-        .add(ValidFederationSubgraph {
-            name: "interface_object_3".to_string(),
-            url: "".to_string(),
-            schema: ValidFederationSchema::new(
-                Schema::parse_and_validate(three_sdl, "./interface_object_3.graphql").unwrap(),
-            )
-            .unwrap(),
-        })
-        .unwrap();
+    let subgraphs = subgraphs! {
+      "interface_object_1" => "../sources/connect/expand/merge/interface_object_1.graphql",
+      "interface_object_2" => "../sources/connect/expand/merge/interface_object_2.graphql",
+      "interface_object_3" => "../sources/connect/expand/merge/interface_object_3.graphql",
+    };
 
     let result = merge_federation_subgraphs(subgraphs).unwrap();
 
