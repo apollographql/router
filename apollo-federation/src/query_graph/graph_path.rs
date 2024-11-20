fece2a3c7e2d184cf64b90557b5a614280413cb5
Chris Lenfest <clenfest@apollographql.com>
2024-11-19 19:16:41 -0600

    fix match detection in can_satisfy_conditions

--- apollo-federation/src/query_graph/graph_path.rs
+++ apollo-federation/src/query_graph/graph_path.rs
@@ -1708,13 +1708,14 @@ where
                     if !was_unsatisfied && !context_map.contains_key(&ctx.named_parameter) {
                         if let Some(parent_type) = parent_type {
                             let parent_type = parent_type.parent();
+                            let subgraph_schema = self.graph.schema_by_source(&ctx.subgraph_name)?;
                             let matches = ctx.types_with_context_set.iter().any(|pos| {
                                 if (pos.type_name() == parent_type.type_name()) {
                                     return true;
                                 }
-                                match &parent_type {
+                                match &pos {
                                     CompositeTypeDefinitionPosition::Object(obj_pos) => {
-                                        if let Ok(obj) = obj_pos.get(schema.schema()) {
+                                        if let Ok(obj) = obj_pos.get(subgraph_schema.schema()) {
                                             if let Some(_) =
                                                 obj.implements_interfaces.get(pos.type_name())
                                             {
@@ -1723,17 +1724,15 @@ where
                                         }
                                     }
                                     CompositeTypeDefinitionPosition::Interface(iface_pos) => {
-                                        if let Ok(iface) = iface_pos.get(schema.schema()) {
-                                            if let Some(_) =
-                                                iface.implements_interfaces.get(pos.type_name())
-                                            {
+                                        if let Ok(iface) = iface_pos.get(subgraph_schema.schema()) {
+                                            if iface.implements_interfaces.iter().any(|item| &item.name == parent_type.type_name()) {
                                                 return true;
                                             }
                                         }
                                     }
                                     CompositeTypeDefinitionPosition::Union(union_pos) => {
-                                        if let Ok(un) = union_pos.get(schema.schema()) {
-                                            if let Some(_) = un.members.get(pos.type_name()) {
+                                        if let Ok(un) = union_pos.get(subgraph_schema.schema()) {
+                                            if un.members.iter().any(|item| &item.name == parent_type.type_name()) {
                                                 return true;
                                             }
                                         }
@@ -1742,9 +1741,8 @@ where
                                 false
                             });
                             if matches {
-                                let schema = self.graph.schema_by_source(&ctx.subgraph_name)?;
                                 let selection_set = parse_field_set(
-                                    schema,
+                                    subgraph_schema,
                                     parent_type.type_name().clone(),
                                     &ctx.selection,
                                 )?
@@ -1758,7 +1756,7 @@ where
                                                 obj,
                                             )) = &fragment.type_condition_position
                                             {
-                                                if !schema
+                                                if !subgraph_schema
                                                     .possible_runtime_types(parent_type.clone())?
                                                     .contains(obj)
                                                 {
