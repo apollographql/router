ccf625b85690321228c5ea8ace7ff9fdaf94a31f
Chris Lenfest <clenfest@apollographql.com>
2024-11-20 13:08:31 -0600

    Clone context execution tests for rust query planner

--- apollo-router/tests/set_context.rs
+++ apollo-router/tests/set_context.rs
@@ -32,9 +32,10 @@ macro_rules! snap
     }
 }
 
-async fn run_single_request(query: &str, mocks: &[(&'static str, &'static str)]) -> Response {
-    let harness = setup_from_mocks(
-        json! {{
+fn get_configuration(rust_qp: bool) -> serde_json::Value {
+    if rust_qp {
+        return json! {{
+            "experimental_query_planner_mode": "new",
             "experimental_type_conditioned_fetching": true,
             // will make debugging easier
             "plugins": {
@@ -47,9 +48,28 @@ async fn run_single_request(query: &str, mocks: &[(&'static str, &'static str)])
                 // TODO(@goto-bus-stop): need to update the mocks and remove this, #6013
                 "generate_query_fragments": false,
             }
-        }},
-        mocks,
-    );
+        }};
+    }
+    return json! {{
+        "experimental_type_conditioned_fetching": true,
+        // will make debugging easier
+        "plugins": {
+            "experimental.expose_query_plan": true
+        },
+        "include_subgraph_errors": {
+            "all": true
+        },
+        "supergraph": {
+            // TODO(@goto-bus-stop): need to update the mocks and remove this, #6013
+            "generate_query_fragments": false,
+        }
+    }}
+}
+
+
+async fn run_single_request(query: &str, rust_qp: bool, mocks: &[(&'static str, &'static str)]) -> Response {
+    let configuration = get_configuration(rust_qp);
+    let harness = setup_from_mocks(configuration, mocks);
     let supergraph_service = harness.build_supergraph().await.unwrap();
     let request = supergraph::Request::fake_builder()
         .query(query.to_string())
@@ -83,8 +103,36 @@ async fn test_set_context() {
 
     let response = run_single_request(
         QUERY,
+        false,
         &[
             ("Subgraph1", include_str!("fixtures/set_context/one.json")),
+            ("Subgraph2", include_str!("fixtures/set_context/two.json")),
+        ],
+    )
+    .await;
+
+    snap!(response);
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_set_context_rust_qp() {
+    static QUERY: &str = r#"
+        query set_context_rust_qp {
+            t {
+                __typename
+                id
+                u {
+                    __typename
+                    field
+                }
+            }
+        }"#;
+
+    let response = run_single_request(
+        QUERY,
+        true,
+        &[
+            ("Subgraph1", include_str!("fixtures/set_context/one_rust_qp.json")),
             ("Subgraph2", include_str!("fixtures/set_context/two.json")),
         ],
     )
@@ -107,8 +155,34 @@ async fn test_set_context_no_typenames() {
 
     let response = run_single_request(
         QUERY_NO_TYPENAMES,
+        false,
         &[
             ("Subgraph1", include_str!("fixtures/set_context/one.json")),
+            ("Subgraph2", include_str!("fixtures/set_context/two.json")),
+        ],
+    )
+    .await;
+
+    snap!(response);
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_set_context_no_typenames_rust_qp() {
+    static QUERY_NO_TYPENAMES: &str = r#"
+        query set_context_no_typenames_rust_qp {
+            t {
+                id
+                u {
+                    field
+                }
+            }
+        }"#;
+
+    let response = run_single_request(
+        QUERY_NO_TYPENAMES,
+        true,
+        &[
+            ("Subgraph1", include_str!("fixtures/set_context/one_rust_qp.json")),
             ("Subgraph2", include_str!("fixtures/set_context/two.json")),
         ],
     )
@@ -131,8 +205,34 @@ async fn test_set_context_list() {
 
     let response = run_single_request(
         QUERY_WITH_LIST,
+        false,
         &[
             ("Subgraph1", include_str!("fixtures/set_context/one.json")),
+            ("Subgraph2", include_str!("fixtures/set_context/two.json")),
+        ],
+    )
+    .await;
+
+    snap!(response);
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_set_context_list_rust_qp() {
+    static QUERY_WITH_LIST: &str = r#"
+        query set_context_list_rust_qp {
+            t {
+                id
+                uList {
+                    field
+                }
+            }
+        }"#;
+
+    let response = run_single_request(
+        QUERY_WITH_LIST,
+        true,
+        &[
+            ("Subgraph1", include_str!("fixtures/set_context/one_rust_qp.json")),
             ("Subgraph2", include_str!("fixtures/set_context/two.json")),
         ],
     )
@@ -155,8 +255,34 @@ async fn test_set_context_list_of_lists() {
 
     let response = run_single_request(
         QUERY_WITH_LIST_OF_LISTS,
+        false,
         &[
             ("Subgraph1", include_str!("fixtures/set_context/one.json")),
+            ("Subgraph2", include_str!("fixtures/set_context/two.json")),
+        ],
+    )
+    .await;
+
+    snap!(response);
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_set_context_list_of_lists_rust_qp() {
+    static QUERY_WITH_LIST_OF_LISTS: &str = r#"
+        query QueryLL {
+            tList {
+                id
+                uList {
+                    field
+                }
+            }
+        }"#;
+
+    let response = run_single_request(
+        QUERY_WITH_LIST_OF_LISTS,
+        true,
+        &[
+            ("Subgraph1", include_str!("fixtures/set_context/one_rust_qp.json")),
             ("Subgraph2", include_str!("fixtures/set_context/two.json")),
         ],
     )
@@ -185,8 +311,40 @@ async fn test_set_context_union() {
 
     let response = run_single_request(
         QUERY_WITH_UNION,
+        false,
         &[
             ("Subgraph1", include_str!("fixtures/set_context/one.json")),
+            ("Subgraph2", include_str!("fixtures/set_context/two.json")),
+        ],
+    )
+    .await;
+
+    snap!(response);
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_set_context_union_rust_qp() {
+    static QUERY_WITH_UNION: &str = r#"
+        query QueryUnion {
+            k {
+                ... on A {
+                    v {
+                        field
+                    }
+                }
+                ... on B {
+                    v {
+                        field
+                    }
+                }
+            }
+        }"#;
+
+    let response = run_single_request(
+        QUERY_WITH_UNION,
+        true,
+        &[
+            ("Subgraph1", include_str!("fixtures/set_context/one_rust_qp.json")),
             ("Subgraph2", include_str!("fixtures/set_context/two.json")),
         ],
     )
@@ -209,6 +367,35 @@ async fn test_set_context_with_null() {
 
     let response = run_single_request(
         QUERY,
+        false,
+        &[
+            (
+                "Subgraph1",
+                include_str!("fixtures/set_context/one_null_param.json"),
+            ),
+            ("Subgraph2", include_str!("fixtures/set_context/two.json")),
+        ],
+    )
+    .await;
+
+    insta::assert_json_snapshot!(response);
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_set_context_with_null_rust_qp() {
+    static QUERY: &str = r#"
+        query Query_Null_Param {
+            t {
+                id
+                u {
+                    field
+                }
+            }
+        }"#;
+
+    let response = run_single_request(
+        QUERY,
+        true,
         &[
             (
                 "Subgraph1",
@@ -238,8 +425,36 @@ async fn test_set_context_type_mismatch() {
 
     let response = run_single_request(
         QUERY,
+        false,
         &[
             ("Subgraph1", include_str!("fixtures/set_context/one.json")),
+            ("Subgraph2", include_str!("fixtures/set_context/two.json")),
+        ],
+    )
+    .await;
+
+    snap!(response);
+}
+
+// this test returns the contextual value with a different than expected type
+// this currently works, but perhaps should do type valdiation in the future to reject
+#[tokio::test(flavor = "multi_thread")]
+async fn test_set_context_type_mismatch_rust_qp() {
+    static QUERY: &str = r#"
+        query Query_type_mismatch {
+            t {
+                id
+                u {
+                    field
+                }
+            }
+        }"#;
+
+    let response = run_single_request(
+        QUERY,
+        true,
+        &[
+            ("Subgraph1", include_str!("fixtures/set_context/one_rust_qp.json")),
             ("Subgraph2", include_str!("fixtures/set_context/two.json")),
         ],
     )
@@ -265,6 +480,38 @@ async fn test_set_context_unrelated_fetch_failure() {
 
     let response = run_single_request(
         QUERY,
+        false,
+        &[
+            (
+                "Subgraph1",
+                include_str!("fixtures/set_context/one_fetch_failure.json"),
+            ),
+            ("Subgraph2", include_str!("fixtures/set_context/two.json")),
+        ],
+    )
+    .await;
+
+    snap!(response);
+}
+
+// fetch from unrelated (to context) subgraph fails
+// validates that the error propagation is correct
+#[tokio::test(flavor = "multi_thread")]
+async fn test_set_context_unrelated_fetch_failure_rust_qp() {
+    static QUERY: &str = r#"
+        query Query_fetch_failure {
+            t {
+                id
+                u {
+                    field
+                    b
+                }
+            }
+        }"#;
+
+    let response = run_single_request(
+        QUERY,
+        true,
         &[
             (
                 "Subgraph1",
@@ -294,6 +541,37 @@ async fn test_set_context_dependent_fetch_failure() {
 
     let response = run_single_request(
         QUERY,
+        false,
+        &[
+            (
+                "Subgraph1",
+                include_str!("fixtures/set_context/one_dependent_fetch_failure.json"),
+            ),
+            ("Subgraph2", include_str!("fixtures/set_context/two.json")),
+        ],
+    )
+    .await;
+
+    snap!(response);
+}
+
+// subgraph fetch fails where context depends on results of fetch.
+// validates that no fetch will get called that passes context
+#[tokio::test(flavor = "multi_thread")]
+async fn test_set_context_dependent_fetch_failure_rust_qp() {
+    static QUERY: &str = r#"
+        query Query_fetch_dependent_failure {
+            t {
+                id
+                u {
+                    field
+                }
+            }
+        }"#;
+
+    let response = run_single_request(
+        QUERY,
+        true,
         &[
             (
                 "Subgraph1",
