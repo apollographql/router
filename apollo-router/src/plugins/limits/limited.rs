b4813904b7fd697d2459f99708d6ac401c697ecd
Renée Kooi <renee.kooi@apollographql.com>
2024-12-04 14:33:49 +0100

    Use our custom `Limited` body type
    It has a different behaviour from the tower-http one.
    We should try to move to tower-http's builtin limiting layer in the
    future.
    
    For now, this adjusts the existing custom Limited body type to use the
    new http-body method `poll_frame` instead of the separate `poll_data`
    and `poll_trailers`.

--- apollo-router/src/plugins/limits/limited.rs
+++ apollo-router/src/plugins/limits/limited.rs
@@ -1,16 +1,13 @@
-pub(crate) use http_body_util::Limited;
-/*
 use std::pin::Pin;
 use std::task::Context;
 use std::task::Poll;
 
 use bytes::Buf;
-use http::HeaderMap;
 use http_body::SizeHint;
 use pin_project_lite::pin_project;
 use tokio::sync::OwnedSemaphorePermit;
 
-use crate::plugins::limits::layer::BodyLimitControl;
+use super::layer::BodyLimitControl;
 
 pin_project! {
     /// An implementation of http_body::Body that limits the number of bytes read from the inner body.
@@ -74,15 +71,19 @@ where
     type Data = Body::Data;
     type Error = Body::Error;
 
-    fn poll_data(
+    fn poll_frame(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
-    ) -> Poll<Option<Result<Self::Data, Self::Error>>> {
+    ) -> Poll<Option<Result<http_body::Frame<Self::Data>, Self::Error>>> {
         let mut this = self.project();
-        let res = match this.inner.poll_data(cx) {
+        let res = match this.inner.poll_frame(cx) {
             Poll::Pending => return Poll::Pending,
             Poll::Ready(None) => None,
-            Poll::Ready(Some(Ok(data))) => {
+            Poll::Ready(Some(Ok(frame))) => {
+                let Some(data) = frame.data_ref() else {
+                    return Poll::Ready(Some(Ok(frame)));
+                };
+
                 if data.remaining() > this.control.remaining() {
                     // This is the difference between http_body::Limited and our implementation.
                     // Dropping this mutex allows the containing layer to immediately return an error response
@@ -92,7 +93,7 @@ where
                     return Poll::Pending;
                 } else {
                     this.control.increment(data.remaining());
-                    Some(Ok(data))
+                    Some(Ok(frame))
                 }
             }
             Poll::Ready(Some(Err(err))) => Some(Err(err)),
@@ -101,22 +102,10 @@ where
         Poll::Ready(res)
     }
 
-    fn poll_trailers(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-    ) -> Poll<Result<Option<HeaderMap>, Self::Error>> {
-        let this = self.project();
-        let res = match this.inner.poll_trailers(cx) {
-            Poll::Pending => return Poll::Pending,
-            Poll::Ready(Ok(data)) => Ok(data),
-            Poll::Ready(Err(err)) => Err(err),
-        };
-
-        Poll::Ready(res)
-    }
     fn is_end_stream(&self) -> bool {
         self.inner.is_end_stream()
     }
+
     fn size_hint(&self) -> SizeHint {
         match u64::try_from(self.control.remaining()) {
             Ok(n) => {
@@ -140,7 +129,7 @@ mod test {
     use std::pin::Pin;
     use std::sync::Arc;
 
-    use http_body::Body;
+    use http_body_util::StreamBody;
     use tower::BoxError;
 
     use crate::plugins::limits::layer::BodyLimitControl;
@@ -189,7 +178,7 @@ mod test {
         let lock = semaphore.clone().try_acquire_owned().unwrap();
 
         let mut limited = super::Limited::new(
-            hyper::Body::wrap_stream(futures::stream::iter(vec![
+            StreamBody::new(futures::stream::iter(vec![
                 Ok::<&str, BoxError>("hello"),
                 Ok("world"),
             ])),
@@ -212,4 +201,3 @@ mod test {
         assert!(semaphore.try_acquire().is_ok());
     }
 }
-*/
