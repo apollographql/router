29c84b236aa7c3297941302fd30189154b2bd1c4
Taylor Ninesling <taylor.ninesling@apollographql.com>
2025-01-02 12:50:18 -0600

    Simplify attribute extraction by collecting to HashMap during export

--- apollo-router/src/plugins/telemetry/apollo_otlp_exporter.rs
+++ apollo-router/src/plugins/telemetry/apollo_otlp_exporter.rs
@@ -30,6 +30,7 @@ use super::config_new::attributes::SUBGRAPH_NAME;
 use super::otlp::Protocol;
 use super::tracing::apollo_telemetry::encode_ftv1_trace;
 use super::tracing::apollo_telemetry::extract_ftv1_trace_with_error_count;
+use super::tracing::apollo_telemetry::extract_string;
 use super::tracing::apollo_telemetry::LightSpanData;
 use super::tracing::apollo_telemetry::APOLLO_PRIVATE_FTV1;
 use crate::plugins::telemetry::apollo::router_id;
@@ -199,7 +200,10 @@ impl ApolloOtlpExporter {
             .find(|kv| kv.key == APOLLO_PRIVATE_FTV1)
         {
             let subgraph_name = span
-                .get_string_attribute(&SUBGRAPH_NAME)
+                .attributes
+                .iter()
+                .find(|kv| kv.key == SUBGRAPH_NAME)
+                .and_then(|kv| extract_string(&kv.value))
                 .unwrap_or_default();
             let subgraph_error_config = self
                 .errors_configuration
