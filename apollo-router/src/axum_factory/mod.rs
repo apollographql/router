7b0327f920f0e444b073376d997884e59a290b95
Taylor Ninesling <taylor.ninesling@apollographql.com>
2025-01-09 10:56:03 -0600

    Remove extra colon characters to avoid confusion with old matcher format

--- apollo-router/src/axum_factory/mod.rs
+++ apollo-router/src/axum_factory/mod.rs
@@ -14,13 +14,12 @@ pub(crate) use axum_http_server_factory::span_mode;
 pub(crate) use axum_http_server_factory::AxumHttpServerFactory;
 pub(crate) use axum_http_server_factory::CanceledRequest;
 pub(crate) use listeners::ListenAddrAndRouter;
-
 use once_cell::sync::Lazy;
 use regex::Regex;
 
 static ENDPOINT_CALLBACK: OnceLock<Arc<dyn Fn(Router) -> Router + Send + Sync>> = OnceLock::new();
 static NAMED_PARAMETER_REGEX: Lazy<Regex> =
-    Lazy::new(|| Regex::new(r"/(:[^/]+)").expect("valid regex"));
+    Lazy::new(|| Regex::new(r"/:([^/]+)").expect("valid regex"));
 static NAMED_WILDCARD_REGEX: Lazy<Regex> =
     Lazy::new(|| Regex::new(r"/(\*[^/]+)").expect("valid regex"));
 
@@ -38,7 +37,7 @@ pub fn unsupported_set_axum_router_callback(
 /// Axum 0.8 upgraded its `matchit` dependency to 0.8.4, which included a breaking change requiring
 /// parameters and wildcards to be wrapped in curly braces. So, we do a simple rewrite to allow
 /// the previous syntax in the supergraph config.
-/// Named param: /foo/:bar/baz => /foo/{:bar}/baz
+/// Named param: /foo/:bar/baz => /foo/{bar}/baz
 /// Final wildcard: /foo/*wild => /foo/{*wild}
 pub(crate) fn rewrite_path_for_axum_0_8(path: &str) -> String {
     let path = NAMED_PARAMETER_REGEX.replace_all(&path, "/{$1}");
