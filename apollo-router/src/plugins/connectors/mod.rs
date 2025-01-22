100a830eecbcfcf365b349f52f61914fed299ef9
Nicholas Cioli <nicholas.cioli@apollographql.com>
2025-01-22 17:06:06 -0500

    refactor and address comments on auth

--- apollo-router/src/plugins/connectors/mod.rs
+++ apollo-router/src/plugins/connectors/mod.rs
@@ -4,6 +4,7 @@ mod form_encoding;
 pub(crate) mod handle_responses;
 pub(crate) mod http;
 pub(crate) mod http_json_transport;
+pub(crate) mod incompatible;
 pub(crate) mod make_requests;
 pub(crate) mod plugin;
 pub(crate) mod query_plans;
--- apollo-router/tests/integration/connectors.rs
+++ apollo-router/tests/integration/connectors.rs
@@ -4,11 +4,8 @@ use tower::BoxError;
 
 use crate::integration::IntegrationTest;
 
-const INCOMPATIBLE_PLUGINS_CONFIG: &str =
-    include_str!("../fixtures/connectors/incompatible.router.yaml");
-
 #[tokio::test(flavor = "multi_thread")]
-async fn test_incompatible_plugin_warnings() -> Result<(), BoxError> {
+async fn test_auth_incompatible_warnings_on_all() -> Result<(), BoxError> {
     // Ensure that we have the test keys before running
     // Note: The [IntegrationTest] ensures that these test credentials get
     // set before running the router.
@@ -18,7 +15,22 @@ async fn test_incompatible_plugin_warnings() -> Result<(), BoxError> {
     };
 
     let mut router = IntegrationTest::builder()
-        .config(INCOMPATIBLE_PLUGINS_CONFIG)
+        .config(
+            r#"
+            authentication:
+              subgraph:
+                all:
+                  aws_sig_v4:
+                    default_chain:
+                      profile_name: "my-test-profile"
+                      region: "us-east-1"
+                      service_name: "lambda"
+                      assume_role:
+                        role_arn: "test-arn"
+                        session_name: "test-session"
+                        external_id: "test-id"
+        "#,
+        )
         .supergraph(PathBuf::from_iter([
             "tests",
             "fixtures",
@@ -29,30 +41,157 @@ async fn test_incompatible_plugin_warnings() -> Result<(), BoxError> {
         .await;
 
     router.start().await;
+    router
+        .assert_log_contains(r#""subgraphs":"connectors","message":"plugin `authentication` is enabled for connector-enabled subgraphs"#)
+        .await;
 
-    // Make sure that we have the warnings we expect
-    // Note: This order is sadly required since asserting that logs exist consume
-    // previous statements...
-    let plugins = [
-        "apq",
-        "authentication",
-        "batching",
-        "coprocessor",
-        "headers",
-        "preview_entity_cache",
-        "telemetry",
-        "tls",
-        "traffic_shaping",
-        // These must be at the end
-        "demand_control",
-        "rhai",
-    ];
-    for plugin in plugins {
-        let msg = format!(
-            r#""subgraphs":"connectors","message":"plugin `{plugin}` is enabled for connector-enabled subgraphs, which is currently unsupported"#
-        );
-        router.assert_log_contains(&msg).await;
-    }
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_auth_incompatible_warnings_on_subgraph() -> Result<(), BoxError> {
+    // Ensure that we have the test keys before running
+    // Note: The [IntegrationTest] ensures that these test credentials get
+    // set before running the router.
+    if std::env::var("TEST_APOLLO_KEY").is_err() || std::env::var("TEST_APOLLO_GRAPH_REF").is_err()
+    {
+        return Ok(());
+    };
+
+    let mut router = IntegrationTest::builder()
+        .config(
+            r#"
+            authentication:
+              subgraph:
+                subgraphs:
+                  connectors:
+                    aws_sig_v4:
+                      hardcoded:
+                        access_key_id: "my-access-key"
+                        secret_access_key: "my-secret-access-key"
+                        region: "us-east-1"
+                        service_name: "vpc-lattice-svcs"
+        "#,
+        )
+        .supergraph(PathBuf::from_iter([
+            "tests",
+            "fixtures",
+            "connectors",
+            "quickstart.graphql",
+        ]))
+        .build()
+        .await;
+
+    router.start().await;
+    router
+        .assert_log_contains(r#""subgraphs":"connectors","message":"plugin `authentication` is enabled for connector-enabled subgraphs"#)
+        .await;
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_auth_incompatible_warnings_with_overrides() -> Result<(), BoxError> {
+    // Ensure that we have the test keys before running
+    // Note: The [IntegrationTest] ensures that these test credentials get
+    // set before running the router.
+    if std::env::var("TEST_APOLLO_KEY").is_err() || std::env::var("TEST_APOLLO_GRAPH_REF").is_err()
+    {
+        return Ok(());
+    };
+
+    let mut router = IntegrationTest::builder()
+        .config(
+            r#"
+            authentication:
+              subgraph:
+                subgraphs:
+                  connectors:
+                    aws_sig_v4:
+                      hardcoded:
+                        access_key_id: "my-access-key"
+                        secret_access_key: "my-secret-access-key"
+                        region: "us-east-1"
+                        service_name: "vpc-lattice-svcs"
+              connector:
+                sources:
+                  connectors.something:
+                    aws_sig_v4:
+                      default_chain:
+                        profile_name: "default"
+                        region: "us-east-1"
+                        service_name: "lambda"
+                        assume_role:
+                          role_arn: "arn:aws:iam::XXXXXXXXXXXX:role/lambaexecute"
+                          session_name: "connector"
+        "#,
+        )
+        .supergraph(PathBuf::from_iter([
+            "tests",
+            "fixtures",
+            "connectors",
+            "quickstart.graphql",
+        ]))
+        .build()
+        .await;
+
+    router.start().await;
+    router
+        .assert_log_contains(r#""subgraph":"connectors","sources":"jsonPlaceholder","message":"plugin `authentication` is enabled for a connector-enabled subgraph"#)
+        .await;
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_auth_no_incompatible_warnings_with_overrides() -> Result<(), BoxError> {
+    // Ensure that we have the test keys before running
+    // Note: The [IntegrationTest] ensures that these test credentials get
+    // set before running the router.
+    if std::env::var("TEST_APOLLO_KEY").is_err() || std::env::var("TEST_APOLLO_GRAPH_REF").is_err()
+    {
+        return Ok(());
+    };
+
+    let mut router = IntegrationTest::builder()
+        .config(
+            r#"
+            authentication:
+              subgraph:
+                subgraphs:
+                  connectors:
+                    aws_sig_v4:
+                      hardcoded:
+                        access_key_id: "my-access-key"
+                        secret_access_key: "my-secret-access-key"
+                        region: "us-east-1"
+                        service_name: "vpc-lattice-svcs"
+              connector:
+                sources:
+                  connectors.jsonPlaceholder:
+                    aws_sig_v4:
+                      default_chain:
+                        profile_name: "default"
+                        region: "us-east-1"
+                        service_name: "lambda"
+                        assume_role:
+                          role_arn: "arn:aws:iam::XXXXXXXXXXXX:role/lambaexecute"
+                          session_name: "connector"
+        "#,
+        )
+        .supergraph(PathBuf::from_iter([
+            "tests",
+            "fixtures",
+            "connectors",
+            "quickstart.graphql",
+        ]))
+        .build()
+        .await;
+
+    router.start().await;
+    router
+        .assert_log_not_contains(r#""subgraph":"connectors","sources":"jsonPlaceholder","message":"plugin `authentication` is enabled for a connector-enabled subgraph"#)
+        .await;
 
     Ok(())
 }
