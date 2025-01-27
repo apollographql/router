9104cceec04a5cf9d0213ae26b1fddb1e82c7d63
Aaron Arinder <aaronarinder@gmail.com>
2025-01-27 09:48:17 -0500

    gourd

--- apollo-router/src/plugins/router_limits/mod.rs
+++ apollo-router/src/plugins/router_limits/mod.rs
@@ -152,95 +152,6 @@ mod test {
     #[tokio::test(flavor = "multi_thread")]
     async fn it_enforces_tps_limit_when_license() {
         // GIVEN
-        // * router limits plugin
-        // * license with limits
-
-        let license = LicenseState::Licensed {
-            limits: Some(LicenseLimits {
-                tps: Some(TpsLimit {
-                    capacity: 1,
-                    interval: Duration::from_millis(500),
-                }),
-            }),
-        };
-        let router_limits_plugin = get_router_limits_plugin(license).await.unwrap();
-
-        let mut mock_service = MockRouterService::new();
-        mock_service.expect_call().times(0..3).returning(|_| {
-            Ok(RouterResponse::fake_builder()
-                .data(json!({ "test": 5678_u32 }))
-                .build()
-                .unwrap())
-        });
-        mock_service
-            .expect_clone()
-            .returning(MockRouterService::new);
-
-        // WHEN
-        // * the router is called three times with a capacity of 1 and a interval of 500ms with a
-        // delay between the second and third calls
-
-        // THEN
-        // * the first call succeeds
-        // * the second call violates the tps limit
-        // * the third call, being out of the rate limiting interval, succeeds
-        let mut svc = router_limits_plugin.router_service(mock_service.boxed());
-        let response: RouterResponse = svc
-            .ready()
-            .await
-            .expect("it is ready")
-            .call(RouterRequest::fake_builder().build().unwrap())
-            .await
-            .unwrap();
-        assert_eq!(StatusCode::OK, response.response.status());
-
-        let response: RouterResponse = svc
-            .ready()
-            .await
-            .expect("it is ready")
-            .call(RouterRequest::fake_builder().build().unwrap())
-            .await
-            .unwrap();
-        assert_eq!(StatusCode::TOO_MANY_REQUESTS, response.response.status());
-
-        let j: serde_json::Value = serde_json::from_slice(
-            &crate::services::router::body::into_bytes(response.response)
-                .await
-                .expect("we have a body"),
-        )
-        .expect("our body is valid json");
-
-        // THEN
-        // * there's an appropriate rate limiting message
-        assert_eq!(
-            "Your request has been rate limited",
-            j["errors"][0]["message"]
-        );
-
-        // THEN
-        // * there's an appropriate graphql extension code (it's important this stay stable for
-        // collecting metrics)
-        assert_eq!(
-            "ROUTER_TPS_LIMIT_REACHED",
-            j["errors"][0]["extensions"]["code"]
-        );
-
-        tokio::time::sleep(Duration::from_millis(500)).await;
-
-        let response: RouterResponse = svc
-            .ready()
-            .await
-            .expect("it is ready")
-            .call(RouterRequest::fake_builder().build().unwrap())
-            .await
-            .unwrap();
-
-        assert_eq!(StatusCode::OK, response.response.status());
-    }
-
-    #[tokio::test(flavor = "multi_thread")]
-    async fn test_tps_limits_multi_threaded() {
-        // GIVEN
         // * a license with tps limits set to 1 req per 200ms
         // * the router limits plugin
         let license = LicenseState::Licensed {
@@ -287,6 +198,47 @@ mod test {
         assert!(r3
             .await
             .is_ok_and(|resp| resp.response.status().is_success()));
+    }
+
+    #[tokio::test]
+    async fn it_emits_metrics_when_tps_enforced() {
+        async {
+            let blah = |_req| async {
+                tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+                Ok(router::Response::fake_builder()
+                    .data(serde_json::json!({"data": {"field": "value"}}))
+                    .header("x-custom-header", "test-value")
+                    .build()
+                    .unwrap())
+            };
+            // GIVEN
+            // * a license with tps limits set to 1 req per 200ms
+            // * the router limits plugin
+            let license = LicenseState::Licensed {
+                limits: Some(LicenseLimits {
+                    tps: Some(TpsLimit {
+                        capacity: 1,
+                        interval: Duration::from_millis(150),
+                    }),
+                }),
+            };
+
+            let test_harness: PluginTestHarness<RouterLimits> =
+                PluginTestHarness::builder().license(license).build().await;
+
+            let service = test_harness.router_service(blah.clone());
+
+            // WHEN
+            // * two reqs happen
+            service.call_default().await;
+            service.call_default().await;
+
+            // THEN
+            // * we get a metric saying the tps limit was enforced
+            assert_counter!("apollo.router_limits.tps.limit_enforced", 1);
+        }
+        .with_metrics()
+        .await;
     }
 
     #[tokio::test(flavor = "multi_thread")]
