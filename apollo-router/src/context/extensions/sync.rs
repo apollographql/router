a4b6427897dd15445227abb1d4d8633b50051baf
Simon Sapin <simon@apollographql.com>
2025-01-27 11:38:47 +0100

    Remove time-based check that ExtensionsGuard isn’t held too long
    This is a less of a problem now that `.lock() -> ExtensionsGuard` is removed
    and only `.with_lock(impl FnOnce(ExtensionsGuard))` remains:
    the latter cannot keep the guard alive across `.await`
    
    This check was flaky on CI when CPUs are busy with lots of tests running
    in parallel.

--- apollo-router/src/context/extensions/sync.rs
+++ apollo-router/src/context/extensions/sync.rs
@@ -1,10 +1,6 @@
 use std::ops::Deref;
 use std::ops::DerefMut;
 use std::sync::Arc;
-#[cfg(debug_assertions)]
-use std::time::Duration;
-#[cfg(debug_assertions)]
-use std::time::Instant;
 
 /// You can use `Extensions` to pass data between plugins that is not serializable. Such data is not accessible from Rhai or co-processoers.
 ///
@@ -29,8 +25,6 @@ impl ExtensionsMutex {
 }
 
 pub struct ExtensionsGuard<'a> {
-    #[cfg(debug_assertions)]
-    start: Instant,
     guard: parking_lot::MutexGuard<'a, super::Extensions>,
 }
 impl<'a> ExtensionsGuard<'a> {
@@ -39,8 +33,6 @@ impl<'a> ExtensionsGuard<'a> {
         // The guard MUST be initialized first otherwise time waiting for a lock is included in this time.
         Self {
             guard: guard.lock(),
-            #[cfg(debug_assertions)]
-            start: Instant::now(),
         }
     }
 }
@@ -56,21 +48,5 @@ impl Deref for ExtensionsGuard<'_> {
 impl DerefMut for ExtensionsGuard<'_> {
     fn deref_mut(&mut self) -> &mut super::Extensions {
         &mut self.guard
-    }
-}
-
-#[cfg(debug_assertions)]
-impl Drop for ExtensionsGuard<'_> {
-    fn drop(&mut self) {
-        // In debug builds we check that extensions is never held for too long.
-        // We  only check if the current runtime is multi-threaded, because a bunch of unit tests fail the assertion and these need to be investigated separately.
-        if let Ok(runtime) = tokio::runtime::Handle::try_current() {
-            if runtime.runtime_flavor() == tokio::runtime::RuntimeFlavor::MultiThread {
-                let elapsed = self.start.elapsed();
-                if elapsed > Duration::from_millis(10) {
-                    panic!("ExtensionsGuard held for {}ms. This is probably a bug that will stall the Router and cause performance problems. Run with `RUST_BACKTRACE=1` environment variable to display a backtrace", elapsed.as_millis());
-                }
-            }
-        }
     }
 }
