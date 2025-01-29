7e73365ed34e0637863e79f2f23031dfbffb103d
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2025-01-29 00:47:24 -0600

    missed file

--- apollo-router/src/configuration/mod.rs
+++ apollo-router/src/configuration/mod.rs
@@ -387,7 +387,6 @@ impl Configuration {
         QueryPlannerConfig {
             subgraph_graphql_validation: false,
             generate_query_fragments: self.supergraph.generate_query_fragments,
-            generate_query_fragments_v2: false,
             incremental_delivery: QueryPlanIncrementalDeliveryConfig {
                 enable_defer: self.supergraph.defer_support,
             },
