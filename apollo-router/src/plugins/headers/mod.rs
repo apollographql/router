7b129df7d7573e3329f6b4a8835b80f4d09c97c9
bryn <bryn@apollographql.com>
2025-01-30 16:16:07 +0000

    Improve tests. This required improving the test harness to allow env to be passed into the closure.

--- apollo-router/src/plugins/headers/mod.rs
+++ apollo-router/src/plugins/headers/mod.rs
@@ -1110,115 +1110,86 @@ mod test {
         }
     }
 
-    #[tokio::test]
-    async fn test_propagate_passthrough() -> Result<(), BoxError> {
+    async fn assert_headers(
+        config: &'static str,
+        input: Vec<(&'static str, &'static str)>,
+        output: Vec<(&'static str, &'static str)>,
+    ) {
         let test_harness = PluginTestHarness::<Headers>::builder()
-            .config(include_str!("fixtures/propagate_passthrough.router.yaml"))
+            .config(config)
             .build()
             .await;
-        let service = test_harness.subgraph_service("test", |r| async move {
-            Ok(subgraph::Response::fake_builder()
-                .headers(r.subgraph_request.headers().clone())
-                .build())
+        let service = test_harness.subgraph_service("test", move |r| {
+            let output = output.clone();
+            async move {
+                // Assert the headers here
+                let headers = r.subgraph_request.headers();
+                for (name, value) in output.iter() {
+                    if let Some(header) = headers.get(*name) {
+                        assert_eq!(header.to_str().unwrap(), *value);
+                    } else {
+                        panic!("missing header {}", name);
+                    }
+                }
+                Ok(subgraph::Response::fake_builder().build())
+            }
         });
 
-        let res = service
+        let mut req = http::Request::builder();
+        for (name, value) in input.iter() {
+            req = req.header(*name, *value);
+        }
+
+        service
             .call(
                 subgraph::Request::fake_builder()
                     .supergraph_request(Arc::new(
-                        http::Request::builder()
-                            .header("a", "av")
-                            .header("c", "cv")
-                            .body(graphql::Request::default())
-                            .unwrap(),
+                        req.body(graphql::Request::default())
+                            .expect("valid request"),
                     ))
                     .build(),
             )
-            .await?;
-        assert_eq!(
-            res.response.headers().get("a").unwrap().to_str().unwrap(),
-            "av"
-        );
-        assert_eq!(
-            res.response.headers().get("b").unwrap().to_str().unwrap(),
-            "av"
-        );
-        assert_eq!(
-            res.response.headers().get("c").unwrap().to_str().unwrap(),
-            "cv"
-        );
-
-        let res = service
-            .call(
-                subgraph::Request::fake_builder()
-                    .supergraph_request(Arc::new(
-                        http::Request::builder()
-                            .header("b", "bv")
-                            .header("c", "cv")
-                            .body(graphql::Request::default())
-                            .unwrap(),
-                    ))
-                    .build(),
-            )
-            .await?;
-        assert_eq!(
-            res.response.headers().get("b").unwrap().to_str().unwrap(),
-            "bv"
-        );
-        assert_eq!(
-            res.response.headers().get("c").unwrap().to_str().unwrap(),
-            "cv"
-        );
-
-        Ok(())
+            .await
+            .unwrap();
     }
 
     #[tokio::test]
-    async fn test_propagate_passthrough_defaulted() -> Result<(), BoxError> {
-        let test_harness = PluginTestHarness::<Headers>::builder()
-            .config(include_str!(
-                "fixtures/propagate_passthrough_defaulted.router.yaml"
-            ))
-            .build()
-            .await;
-        let service = test_harness.subgraph_service("test", |r| async move {
-            let header = r
-                .subgraph_request
-                .headers()
-                .get("b")
-                .expect("expected header")
-                .to_str()
-                .expect("expected header value");
-            assert!(header == "defaulted" || header == "av");
-            Ok(subgraph::Response::fake_builder().build())
-        });
+    async fn test_propagate_passthrough() {
+        assert_headers(
+            include_str!("fixtures/propagate_passthrough.router.yaml"),
+            vec![("a", "av"), ("c", "cv")],
+            vec![("a", "av"), ("b", "av"), ("c", "cv")],
+        )
+        .await;
 
-        let _ = service
-            .call(
-                subgraph::Request::fake_builder()
-                    .supergraph_request(Arc::new(
-                        http::Request::builder()
-                            .header("a", "av")
-                            .body(graphql::Request::default())
-                            .unwrap(),
-                    ))
-                    .build(),
-            )
-            .await;
+        assert_headers(
+            include_str!("fixtures/propagate_passthrough.router.yaml"),
+            vec![("b", "bv"), ("c", "cv")],
+            vec![("b", "bv"), ("c", "cv")],
+        )
+        .await;
+    }
 
-        let _ = service
-            .call(
-                subgraph::Request::fake_builder()
-                    .supergraph_request(Arc::new(
-                        http::Request::builder()
-                            .header("b", "bv")
-                            .body(graphql::Request::default())
-                            .unwrap(),
-                    ))
-                    .build(),
-            )
-            .await;
+    #[tokio::test]
+    async fn test_propagate_passthrough_defaulted() {
+        assert_headers(
+            include_str!("fixtures/propagate_passthrough_defaulted.router.yaml"),
+            vec![("a", "av")],
+            vec![("b", "av")],
+        )
+        .await;
 
-        Ok(())
+        assert_headers(
+            include_str!("fixtures/propagate_passthrough_defaulted.router.yaml"),
+            vec![("b", "bv")],
+            vec![("b", "bv")],
+        )
+        .await;
+        assert_headers(
+            include_str!("fixtures/propagate_passthrough_defaulted.router.yaml"),
+            vec![("c", "cv")],
+            vec![("b", "defaulted")],
+        )
+        .await;
     }
 }
