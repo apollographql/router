6b73d582478e0918d7aa8244ea2da6817ae18ca7
Matthew Hawkins <matthew.hawkins@apollographql.com>
2025-02-07 07:21:14 -0700

    Add assertion on total allocations

--- apollo-federation/tests/connectors/validation.rs
+++ apollo-federation/tests/connectors/validation.rs
@@ -4,7 +4,11 @@ pub(crate) static ALLOC: dhat::Alloc = dhat::Alloc;
 #[test]
 fn valid_large_body() {
     const SCHEMA: &str = "src/sources/connect/validation/test_data/valid_large_body.graphql";
+
+    // These values should be kept slightly larger (~10%) than the current heap usage to catch
+    // significant increases. Failure of the test can be diagnosed using the dhat-heap.json file.
     const MAX_BYTES: usize = 204_800; // 200 KiB
+    const MAX_ALLOCATIONS: u64 = 22_300;
 
     let schema = std::fs::read_to_string(SCHEMA).unwrap();
 
@@ -14,4 +18,5 @@ fn valid_large_body() {
 
     let stats = dhat::HeapStats::get();
     dhat::assert!(stats.max_bytes < MAX_BYTES);
+    dhat::assert!(stats.total_blocks < MAX_ALLOCATIONS);
 }
