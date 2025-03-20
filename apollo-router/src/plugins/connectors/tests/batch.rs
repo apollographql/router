e20f404502ca1274562bfc6612f303c8b81ac19f
Lenny Burdette <lenny@apollographql.com>
2025-03-20 14:16:17 -0400

    support @connect on types with $this for simple entity resolvers
    keeps things consistent with $batch

--- apollo-router/src/plugins/connectors/tests/batch.rs
+++ apollo-router/src/plugins/connectors/tests/batch.rs
@@ -6,8 +6,6 @@ use wiremock::matchers::path;
 
 use super::req_asserts::Matcher;
 
-const BATCH_SDL: &str = include_str!("../testdata/batch.graphql");
-
 #[tokio::test]
 async fn basic_batch() {
     let mock_server = MockServer::start().await;
@@ -41,7 +39,7 @@ async fn basic_batch() {
         .await;
 
     let response = super::execute(
-        BATCH_SDL,
+        include_str!("../testdata/batch.graphql"),
         &mock_server.uri(),
         "query { users { id name username } }",
         Default::default(),
@@ -82,6 +80,93 @@ async fn basic_batch() {
                 .method("POST")
                 .path("/users-batch")
                 .body(serde_json::json!({ "ids": [3,1,2] })),
+        ],
+    );
+}
+
+#[tokio::test]
+async fn connect_on_type() {
+    let mock_server = MockServer::start().await;
+    Mock::given(method("GET"))
+        .and(path("/users"))
+        .respond_with(ResponseTemplate::new(200).set_body_json(serde_json::json!([
+        { "id": 3 },
+        { "id": 1 },
+        { "id": 2 }])))
+        .mount(&mock_server)
+        .await;
+    Mock::given(method("GET"))
+        .and(path("/users/1"))
+        .respond_with(ResponseTemplate::new(200).set_body_json(serde_json::json!(
+        {
+          "id": 1,
+          "name": "Leanne Graham",
+          "username": "Bret"
+        })))
+        .mount(&mock_server)
+        .await;
+    Mock::given(method("GET"))
+        .and(path("/users/2"))
+        .respond_with(ResponseTemplate::new(200).set_body_json(serde_json::json!(
+        {
+          "id": 2,
+          "name": "Ervin Howell",
+          "username": "Antonette"
+        })))
+        .mount(&mock_server)
+        .await;
+    Mock::given(method("GET"))
+        .and(path("/users/3"))
+        .respond_with(ResponseTemplate::new(200).set_body_json(serde_json::json!(
+        {
+          "id": 3,
+          "name": "Clementine Bauch",
+          "username": "Samantha"
+        })))
+        .mount(&mock_server)
+        .await;
+
+    let response = super::execute(
+        include_str!("../testdata/connect-on-type.graphql"),
+        &mock_server.uri(),
+        "query { users { id name username } }",
+        Default::default(),
+        None,
+        |_| {},
+    )
+    .await;
+
+    insta::assert_json_snapshot!(response, @r#"
+    {
+      "data": {
+        "users": [
+          {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "username": "Samantha"
+          },
+          {
+            "id": 1,
+            "name": "Leanne Graham",
+            "username": "Bret"
+          },
+          {
+            "id": 2,
+            "name": "Ervin Howell",
+            "username": "Antonette"
+          }
+        ]
+      }
+    }
+    "#);
+
+    super::req_asserts::matches(
+        &mock_server.received_requests().await.unwrap(),
+        vec![
+            Matcher::new().method("GET").path("/users"),
+            Matcher::new().method("GET").path("/users/3"),
+            Matcher::new().method("GET").path("/users/1"),
+            Matcher::new().method("GET").path("/users/2"),
         ],
     );
 }
