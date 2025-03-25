cd4953b75c2e8b3cc71ffdb1028ff0331b81b933
Dylan Anthony <dylan@apollographql.com>
2025-03-25 10:30:53 -0600

    Prevent composition of 0.2.
    Unless a build of composition specifically opts in to the unstable 0.2 feature now, composition will reject 0.2 versions.

--- apollo-federation/src/sources/connect/spec/mod.rs
+++ apollo-federation/src/sources/connect/spec/mod.rs
@@ -156,6 +156,14 @@ impl ConnectSpec {
             ConnectSpec::V0_2 => CONNECT_V0_2_LOCATIONS,
         }
     }
+
+    pub(crate) const fn available() -> &'static [ConnectSpec] {
+        if cfg!(any(feature = "connect_v02", test)) {
+            &[ConnectSpec::V0_1, ConnectSpec::V0_2]
+        } else {
+            &[ConnectSpec::V0_1]
+        }
+    }
 }
 
 impl TryFrom<&Version> for ConnectSpec {
@@ -163,6 +171,7 @@ impl TryFrom<&Version> for ConnectSpec {
     fn try_from(version: &Version) -> Result<Self, Self::Error> {
         match (version.major, version.minor) {
             (0, 1) => Ok(Self::V0_1),
+            #[cfg(any(feature = "connect_v02", test))]
             (0, 2) => Ok(Self::V0_2),
             _ => Err(SingleFederationError::UnknownLinkVersion {
                 message: format!("Unknown connect version: {version}"),
