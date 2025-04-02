70f8ceb0460909c038b1fe3b0f9b08844fcfaca8
Iryna Shestak <shestak.irina@gmail.com>
2025-04-02 17:03:06 +0200

    remove tokio worker threads metric

--- apollo-router/src/executable.rs
+++ apollo-router/src/executable.rs
@@ -345,9 +345,9 @@ pub fn main() -> Result<()> {
         builder.worker_threads(nb);
     }
     let runtime = builder.build()?;
-    let metrics = Handle::current().metrics();
-    let n = metrics.num_workers();
-    tracing::info!(n = n, "starting router with tokio worker threads count");
+    // let metrics = Handle::current().metrics();
+    // let n = metrics.num_workers();
+    // tracing::info!(n = n, "starting router with tokio worker threads count");
     runtime.block_on(Executable::builder().start())
 }
 
