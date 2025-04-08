c75fa6c8ecbd398d2c1a7d7bf58265b43c87fb25
Renée Kooi <renee.kooi@apollographql.com>
2025-04-08 12:10:50 +0200

    Add failing test for ROUTER-1320

--- apollo-federation/tests/query_plan/build_query_plan_tests/subscriptions.rs
+++ apollo-federation/tests/query_plan/build_query_plan_tests/subscriptions.rs
@@ -191,3 +191,93 @@ fn trying_to_use_defer_with_a_subscription_results_in_an_error() {
         .build_query_plan(&document, Some(name!(MySubscription)), Default::default())
         .expect_err("should return an error");
 }
+
+#[test]
+fn trying_to_use_skip_with_a_subscription_results_in_an_error() {
+    let planner = planner!(
+    SubgraphA: r#"
+            type Query {
+                me: User!
+            }
+
+            type Subscription {
+                onNewUser: User!
+            }
+
+            type User @key(fields: "id") {
+                id: ID!
+                name: String!
+            }
+        "#,
+    SubgraphB: r#"
+            type Query {
+                foo: Int
+            }
+
+            type User @key(fields: "id") {
+                id: ID!
+                address: String!
+            }
+        "#,
+    );
+
+    let document = ExecutableDocument::parse_and_validate(
+        planner.api_schema().schema(),
+        r#"
+        subscription MySubscription($v: Boolean!) {
+          onNewUser @skip(if: $v) { id name }
+        }
+        "#,
+        "trying_to_use_skip_with_a_subcription_results_in_an_error.graphql",
+    )
+    .unwrap();
+
+    planner
+        .build_query_plan(&document, Some(name!(MySubscription)), Default::default())
+        .expect_err("should return an error");
+}
+
+#[test]
+fn trying_to_use_include_with_a_subscription_results_in_an_error() {
+    let planner = planner!(
+    SubgraphA: r#"
+            type Query {
+                me: User!
+            }
+
+            type Subscription {
+                onNewUser: User!
+            }
+
+            type User @key(fields: "id") {
+                id: ID!
+                name: String!
+            }
+        "#,
+    SubgraphB: r#"
+            type Query {
+                foo: Int
+            }
+
+            type User @key(fields: "id") {
+                id: ID!
+                address: String!
+            }
+        "#,
+    );
+
+    let document = ExecutableDocument::parse_and_validate(
+        planner.api_schema().schema(),
+        r#"
+        subscription MySubscription($v: Boolean!) {
+          onNewUser @include(if: $v) { id name }
+        }
+        "#,
+        "trying_to_use_include_with_a_subcription_results_in_an_error.graphql",
+    )
+    .unwrap();
+
+    planner
+        .build_query_plan(&document, Some(name!(MySubscription)), Default::default())
+        .expect_err("should return an error");
+}
