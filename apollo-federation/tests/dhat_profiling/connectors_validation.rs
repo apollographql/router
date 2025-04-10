ebcdbf79659d2baabba448dd917bddb78e35128b
Ben Newman <ben@apollographql.com>
2025-04-10 10:25:26 -0400

    Tweak valid_large_body test to validate even larger bodies.

--- apollo-federation/tests/dhat_profiling/connectors_validation.rs
+++ apollo-federation/tests/dhat_profiling/connectors_validation.rs
@@ -9,8 +9,10 @@ pub(crate) static ALLOC: dhat::Alloc = dhat::Alloc;
 fn valid_large_body() {
     const SCHEMA: &str = "src/sources/connect/validation/test_data/valid_large_body.graphql";
 
-    const MAX_BYTES: usize = 204_800; // 200 KiB
-    const MAX_ALLOCATIONS: u64 = 22_300;
+    // Both of these values are rounded up to the next hundred to provide a
+    // small amount of wiggle room.
+    const MAX_BYTES: usize = 283_500; // 276.8 KiB
+    const MAX_ALLOCATIONS: u64 = 23_700;
 
     let schema = std::fs::read_to_string(SCHEMA).unwrap();
 
@@ -19,6 +21,16 @@ fn valid_large_body() {
     apollo_federation::sources::connect::validation::validate(&schema, SCHEMA);
 
     let stats = dhat::HeapStats::get();
-    dhat::assert!(stats.max_bytes < MAX_BYTES);
-    dhat::assert!(stats.total_blocks < MAX_ALLOCATIONS);
+    dhat::assert!(
+        stats.max_bytes < MAX_BYTES,
+        "{} < {}",
+        stats.max_bytes,
+        MAX_BYTES
+    );
+    dhat::assert!(
+        stats.total_blocks < MAX_ALLOCATIONS,
+        "{} < {}",
+        stats.total_blocks,
+        MAX_ALLOCATIONS
+    );
 }
