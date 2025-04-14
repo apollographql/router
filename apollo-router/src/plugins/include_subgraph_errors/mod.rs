1a139f812a9d7373397bffa36f8bc5d43b29ad46
bryn <bryn@apollographql.com>
2025-04-14 23:31:58 +0100

    Disable processing for non-subgraph errors

--- apollo-router/src/plugins/include_subgraph_errors/mod.rs
+++ apollo-router/src/plugins/include_subgraph_errors/mod.rs
@@ -64,56 +64,56 @@ impl Plugin for IncludeSubgraphErrors {
             .map_response(move |response: SupergraphResponse| {
                 response.map_stream(move |mut graphql_response: graphql::Response| {
                     for error in &mut graphql_response.errors {
-                        // If no subgraph name is found then we'll use the default rules
-                        let subgraph_name = error.subgraph_name().unwrap_or_default();
-                        // Get the effective config for this specific subgraph, or use default
-                        let effective_config = config.subgraphs.get(&subgraph_name).unwrap_or(&config.default);
+                        if let Some(subgraph_name) = error.subgraph_name() {
+                            // Get the effective config for this specific subgraph, or use default
+                            let effective_config = config.subgraphs.get(&subgraph_name).unwrap_or(&config.default);
 
-                        if !effective_config.include_errors {
-                            tracing::debug!("Redacting errors for subgraph '{}' based on config: include_errors=false", subgraph_name);
-                            // Redact fully if errors should not be included
-                            error.message = REDACTED_ERROR_MESSAGE.to_string();
-                            error.extensions = Object::new(); // Clear all extensions
-                        } else {
-                            tracing::debug!("Processing errors for subgraph '{}' based on config: {:?}", subgraph_name, effective_config);
-                            // Process errors based on the effective config
-                            // 1. Redact message if needed
-                            if effective_config.redact_message {
+                            if !effective_config.include_errors {
+                                tracing::debug!("Redacting errors for subgraph '{}' based on config: include_errors=false", subgraph_name);
+                                // Redact fully if errors should not be included
                                 error.message = REDACTED_ERROR_MESSAGE.to_string();
-                            }
+                                error.extensions = Object::new(); // Clear all extensions
+                            } else {
+                                tracing::debug!("Processing errors for subgraph '{}' based on config: {:?}", subgraph_name, effective_config);
+                                // Process errors based on the effective config
+                                // 1. Redact message if needed
+                                if effective_config.redact_message {
+                                    error.message = REDACTED_ERROR_MESSAGE.to_string();
+                                }
 
-                            // 2. Add 'service' extension (unless denied)
-                            let service_key = "service".to_string();
-                            let is_service_denied = effective_config
-                                .deny_extensions_keys
-                                .as_ref()
-                                .map_or(false, |deny| deny.contains(&service_key));
-                            let is_service_allowed = effective_config
-                                .allow_extensions_keys
-                                .as_ref()
-                                .map_or(true, |allow| allow.contains(&service_key)); // Allowed if no allow list or if present in allow list
+                                // 2. Add 'service' extension (unless denied)
+                                let service_key = "service".to_string();
+                                let is_service_denied = effective_config
+                                    .deny_extensions_keys
+                                    .as_ref()
+                                    .map_or(false, |deny| deny.contains(&service_key));
+                                let is_service_allowed = effective_config
+                                    .allow_extensions_keys
+                                    .as_ref()
+                                    .map_or(true, |allow| allow.contains(&service_key)); // Allowed if no allow list or if present in allow list
 
-                            if !is_service_denied && is_service_allowed && !subgraph_name.is_empty() {
-                                error
-                                    .extensions
-                                    .entry(service_key)
-                                    .or_insert(subgraph_name.clone().into());
-                            }
+                                if !is_service_denied && is_service_allowed {
+                                    error
+                                        .extensions
+                                        .entry(service_key)
+                                        .or_insert(subgraph_name.clone().into());
+                                }
 
-                            // 3. Filter extensions based on allow list
-                            if let Some(allow_keys) = &effective_config.allow_extensions_keys {
-                                let mut original_extensions = std::mem::take(&mut error.extensions);
-                                for key in allow_keys {
-                                    if let Some((key, value)) = original_extensions.remove_entry(key.as_str()) {
-                                        error.extensions.insert(key, value);
+                                // 3. Filter extensions based on allow list
+                                if let Some(allow_keys) = &effective_config.allow_extensions_keys {
+                                    let mut original_extensions = std::mem::take(&mut error.extensions);
+                                    for key in allow_keys {
+                                        if let Some((key, value)) = original_extensions.remove_entry(key.as_str()) {
+                                            error.extensions.insert(key, value);
+                                        }
                                     }
                                 }
-                            }
 
-                            // 4. Remove extensions based on deny list (applied *after* allow list)
-                            if let Some(deny_keys) = &effective_config.deny_extensions_keys {
-                                for key in deny_keys {
-                                    error.extensions.remove(key.as_str());
+                                // 4. Remove extensions based on deny list (applied *after* allow list)
+                                if let Some(deny_keys) = &effective_config.deny_extensions_keys {
+                                    for key in deny_keys {
+                                        error.extensions.remove(key.as_str());
+                                    }
                                 }
                             }
                         }
