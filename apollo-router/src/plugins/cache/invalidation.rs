829a7525b43115b801e6daf424e6ed7981ce27a7
Duckki Oe <duckki.oe@apollographql.com>
2025-04-15 05:24:13 -0700

    fix(entity-cache): handle multiple key directives (#7228)

--- apollo-router/src/plugins/cache/invalidation.rs
+++ apollo-router/src/plugins/cache/invalidation.rs
@@ -5,7 +5,6 @@ use fred::error::RedisError;
 use fred::types::Scanner;
 use futures::StreamExt;
 use futures::stream;
-use indexmap::IndexMap;
 use itertools::Itertools;
 use serde::Deserialize;
 use serde::Serialize;
@@ -225,7 +224,7 @@ pub(crate) enum InvalidationRequest {
     Entity {
         subgraph: String,
         r#type: String,
-        key: IndexMap<ByteString, Value>,
+        key: serde_json_bytes::Map<ByteString, Value>,
     },
 }
 
