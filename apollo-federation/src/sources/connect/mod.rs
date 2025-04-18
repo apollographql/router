12934d9db1ceadb36c1d5fa3d507409412d7dc39
Dylan Anthony <dylan@apollographql.com>
2025-04-18 11:52:01 -0600

    Move `make_uri` into `apollo-federation` `HttpJsonTransport`
    This makes some tests easier to read and gives us the ability to consume `make_uri` from the playground.

--- apollo-federation/src/sources/connect/mod.rs
+++ apollo-federation/src/sources/connect/mod.rs
@@ -33,6 +33,7 @@ pub use self::models::EntityResolver;
 pub use self::models::HTTPMethod;
 pub use self::models::HeaderSource;
 pub use self::models::HttpJsonTransport;
+pub use self::models::MakeUriError;
 use crate::schema::position::ObjectFieldDefinitionPosition;
 use crate::schema::position::ObjectOrInterfaceFieldDefinitionPosition;
 use crate::schema::position::ObjectOrInterfaceFieldDirectivePosition;
