26d005512236cb2ab9d5bd9794e42369ea89bfd6
Ben Newman <ben@apollographql.com>
2025-04-22 13:47:11 -0400

    Test left associativity of `PathList::compute_output_shape`.
    Tests to prove https://apollographql.atlassian.net/browse/CNN-722 will
    be resolved by the previous commit.

--- apollo-federation/src/connectors/json_selection/apply_to.rs
+++ apollo-federation/src/connectors/json_selection/apply_to.rs
@@ -2863,6 +2863,113 @@ mod tests {
     }
 
     #[test]
+    fn test_left_associative_output_shapes() {
+        assert_eq!(selection!("$batch.id").shape().pretty_print(), "$batch.id");
+
+        assert_eq!(
+            selection!("$batch.id->first").shape().pretty_print(),
+            "$batch.id.0",
+        );
+
+        assert_eq!(
+            selection!("$batch.id->last").shape().pretty_print(),
+            "$batch.id.*",
+        );
+
+        let mut named_shapes = IndexMap::default();
+        named_shapes.insert(
+            "$batch",
+            Shape::list(
+                Shape::record(
+                    {
+                        let mut map = Shape::empty_map();
+                        map.insert("id".to_string(), Shape::int([]));
+                        map
+                    },
+                    [],
+                ),
+                [],
+            ),
+        );
+
+        let root_shape = Shape::name("$root", []);
+
+        let computed_batch_id = selection!("$batch.id").compute_output_shape(
+            root_shape.clone(),
+            &named_shapes,
+            &SourceId::Other("JSONSelection".into()),
+        );
+        assert_eq!(computed_batch_id.pretty_print(), "List<Int>");
+
+        let computed_first = selection!("$batch.id->first").compute_output_shape(
+            root_shape.clone(),
+            &named_shapes,
+            &SourceId::Other("JSONSelection".into()),
+        );
+        assert_eq!(computed_first.pretty_print(), "One<Int, None>");
+
+        let computed_last = selection!("$batch.id->last").compute_output_shape(
+            root_shape.clone(),
+            &named_shapes,
+            &SourceId::Other("JSONSelection".into()),
+        );
+        assert_eq!(computed_last.pretty_print(), "One<Int, None>");
+
+        assert_eq!(
+            selection!("$batch.id->jsonStringify")
+                .shape()
+                .pretty_print(),
+            "String",
+        );
+
+        assert_eq!(
+            selection!("$batch.id->map([@])->echo([@])->jsonStringify")
+                .shape()
+                .pretty_print(),
+            "String",
+        );
+
+        assert_eq!(
+            selection!("$batch.id->map(@)->echo(@)")
+                .shape()
+                .pretty_print(),
+            "List<$batch.id.*>",
+        );
+
+        assert_eq!(
+            selection!("$batch.id->map(@)->echo([@])")
+                .shape()
+                .pretty_print(),
+            "[List<$batch.id.*>]",
+        );
+
+        assert_eq!(
+            selection!("$batch.id->map([@])->echo(@)")
+                .shape()
+                .pretty_print(),
+            "List<[$batch.id.*]>",
+        );
+
+        assert_eq!(
+            selection!("$batch.id->map([@])->echo([@])")
+                .shape()
+                .pretty_print(),
+            "[List<[$batch.id.*]>]",
+        );
+
+        assert_eq!(
+            selection!("$batch.id->map([@])->echo([@])")
+                .compute_output_shape(
+                    root_shape,
+                    &named_shapes,
+                    &SourceId::Other("JSONSelection".into()),
+                )
+                .pretty_print(),
+            "[List<[Int]>]",
+        );
+    }
+
+    #[test]
     fn test_lit_paths() {
         let data = json!({
             "value": {
