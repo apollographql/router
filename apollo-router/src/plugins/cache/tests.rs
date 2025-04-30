17c3949858145e1f6ea96b94c17658d97cea681d
Benjamin <5719034+bnjjj@users.noreply.github.com>
2025-04-30 13:01:25 +0200

    add more integration test
    Signed-off-by: Benjamin <5719034+bnjjj@users.noreply.github.com>

--- apollo-router/src/plugins/cache/tests.rs
+++ apollo-router/src/plugins/cache/tests.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::sync::Arc;
-use std::time::Duration;
 
 use apollo_compiler::Schema;
 use bytes::Bytes;
--- apollo-router/tests/integration/redis.rs
+++ apollo-router/tests/integration/redis.rs
@@ -696,6 +696,291 @@ async fn entity_cache_basic() -> Result<(), BoxError> {
 }
 
 #[tokio::test(flavor = "multi_thread")]
+async fn entity_cache_with_nested_field_set() -> Result<(), BoxError> {
+    if !graph_os_enabled() {
+        return Ok(());
+    }
+    let schema = include_str!("../../src/testdata/supergraph_nested_fields.graphql");
+
+    let config = RedisConfig::from_url("redis://127.0.0.1:6379").unwrap();
+    let client = RedisClient::new(config, None, None, None);
+    let connection_task = client.connect();
+    client.wait_for_connect().await.unwrap();
+
+    let subgraphs = serde_json::json!({
+        "products": {
+            "query": {"allProducts": [{
+                "id": "1",
+                "name": "Test",
+                "sku": "150",
+                "createdBy": { "__typename": "User", "email": "test@test.com", "country": {"a": "France"} }
+            }]},
+            "headers": {"cache-control": "public"},
+        },
+        "users": {
+            "entities": [{
+                "__typename": "User",
+                "email": "test@test.com",
+                "name": "test",
+                "country": {
+                    "a": "France"
+                }
+            }],
+            "headers": {"cache-control": "public"},
+        }
+    });
+
+    let supergraph = apollo_router::TestHarness::builder()
+        .with_subgraph_network_requests()
+        .configuration_json(json!({
+            "preview_entity_cache": {
+                "enabled": true,
+                "invalidation": {
+                    "listen": "127.0.0.1:4000",
+                    "path": "/invalidation"
+                },
+                "subgraph": {
+                    "all": {
+                        "enabled": true,
+                        "redis": {
+                            "urls": ["redis://127.0.0.1:6379"],
+                            "ttl": "2s",
+                            "pool_size": 3
+                        },
+                    }
+                }
+            },
+            "include_subgraph_errors": {
+                "all": true
+            },
+            "supergraph": {
+                // TODO(@goto-bus-stop): need to update the mocks and remove this, #6013
+                "generate_query_fragments": false,
+            },
+            "experimental_mock_subgraphs": subgraphs.clone()
+        }))
+        .unwrap()
+        .schema(schema)
+        .build_supergraph()
+        .await
+        .unwrap();
+    let query = "query { allProducts { name createdBy { name country { a } } } }";
+
+    let request = supergraph::Request::fake_builder()
+        .query(query)
+        .method(Method::POST)
+        .build()
+        .unwrap();
+
+    let response = supergraph
+        .oneshot(request)
+        .await
+        .unwrap()
+        .next_response()
+        .await
+        .unwrap();
+    insta::assert_json_snapshot!(response);
+
+    // if this is failing due to a cache key change, hook up redis-cli with the MONITOR command to see the keys being set
+    let s:String = client
+          .get("version:1.0:subgraph:products:type:Query:hash:bead1beb068f19990b462d5d7c17974a33b822f4b3439407a9f820ce1cb47de9:data:d9d84a3c7ffc27b0190a671212f3740e5b8478e84e23825830e97822e25cf05c")
+          .await
+          .unwrap();
+    let v: Value = serde_json::from_str(&s).unwrap();
+    insta::assert_json_snapshot!(v.as_object().unwrap().get("data").unwrap());
+
+    let s: String = client
+        .get("version:1.0:subgraph:users:type:User:entity:210e26346d676046faa9fb55d459273a43e5b5397a1a056f179a3521dc5643aa:representation:7cd02a08f4ea96f0affa123d5d3f56abca20e6014e060fe5594d210c00f64b27:hash:cfc5f467f767710804724ff6a05c3f63297328cd8283316adb25f5642e1439ad:data:d9d84a3c7ffc27b0190a671212f3740e5b8478e84e23825830e97822e25cf05c")
+        .await
+        .unwrap();
+    let v: Value = serde_json::from_str(&s).unwrap();
+    insta::assert_json_snapshot!(v.as_object().unwrap().get("data").unwrap());
+
+    let supergraph = apollo_router::TestHarness::builder()
+        .with_subgraph_network_requests()
+        .configuration_json(json!({
+            "preview_entity_cache": {
+                "enabled": true,
+                "invalidation": {
+                    "listen": "127.0.0.1:4000",
+                    "path": "/invalidation"
+                },
+                "subgraph": {
+                    "all": {
+                        "enabled": false,
+                        "redis": {
+                            "urls": ["redis://127.0.0.1:6379"],
+                            "ttl": "2s"
+                        },
+                    },
+                    "subgraphs": {
+                        "products": {
+                            "enabled": true,
+                            "ttl": "60s"
+                        },
+                        "reviews": {
+                            "enabled": true,
+                            "ttl": "10s"
+                        }
+                    }
+                }
+            },
+            "include_subgraph_errors": {
+                "all": true
+            },
+            "supergraph": {
+                // TODO(@goto-bus-stop): need to update the mocks and remove this, #6013
+                "generate_query_fragments": false,
+            },
+            "experimental_mock_subgraphs": subgraphs.clone()
+        }))
+        .unwrap()
+        .schema(schema)
+        .build_supergraph()
+        .await
+        .unwrap();
+
+    let request = supergraph::Request::fake_builder()
+        .query(query)
+        .method(Method::POST)
+        .build()
+        .unwrap();
+
+    let response = supergraph
+        .oneshot(request)
+        .await
+        .unwrap()
+        .next_response()
+        .await
+        .unwrap();
+    insta::assert_json_snapshot!(response);
+
+    let s: String = client
+        .get("version:1.0:subgraph:users:type:User:entity:210e26346d676046faa9fb55d459273a43e5b5397a1a056f179a3521dc5643aa:representation:7cd02a08f4ea96f0affa123d5d3f56abca20e6014e060fe5594d210c00f64b27:hash:cfc5f467f767710804724ff6a05c3f63297328cd8283316adb25f5642e1439ad:data:d9d84a3c7ffc27b0190a671212f3740e5b8478e84e23825830e97822e25cf05c")
+        .await
+        .unwrap();
+    let v: Value = serde_json::from_str(&s).unwrap();
+    insta::assert_json_snapshot!(v.as_object().unwrap().get("data").unwrap());
+
+    const SECRET_SHARED_KEY: &str = "supersecret";
+    let http_service = apollo_router::TestHarness::builder()
+        .with_subgraph_network_requests()
+        .configuration_json(json!({
+            "preview_entity_cache": {
+                "enabled": true,
+                "invalidation": {
+                    "listen": "127.0.0.1:4000",
+                    "path": "/invalidation"
+                },
+                "subgraph": {
+                    "all": {
+                        "enabled": true,
+                        "redis": {
+                            "urls": ["redis://127.0.0.1:6379"],
+                            "ttl": "2s"
+                        },
+                        "invalidation": {
+                            "enabled": true,
+                            "shared_key": SECRET_SHARED_KEY
+                        }
+                    },
+                    "subgraphs": {
+                        "products": {
+                            "enabled": true,
+                            "ttl": "60s",
+                            "invalidation": {
+                                "enabled": true,
+                                "shared_key": SECRET_SHARED_KEY
+                            }
+                        },
+                        "reviews": {
+                            "enabled": true,
+                            "ttl": "10s",
+                            "invalidation": {
+                                "enabled": true,
+                                "shared_key": SECRET_SHARED_KEY
+                            }
+                        }
+                    }
+                }
+            },
+            "include_subgraph_errors": {
+                "all": true
+            },
+            "supergraph": {
+                // TODO(@goto-bus-stop): need to update the mocks and remove this, #6013
+                "generate_query_fragments": false,
+            },
+            "experimental_mock_subgraphs": subgraphs.clone()
+        }))
+        .unwrap()
+        .schema(schema)
+        .build_http_service()
+        .await
+        .unwrap();
+
+    let request = http::Request::builder()
+        .uri("http://127.0.0.1:4000/invalidation")
+        .method(http::Method::POST)
+        .header(
+            http::header::CONTENT_TYPE,
+            HeaderValue::from_static("application/json"),
+        )
+        .header(
+            http::header::AUTHORIZATION,
+            HeaderValue::from_static(SECRET_SHARED_KEY),
+        )
+        .body(from_bytes(
+            serde_json::to_vec(&vec![json!({
+                "subgraph": "users",
+                "kind": "entity",
+                "type": "User",
+                "key": {
+                    "email": "test@test.com",
+                    "country": {
+                        "a": "France"
+                    }
+                }
+            })])
+            .unwrap(),
+        ))
+        .unwrap();
+    let response = http_service.oneshot(request).await.unwrap();
+    let response_status = response.status();
+    let mut resp: serde_json::Value = serde_json::from_str(
+        &apollo_router::services::router::body::into_string(response.into_body())
+            .await
+            .unwrap(),
+    )
+    .unwrap();
+
+    assert_eq!(
+        resp.as_object_mut()
+            .unwrap()
+            .get("count")
+            .unwrap()
+            .as_u64()
+            .unwrap(),
+        1u64
+    );
+    assert!(response_status.is_success());
+
+    // This should be in error because we invalidated this entity
+    assert!(client
+        .get::<String, _>("version:1.0:subgraph:users:type:User:entity:210e26346d676046faa9fb55d459273a43e5b5397a1a056f179a3521dc5643aa:representation:7cd02a08f4ea96f0affa123d5d3f56abca20e6014e060fe5594d210c00f64b27:hash:cfc5f467f767710804724ff6a05c3f63297328cd8283316adb25f5642e1439ad:data:d9d84a3c7ffc27b0190a671212f3740e5b8478e84e23825830e97822e25cf05c")
+        .await.is_err());
+    // This entry should still be in redis because we didn't invalidate this entry
+    assert!(client
+          .get::<String, _>("version:1.0:subgraph:products:type:Query:hash:bead1beb068f19990b462d5d7c17974a33b822f4b3439407a9f820ce1cb47de9:data:d9d84a3c7ffc27b0190a671212f3740e5b8478e84e23825830e97822e25cf05c")
+          .await.is_ok());
+
+    client.quit().await.unwrap();
+    // calling quit ends the connection and event listener tasks
+    let _ = connection_task.await;
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
 async fn entity_cache_authorization() -> Result<(), BoxError> {
     if !graph_os_enabled() {
         return Ok(());
