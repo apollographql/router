89bf460598e4d55aabe24e3498770dfc9ed4d745
Ross Regitsky <ross.regitsky@apollographql.com>
2025-05-19 10:19:12 -0400

    fix old apq name in snapshot test

--- apollo-router/src/plugins/telemetry/metrics/apollo/studio.rs
+++ apollo-router/src/plugins/telemetry/metrics/apollo/studio.rs
@@ -667,7 +667,7 @@ mod test {
                     query_metadata: None,
                 },
             )]),
-            router_features_enabled: vec!["apq".to_string(), "entity_cache".to_string()],
+            router_features_enabled: vec!["distributed_apq_cache".to_string(), "entity_cache".to_string()],
         }
     }
 
