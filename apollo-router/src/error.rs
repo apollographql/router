f506777222944e842c3eba3aa5e2a5ac3a58f882
Ross Regitsky <ross.regitsky@apollographql.com>
2025-05-21 13:43:57 -0400

    fix one more error builder spot

--- apollo-router/src/error.rs
+++ apollo-router/src/error.rs
@@ -158,10 +158,6 @@ impl FetchError {
             .message(self.to_string())
             .locations(Vec::default())
             .and_path(path)
-            // Extension code is required, but is only used if extensions doesn't have it. We always
-            // have a  code so this value will be ignored.
-            // TODO better way to unwrap the actual code from value just in case?
-            .extension_code("")
             .extensions(value.as_object().unwrap().to_owned())
             .build()
     }
