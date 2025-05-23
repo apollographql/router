4bcc1ba69c05c6135ad26ff9f8526602951f7217
Duckki Oe <duckki.oe@apollographql.com>
2025-05-23 11:59:22 -0700

    feat(federation/composition): port `satisfiabilityError` function (#7525)

--- apollo-federation/src/query_graph/graph_path/transition.rs
+++ apollo-federation/src/query_graph/graph_path/transition.rs
@@ -1,9 +1,28 @@
 use petgraph::graph::EdgeIndex;
 
+use crate::operation::Field;
 use crate::query_graph::QueryGraphEdgeTransition;
 use crate::query_graph::graph_path::GraphPath;
+use crate::query_graph::graph_path::GraphPathTriggerVariant;
+use crate::schema::position::CompositeTypeDefinitionPosition;
 
 /// A `GraphPath` whose triggers are query graph transitions in some other query graph (essentially
 /// meaning that the path has been guided by a walk through that other query graph).
 #[allow(dead_code)]
 pub(crate) type TransitionGraphPath = GraphPath<QueryGraphEdgeTransition, EdgeIndex>;
+
+impl GraphPathTriggerVariant for QueryGraphEdgeTransition {
+    fn get_field_parent_type(&self) -> Option<CompositeTypeDefinitionPosition> {
+        match self {
+            QueryGraphEdgeTransition::FieldCollection {
+                field_definition_position,
+                ..
+            } => Some(field_definition_position.parent()),
+            _ => None,
+        }
+    }
+
+    fn get_field_mut(&mut self) -> Option<&mut Field> {
+        None
+    }
+}
