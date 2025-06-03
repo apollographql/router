c807b0578033cb1a073c14fda72481da78cea937
Renée Kooi <renee.kooi@apollographql.com>
2025-06-03 14:05:23 +0200

    edition2024

--- apollo-router-core/src/lib.rs
+++ apollo-router-core/src/lib.rs
@@ -1,11 +1,1 @@
-extern crate apollo_compiler;
-extern crate apollo_federation;
-extern crate bytes;
-extern crate futures;
-extern crate http_body;
-extern crate http_body_util;
-extern crate serde_json;
-extern crate thiserror;
-extern crate tower;
-
 mod services;
