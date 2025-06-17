4b998eeacccaec4c8fb187ef785aae55b9f87acb
Ross Regitsky <ross.regitsky@apollographql.com>
2025-06-17 11:56:40 -0400

    comment change to retrigger tests

--- apollo-router/src/plugins/coprocessor/mod.rs
+++ apollo-router/src/plugins/coprocessor/mod.rs
@@ -1104,7 +1104,7 @@ where
             .map(|b| b.map(http_body::Frame::data).map_err(axum::Error::new)),
     ));
 
-    // Finally, return a response which has a Body that wraps our stream of response chunks.
+    // Finally, return a response which has a Body that wraps our stream of response chunks
     router::Response::http_response_builder()
         .context(context)
         .response(http::Response::from_parts(parts, final_stream))
