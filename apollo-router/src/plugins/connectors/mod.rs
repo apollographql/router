51736bf0d97c9f5b88b2e25638f52abf6e8aab6a
Lenny Burdette <lenny@apollographql.com>
2025-06-17 15:59:22 -0400

    move request input and variable merging into apollo-federation
    passing context to a-f code is handled by a trait

--- apollo-router/src/plugins/connectors/mod.rs
+++ apollo-router/src/plugins/connectors/mod.rs
@@ -12,3 +12,15 @@ pub(crate) mod tracing;
 
 #[cfg(test)]
 pub(crate) mod tests;
+
+use apollo_federation::connectors::runtime::inputs::ContextReader;
+
+impl ContextReader for &crate::Context {
+    fn to_json_map(
+        &self,
+    ) -> serde_json_bytes::Map<serde_json_bytes::ByteString, serde_json_bytes::Value> {
+        self.iter()
+            .map(|entry| (entry.key().as_str().into(), entry.value().clone()))
+            .collect()
+    }
+}
