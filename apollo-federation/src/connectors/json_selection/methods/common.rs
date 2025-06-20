6a264a16fd23a2309e65a9d23fb4ceb44cd88eff
Andrew McGivery <andrew.mcgivery@apollographql.com>
2025-06-20 11:34:02 -0400

    Account for ShapeCase::Name

--- apollo-federation/src/connectors/json_selection/methods/common.rs
+++ apollo-federation/src/connectors/json_selection/methods/common.rs
@@ -3,9 +3,9 @@ use shape::ShapeCase;
 pub(crate) fn is_comparable_shape_combination(shape1: &ShapeCase, shape2: &ShapeCase) -> bool {
     matches!(
         (shape1, shape2),
-        // Unknown is compatible with comparable types
-        (ShapeCase::Unknown, ShapeCase::Int(_) | ShapeCase::Float | ShapeCase::String(_) | ShapeCase::Unknown) |
-          (ShapeCase::Int(_) | ShapeCase::Float | ShapeCase::String(_), ShapeCase::Unknown) |
+        // Unknown and Name are compatible with comparable types
+        (ShapeCase::Unknown | ShapeCase::Name(_, _), ShapeCase::Int(_) | ShapeCase::Float | ShapeCase::String(_) | ShapeCase::Unknown | ShapeCase::Name(_, _)) |
+          (ShapeCase::Int(_) | ShapeCase::Float | ShapeCase::String(_), ShapeCase::Unknown | ShapeCase::Name(_, _)) |
           // Numeric types are cross-compatible
           (ShapeCase::Int(_), ShapeCase::Int(_) | ShapeCase::Float) |
           (ShapeCase::Float, ShapeCase::Int(_) | ShapeCase::Float) |
@@ -16,8 +16,10 @@ pub(crate) fn is_comparable_shape_combination(shape1: &ShapeCase, shape2: &Shape
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use shape::ShapeCase;
+    use shape::location::Located;
+
+    use super::*;
 
     #[rstest::rstest]
     #[case(&ShapeCase::Unknown, &ShapeCase::Unknown, true)]
@@ -32,6 +34,15 @@ mod tests {
     #[case(&ShapeCase::String(Some("a".to_string())), &ShapeCase::String(Some("a".to_string())), true)]
     #[case(&ShapeCase::String(Some("a".to_string())), &ShapeCase::Unknown, true)]
     #[case(&ShapeCase::Unknown, &ShapeCase::String(Some("a".to_string())), true)]
+    #[case(&ShapeCase::Name(Located::new("name".to_string(), []), vec![]), &ShapeCase::Name(Located::new("name".to_string(), []), vec![]), true)]
+    #[case(&ShapeCase::Name(Located::new("name".to_string(), []), vec![]), &ShapeCase::Unknown, true)]
+    #[case(&ShapeCase::Unknown, &ShapeCase::Name(Located::new("name".to_string(), []), vec![]), true)]
+    #[case(&ShapeCase::Name(Located::new("name".to_string(), []), vec![]), &ShapeCase::Int(Some(1)), true)]
+    #[case(&ShapeCase::Int(Some(1)), &ShapeCase::Name(Located::new("name".to_string(), []), vec![]), true)]
+    #[case(&ShapeCase::Name(Located::new("name".to_string(), []), vec![]), &ShapeCase::Float, true)]
+    #[case(&ShapeCase::Float, &ShapeCase::Name(Located::new("name".to_string(), []), vec![]), true)]
+    #[case(&ShapeCase::Name(Located::new("name".to_string(), []), vec![]), &ShapeCase::String(Some("a".to_string())), true)]
+    #[case(&ShapeCase::String(Some("a".to_string())), &ShapeCase::Name(Located::new("name".to_string(), []), vec![]), true)]
     fn test_is_comparable_shape_combination_positive_cases(
         #[case] shape1: &ShapeCase,
         #[case] shape2: &ShapeCase,
@@ -59,6 +70,10 @@ mod tests {
     #[case(&ShapeCase::String(Some("a".to_string())), &ShapeCase::Null, false)]
     #[case(&ShapeCase::Null, &ShapeCase::String(Some("a".to_string())), false)]
     #[case(&ShapeCase::Bool(Some(true)), &ShapeCase::Null, false)]
+    #[case(&ShapeCase::Name(Located::new("name".to_string(), []), vec![]), &ShapeCase::Bool(Some(true)), false)]
+    #[case(&ShapeCase::Bool(Some(true)), &ShapeCase::Name(Located::new("name".to_string(), []), vec![]), false)]
+    #[case(&ShapeCase::Name(Located::new("name".to_string(), []), vec![]), &ShapeCase::Null, false)]
+    #[case(&ShapeCase::Null, &ShapeCase::Name(Located::new("name".to_string(), []), vec![]), false)]
     #[case(&ShapeCase::Null, &ShapeCase::Bool(Some(true)), false)]
     fn test_is_comparable_shape_combination_negative_cases(
         #[case] shape1: &ShapeCase,
