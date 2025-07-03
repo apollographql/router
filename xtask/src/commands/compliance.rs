ed40b214bf8dc2f7a145eb5040735f58cd6db280
Renée Kooi <renee.kooi@apollographql.com>
2025-07-03 10:23:07 +0200

    ci: enable `xtask check-compliance` on windows again
    Fixes #3237
    
    Maybe it Just Works now

--- xtask/src/commands/compliance.rs
+++ xtask/src/commands/compliance.rs
@@ -6,11 +6,6 @@ pub struct Compliance {}
 
 impl Compliance {
     pub fn run(&self) -> Result<()> {
-        // Cargo deny is triggering `git credential-manager-core get`
-        // On windows CI this will hang as it requires user input.
-        // The root cause seems to be the krates step in cargo-deny but did not manage to figure it out.
-        // Disabling as a temporary measure, but we must fix this soon. https://github.com/apollographql/router/issues/3237
-        #[cfg(not(windows))]
         cargo!(["deny", "-L", "error", "check"]);
         Ok(())
     }
