663655c44947f4b1da1b56251e8783fc3312f7a2
Renée Kooi <renee.kooi@apollographql.com>
2025-07-10 14:52:02 +0200

    enable flaky tests

--- apollo-router/tests/integration/subscriptions/ws_passthrough.rs
+++ apollo-router/tests/integration/subscriptions/ws_passthrough.rs
@@ -420,7 +420,6 @@ async fn test_subscription_ws_passthrough_error_payload() -> Result<(), BoxError
 
 // We have disabled this test because this test is failing for reasons that are understood, but are now preventing us from doing other fixes. We will ensure this is fixed by tracking this in the attached ticket as a follow up on its own PR.
 // The bug is basically an inconsistency in the way we're returning an error, sometimes it's consider as a critical error, sometimes not.
-#[ignore = "ROUTER-1343"]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_subscription_ws_passthrough_pure_error_payload() -> Result<(), BoxError> {
     if !graph_os_enabled() {
@@ -505,7 +504,6 @@ async fn test_subscription_ws_passthrough_pure_error_payload() -> Result<(), Box
 
 // We have disabled this test because this test is failing for reasons that are understood, but are now preventing us from doing other fixes. We will ensure this is fixed by tracking this in the attached ticket as a follow up on its own PR.
 // The bug is basically an inconsistency in the way we're returning an error, sometimes it's consider as a critical error, sometimes not.
-#[ignore = "ROUTER-1343"]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_subscription_ws_passthrough_pure_error_payload_with_coprocessor()
 -> Result<(), BoxError> {
