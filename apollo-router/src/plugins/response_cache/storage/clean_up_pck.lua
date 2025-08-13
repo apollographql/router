50bea886d9622052ae479c0baf6dbbe2998cec3b
carodewig <16093297+carodewig@users.noreply.github.com>
2025-08-13 10:02:14 -0400

    remove reverse index code

--- apollo-router/src/plugins/response_cache/storage/clean_up_pck.lua
+++
@@ -1,9 +0,0 @@
-local pck_key = KEYS[1]
-local cache_tags_key = KEYS[2]
-
-if redis.call('EXISTS', pck_key) == 1 then
-    return ""
-else
-    return redis.call('GETDEL', cache_tags_key)
-end
-
--- apollo-router/src/plugins/response_cache/storage/redis.rs
+++ apollo-router/src/plugins/response_cache/storage/redis.rs
@@ -4,21 +4,16 @@ use std::time::Duration;
 
 use fred::clients::Client;
 use fred::clients::Pipeline;
-use fred::interfaces::ConfigInterface;
-use fred::interfaces::EventInterface;
 use fred::interfaces::KeysInterface;
 use fred::interfaces::LuaInterface;
-use fred::interfaces::PubsubInterface;
 use fred::interfaces::SortedSetsInterface;
 use fred::types::Expiration;
-use fred::types::Value;
 use fred::types::scan::Scanner;
 use fred::types::sorted_sets::Ordering;
 use futures::StreamExt;
 use futures::future::join_all;
 use serde::Deserialize;
 use serde::Serialize;
-use tokio::sync::broadcast::error::RecvError;
 use tokio::time::MissedTickBehavior;
 use tower::BoxError;
 
@@ -75,10 +70,6 @@ impl Storage {
             keyspace_storage,
         };
 
-        // TODO: also test performance, db size, etc without keyevents!
-        s.enable_keyevent_notifications().await;
-        s.subscribe_to_keyspace_events().await;
-
         s.perform_periodic_maintenance().await;
 
         Ok(s)
@@ -91,12 +82,6 @@ impl Storage {
     fn primary_cache_key(key: &str) -> String {
         // surround key with curly braces so that the key determines the shard (if enabled)
         format!("pck:{{{key}}}")
-    }
-
-    fn cache_tags_key(key: &str) -> String {
-        // surround key with curly braces so that the key determines the shard (if enabled)
-        // this ensures that the primary cache key and its cache tags live on the same shard
-        format!("cache-tags:{{{key}}}")
     }
 
     fn pck_prefix(&self) -> String {
@@ -112,7 +97,6 @@ impl Storage {
     ) -> StorageResult<()> {
         // TODO: how does this work with multiple shards?
         let expire_at = now + document.expire.as_secs();
-        let expire_at_doubled = now + document.expire.as_secs() * 2;
 
         let pck = self.make_key(Self::primary_cache_key(&document.cache_key));
         let value = CacheValue {
@@ -154,7 +138,7 @@ impl Storage {
 
         // TODO: what if this fails in execution (not in queuing)?
         for key in &document.invalidation_keys {
-            // TODO: set expire time for sorted set?
+            // TODO: set expire time for sorted set? might not be needed thanks to periodic mx
             let _: () = pipeline
                 .zadd(
                     key,
@@ -166,19 +150,6 @@ impl Storage {
                 )
                 .await?;
         }
-
-        let cache_tags_key = self.make_key(Self::cache_tags_key(&document.cache_key));
-        // the cache_tags_key is effectively a set of values, but atomic operations (ie GETDEL) are only
-        // possible on a string; plus, we don't need the set primitives for this key.
-        let _: () = pipeline
-            .set(
-                cache_tags_key,
-                &serde_json::to_string(&document.invalidation_keys).unwrap(),
-                Some(Expiration::EXAT(expire_at_doubled as i64)), // expire much later than the key itself to allow time for cleanup
-                None,
-                false,
-            )
-            .await?;
 
         Ok(())
     }
@@ -207,84 +178,6 @@ impl Storage {
 
         let count = self.storage.delete_from_scan_result(all_keys).await;
         Ok(count.unwrap_or(0) as u64)
-    }
-
-    pub(crate) async fn enable_keyevent_notifications(&self) {
-        // TODO
-        let client = self.keyspace_storage.client();
-        for server in self.keyspace_storage.servers() {
-            client
-                .with_cluster_node(server.clone())
-                .config_set("notify-keyspace-events", "Exeg")
-                .await
-                .unwrap_or_else(|_| panic!("Failed to configure server {server:?}"));
-        }
-    }
-
-    pub(crate) async fn subscribe_to_keyspace_events(&self) {
-        let prefixes = vec![
-            String::from("__key*__:del"),
-            String::from("__key*__:expired"),
-            String::from("__key*__:evicted"),
-        ];
-        // TODO: configure keyspace storage with pool size of 1 ?
-        // https://github.com/aembke/fred.rs/blob/main/examples/keyspace.rs#L84
-        let subscription_client = self.keyspace_storage.client();
-        let reconnect_subscriber = subscription_client.clone();
-        let prefixes_clone = prefixes.clone();
-        // resubscribe to PREFIXES whenever we reconnect to a server
-        let _reconnect_task = tokio::spawn(async move {
-            let mut reconnect_rx = reconnect_subscriber.reconnect_rx();
-
-            // in 7.x the reconnection interface added a `Server` struct to reconnect events to make this easier.
-            while let Ok(server) = reconnect_rx.recv().await {
-                tracing::debug!(
-                    "Reconnected to {}. Subscribing to keyspace events...",
-                    server
-                );
-                // TODO: actually handle failures here
-                let _ = reconnect_subscriber
-                    .with_cluster_node(server)
-                    .psubscribe(prefixes_clone.clone())
-                    .await;
-            }
-        });
-
-        let storage = self.storage.clone();
-
-        // set up a task that listens for keyspace events
-        let pck_prefix = self.pck_prefix();
-        let _keyspace_task = tokio::spawn(async move {
-            // have to subscribe!!
-            let _ = subscription_client.psubscribe(prefixes).await;
-
-            let mut keyspace_rx = subscription_client.keyspace_event_rx();
-            loop {
-                match keyspace_rx.recv().await {
-                    Ok(event) => {
-                        if (event.operation == "del"
-                            || event.operation == "expired"
-                            || event.operation == "evicted")
-                            && (event.key.as_str_lossy().starts_with(&pck_prefix))
-                        {
-                            // need a non-subscription client! aka cannot just use this client..
-                            let client = storage.client();
-                            tokio::spawn(async move {
-                                let _ = handle_pck_deletion(
-                                    client,
-                                    event.key.as_str_lossy().to_string(),
-                                )
-                                .await;
-
-                                // TODO: report success / failure rate
-                            });
-                        }
-                    }
-                    Err(RecvError::Closed) => break,
-                    Err(RecvError::Lagged(_)) => continue,
-                }
-            }
-        });
     }
 
     pub(crate) async fn perform_periodic_maintenance(&self) {
@@ -332,30 +225,6 @@ impl Storage {
             }
         });
     }
-}
-
-async fn handle_pck_deletion(client: Client, key: String) -> StorageResult<()> {
-    // TODO: how to handle cache-key-* being on a different shard than the pck? cannot have cross-shard transactions
-    // script will atomically watch PCK and only if it does not exist will it call GETDEL
-    const SCRIPT: &str = include_str!("clean_up_pck.lua");
-
-    let cache_tags_key = key.clone().replacen("pck:", "cache-tags:", 1);
-
-    let result: Value = client
-        .eval(SCRIPT, (key.clone(), cache_tags_key), ())
-        .await?;
-    let keys = result.as_string().ok_or(Error::Placeholder)?;
-
-    // let keys: String = client.eval(SCRIPT, (key, cache_tags_key), ()).await?;
-    let cache_tags: Vec<String> = serde_json::from_str(&keys)?;
-
-    // TODO: this is the part that probably should still be atomic but won't be for now...
-    let pipeline = client.pipeline();
-    for cache_tag in cache_tags {
-        let _: () = pipeline.zrem(cache_tag, key.clone()).await?;
-    }
-    let _: Vec<Value> = pipeline.all().await?;
-    Ok(())
 }
 
 impl CacheStorage for Storage {
