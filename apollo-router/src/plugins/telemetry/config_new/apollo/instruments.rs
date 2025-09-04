dcc9f9dfbf670ae11694600ec87f97c7837f44f9
Ross Regitsky <ross.regitsky@apollographql.com>
2025-09-04 14:36:10 -0400

    Add connector.source attr to subgraph insights histogram

--- apollo-router/src/plugins/telemetry/config_new/apollo/instruments.rs
+++ apollo-router/src/plugins/telemetry/config_new/apollo/instruments.rs
@@ -7,7 +7,7 @@ use tower::BoxError;
 
 use crate::Context;
 use crate::metrics;
-use crate::plugins::telemetry::APOLLO_CLIENT_NAME_ATTRIBUTE;
+use crate::plugins::telemetry::{APOLLO_CLIENT_NAME_ATTRIBUTE, APOLLO_CONNECTOR_SOURCE_ATTRIBUTE};
 use crate::plugins::telemetry::APOLLO_CLIENT_VERSION_ATTRIBUTE;
 use crate::plugins::telemetry::APOLLO_HAS_ERRORS_ATTRIBUTE;
 use crate::plugins::telemetry::APOLLO_OPERATION_ID_ATTRIBUTE;
@@ -21,6 +21,8 @@ use crate::plugins::telemetry::config_new::connector::ConnectorRequest;
 use crate::plugins::telemetry::config_new::connector::ConnectorResponse;
 use crate::plugins::telemetry::config_new::connector::attributes::ConnectorAttributes;
 use crate::plugins::telemetry::config_new::connector::selectors::ConnectorSelector;
+use crate::plugins::telemetry::config_new::connector::selectors::ConnectorSelector::ConnectorSource;
+use crate::plugins::telemetry::config_new::connector::selectors::ConnectorSource::Name;
 use crate::plugins::telemetry::config_new::extendable::Extendable;
 use crate::plugins::telemetry::config_new::instruments::APOLLO_ROUTER_OPERATIONS_FETCH_DURATION;
 use crate::plugins::telemetry::config_new::instruments::CustomHistogram;
@@ -231,6 +233,12 @@ impl ApolloConnectorInstruments {
                         connector_on_response_error: true,
                     },
                 ),
+                (
+                    APOLLO_CONNECTOR_SOURCE_ATTRIBUTE.to_string(),
+                    ConnectorSelector::ConnectorSource {
+                        connector_source: Name,
+                    },
+                )
             ]),
         };
         let attribute_count = selectors.custom.len() + 1; // 1 for subgraph_name on attributes
