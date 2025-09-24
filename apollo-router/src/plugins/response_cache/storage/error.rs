fad557b052a627edea6899cfeba35d1853f806e6
carodewig <16093297+carodewig@users.noreply.github.com>
2025-09-24 13:13:02 -0400

    refactor: use thiserror for Error enum

--- apollo-router/src/plugins/response_cache/storage/error.rs
+++ apollo-router/src/plugins/response_cache/storage/error.rs
@@ -1,51 +1,25 @@
-use std::fmt::Display;
-use std::fmt::Formatter;
-
 use serde_json::error::Category;
 
 use crate::plugins::response_cache::ErrorCode;
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub(crate) enum Error {
-    Database(sqlx::Error),
-    Serialize(serde_json::Error),
-    Timeout,
+    #[error("{0}")]
+    Database(#[from] sqlx::Error),
+
+    #[error("{0}")]
+    Serialize(#[from] serde_json::Error),
+
+    #[error("TIMED_OUT")]
+    Timeout(#[from] tokio::time::error::Elapsed),
 }
 
 impl Error {
     pub(crate) fn is_row_not_found(&self) -> bool {
         match self {
             Error::Database(err) => matches!(err, &sqlx::Error::RowNotFound),
-            Error::Serialize(_) | Error::Timeout => false,
+            Error::Serialize(_) | Error::Timeout(_) => false,
         }
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::Database(err) => f.write_str(&err.to_string()),
-            Error::Serialize(err) => f.write_str(&err.to_string()),
-            Error::Timeout => f.write_str("TIMED_OUT"),
-        }
-    }
-}
-
-impl From<sqlx::Error> for Error {
-    fn from(err: sqlx::Error) -> Self {
-        Error::Database(err)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(err: serde_json::Error) -> Self {
-        Error::Serialize(err)
-    }
-}
-
-impl From<tokio::time::error::Elapsed> for Error {
-    fn from(_: tokio::time::error::Elapsed) -> Self {
-        Error::Timeout
     }
 }
 
@@ -59,9 +33,7 @@ impl ErrorCode for Error {
                 Category::Data => "Serialize::Data",
                 Category::Eof => "Serialize::EOF",
             },
-            Error::Timeout => "TIMED_OUT",
+            Error::Timeout(_) => "TIMED_OUT",
         }
     }
 }
-
-impl std::error::Error for Error {}
