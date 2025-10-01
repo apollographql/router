c77255ac97538e52267a8504d5ef6ec817018a5c
bryn <bryn@apollographql.com>
2025-10-01 16:20:38 +0100

    (refactor) Rename fields in activation

--- apollo-router/src/plugins/telemetry/reload/activation.rs
+++ apollo-router/src/plugins/telemetry/reload/activation.rs
@@ -31,23 +31,24 @@ use crate::plugins::telemetry::reload::otel::reload_fmt;
 /// are also shut down in a safe way.
 pub(crate) struct Activation {
     /// The new tracer provider. None means leave the existing one
-    trace_provider: Option<opentelemetry_sdk::trace::TracerProvider>,
+    new_trace_provider: Option<opentelemetry_sdk::trace::TracerProvider>,
 
     /// The new tracer propagator. None means leave the existing one
-    trace_propagator: Option<TextMapCompositePropagator>,
+    new_trace_propagator: Option<TextMapCompositePropagator>,
 
     /// The new metrics providers. Absent entry for a particular meter provider type
     /// means leave the existing one as is
-    meter_providers: HashMap<MeterProviderType, FilterMeterProvider>,
+    new_meter_providers: HashMap<MeterProviderType, FilterMeterProvider>,
 
     /// The registry that backs prometheus
-    /// If this is None then Prometheus is not active
+    /// Unlike the other fields in this struct there is no noop implementation
+    /// Therefore if this is None then Prometheus is not active
     /// This will be defaulted to the last applied registry via static unfortunately
     /// We can remove this static if eventually we have a facility for plugins to maintain state across reloads.
     prometheus_registry: Option<Registry>,
 
     /// The new format layer
-    logging_fmt_layer: Option<Box<dyn Layer<LayeredTracer> + Send + Sync>>,
+    new_logging_fmt_layer: Option<Box<dyn Layer<LayeredTracer> + Send + Sync>>,
 
     /// Test instrumentation to track what components were set
     #[cfg(test)]
@@ -71,12 +72,12 @@ static REGISTRY: LazyLock<Mutex<Option<Registry>>> = LazyLock::new(Default::defa
 impl Activation {
     pub(crate) fn new() -> Activation {
         Activation {
-            trace_provider: Default::default(),
-            trace_propagator: Default::default(),
-            meter_providers: Default::default(),
+            new_trace_provider: Default::default(),
+            new_trace_propagator: Default::default(),
+            new_meter_providers: Default::default(),
             // We can remove this is we allow state to be maintained across plugin reloads
             prometheus_registry: REGISTRY.lock().clone(),
-            logging_fmt_layer: Default::default(),
+            new_logging_fmt_layer: Default::default(),
             #[cfg(test)]
             test_instrumentation: Default::default(),
         }
@@ -86,7 +87,7 @@ impl Activation {
         &mut self,
         logging_layer: Box<dyn Layer<LayeredTracer> + Send + Sync>,
     ) {
-        self.logging_fmt_layer = Some(logging_layer);
+        self.new_logging_fmt_layer = Some(logging_layer);
         #[cfg(test)]
         {
             self.test_instrumentation.logging_layer_set = true;
@@ -94,7 +95,7 @@ impl Activation {
     }
 
     pub(crate) fn with_tracer_propagator(&mut self, tracer_propagator: TextMapCompositePropagator) {
-        self.trace_propagator = Some(tracer_propagator);
+        self.new_trace_propagator = Some(tracer_propagator);
         #[cfg(test)]
         {
             self.test_instrumentation.tracer_propagator_set = true;
@@ -106,7 +107,7 @@ impl Activation {
         meter_providers: impl IntoIterator<Item = (MeterProviderType, FilterMeterProvider)>,
     ) {
         for (meter_provider_type, meter_provider) in meter_providers {
-            self.meter_providers
+            self.new_meter_providers
                 .insert(meter_provider_type, meter_provider);
             #[cfg(test)]
             {
@@ -121,7 +122,7 @@ impl Activation {
         &mut self,
         tracer_provider: opentelemetry_sdk::trace::TracerProvider,
     ) {
-        self.trace_provider = Some(tracer_provider);
+        self.new_trace_provider = Some(tracer_provider);
         #[cfg(test)]
         {
             self.test_instrumentation.tracer_provider_set = true;
@@ -159,7 +160,7 @@ impl Activation {
         // Only apply things if we were executing in the context of a vanilla the Apollo executable.
         // Users that are rolling their own routers will need to set up telemetry themselves.
         if let Some(hot_tracer) = OPENTELEMETRY_TRACER_HANDLE.get()
-            && let Some(tracer_provider) = self.trace_provider.take()
+            && let Some(tracer_provider) = self.new_trace_provider.take()
         {
             let tracer = tracer_provider
                 .tracer_builder(GLOBAL_TRACER_NAME)
@@ -179,8 +180,8 @@ impl Activation {
         let global_meter_provider = meter_provider_internal();
         // Note that we are essentially swapping the new meter providers with the old.
         // The meter providers will be dealt with in Drop.
-        for (meter_provider_type, meter_provider) in std::mem::take(&mut self.meter_providers) {
-            self.meter_providers.insert(
+        for (meter_provider_type, meter_provider) in std::mem::take(&mut self.new_meter_providers) {
+            self.new_meter_providers.insert(
                 meter_provider_type,
                 global_meter_provider.set(meter_provider_type, meter_provider),
             );
@@ -188,13 +189,13 @@ impl Activation {
     }
 
     fn reload_logging(&mut self) {
-        if let Some(fmt_layer) = self.logging_fmt_layer.take() {
+        if let Some(fmt_layer) = self.new_logging_fmt_layer.take() {
             reload_fmt(fmt_layer);
         }
     }
 
     fn reload_trace_propagation(&mut self) {
-        if let Some(propagator) = self.trace_propagator.take() {
+        if let Some(propagator) = self.new_trace_propagator.take() {
             opentelemetry::global::set_text_map_propagator(propagator);
         }
     }
@@ -205,13 +206,13 @@ impl Activation {
 /// The solution to this is to move the structs into a blocking task so that they can shut down safely.
 impl Drop for Activation {
     fn drop(&mut self) {
-        for (_, meter_provider) in std::mem::take(&mut self.meter_providers) {
+        for (_, meter_provider) in std::mem::take(&mut self.new_meter_providers) {
             block_in_place(move || {
                 drop(meter_provider);
             });
         }
 
-        if let Some(tracer_provider) = self.trace_provider.take() {
+        if let Some(tracer_provider) = self.new_trace_provider.take() {
             block_in_place(move || {
                 drop(tracer_provider);
             });
