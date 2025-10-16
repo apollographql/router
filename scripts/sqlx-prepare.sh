6ad9d29c5e49ae1f29f6c0812870815993e9507c
Jesse Rosenberger <git@jro.cc>
2025-10-16 18:49:40 +0300

    Delete scripts/sqlx-prepare.sh

--- scripts/sqlx-prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# In case of the error below when adding or changing an SQL query,
-# run this script to update their metadata.
-#
-#    error: `SQLX_OFFLINE=true` but there is no cached data for this query
-
-cd $(dirname $0)/../apollo-router
-cargo sqlx prepare -D postgres://localhost -- --all-targets
