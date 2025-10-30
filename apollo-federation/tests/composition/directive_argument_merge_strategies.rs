36961f6a6e742d0dae100c9210bee74302e503b5
Chidimma O <conwuegb@users.noreply.github.com>
2025-10-30 16:55:57 -0400

    Add test for nullable_and strategy

--- apollo-federation/tests/composition/directive_argument_merge_strategies.rs
+++ apollo-federation/tests/composition/directive_argument_merge_strategies.rs
@@ -15,14 +15,12 @@ mod tests {
        public-facing directives and thus are not represented in this set of tests:
        - min
        - intersection
-       - nullable_and
        - nullable_union
     */
 
     #[test]
     fn works_for_max_argument_merge_strategy() {
-        // NOTE: @cost uses the MAX strategy for merging arguments,
-        // So we'll use it as a proxy for the @max directive
+        // NOTE: @cost uses the MAX strategy for merging arguments
 
         let subgraph1 = ServiceDefinition {
             name: "Subgraph1",
@@ -30,7 +28,7 @@ mod tests {
                 type Query {
                     t: T
                 }
-                
+
                 type T
                   @key(fields: "k")
                   @cost(weight: 3)
@@ -43,7 +41,7 @@ mod tests {
         let subgraph2 = ServiceDefinition {
             name: "Subgraph2",
             type_defs: r#"
-                type T 
+                type T
                   @key(fields: "k")
                   @cost(weight: 2)
                 {
@@ -114,8 +112,7 @@ mod tests {
 
     #[test]
     fn works_for_union_argument_merge_strategy() {
-        // NOTE: @requiresScopes uses the UNION strategy for merging arguments,
-        // So we'll use it as a proxy for the @union directive
+        // NOTE: @requiresScopes uses the UNION strategy for merging arguments
 
         let subgraph1 = ServiceDefinition {
             name: "Subgraph1",
@@ -123,10 +120,10 @@ mod tests {
             type Query {
               t: T!
             }
-    
-            type T 
+
+            type T
               @key(fields: "k")
-              @requiresScopes(scopes: ["foo", "bar"]) 
+              @requiresScopes(scopes: ["foo", "bar"])
             {
               k: ID @requiresScopes(scopes: [])
             }
@@ -135,10 +132,10 @@ mod tests {
 
         let subgraph2 = ServiceDefinition {
             name: "Subgraph2",
-            type_defs: r#"    
-            type T 
+            type_defs: r#"
+            type T
               @key(fields: "k")
-              @requiresScopes(scopes: ["foo"]) 
+              @requiresScopes(scopes: ["foo"])
             {
               k: ID @requiresScopes(scopes: ["v1", "v2"])
               a: Int
@@ -217,9 +214,126 @@ mod tests {
     }
 
     #[test]
+    fn works_for_nullable_and_argument_merge_strategy() {
+        // NOTE: @listSize(requireOneSlicingArgument:) is merged using the NULLABLE_AND strategy
+
+        let subgraph1 = ServiceDefinition {
+            name: "Subgraph1",
+            type_defs: r#"
+                type Query {
+                    t: [T!] @shareable @listSize(requireOneSlicingArgument: false)
+                }
+
+                type T @key(fields: "k") {
+                    k: [ID] @listSize(requireOneSlicingArgument: true)
+                    c: [Int]! @shareable @listSize(requireOneSlicingArgument: null)
+                }
+            "#,
+        };
+
+        let subgraph2 = ServiceDefinition {
+            name: "Subgraph2",
+            type_defs: r#"
+                type Query {
+                    t: [T!] @shareable @listSize(requireOneSlicingArgument: true)
+                }
+
+                type T @key(fields: "k") {
+                    k: [ID] @listSize(requireOneSlicingArgument: null)
+                    a: String
+                    b: [Int] @listSize(requireOneSlicingArgument: false)
+                    c: [Int]! @shareable @listSize(requireOneSlicingArgument: null)
+                }
+            "#,
+        };
+
+        let result = compose_as_fed2_subgraphs(&[subgraph1, subgraph2]);
+        let result_sg = result.expect("Composition should succeed");
+
+        // Check expected hints
+        let expected_hints = vec![
+            CompositionHint {
+                code: String::from("MERGED_NON_REPEATABLE_DIRECTIVE_ARGUMENTS"),
+                message: String::from(
+                    r#"Directive @listSize is applied to "Query.t" in multiple subgraphs with different arguments. Merging strategies used by arguments: { assumedSize: NULLABLE_MAX, slicingArguments: NULLABLE_UNION, sizedFields: NULLABLE_UNION, requireOneSlicingArgument: NULLABLE_AND }"#,
+                ),
+                locations: Vec::new(),
+            },
+            CompositionHint {
+                code: String::from("MERGED_NON_REPEATABLE_DIRECTIVE_ARGUMENTS"),
+                message: String::from(
+                    r#"Directive @listSize is applied to "T.k" in multiple subgraphs with different arguments. Merging strategies used by arguments: { assumedSize: NULLABLE_MAX, slicingArguments: NULLABLE_UNION, sizedFields: NULLABLE_UNION, requireOneSlicingArgument: NULLABLE_AND }"#,
+                ),
+                locations: Vec::new(),
+            },
+        ];
+        assert_hints_equal(result_sg.hints(), &expected_hints);
+
+        // Check merging using NULLABLE_AND succeeded
+        let schema = result_sg.schema().schema();
+        let sdl = print_sdl(schema);
+        assert!(
+            sdl.contains(
+                r#"@link(url: "https://specs.apollo.dev/cost/v0.1", import: ["listSize"])"#
+            )
+        );
+
+        let t = coord!(Query.t)
+            .lookup_field(schema)
+            .expect("Query.t should be defined on the supergraph");
+        let t_requires_scopes_directive = t
+            .directives
+            .iter()
+            .find(|d| d.name == "listSize")
+            .expect("@listSize directive should be present on T");
+        assert_eq!(
+            t_requires_scopes_directive.to_string(),
+            r#"@listSize(requireOneSlicingArgument: false)"#
+        );
+
+        let k = coord!(T.k)
+            .lookup_field(schema)
+            .expect("T.k should be defined on the supergraph");
+        let k_requires_scopes_directive = k
+            .directives
+            .iter()
+            .find(|d| d.name == "listSize")
+            .expect("@listSize directive should be present on T.k");
+        assert_eq!(
+            k_requires_scopes_directive.to_string(),
+            r#"@listSize(requireOneSlicingArgument: true)"#
+        );
+
+        let b = coord!(T.b)
+            .lookup_field(schema)
+            .expect("T.b should be defined on the supergraph");
+        let b_requires_scopes_directive = b
+            .directives
+            .iter()
+            .find(|d| d.name == "listSize")
+            .expect("@listSize directive should be present on T.b");
+        assert_eq!(
+            b_requires_scopes_directive.to_string(),
+            r#"@listSize(requireOneSlicingArgument: false)"#
+        );
+
+        let c = coord!(T.c)
+            .lookup_field(schema)
+            .expect("T.b should be defined on the supergraph");
+        let b_requires_scopes_directive = c
+            .directives
+            .iter()
+            .find(|d| d.name == "listSize")
+            .expect("@listSize directive should be present on T.c");
+        assert_eq!(
+            b_requires_scopes_directive.to_string(),
+            r#"@listSize(requireOneSlicingArgument: null)"#
+        );
+    }
+
+    #[test]
     fn works_for_nullable_max_argument_merge_strategy() {
-        // NOTE: @listSize(assumedSize:) uses the NULLABLE_MAX strategy for merging arguments,
-        // So we'll use it as a proxy for the @nullable_max directive
+        // NOTE: @listSize(assumedSize:) is merged using the NULLABLE_MAX strategy
 
         let subgraph1 = ServiceDefinition {
             name: "Subgraph1",
@@ -271,7 +385,7 @@ mod tests {
         ];
         assert_hints_equal(result_sg.hints(), &expected_hints);
 
-        // Check merging using NULLABLE_AND succeeded
+        // Check merging using NULLABLE_MAX succeeded
         let schema = result_sg.schema().schema();
         let sdl = print_sdl(schema);
         assert!(
