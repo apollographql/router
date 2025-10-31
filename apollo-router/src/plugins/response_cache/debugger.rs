23cf2d24340dfe0b1a8f473bd4dbbfc5805423c3
Benjamin <5719034+bnjjj@users.noreply.github.com>
2025-10-31 17:31:51 +0100

    address review comments
    Signed-off-by: Benjamin <5719034+bnjjj@users.noreply.github.com>

--- apollo-router/src/plugins/response_cache/debugger.rs
+++ apollo-router/src/plugins/response_cache/debugger.rs
@@ -7,7 +7,7 @@ use crate::plugins::response_cache::cache_control::CacheControl;
 
 pub(super) type CacheKeysContext = Vec<CacheKeyContext>;
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub(super) struct CacheKeyContext {
     pub(super) key: String,
@@ -55,32 +55,13 @@ pub(crate) enum CacheEntryKind {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-#[cfg_attr(test, derive(PartialEq, Eq, Hash))]
+#[cfg_attr(test, derive(PartialEq, Eq, Hash, PartialOrd, Ord))]
 #[serde(rename_all = "camelCase")]
 pub(crate) enum CacheKeySource {
     /// Data fetched from subgraph
     Subgraph,
     /// Data fetched from cache
     Cache,
-}
-
-#[cfg(test)]
-impl PartialOrd for CacheKeySource {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-#[cfg(test)]
-impl Ord for CacheKeySource {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        match (self, other) {
-            (CacheKeySource::Subgraph, CacheKeySource::Subgraph) => std::cmp::Ordering::Equal,
-            (CacheKeySource::Subgraph, CacheKeySource::Cache) => std::cmp::Ordering::Greater,
-            (CacheKeySource::Cache, CacheKeySource::Subgraph) => std::cmp::Ordering::Less,
-            (CacheKeySource::Cache, CacheKeySource::Cache) => std::cmp::Ordering::Equal,
-        }
-    }
 }
 
 impl CacheKeyContext {
