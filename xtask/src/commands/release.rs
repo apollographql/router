e8431fd92a2c81c09f0c266c6158a598c8052a3e
Jesse Rosenberger <git@jro.cc>
2025-11-17 19:35:06 +0200

    chore(xtask): update reqwest and dependencies (#8622)

--- xtask/src/commands/release.rs
+++ xtask/src/commands/release.rs
@@ -9,6 +9,8 @@ use walkdir::WalkDir;
 use xtask::*;
 
 use crate::commands::changeset::slurp_and_remove_changesets;
+use crate::commands::Compliance;
+use crate::commands::Licenses;
 mod process;
 
 #[derive(Debug, clap::Subcommand)]
@@ -401,10 +403,10 @@ impl Prepare {
     /// Run `cargo xtask check-compliance`.
     fn check_compliance(&self) -> Result<()> {
         println!("checking compliance");
-        cargo!(["xtask", "check-compliance"]);
+        Compliance {}.run()?;
         if !self.skip_license_check {
             println!("updating licenses.html");
-            cargo!(["xtask", "licenses"]);
+            Licenses {}.run()?;
         }
         Ok(())
     }
