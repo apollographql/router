f2aaf3b58dfe05d2e0d44ddb4ab42c9c3ac0ecf1
Caroline Rodewig <16093297+carodewig@users.noreply.github.com>
2025-11-21 08:40:09 -0500

    refactor: remove a few uses of serde_json_bytes in response caching (#8394)

--- apollo-router/src/plugins/response_cache/plugin.rs
+++ apollo-router/src/plugins/response_cache/plugin.rs
@@ -86,7 +86,7 @@ use crate::spec::QueryHash;
 use crate::spec::TYPENAME;
 
 /// Change this key if you introduce a breaking change in response caching algorithm to make sure it won't take the previous entries
-pub(crate) const RESPONSE_CACHE_VERSION: &str = "1.0";
+pub(crate) const RESPONSE_CACHE_VERSION: &str = "1.1";
 pub(crate) const CACHE_TAG_DIRECTIVE_NAME: &str = "federation__cacheTag";
 pub(crate) const ENTITIES: &str = "_entities";
 pub(crate) const REPRESENTATIONS: &str = "representations";
@@ -2596,7 +2596,7 @@ mod tests {
         let storage = Storage::new(&Config::test(false, "test_subgraph_enabled"), drop_rx)
             .await
             .unwrap();
-        let map = serde_json::json!({
+        let map = serde_json_bytes::json!({
             "user": {
                 "private_id": "sub"
             },
@@ -2612,7 +2612,7 @@ mod tests {
 
         let mut response_cache = ResponseCache::for_test(
             storage.clone(),
-            serde_json::from_value(map).unwrap(),
+            serde_json_bytes::from_value(map).unwrap(),
             valid_schema.clone(),
             true,
             drop_tx,
@@ -2622,13 +2622,13 @@ mod tests {
 
         assert!(response_cache.subgraph_enabled("user"));
         assert!(!response_cache.subgraph_enabled("archive"));
-        let subgraph_config = serde_json::json!({
+        let subgraph_config = serde_json_bytes::json!({
             "all": {
                 "enabled": false
             },
             "subgraphs": response_cache.subgraphs.subgraphs.clone()
         });
-        response_cache.subgraphs = Arc::new(serde_json::from_value(subgraph_config).unwrap());
+        response_cache.subgraphs = Arc::new(serde_json_bytes::from_value(subgraph_config).unwrap());
         assert!(!response_cache.subgraph_enabled("archive"));
         assert!(response_cache.subgraph_enabled("user"));
         assert!(response_cache.subgraph_enabled("orga"));
@@ -2641,7 +2641,7 @@ mod tests {
         let storage = Storage::new(&Config::test(false, "test_subgraph_ttl"), drop_rx)
             .await
             .unwrap();
-        let map = serde_json::json!({
+        let map = serde_json_bytes::json!({
             "user": {
                 "private_id": "sub",
                 "ttl": "2s"
@@ -2659,7 +2659,7 @@ mod tests {
 
         let mut response_cache = ResponseCache::for_test(
             storage.clone(),
-            serde_json::from_value(map).unwrap(),
+            serde_json_bytes::from_value(map).unwrap(),
             valid_schema.clone(),
             true,
             drop_tx,
