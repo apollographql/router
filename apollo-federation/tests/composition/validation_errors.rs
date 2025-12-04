044bf89f224865a7dbfccc8f34215d8543624b31
Taylor Ninesling <taylor.ninesling@apollographql.com>
2025-12-04 12:52:01 -0600

    Enable validation error tests

--- apollo-federation/tests/composition/validation_errors.rs
+++ apollo-federation/tests/composition/validation_errors.rs
@@ -1,19 +1,9 @@
-use apollo_federation::error::CompositionError;
-use apollo_federation::supergraph::Supergraph;
-
 use super::ServiceDefinition;
+use super::assert_composition_errors;
 use super::compose_as_fed2_subgraphs;
-
-fn error_messages<S>(result: &Result<Supergraph<S>, Vec<CompositionError>>) -> Vec<String> {
-    match result {
-        Ok(_) => panic!("Expected an error, but got a successful composition"),
-        Err(err) => err.iter().map(|e| e.to_string()).collect(),
-    }
-}
 mod requires_tests {
     use super::*;
 
-    #[ignore = "until merge implementation completed"]
     #[test]
     fn fails_if_it_cannot_satisfy_a_requires() {
         let subgraph_a = ServiceDefinition {
@@ -43,37 +33,37 @@ mod requires_tests {
         };
 
         let result = compose_as_fed2_subgraphs(&[subgraph_a, subgraph_b]);
-        let messages = error_messages(&result);
-        assert_eq!(
-            messages,
-            [
-                r#"
-                The following supergraph API query:
-                {
-                    a {
-                    y
-                    }
-                }
-                cannot be satisfied by the subgraphs because:
-                - from subgraph "A": cannot find field "A.y".
-                - from subgraph "B": cannot satisfy @require conditions on field "A.y" (please ensure that this is not due to key field "id" being accidentally marked @external).
-            "#,
-                r#"
-                The following supergraph API query:
-                {
-                    a {
-                    z
-                    }
-                }
-                cannot be satisfied by the subgraphs because:
-                - from subgraph "A": cannot find field "A.z".
-                - from subgraph "B": cannot satisfy @require conditions on field "A.z" (please ensure that this is not due to key field "id" being accidentally marked @external).
-            "#,
-            ]
+        assert_composition_errors(
+            &result,
+            &[
+                (
+                    "SATISFIABILITY_ERROR",
+                    r#"The following supergraph API query:
+{
+  a {
+    y
+  }
+}
+cannot be satisfied by the subgraphs because:
+- from subgraph "A": cannot find field "A.y".
+- from subgraph "B": cannot satisfy @requires conditions on field "A.y" (please ensure that this is not due to key field "id" being accidentally marked @external)."#,
+                ),
+                (
+                    "SATISFIABILITY_ERROR",
+                    r#"The following supergraph API query:
+{
+  a {
+    z
+  }
+}
+cannot be satisfied by the subgraphs because:
+- from subgraph "A": cannot find field "A.z".
+- from subgraph "B": cannot satisfy @requires conditions on field "A.z" (please ensure that this is not due to key field "id" being accidentally marked @external)."#,
+                ),
+            ],
         );
     }
 
-    #[ignore = "until merge implementation completed"]
     #[test]
     fn fails_if_no_usable_post_requires_keys() {
         let subgraph_a = ServiceDefinition {
@@ -106,22 +96,22 @@ mod requires_tests {
         };
 
         let result = compose_as_fed2_subgraphs(&[subgraph_a, subgraph_b]);
-        let messages = error_messages(&result);
-        assert_eq!(
-            messages,
-            [r#"
-                The following supergraph API query:
-                {
-                    getT1s {
-                        f2 {
-                            ...
-                        }
-                    }
-                }
-                cannot be satisfied by the subgraphs because:
-                - from subgraph "B": @require condition on field "T1.f2" can be satisfied but missing usable key on "T1" in subgraph "B" to resume query.
-                - from subgraph "A": cannot find field "T1.f2".
-            "#]
+        assert_composition_errors(
+            &result,
+            &[(
+                "SATISFIABILITY_ERROR",
+                r#"The following supergraph API query:
+{
+  getT1s {
+    f2 {
+      ...
+    }
+  }
+}
+cannot be satisfied by the subgraphs because:
+- from subgraph "B": @require condition on field "T1.f2" can be satisfied but missing usable key on "T1" in subgraph "B" to resume query.
+- from subgraph "A": cannot find field "T1.f2"."#,
+            )],
         );
     }
 }
@@ -129,7 +119,6 @@ mod requires_tests {
 mod non_resolvable_keys_tests {
     use super::*;
 
-    #[ignore = "until merge implementation completed"]
     #[test]
     fn fails_if_key_is_declared_non_resolvable_but_would_be_needed() {
         let subgraph_a = ServiceDefinition {
@@ -156,21 +145,21 @@ mod non_resolvable_keys_tests {
         };
 
         let result = compose_as_fed2_subgraphs(&[subgraph_a, subgraph_b]);
-        let messages = error_messages(&result);
-        assert_eq!(
-            messages,
-            [r#"
-                The following supergraph API query:
-                {
-                    getTs {
-                        f
-                    }
-                }
-                cannot be satisfied by the subgraphs because:
-                - from subgraph "B":
-                    - cannot find field "T.f".
-                    - cannot move to subgraph "A", which has field "T.f", because none of the @key defined on type "T" in subgraph "A" are resolvable (they are all declared with their "resolvable" argument set to false).
-            "#]
+        assert_composition_errors(
+            &result,
+            &[(
+                "SATISFIABILITY_ERROR",
+                r#"The following supergraph API query:
+{
+  getTs {
+    f
+  }
+}
+cannot be satisfied by the subgraphs because:
+- from subgraph "B":
+  - cannot find field "T.f".
+  - cannot move to subgraph "A", which has field "T.f", because none of the @key defined on type "T" in subgraph "A" are resolvable (they are all declared with their "resolvable" argument set to false)."#,
+            )],
         );
     }
 }
@@ -178,7 +167,6 @@ mod non_resolvable_keys_tests {
 mod interface_object_tests {
     use super::*;
 
-    #[ignore = "until merge implementation completed"]
     #[test]
     fn fails_on_interface_object_usage_with_missing_key_on_interface() {
         let subgraph_a = ServiceDefinition {
@@ -216,39 +204,39 @@ mod interface_object_tests {
         };
 
         let result = compose_as_fed2_subgraphs(&[subgraph_a, subgraph_b]);
-        let messages = error_messages(&result);
-        assert_eq!(
-            messages,
-            [
-                r#"
-                The following supergraph API query:
-                {
-                    iFromB {
-                        ... on A {
-                            ...
-                        }
-                    }
-                }
-                cannot be satisfied by the subgraphs because:
-                - from subgraph "subgraphB": no subgraph can be reached to resolve the implementation type of @interfaceObject type "I".
-            "#,
-                r#"
-                The following supergraph API query:
-                {
-                    iFromB {
-                        ... on B {
-                            ...
-                        }
-                    }
-                }
-                cannot be satisfied by the subgraphs because:
-                - from subgraph "subgraphB": no subgraph can be reached to resolve the implementation type of @interfaceObject type "I".
-            "#
-            ]
+        assert_composition_errors(
+            &result,
+            &[
+                (
+                    "SATISFIABILITY_ERROR",
+                    r#"The following supergraph API query:
+{
+  iFromB {
+    ... on A {
+      ...
+    }
+  }
+}
+cannot be satisfied by the subgraphs because:
+- from subgraph "subgraphB": no subgraph can be reached to resolve the implementation type of @interfaceObject type "I"."#,
+                ),
+                (
+                    "SATISFIABILITY_ERROR",
+                    r#"The following supergraph API query:
+{
+  iFromB {
+    ... on B {
+      ...
+    }
+  }
+}
+cannot be satisfied by the subgraphs because:
+- from subgraph "subgraphB": no subgraph can be reached to resolve the implementation type of @interfaceObject type "I"."#,
+                ),
+            ],
         );
     }
 
-    #[ignore = "until merge implementation completed"]
     #[test]
     fn fails_on_interface_object_with_some_unreachable_implementation() {
         let subgraph_a = ServiceDefinition {
@@ -295,26 +283,26 @@ mod interface_object_tests {
         };
 
         let result = compose_as_fed2_subgraphs(&[subgraph_a, subgraph_b, subgraph_c]);
-        let messages = error_messages(&result);
-        assert_eq!(
-            messages,
-            [r#"
-                The following supergraph API query:
-                {
-                    iFromB {
-                        ... on A {
-                            z
-                        }
-                    }
-                }
-                cannot be satisfied by the subgraphs because:
-                - from subgraph "subgraphB":
-                    - cannot find implementation type "A" (supergraph interface "I" is declared with @interfaceObject in "subgraphB").
-                    - cannot move to subgraph "subgraphC", which has field "A.z", because interface "I" is not defined in this subgraph (to jump to "subgraphC", it would need to both define interface "I" and have a @key on it).
-                - from subgraph "subgraphA":
-                    - cannot find field "A.z".
-                    - cannot move to subgraph "subgraphC", which has field "A.z", because type "A" has no @key defined in subgraph "subgraphC".
-            "#]
+        assert_composition_errors(
+            &result,
+            &[(
+                "SATISFIABILITY_ERROR",
+                r#"The following supergraph API query:
+{
+  iFromB {
+    ... on A {
+      z
+    }
+  }
+}
+cannot be satisfied by the subgraphs because:
+- from subgraph "subgraphB":
+  - cannot find implementation type "A" (supergraph interface "I" is declared with @interfaceObject in "subgraphB").
+  - cannot move to subgraph "subgraphC", which has field "A.z", because interface "I" is not defined in this subgraph (to jump to "subgraphC", it would need to both define interface "I" and have a @key on it).
+- from subgraph "subgraphA":
+  - cannot find field "A.z".
+  - cannot move to subgraph "subgraphC", which has field "A.z", because type "A" has no @key defined in subgraph "subgraphC"."#,
+            )],
         );
     }
 }
@@ -323,7 +311,6 @@ mod interface_object_tests {
 mod shared_field_runtime_types_tests {
     use super::*;
 
-    #[ignore = "until merge implementation completed"]
     #[test]
     fn errors_for_interfaces() {
         let subgraph_a = ServiceDefinition {
@@ -363,25 +350,24 @@ mod shared_field_runtime_types_tests {
         };
 
         let result = compose_as_fed2_subgraphs(&[subgraph_a, subgraph_b]);
-        let messages = error_messages(&result);
-        assert_eq!(
-            messages,
-            [r#"
-                For the following supergraph API query:
-                {
-                    a {
-                        ...
-                    }
-                }
-                Shared field "Query.a" return type "A" has a non-intersecting set of possible runtime types across subgraphs. Runtime types in subgraphs are:
-                - in subgraph "A", type "I1";
-                - in subgraph "B", type "I2".
-                This is not allowed as shared fields must resolve the same way in all subgraphs, and that imply at least some common runtime types between the subgraphs.
-            "#]
+        assert_composition_errors(
+            &result,
+            &[(
+                "SATISFIABILITY_ERROR",
+                r#"For the following supergraph API query:
+{
+  a {
+    ...
+  }
+}
+Shared field "Query.a" return type "A" has a non-intersecting set of possible runtime types across subgraphs. Runtime types in subgraphs are:
+ - in subgraph "A", type "I1";
+ - in subgraph "B", type "I2".
+This is not allowed as shared fields must resolve the same way in all subgraphs, and that imply at least some common runtime types between the subgraphs."#,
+            )],
         );
     }
 
-    #[ignore = "until merge implementation completed"]
     #[test]
     fn errors_for_unions() {
         let subgraph_a = ServiceDefinition {
@@ -429,23 +415,23 @@ mod shared_field_runtime_types_tests {
         };
 
         let result = compose_as_fed2_subgraphs(&[subgraph_a, subgraph_b]);
-        let messages = error_messages(&result);
-        assert_eq!(
-            messages,
-            [r#"
-                For the following supergraph API query:
-                {
-                    e {
-                        s {
-                            ...
-                        }
-                    }
-                }
-                Shared field "E.s" return type "U!" has a non-intersecting set of possible runtime types across subgraphs. Runtime types in subgraphs are:
-                - in subgraph "A", types "A" and "B";
-                - in subgraph "B", types "C" and "D".
-                This is not allowed as shared fields must resolve the same way in all subgraphs, and that imply at least some common runtime types between the subgraphs.
-            "#]
+        assert_composition_errors(
+            &result,
+            &[(
+                "SATISFIABILITY_ERROR",
+                r#"For the following supergraph API query:
+{
+  e {
+    s {
+      ...
+    }
+  }
+}
+Shared field "E.s" return type "U!" has a non-intersecting set of possible runtime types across subgraphs. Runtime types in subgraphs are:
+ - in subgraph "A", types "A" and "B";
+ - in subgraph "B", types "C" and "D".
+This is not allowed as shared fields must resolve the same way in all subgraphs, and that imply at least some common runtime types between the subgraphs."#,
+            )],
         );
     }
 }
@@ -486,32 +472,35 @@ mod shareable_mutation_fields_tests {
         };
 
         let result = compose_as_fed2_subgraphs(&[subgraph_a, subgraph_b]);
-        let messages = error_messages(&result);
-        insta::assert_snapshot!(messages.join("\n"), @r###"
-        Supergraph API queries using the mutation field "Mutation.f" at top-level must be satisfiable without needing to call that field from multiple subgraphs, but every subgraph with that field encounters satisfiability errors. Please fix these satisfiability errors for (at least) one of the following subgraphs with the mutation field:
-        - When calling "Mutation.f" at top-level from subgraph "A":
-          The following supergraph API query:
-          mutation {
-            f {
-              y
-            }
-          }
-          cannot be satisfied by the subgraphs because:
-          - from subgraph "A":
-            - cannot find field "F.y".
-            - cannot move to subgraph "B", which has field "F.y", because type "F" has no @key defined in subgraph "B".
-        - When calling "Mutation.f" at top-level from subgraph "B":
-          The following supergraph API query:
-          mutation {
-            f {
-              x
-            }
-          }
-          cannot be satisfied by the subgraphs because:
-          - from subgraph "B":
-            - cannot find field "F.x".
-            - cannot move to subgraph "A", which has field "F.x", because type "F" has no @key defined in subgraph "A".
-        "###);
+        assert_composition_errors(
+            &result,
+            &[(
+                "SATISFIABILITY_ERROR",
+                r#"Supergraph API queries using the mutation field "Mutation.f" at top-level must be satisfiable without needing to call that field from multiple subgraphs, but every subgraph with that field encounters satisfiability errors. Please fix these satisfiability errors for (at least) one of the following subgraphs with the mutation field:
+- When calling "Mutation.f" at top-level from subgraph "A":
+  The following supergraph API query:
+  mutation {
+    f {
+      y
+    }
+  }
+  cannot be satisfied by the subgraphs because:
+  - from subgraph "A":
+    - cannot find field "F.y".
+    - cannot move to subgraph "B", which has field "F.y", because type "F" has no @key defined in subgraph "B".
+- When calling "Mutation.f" at top-level from subgraph "B":
+  The following supergraph API query:
+  mutation {
+    f {
+      x
+    }
+  }
+  cannot be satisfied by the subgraphs because:
+  - from subgraph "B":
+    - cannot find field "F.x".
+    - cannot move to subgraph "A", which has field "F.x", because type "F" has no @key defined in subgraph "A"."#,
+            )],
+        );
     }
 
     #[test]
@@ -545,19 +534,22 @@ mod shareable_mutation_fields_tests {
         };
 
         let result = compose_as_fed2_subgraphs(&[subgraph_a, subgraph_b]);
-        let messages = error_messages(&result);
-        insta::assert_snapshot!(messages.join("\n"), @r###"
-        The following supergraph API query:
-        mutation {
-          f {
-            y
-          }
-        }
-        cannot be satisfied by the subgraphs because:
-        - from subgraph "A":
-          - cannot find field "F.y".
-          - cannot move to subgraph "B", which has field "F.y", because none of the @key defined on type "F" in subgraph "B" are resolvable (they are all declared with their "resolvable" argument set to false).
-        "###);
+        assert_composition_errors(
+            &result,
+            &[(
+                "SATISFIABILITY_ERROR",
+                r#"The following supergraph API query:
+mutation {
+  f {
+    y
+  }
+}
+cannot be satisfied by the subgraphs because:
+- from subgraph "A":
+  - cannot find field "F.y".
+  - cannot move to subgraph "B", which has field "F.y", because none of the @key defined on type "F" in subgraph "B" are resolvable (they are all declared with their "resolvable" argument set to false)."#,
+            )],
+        );
     }
 
     #[test]
@@ -606,8 +598,8 @@ mod shareable_mutation_fields_tests {
 mod other_validation_errors_tests {
     use super::*;
 
-    #[ignore = "until merge implementation completed"]
     #[test]
+    #[ignore = "requires maxValidationSubgraphPaths configuration support"]
     fn errors_when_max_validation_subgraph_paths_is_exceeded() {
         let subgraph_a = ServiceDefinition {
             name: "A",
@@ -676,12 +668,12 @@ mod other_validation_errors_tests {
         };
 
         let result = compose_as_fed2_subgraphs(&[subgraph_a, subgraph_b]);
-        let messages = error_messages(&result);
-        assert_eq!(
-            messages,
-            [r#"
-                Maximum number of validation subgraph paths exceeded: 12
-            "#]
+        assert_composition_errors(
+            &result,
+            &[(
+                "SATISFIABILITY_ERROR",
+                "Maximum number of validation subgraph paths exceeded: 12",
+            )],
         );
     }
 }
