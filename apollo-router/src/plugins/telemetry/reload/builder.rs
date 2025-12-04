31f246b0b6017872a46fd929a83653e162955a58
rohan-b99 <43239788+rohan-b99@users.noreply.github.com>
2025-12-04 09:57:59 +0000

    Update assertion as meter provider is now added by default

--- apollo-router/src/plugins/telemetry/reload/builder.rs
+++ apollo-router/src/plugins/telemetry/reload/builder.rs
@@ -244,6 +244,8 @@ impl<'a> Builder<'a> {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
     use super::*;
     use crate::plugins::telemetry::apollo;
     use crate::plugins::telemetry::config::Exporters;
@@ -423,10 +425,10 @@ mod tests {
             instr.logging_layer_set,
             "First run should set logging layer"
         );
-        // But no meter providers get added if nothing is configured
-        assert!(
-            instr.meter_providers_added.is_empty(),
-            "No meter providers added on first run when nothing enabled"
+        // One meter provider is added for memory tracking
+        assert_eq!(
+            instr.meter_providers_added,
+            HashSet::from([MeterProviderType::Public])
         );
     }
 
