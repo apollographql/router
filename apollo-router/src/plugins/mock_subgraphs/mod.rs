0b250990a4c1d6060aa86d6c75bfcc1d4d6df033
carodewig <16093297+carodewig@users.noreply.github.com>
2026-01-28 12:22:31 -0500

    test: remove dbg! that can cause test to timeout

--- apollo-router/src/plugins/mock_subgraphs/mod.rs
+++ apollo-router/src/plugins/mock_subgraphs/mod.rs
@@ -131,8 +131,6 @@ impl PluginPrivate for MockSubgraphsPlugin {
                     })
                     .unwrap();
 
-                dbg!(request.subgraph_request.body());
-
                 let mut response = http::Response::builder();
                 let body = if let Some(config) = &config {
                     *response.headers_mut().unwrap() = config.headers.0.clone();
