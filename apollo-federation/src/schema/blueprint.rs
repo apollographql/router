fa2c2105388ff058ea0f43415af653682948f917
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2026-01-30 17:24:53 -0600

    lint

--- apollo-federation/src/schema/blueprint.rs
+++ apollo-federation/src/schema/blueprint.rs
@@ -307,7 +307,11 @@ impl FederationBlueprint {
                 // definition to re-add the "correct" version, we'd have to re-attach existing applications (doable but not
                 // done). This assert is so we notice it quickly if that ever happens (again, unlikely, because fed1 schema
                 // is a backward compatibility thing and there is no reason to expand that too much in the future).
-                if !schema.referencers().get_directive(directive_name).is_empty() {
+                if !schema
+                    .referencers()
+                    .get_directive(directive_name)
+                    .is_empty()
+                {
                     bail!(
                         "Subgraph has applications of @{directive_name} but we are trying to remove the definition."
                     );
