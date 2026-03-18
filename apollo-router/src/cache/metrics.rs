ef4addd8a16159360b34eeeca3806a061a67c080
Aaron Arinder <aaronarinder@gmail.com>
2026-03-18 10:57:06 -0400

    wip
    - testing time

--- apollo-router/src/cache/metrics.rs
+++ apollo-router/src/cache/metrics.rs
@@ -124,6 +124,15 @@ pub(crate) struct RedisMetricsCollector {
     metrics_interval: Duration,
 }
 
+/// Test-only accessor for checking whether the metrics task has been aborted
+#[cfg(test)]
+impl RedisMetricsCollector {
+    #[allow(dead_code)]
+    pub(crate) fn abort_handle(&self) -> Option<AbortHandle> {
+        self.abort_handle.lock().clone()
+    }
+}
+
 impl Drop for RedisMetricsCollector {
     fn drop(&mut self) {
         if let Some(handle) = self.abort_handle.lock().take() {
