fac223722d70ec17541d43a48dbb9b9955b93e7f
Dariusz Kuc <9501705+dariuszkuc@users.noreply.github.com>
2026-05-08 21:08:35 -0500

    Revert "fix(composition): fix `EXTENSION_WITH_NO_BASE` validation (#9357)"
    This reverts commit ae4482934edbb82fcbeaad378425affff7b2e714.

--- apollo-federation/src/merger/merge_type.rs
+++ apollo-federation/src/merger/merge_type.rs
@@ -108,11 +108,7 @@ impl Merger {
                 continue;
             };
 
-            let is_orphan = subgraph.is_orphan_extension_type(element.name());
-            let has_extends_directive = subgraph
-                .extends_directive_name()
-                .is_some_and(|extends_name| element.directives().has(extends_name.as_str()));
-            if is_orphan || has_extends_directive {
+            if subgraph.is_orphan_extension_type(element.name()) {
                 let subgraph_name = subgraph.name.to_string();
                 let element_locations = element.locations(subgraph);
                 subgraphs_with_extension.push((subgraph_name, element_locations));
