7d9a2ab23908e425a15f2d172a33594e494ae818
carodewig <16093297+carodewig@users.noreply.github.com>
2026-06-01 09:46:47 -0400

    deduplicate

--- apollo-router/src/plugins/cache/mod.rs
+++ apollo-router/src/plugins/cache/mod.rs
@@ -1,4 +1,4 @@
-pub(crate) mod cache_control;
+use crate::plugins::response_cache::cache_control::CacheControl;
 pub(crate) mod entity;
 pub(crate) mod invalidation;
 pub(crate) mod invalidation_endpoint;
