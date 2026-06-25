d82845e8ab2388b7d6a1bb47a74d88a64bb933d0
bryn <bryn@apollographql.com>
2026-06-25 19:27:02 +0100

    refactor: remove mockall-based mock service definitions
    All consumers have been migrated to tower_test::mock::pair. Delete the
    mock_service!/mock_async_service! macros and their generated types
    (MockRouterService, MockSubgraphService, MockSupergraphService,
    MockExecutionService, MockConnectorService, MockHttpClientService,
    MockInternalHttpClientService) from plugin/test/service.rs and remove
    their re-exports from plugin/test/mod.rs.

--- apollo-router/src/plugin/test/mod.rs
+++ apollo-router/src/plugin/test/mod.rs
@@ -1,21 +1,12 @@
 //! Utilities which make it easy to test with [`crate::plugin`].
 
-mod mock;
-#[macro_use]
-mod service;
 mod broken;
+mod mock;
 mod restricted;
+mod service;
 
 #[cfg(test)]
 pub use mock::connector::MockConnector;
 pub use mock::subgraph::MockSubgraph;
-pub use service::MockConnectorService;
-pub use service::MockExecutionService;
-pub use service::MockHttpClientService;
-#[cfg(test)]
-pub(crate) use service::MockInternalHttpClientService;
-pub use service::MockRouterService;
-pub use service::MockSubgraphService;
-pub use service::MockSupergraphService;
 
 pub(crate) use self::mock::canned;
