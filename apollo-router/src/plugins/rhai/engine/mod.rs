9d5283ba3066fca3d2f661eccf92476c322b75e3
Renée Kooi <renee.kooi@apollographql.com>
2026-06-26 16:44:02 +0200

    Also import `*`

--- apollo-router/src/plugins/rhai/engine/mod.rs
+++ apollo-router/src/plugins/rhai/engine/mod.rs
@@ -18,23 +18,6 @@ use http::uri::Parts;
 use http::uri::PathAndQuery;
 use http::uri::Scheme;
 use parking_lot::Mutex;
-use rhai::AST;
-use rhai::Array;
-use rhai::Dynamic;
-use rhai::Engine;
-use rhai::EvalAltResult;
-use rhai::FnPtr;
-use rhai::ImmutableString;
-use rhai::Instant;
-use rhai::Map;
-use rhai::NativeCallContext;
-use rhai::Scope;
-use rhai::combine_with_exported_module;
-use rhai::exported_module;
-use rhai::module_resolvers::FileModuleResolver;
-use rhai::plugin::export_module;
-use rhai::serde::from_dynamic;
-use rhai::serde::to_dynamic;
 use tower::BoxError;
 use uuid::Uuid;
 
@@ -59,6 +42,33 @@ use crate::plugins::demand_control::COST_STRATEGY_KEY;
 use crate::plugins::response_cache;
 use crate::plugins::subscription::SUBSCRIPTION_WS_CUSTOM_CONNECTION_PARAMS;
 use crate::query_planner::APOLLO_OPERATION_ID;
+
+// Helper module so we can skip formatting & interleave comments correctly. 💀
+#[rustfmt::skip]
+mod rhai_imports {
+    // Explicitly list those types that we actually use;
+    pub(super) use rhai::AST;
+    pub(super) use rhai::Array;
+    pub(super) use rhai::Dynamic;
+    pub(super) use rhai::Engine;
+    pub(super) use rhai::EvalAltResult;
+    pub(super) use rhai::FnPtr;
+    pub(super) use rhai::ImmutableString;
+    pub(super) use rhai::Instant;
+    pub(super) use rhai::Map;
+    pub(super) use rhai::NativeCallContext;
+    pub(super) use rhai::Scope;
+    pub(super) use rhai::combine_with_exported_module;
+    pub(super) use rhai::exported_module;
+    pub(super) use rhai::module_resolvers::FileModuleResolver;
+    pub(super) use rhai::plugin::export_module;
+    pub(super) use rhai::serde::from_dynamic;
+    pub(super) use rhai::serde::to_dynamic;
+    // and use a glob import because `rhai::export_module` relies on it.
+    pub(super) use rhai::plugin::*;
+}
+
+use rhai_imports::*;
 
 const CANNOT_ACCESS_HEADERS_ON_A_DEFERRED_RESPONSE: &str =
     "cannot access headers on a deferred response";
